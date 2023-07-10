e9ac05b9a2d533d414222d4558acef7dbd46871f
Sonny Vesali <sonny.vesali@proton.me>
2023-07-10 16:22:50 -0500

    ci+cd trial + error

--- packages/backend/dart/scripts/local_test.sh
+++ packages/backend/dart/scripts/local_test.sh
@@ -1,5 +1,3 @@
 supabase stop --no-backup
 supabase start
 flutter test test/01_user_name_table_test.dart
-flutter test test/02_p2p_request_table_test.dart
-flutter test test/03_p2p_scheduling_table_test.dart
--- packages/backend/dart/scripts/test.sh
+++ packages/backend/dart/scripts/test.sh
@@ -1,3 +1,1 @@
 flutter test test/01_user_name_table_test.dart
-flutter test test/02_p2p_request_table_test.dart
-flutter test test/03_p2p_scheduling_table_test.dart
