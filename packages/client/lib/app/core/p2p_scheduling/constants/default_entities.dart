f596cdbceb49b1f5a33d71166a3ba217afa7d4a5
Sonny Vesali <sonny.vesali@proton.me>
2023-07-16 15:11:37 -0500

    small changes

--- packages/client/lib/app/core/p2p_scheduling/constants/default_entities.dart
+++ packages/client/lib/app/core/p2p_scheduling/constants/default_entities.dart
@@ -4,7 +4,7 @@ import 'package:primala/app/core/p2p_scheduling/utils/time_converter.dart';
 import 'spiral_selection_state_type.dart';
 
 class DefaultP2PScheduling {
-  static SpiralSelectionState get rangeEntity => Left([]);
+  static SpiralSelectionState get rangeEntity => const Left([]);
   static SpiralSelectionState get singleTimeEntity => const Right('');
   static TimeConverter get timeConverter =>
       TimeConverter(presentMoment: DateTime.now());
