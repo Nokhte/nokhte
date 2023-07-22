929126926fa10eaa3ab30aee8c00d337477570e1
Sonny Vesali <sonny.vesali@proton.me>
2023-07-22 13:34:19 -0500

    main auth mobx store documentation

--- packages/client/lib/app/modules/authentication/presentation/mobx/main/auth_state_store.dart
+++ packages/client/lib/app/modules/authentication/presentation/mobx/main/auth_state_store.dart
@@ -1,3 +1,20 @@
+/// auth_state_store.dart
+///
+/// Author: Sonny Vesali
+///
+/// This file defines the [AuthStateStore] class, a MobX store that tracks the authentication state
+/// boolean from Supabase. The store extends [_AuthStateStoreBase] and interacts with the UI layer through
+/// the [GetAuthStateStore].
+///
+/// The [AuthStateStore] contains an observable [authState] variable that holds a boolean stream indicating
+/// the current authentication state. It initializes this stream by calling the [authStateGetterStore]
+/// from the [GetAuthStateStore] and converting it into a broadcast stream.
+///
+/// Note: The ignore_for_file directive is used to suppress warnings about library_private_types_in_public_api
+/// and must_be_immutable.
+
+// ignore_for_file: library_private_types_in_public_api, must_be_immutable
+
 // * Mobx Import
 import 'package:mobx/mobx.dart';
 // * Equatable Import
@@ -7,10 +24,10 @@ import 'package:primala/app/modules/authentication/presentation/mobx/getters/get
 // * Mobx Codegen Inclusion
 part 'auth_state_store.g.dart';
 
-// ignore: must_be_immutable, library_private_types_in_public_api
+/// [AuthStateStore] is a MobX store that tracks the authentication state boolean from Supabase.
 class AuthStateStore = _AuthStateStoreBase with _$AuthStateStore;
 
-// ignore: must_be_immutable
+/// [_AuthStateStoreBase] is the base class that extends [Equatable] and tracks the authentication state boolean from Supabase.
 abstract class _AuthStateStoreBase extends Equatable with Store {
   final GetAuthStateStore authStateGetterStore;
 
@@ -18,6 +35,7 @@ abstract class _AuthStateStoreBase extends Equatable with Store {
     required this.authStateGetterStore,
   }) : authState = authStateGetterStore().asBroadcastStream();
 
+  /// [authState] holds a boolean stream indicating the current authentication state.
   @observable
   Stream<bool> authState;
 
