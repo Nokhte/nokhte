7e51df5b3f3816a65fbbd6373d16598eb10a38a3
Sonny Vesali <sonny.vesali@proton.me>
2023-07-30 12:06:22 -0500

    constants + colors documentation

--- packages/client/lib/app/core/breathing_pentagons_stack/constants/constants.dart
+++ packages/client/lib/app/core/breathing_pentagons_stack/constants/constants.dart
@@ -1,3 +1,40 @@
+/// constants.dart
+///
+/// Proofreading Information:
+///   - Proofreader: Sonny Vesali
+///   - Date: July 30th, 2023
+///
+/// Author: Sonny Vesali
+///
+/// This is a barrel file for the `constants` folder, providing easy access to
+/// various constants used throughout the application related to breathing
+/// pentagons animation.
+///
+/// The following files are exported from this barrel:
+///
+///  - `pentagon_colors.dart`: This file contains the color constants used for
+///    the pentagons' gradient colors during the animation.
+///
+///  - `pentagon_painter_helper.dart`: This file contains utility methods to aid
+///    in painting pentagons, including obtaining radial gradient fills for the
+///    pentagons.
+///
+///  - `pentagon_angles.dart`: This file contains constants related to the angles
+///    used in the animation of the breathing pentagons.
+///
+///  - `types/types.dart`: This file contains custom type definitions used in
+///    various parts of the application.
+///
+///  - `movies/movies.dart`: This file is a collection of pre-defined animations
+///    (movies) for the breathing pentagons, such as `AggressiveExpansion` and
+///    `InfiniteSpinner`.
+///
+/// By importing this `constants.dart` barrel file for easy access all
+/// the relevant constants and utility methods needed for the breathing pentagon
+/// animations without having to import each file individually. This enhances
+/// code readability and maintainability by centralizing all the constants and
+/// helper functions under a single entry point.
+
 export 'pentagon_colors.dart';
 export 'pentagon_painter_helper.dart';
 export 'pentagon_angles.dart';
