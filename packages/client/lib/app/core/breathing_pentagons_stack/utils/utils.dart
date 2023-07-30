a070160fa3031d89b61e9a3c5f0ac83eed1039ea
Sonny Vesali <sonny.vesali@proton.me>
2023-07-30 10:06:32 -0500

    docs changes

--- packages/client/lib/app/core/breathing_pentagons_stack/utils/utils.dart
+++ packages/client/lib/app/core/breathing_pentagons_stack/utils/utils.dart
@@ -6,7 +6,8 @@
 ///
 /// Author: Sonny Vesali
 ///
-/// This barrel file exports two utility classes for handling animations:
+/// This barrel file exports two utility classes for handling animations
+/// associated with the `breathingPentagonsButton` widget:
 /// 1. [FindClosestIncrement] A class to find the closest increment for an input.
 /// 2. [GetCurrentAnimation] A class to extract the current state of a [Movie] animation.
 export 'find_closest_increment.dart';
