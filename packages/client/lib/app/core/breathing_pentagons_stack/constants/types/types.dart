66c236c754d6752e14babda63d8bdbaa58beaa83
Sonny Vesali <sonny.vesali@proton.me>
2023-07-30 09:34:50 -0500

    types docs

--- packages/client/lib/app/core/breathing_pentagons_stack/constants/types/types.dart
+++ packages/client/lib/app/core/breathing_pentagons_stack/constants/types/types.dart
@@ -1,1 +1,17 @@
+/// types.dart
+/// Proofreading Information:
+///   - Proofreader: Sonny Vesali
+///   - Date: July 30th 2023
+///
+/// Author: Sonny Vesali
+///
+/// This barrel file exports the `MovieModes` enum from the `movie_modes.dart`
+///
+/// The `MovieModes` enum defines the different 'movies' for the
+/// `BreathingPentagonsStateTracker mobx store. Each movie mode represents a
+/// specific animation behavior for the button to be tracked in the mobx store.
+///
+/// This barrel file allows for easy access to the `MovieModes` enum from other
+/// parts of the codebase, simplifying the import process and promoting code
+/// organization.
 export 'movie_modes.dart';
