7e2f68d3145ba704f2108abbeb5ddd3bc0f1c003
Sonny Vesali <sonny.vesali@proton.me>
2023-08-13 06:44:35 -0400

    supascript addition

--- packages/backend/supabase/functions/collaborator_matcher/index.ts
+++ packages/backend/supabase/functions/collaborator_matcher/index.ts
@@ -40,10 +40,3 @@ serve(async (_res) => {
     { headers: { "Content-Type": "application/json" } },
   );
 });
-
-/* To invoke:
-curl -i --location --request POST 'http://localhost:54321/functions/v1/collaborator_matcher' \
-  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-  --header 'Content-Type: application/json' \
-//  --data '{"name":"Functions"}'
-*/
--- packages/backend/supabase/functions/utils/array_utils.ts
+++ packages/backend/supabase/functions/utils/array_utils.ts
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file no-explicit-any
 export function isEmpty(arr: Array<any>): boolean {
     return arr.length === 0;
 }
