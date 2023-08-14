c7663e0ca2babb2317861ea2de6b8af773724b0d
Sonny Vesali <sonny.vesali@proton.me>
2023-08-14 13:05:14 -0500

    ci/cd trial & error env file change to defaults

--- packages/backend/supabase/test/initiate-collaborator-search-test.ts
+++ packages/backend/supabase/test/initiate-collaborator-search-test.ts
@@ -5,8 +5,11 @@ import { createClient, SupabaseClient } from "supabase";
 // const supabaseUrl = 'http://localhost:54321'
 // const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
 
-const supabaseUrl = Deno.env.get('LOCAL_SUPABASE_URL') ?? ''
-const supabaseKey = Deno.env.get('LOCAL_SUPABASE_ANON_KEY') ?? ''
+// const supabaseUrl = Deno.env.get('LOCAL_SUPABASE_URL') ?? ''
+// const supabaseKey = Deno.env.get('LOCAL_SUPABASE_ANON_KEY') ?? ''
+
+const supabaseUrl = Deno.env.get('SUPABASE_URL') ?? ''
+const supabaseKey = Deno.env.get('SUPABASE_ANON_KEY') ?? ''
 const options = {
   auth: {
     autoRefreshToken: false,
