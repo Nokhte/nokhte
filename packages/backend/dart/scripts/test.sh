9e4e40121342e66cc8b89cb99d138737f0d8b6f9
Sonny Vesali <sonny.vesali@proton.me>
2023-08-27 14:57:13 -0500

    let's try this

--- packages/backend/dart/scripts/test.sh
+++ packages/backend/dart/scripts/test.sh
@@ -1,3 +1,3 @@
-flutter test test/01_user_name_table_test.dart || kill $SERVER_PID
-flutter test test/02_collaborator_phrases_table_test.dart ||  kill $SERVER_PID
-flutter test test/03_synchronous_search_test.dart ||  kill $SERVER_PID
+flutter test test/01_user_name_table_test.dart
+flutter test test/02_collaborator_phrases_table_test.dart
+flutter test test/03_synchronous_search_test.dart
