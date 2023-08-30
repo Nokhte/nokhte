aa890a436efd54c5455319f39b9c345cef4a159d
Sonny Vesali <sonny.vesali@proton.me>
2023-08-30 10:02:08 -0500

    forgot to add test to test.sh

--- packages/backend/dart/scripts/test.sh
+++ packages/backend/dart/scripts/test.sh
@@ -2,3 +2,4 @@ flutter test test/01_user_name_table_test.dart
 flutter test test/02_collaborator_phrases_table_test.dart
 flutter test test/03_synchronous_search_test.dart
 flutter test test/04_fetch_agora_token_test.dart
+flutter test test/05_existing_collaborations_table_test.dart
