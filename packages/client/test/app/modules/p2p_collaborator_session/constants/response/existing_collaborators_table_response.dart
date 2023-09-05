27f429167aa0c40f75545285422075019dcb9427
Sonny Vesali <sonny.vesali@proton.me>
2023-09-05 06:09:27 -0500

    fixed up fixtures for the tests

--- packages/client/test/app/modules/p2p_collaborator_session/constants/response/existing_collaborators_table_response.dart
+++ packages/client/test/app/modules/p2p_collaborator_session/constants/response/existing_collaborators_table_response.dart
@@ -1,10 +1,12 @@
 class ExistingCollaboratorsTable {
   static List get response => [
-        {
-          "collaborator_one": "1111111111111111111",
-          "collaborator_two": "2222222222222222222",
-          "who_gets_the_question": 1,
-        },
+        [
+          {
+            "collaborator_one": "1111111111111111111",
+            "collaborator_two": "2222222222222222222",
+            "who_gets_the_question": 1,
+          }
+        ],
         "1111111111111111111"
       ];
 }
