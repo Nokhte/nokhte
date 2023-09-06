a066c9c440d5e5f5d8417b483ce7b6395c48c365
Sonny Vesali <sonny.vesali@proton.me>
2023-09-06 13:11:59 -0500

    🙈 forgot to add the new test to the script

--- packages/backend/dart/scripts/test.sh
+++ packages/backend/dart/scripts/test.sh
@@ -3,3 +3,4 @@ flutter test test/02_collaborator_phrases_table_test.dart
 flutter test test/03_synchronous_search_test.dart
 flutter test test/04_fetch_agora_token_test.dart
 flutter test test/05_existing_collaborations_table_test.dart
+flutter test test/06_solo_p2p_purpose_documents_table_test.dart
