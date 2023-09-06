3340bc42f3fe3b9e8adf66ae9819b06c5ffa2137
Sonny Vesali <sonny.vesali@proton.me>
2023-09-06 18:26:40 -0500

    entity re-org

--- packages/client/lib/app/modules/p2p_purpose_session/domain/entities/entities.dart
+++ packages/client/lib/app/modules/p2p_purpose_session/domain/entities/entities.dart
@@ -1,6 +1,2 @@
-export './agora_call_token_entity.dart';
-export './agora_sdk_status_entity.dart';
-export './call_status_entity.dart';
-export './channel_id_entity.dart';
-export './local_audio_stream_status_entity.dart';
-export './who_gets_the_question_entity.dart';
+export 'voice_call_related/voice_call_related.dart';
+export 'db_related/db_related.dart';
