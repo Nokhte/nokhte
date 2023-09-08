78187145a5302f22b5866574a61f9527ac704261
Sonny Vesali <sonny.vesali@proton.me>
2023-09-08 13:30:12 -0500

    models done, no need to test them since no mappers

---
+++ packages/client/lib/app/modules/p2p_purpose_session/data/models/collaborative_document/collaborative_doc_content_model.dart
@@ -0,0 +1,5 @@
+import 'package:primala/app/modules/p2p_purpose_session/domain/domain.dart';
+
+class CollaborativeDocContentModel extends CollaborativeDocContentEntity {
+  const CollaborativeDocContentModel({required super.docContent});
+}
--- packages/client/lib/app/modules/p2p_purpose_session/data/models/models.dart
+++ packages/client/lib/app/modules/p2p_purpose_session/data/models/models.dart
@@ -1,2 +1,3 @@
 export 'voice_call/voice_call.dart';
 export './solo_document/solo_document.dart';
+export './collaborative_document/collaborative_document.dart';
