c3e19d37c2a5e65be5cf0f7c5400a9c65017d25f
Sonny Vesali <sonny.vesali@proton.me>
2023-09-08 12:05:03 -0500

    skeletal widget structure planned out

---
+++ packages/client/lib/app/core/widgets/collaborative_text_editor/stack/presentation/mobx/collaborative_text_editor_tracker_store.dart
@@ -0,0 +1,55 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+// * Mobx Import
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+// * Equatable Import
+import 'package:equatable/equatable.dart';
+// * Mobx Codegen Inclusion
+part 'collaborative_text_editor_tracker_store.g.dart';
+
+class CollaborativeTextEditorTrackerStore = _CollaborativeTextEditorTrackerStoreBase
+    with _$CollaborativeTextEditorTrackerStore;
+
+abstract class _CollaborativeTextEditorTrackerStoreBase extends Equatable
+    with Store {
+  @observable
+  TextEditingController controller = TextEditingController();
+
+  @observable
+  FocusNode focusNode = FocusNode();
+
+  @action
+  handleTap() {
+    controller.selection = TextSelection.fromPosition(
+      TextPosition(offset: controller.text.length),
+    );
+    // }
+  }
+
+  @action
+  addEventListeners() {
+    focusNode.addListener(() {
+      if (focusNode.hasFocus) {
+        print("THE USER JUST WENT INTO THE TEXT DOC");
+      } else {
+        print("A USER JUST LEFT THE TEXT DOC");
+      }
+    });
+    controller.addListener(() {
+      // final selection = controller.selection;
+      // print(controller.text); <=== wonderful this is how you push text
+      // print('Cursor Start: ${selection.start}, Cursor End: ${selection.end}');
+    });
+  }
+
+  @action
+  dispose() {
+    controller.dispose();
+    focusNode.dispose();
+  }
+
+  @override
+  List<Object> get props => [
+// some items
+      ];
+}
