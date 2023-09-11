fb891a5e0cf3d053bfb8dbfd49757a059d9de262
Sonny Vesali <sonny.vesali@proton.me>
2023-09-11 06:37:08 -0500

    one more small change with the unit test

--- packages/backend/dart/test/quality_of_life_scripts/quality_of_life_test.dart
+++ packages/backend/dart/test/quality_of_life_scripts/quality_of_life_test.dart
@@ -23,40 +23,40 @@ void main() {
     return realPersonUIDQuery[0]["uid"];
   }
 
-  // test("put npc in the pool searching for user ", () async {
-  //   final userIdResults = await UserSetupConstants.fetchUIDs();
-  //   final npcUserUID = userIdResults[0];
-  //   final realPersonUID = await returnNonNPCUID();
-  //   final realPersonPhraseIDRes = await supabaseAdmin
-  //       .from('collaborator_phrases')
-  //       .select()
-  //       .eq('uid', realPersonUID);
-  //   await InitiateCollaboratorSearch.invoke(
-  //     supabase: supabaseAdmin,
-  //     wayfarerUID: npcUserUID,
-  //     queryPhraseIDs: CollaboratorPhraseIDs(
-  //       adjectiveID: realPersonPhraseIDRes[0]["adjective_id"],
-  //       nounID: realPersonPhraseIDRes[0]["noun_id"],
-  //     ),
-  //   );
-  // });
-  // test("user 1 in the pool searching for npc ", () async {
-  //   final userIdResults = await UserSetupConstants.fetchUIDs();
-  //   final npcUserUID = userIdResults[1];
-  //   final realPersonUID = await returnNonNPCUID();
-  //   final npcPhraseRes = await supabaseAdmin
-  //       .from('collaborator_phrases')
-  //       .select()
-  //       .eq('uid', npcUserUID);
-  //   await InitiateCollaboratorSearch.invoke(
-  //     supabase: supabaseAdmin,
-  //     wayfarerUID: realPersonUID,
-  //     queryPhraseIDs: CollaboratorPhraseIDs(
-  //       adjectiveID: npcPhraseRes[0]["adjective_id"],
-  //       nounID: npcPhraseRes[0]["noun_id"],
-  //     ),
-  //   );
-  // });
+  test("put npc in the pool searching for user ", () async {
+    final userIdResults = await UserSetupConstants.fetchUIDs();
+    final npcUserUID = userIdResults[0];
+    final realPersonUID = await returnNonNPCUID();
+    final realPersonPhraseIDRes = await supabaseAdmin
+        .from('collaborator_phrases')
+        .select()
+        .eq('uid', realPersonUID);
+    await InitiateCollaboratorSearch.invoke(
+      supabase: supabaseAdmin,
+      wayfarerUID: npcUserUID,
+      queryPhraseIDs: CollaboratorPhraseIDs(
+        adjectiveID: realPersonPhraseIDRes[0]["adjective_id"],
+        nounID: realPersonPhraseIDRes[0]["noun_id"],
+      ),
+    );
+  });
+  test("user 1 in the pool searching for npc ", () async {
+    final userIdResults = await UserSetupConstants.fetchUIDs();
+    final npcUserUID = userIdResults[1];
+    final realPersonUID = await returnNonNPCUID();
+    final npcPhraseRes = await supabaseAdmin
+        .from('collaborator_phrases')
+        .select()
+        .eq('uid', npcUserUID);
+    await InitiateCollaboratorSearch.invoke(
+      supabase: supabaseAdmin,
+      wayfarerUID: realPersonUID,
+      queryPhraseIDs: CollaboratorPhraseIDs(
+        adjectiveID: npcPhraseRes[0]["adjective_id"],
+        nounID: npcPhraseRes[0]["noun_id"],
+      ),
+    );
+  });
 
   test("make a collaboration between real person & npc 2", () async {
     final userIdResults = await UserSetupConstants.fetchUIDs();
@@ -69,11 +69,9 @@ void main() {
     );
   });
 
-  // test("update the doc between user and npc 2", () async {
-  //   final userIdResults = await UserSetupConstants.fetchUIDs();
-  //   final npcUserUID = userIdResults[1];
-  //   final realPersonUID = await returnNonNPCUID();
-  //   await WorkingCollaborativeDocumentsQueries.updateExistingDocument(
-  //       supabase: supabaseAdmin, currentUserUID: realPersonUID);
-  // });
+  test("update the doc between user and npc 2", () async {
+    final realPersonUID = await returnNonNPCUID();
+    await WorkingCollaborativeDocumentsQueries.updateExistingDocument(
+        supabase: supabaseAdmin, currentUserUID: realPersonUID);
+  });
 }
