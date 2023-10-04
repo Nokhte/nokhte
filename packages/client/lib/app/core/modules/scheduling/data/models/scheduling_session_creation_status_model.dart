5b755f7671ff736e8c6b29860b5473f3c08c31e7
Sonny Vesali <sonny.vesali@protonmail.com>
2023-10-04 15:50:48 -0500

    comment cleanup again

--- packages/client/lib/app/core/modules/scheduling/data/models/scheduling_session_creation_status_model.dart
+++ packages/client/lib/app/core/modules/scheduling/data/models/scheduling_session_creation_status_model.dart
@@ -1,10 +1,3 @@
-// import 'package:primala/app/core/entities/default_db_status_entity.dart';
-
-// class SchedulingSessionCreationStatusEntity extends DefaultDBStatusEntity {
-//   const SchedulingSessionCreationStatusEntity({required bool isCreated})
-//       : super(isSent: isCreated);
-// }
-
 import 'package:primala/app/core/modules/scheduling/domain/domain.dart';
 
 class SchedulingSessionCreationStatusModel
