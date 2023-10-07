a8e3144083f3e55089c59b8b660a37c733ce4bef
Sonny Vesali <sonny.vesali@protonmail.com>
2023-10-07 15:55:12 -0500

    water movies done☑️

--- packages/client/lib/app/core/widgets/beach_widgets/beach_horizon/stack/movies/dusk_horizon.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_horizon/stack/movies/dusk_horizon.dart
@@ -3,7 +3,7 @@ import 'package:primala/app/core/widgets/beach_widgets/shared/data/water_colors_
 import 'package:simple_animations/simple_animations.dart';
 // 8 stop migration done
 
-class EveningHorizonWaters {
+class DuskHorizonWaters {
   static MovieTween get movie => MovieTween()
     ..scene(
       begin: const Duration(seconds: 0),
@@ -20,112 +20,112 @@ class EveningHorizonWaters {
           '1st Water Gradient Color',
           ColorTween(
             begin: WaterColorsAndStops.toTheDepthsWater[0].color,
-            end: WaterColorsAndStops.schedulingEveningWater[0].color,
+            end: WaterColorsAndStops.schedulingDuskWater[0].color,
           ),
         )
         .tween(
           '2nd Water Gradient Color',
           ColorTween(
             begin: WaterColorsAndStops.toTheDepthsWater[1].color,
-            end: WaterColorsAndStops.schedulingEveningWater[1].color,
+            end: WaterColorsAndStops.schedulingDuskWater[1].color,
           ),
         )
         .tween(
           '3rd Water Gradient Color',
           ColorTween(
             begin: WaterColorsAndStops.toTheDepthsWater[2].color,
-            end: WaterColorsAndStops.schedulingEveningWater[2].color,
+            end: WaterColorsAndStops.schedulingDuskWater[2].color,
           ),
         )
         .tween(
           '4th Water Gradient Color',
           ColorTween(
             begin: WaterColorsAndStops.toTheDepthsWater[3].color,
-            end: WaterColorsAndStops.schedulingEveningWater[3].color,
+            end: WaterColorsAndStops.schedulingDuskWater[3].color,
           ),
         )
         .tween(
           '5th Water Gradient Color',
           ColorTween(
             begin: WaterColorsAndStops.toTheDepthsWater[4].color,
-            end: WaterColorsAndStops.schedulingEveningWater[4].color,
+            end: WaterColorsAndStops.schedulingDuskWater[4].color,
           ),
         )
         .tween(
           '6th Water Gradient Color',
           ColorTween(
             begin: WaterColorsAndStops.toTheDepthsWater[5].color,
-            end: WaterColorsAndStops.schedulingEveningWater[5].color,
+            end: WaterColorsAndStops.schedulingDuskWater[5].color,
           ),
         )
         .tween(
           '7th Water Gradient Color',
           ColorTween(
             begin: WaterColorsAndStops.toTheDepthsWater[6].color,
-            end: WaterColorsAndStops.schedulingEveningWater[6].color,
+            end: WaterColorsAndStops.schedulingDuskWater[6].color,
           ),
         )
         .tween(
           '8th Water Gradient Color',
           ColorTween(
             begin: WaterColorsAndStops.toTheDepthsWater[7].color,
-            end: WaterColorsAndStops.schedulingEveningWater[7].color,
+            end: WaterColorsAndStops.schedulingDuskWater[7].color,
           ),
         )
         .tween(
           '1st Water Gradient Stop',
           Tween<double>(
             begin: WaterColorsAndStops.toTheDepthsWater[0].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[0].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[0].stop,
           ),
         )
         .tween(
           '2nd Water Gradient Stop',
           Tween<double>(
             begin: WaterColorsAndStops.toTheDepthsWater[1].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[1].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[1].stop,
           ),
         )
         .tween(
           '3rd Water Gradient Stop',
           Tween<double>(
             begin: WaterColorsAndStops.toTheDepthsWater[2].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[2].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[2].stop,
           ),
         )
         .tween(
           '4th Water Gradient Stop',
           Tween<double>(
             begin: WaterColorsAndStops.toTheDepthsWater[3].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[3].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[3].stop,
           ),
         )
         .tween(
           '5th Water Gradient Stop',
           Tween<double>(
             begin: WaterColorsAndStops.toTheDepthsWater[4].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[4].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[4].stop,
           ),
         )
         .tween(
           '6th Water Gradient Stop',
           Tween<double>(
             begin: WaterColorsAndStops.toTheDepthsWater[5].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[5].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[5].stop,
           ),
         )
         .tween(
           '7th Water Gradient Stop',
           Tween<double>(
             begin: WaterColorsAndStops.toTheDepthsWater[6].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[6].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[6].stop,
           ),
         )
         .tween(
           '8th Water Gradient Stop',
           Tween<double>(
             begin: WaterColorsAndStops.toTheDepthsWater[7].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[7].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[7].stop,
           ),
         )
     ..scene(
@@ -142,113 +142,113 @@ class EveningHorizonWaters {
         .tween(
           '1st Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[0].color,
-            end: WaterColorsAndStops.schedulingEveningWater[7].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[0].color,
+            end: WaterColorsAndStops.schedulingDuskWater[7].color,
           ),
         )
         .tween(
           '2nd Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[1].color,
-            end: WaterColorsAndStops.schedulingEveningWater[6].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[1].color,
+            end: WaterColorsAndStops.schedulingDuskWater[6].color,
           ),
         )
         .tween(
           '3rd Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[2].color,
-            end: WaterColorsAndStops.schedulingEveningWater[5].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[2].color,
+            end: WaterColorsAndStops.schedulingDuskWater[5].color,
           ),
         )
         .tween(
           '4th Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[3].color,
-            end: WaterColorsAndStops.schedulingEveningWater[4].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[3].color,
+            end: WaterColorsAndStops.schedulingDuskWater[4].color,
           ),
         )
         .tween(
           '5th Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[4].color,
-            end: WaterColorsAndStops.schedulingEveningWater[3].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[4].color,
+            end: WaterColorsAndStops.schedulingDuskWater[3].color,
           ),
         )
         .tween(
           '6th Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[5].color,
-            end: WaterColorsAndStops.schedulingEveningWater[2].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[5].color,
+            end: WaterColorsAndStops.schedulingDuskWater[2].color,
           ),
         )
         .tween(
           '7th Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[6].color,
-            end: WaterColorsAndStops.schedulingEveningWater[1].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[6].color,
+            end: WaterColorsAndStops.schedulingDuskWater[1].color,
           ),
         )
         .tween(
           '8th Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[7].color,
-            end: WaterColorsAndStops.schedulingEveningWater[0].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[7].color,
+            end: WaterColorsAndStops.schedulingDuskWater[0].color,
           ),
         )
         .tween(
           '1st Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[0].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[0].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[0].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[0].stop,
           ),
         )
         .tween(
           '2nd Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[1].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[1].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[1].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[1].stop,
           ),
         )
         .tween(
           '3rd Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[2].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[2].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[2].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[2].stop,
           ),
         )
         .tween(
           '4th Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[3].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[3].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[3].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[3].stop,
           ),
         )
         .tween(
           '5th Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[4].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[4].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[4].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[4].stop,
           ),
         )
         .tween(
           '6th Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[5].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[5].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[5].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[5].stop,
           ),
         )
         .tween(
           '7th Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[6].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[6].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[6].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[6].stop,
           ),
         )
         .tween(
           '8th Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[7].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[7].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[7].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[7].stop,
           ),
         )
     ..scene(
@@ -265,113 +265,113 @@ class EveningHorizonWaters {
         .tween(
           '1st Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[7].color,
-            end: WaterColorsAndStops.schedulingEveningWater[7].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[7].color,
+            end: WaterColorsAndStops.schedulingDuskWater[7].color,
           ),
         )
         .tween(
           '2nd Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[6].color,
-            end: WaterColorsAndStops.schedulingEveningWater[6].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[6].color,
+            end: WaterColorsAndStops.schedulingDuskWater[6].color,
           ),
         )
         .tween(
           '3rd Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[5].color,
-            end: WaterColorsAndStops.schedulingEveningWater[5].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[5].color,
+            end: WaterColorsAndStops.schedulingDuskWater[5].color,
           ),
         )
         .tween(
           '4th Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[4].color,
-            end: WaterColorsAndStops.schedulingEveningWater[4].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[4].color,
+            end: WaterColorsAndStops.schedulingDuskWater[4].color,
           ),
         )
         .tween(
           '5th Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[3].color,
-            end: WaterColorsAndStops.schedulingEveningWater[3].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[3].color,
+            end: WaterColorsAndStops.schedulingDuskWater[3].color,
           ),
         )
         .tween(
           '6th Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[2].color,
-            end: WaterColorsAndStops.schedulingEveningWater[2].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[2].color,
+            end: WaterColorsAndStops.schedulingDuskWater[2].color,
           ),
         )
         .tween(
           '7th Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[1].color,
-            end: WaterColorsAndStops.schedulingEveningWater[1].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[1].color,
+            end: WaterColorsAndStops.schedulingDuskWater[1].color,
           ),
         )
         .tween(
           '8th Water Gradient Color',
           ColorTween(
-            begin: WaterColorsAndStops.schedulingEveningWater[0].color,
-            end: WaterColorsAndStops.schedulingEveningWater[0].color,
+            begin: WaterColorsAndStops.schedulingDuskWater[0].color,
+            end: WaterColorsAndStops.schedulingDuskWater[0].color,
           ),
         )
         .tween(
           '1st Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[0].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[0].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[0].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[0].stop,
           ),
         )
         .tween(
           '2nd Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[1].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[1].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[1].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[1].stop,
           ),
         )
         .tween(
           '3rd Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[2].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[2].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[2].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[2].stop,
           ),
         )
         .tween(
           '4th Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[3].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[3].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[3].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[3].stop,
           ),
         )
         .tween(
           '5th Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[4].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[4].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[4].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[4].stop,
           ),
         )
         .tween(
           '6th Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[5].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[5].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[5].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[5].stop,
           ),
         )
         .tween(
           '7th Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[6].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[6].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[6].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[6].stop,
           ),
         )
         .tween(
           '8th Water Gradient Stop',
           Tween<double>(
-            begin: WaterColorsAndStops.schedulingEveningWater[7].stop,
-            end: WaterColorsAndStops.schedulingEveningWater[7].stop,
+            begin: WaterColorsAndStops.schedulingDuskWater[7].stop,
+            end: WaterColorsAndStops.schedulingDuskWater[7].stop,
           ),
         );
 }
