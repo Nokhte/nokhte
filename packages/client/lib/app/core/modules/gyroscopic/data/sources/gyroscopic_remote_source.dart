6a18e5ec51e34330cf4e2fdf0218fbdc76e5ebaf
Sonny Vesali <sonny.vesali@protonmail.com>
2023-10-11 07:32:17 -0500

    remote source normalized automatically

--- packages/client/lib/app/core/modules/gyroscopic/data/sources/gyroscopic_remote_source.dart
+++ packages/client/lib/app/core/modules/gyroscopic/data/sources/gyroscopic_remote_source.dart
@@ -8,9 +8,30 @@ abstract class GyroscopicRemoteSource {
 class GyroscopicRemoteSourceImpl implements GyroscopicRemoteSource {
   @override
   Stream<int> getDirectionAngle() {
-    return FlutterCompass.events
-            ?.map((event) => event.heading!.floor())
-            .distinct() ??
-        Stream.value(-199);
+    return _transformedCompassStream();
+  }
+
+  Stream<int> _transformedCompassStream() async* {
+    int firstValue = -1; // Initialize with a default value
+
+    if (FlutterCompass.events == null) {
+      yield -1;
+    } else {
+      await for (final event in FlutterCompass.events ??
+          Stream.value(
+            CompassEvent.fromList([-1.0, 0.0, 0.0]),
+          )) {
+        final currentHeading = event.heading!.floor();
+
+        if (firstValue == -1) {
+          // Set the first value
+          firstValue = currentHeading;
+        }
+
+        final offset = currentHeading - firstValue;
+        final shiftedValue = (offset < 0) ? 360 + offset : offset;
+        yield shiftedValue;
+      }
+    }
   }
 }
