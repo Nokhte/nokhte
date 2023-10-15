563ca48c5a191e56310c06e3e94aa74a0c06a252
Sonny Vesali <sonny.vesali@protonmail.com>
2023-10-15 17:20:34 -0500

    refactored for better widget still unfinished

--- packages/client/lib/app/core/widgets/conveyer_belt_text/stack/presentation/widget/conveyer_belt_text.dart
+++ packages/client/lib/app/core/widgets/conveyer_belt_text/stack/presentation/widget/conveyer_belt_text.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:primala/app/core/types/types.dart';
 import 'package:primala/app/core/widgets/conveyer_belt_text/stack/presentation/presentation.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 class ConveyerBeltText extends StatelessWidget {
   final ConveyerBeltTextStore trackerStore;
@@ -15,32 +18,16 @@ class ConveyerBeltText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(
-        "LM ${trackerStore.leftMostValue} L ${trackerStore.leftValue} C ${trackerStore.centerValue} R ${trackerStore.rightValue} RM ${trackerStore.rightMostValue}");
-    print("${trackerStore.theFocusedList.toString()} ");
-    print(
-        "${trackerStore.currentlySelectedIndex} ${trackerStore.focusListCardinalLength}");
-    // print("${trackerStore.leftValue}");
-    return ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 4,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.only(top: 95.0),
-            child: Center(
-              child: Padding(
-                padding: index == 0
-                    ? EdgeInsets.all(15.0)
-                    : const EdgeInsets.only(left: 50.0),
-                child: PlatformText(
-                  style: GoogleFonts.jost(
-                    fontSize: 30.0,
-                  ),
-                  trackerStore.theFocusedList[index].formatted,
-                ),
-              ),
-            ),
-          );
-        });
+    return Observer(builder: (context) {
+      return CustomAnimationBuilder(
+          tween: MovieTween(),
+          duration: Seconds.get(0),
+          builder: (context, value, child) {
+            return Stack(children: [
+              for (int i = 0; i < trackerStore.theFocusedList.length; i++)
+                PlatformText(trackerStore.theFocusedList[i].formatted),
+            ]);
+          });
+    });
   }
 }
