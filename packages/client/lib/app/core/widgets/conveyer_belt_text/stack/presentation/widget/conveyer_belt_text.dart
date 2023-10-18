133e188fe7f1294433b6395b431ca84528580b1b
Sonny Vesali <sonny.vesali@protonmail.com>
2023-10-18 09:43:13 -0500

    fade in done

--- packages/client/lib/app/core/widgets/conveyer_belt_text/stack/presentation/widget/conveyer_belt_text.dart
+++ packages/client/lib/app/core/widgets/conveyer_belt_text/stack/presentation/widget/conveyer_belt_text.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:primala/app/core/types/types.dart';
 import 'package:primala/app/core/widgets/conveyer_belt_text/stack/presentation/presentation.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:simple_animations/simple_animations.dart';
@@ -13,34 +14,34 @@ class ConveyerBeltText extends StatelessWidget {
     required this.size,
     required this.trackerStore,
   });
-  // a few things I recommend
-  // 1. relative padding
-  // 2. figure out the movie
-  //
 
   @override
   Widget build(BuildContext context) {
     return Observer(builder: (context) {
-      return CustomAnimationBuilder(
-          onCompleted: () => trackerStore.onCompletedMovie(),
-          control: trackerStore.control,
-          tween: trackerStore.movie,
-          duration: trackerStore.movie.duration,
-          builder: (context, value, child) {
-            return Stack(clipBehavior: Clip.none, children: [
-              for (int i = 0; i < 5; i++)
-                Positioned(
-                  left: value.get('$i left position'),
-                  top: value.get('$i top position'),
-                  child: PlatformText(
-                      style: GoogleFonts.jost(
-                        fontSize: 30,
-                        color: Colors.white,
-                      ),
-                      trackerStore.uiArray[i]),
-                ),
-            ]);
-          });
+      return AnimatedOpacity(
+        opacity: trackerStore.showWidget ? 1 : 0,
+        duration: Seconds.get(2),
+        child: CustomAnimationBuilder(
+            onCompleted: () => trackerStore.onCompletedMovie(),
+            control: trackerStore.control,
+            tween: trackerStore.movie,
+            duration: trackerStore.movie.duration,
+            builder: (context, value, child) {
+              return Stack(clipBehavior: Clip.none, children: [
+                for (int i = 0; i < 5; i++)
+                  Positioned(
+                    left: value.get('$i left position'),
+                    top: value.get('$i top position'),
+                    child: PlatformText(
+                        style: GoogleFonts.jost(
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                        trackerStore.uiArray[i]),
+                  ),
+              ]);
+            }),
+      );
     });
   }
 }
