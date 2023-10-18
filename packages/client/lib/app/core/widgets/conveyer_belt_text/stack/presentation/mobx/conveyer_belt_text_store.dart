1552d2533fb449c096cacadbd61ebd901f2aee02
Sonny Vesali <sonny.vesali@protonmail.com>
2023-10-18 15:07:41 -0500

    comment cleanup

--- packages/client/lib/app/core/widgets/conveyer_belt_text/stack/presentation/mobx/conveyer_belt_text_store.dart
+++ packages/client/lib/app/core/widgets/conveyer_belt_text/stack/presentation/mobx/conveyer_belt_text_store.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 // * Mobx Import
-import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 // * Equatable Import
 import 'package:equatable/equatable.dart';
