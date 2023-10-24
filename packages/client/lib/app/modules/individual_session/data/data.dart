4230221dc8a8cda185646b8ab2408b78d47b6c8a
Sonny Vesali <sonny.vesali@protonmail.com>
2023-10-24 17:27:55 -0500

    ddd skeleton for individual session module

---
+++ packages/client/lib/app/modules/individual_session/data/data.dart
@@ -0,0 +1,5 @@
+export './models/models.dart';
+export 'contracts/individual_session_contract_impl.dart';
+export './sources/module_remote_source.dart';
+// export './constracts/module_contract_impl.dart';
+// export './source/module_remote_source.dart';
