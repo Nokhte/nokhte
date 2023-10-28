ed05cb2cbf3b15c041ba4e91b51d8aceb190c2d9
Sonny Vesali <sonny.vesali@protonmail.com>
2023-10-28 15:23:06 -0500

    perspectives session skeleton

---
+++ packages/client/lib/app/modules/perspectives_session/data/data.dart
@@ -0,0 +1,5 @@
+export './models/models.dart';
+export 'contracts/perspectives_session_contract_impl.dart';
+export 'sources/perspectives_session_remote_source.dart';
+// export './constracts/module_contract_impl.dart';
+// export './source/module_remote_source.dart';
