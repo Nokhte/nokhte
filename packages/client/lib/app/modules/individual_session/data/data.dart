1049bfb27a32d01257870cf8914893a75e06899e
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-06 06:14:05 -0600

    DDD Skeleton

---
+++ packages/client/lib/app/modules/individual_session/data/data.dart
@@ -0,0 +1,5 @@
+export './models/models.dart';
+export 'contracts/individual_session_contract_impl.dart';
+export 'sources/individual_session_remote_source.dart';
+// export './constracts/module_contract_impl.dart';
+// export './source/module_remote_source.dart';
