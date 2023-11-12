cef8c6dbb3b01dc24fa162e5177e6d6b4354ec33
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-12 15:43:59 -0600

    injections into the coordinators

--- packages/client/lib/app/modules/collective_session/collective_session_module.dart
+++ packages/client/lib/app/modules/collective_session/collective_session_module.dart
@@ -1,9 +1,16 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:nokhte/app/core/modules/audio_player/audio_player_module.dart';
+import 'package:nokhte/app/core/modules/audio_player/mobx/mobx.dart';
 import 'package:nokhte/app/core/modules/collaborative_doc/collaborative_doc_module.dart';
 import 'package:nokhte/app/core/modules/gyroscopic/gyroscopic_module.dart';
+import 'package:nokhte/app/core/modules/gyroscopic/presentation/mobx/api/api.dart';
 import 'package:nokhte/app/core/modules/voice_call/voice_call_module.dart';
+import 'package:nokhte/app/core/network/network_info.dart';
+import 'package:nokhte/app/modules/collective_session/data/data.dart';
+import 'package:nokhte/app/modules/collective_session/domain/domain.dart';
+import 'package:nokhte/app/modules/collective_session/presentation/mobx/coordinators/collective_session_phase1_coordinator.dart';
 import 'package:nokhte/app/modules/collective_session/presentation/presentation.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class CollectiveSessionModule extends Module {
   @override
@@ -14,7 +21,60 @@ class CollectiveSessionModule extends Module {
         AudioPlayerModule(),
       ];
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        Bind.singleton<CollectiveSessionRemoteSourceImpl>(
+          (i) => CollectiveSessionRemoteSourceImpl(
+            supabase: Modular.get<SupabaseClient>(),
+          ),
+        ),
+        Bind.singleton<CollectiveSessionContractImpl>(
+          (i) => CollectiveSessionContractImpl(
+            networkInfo: Modular.get<NetworkInfoImpl>(),
+            remoteSource: i<CollectiveSessionRemoteSource>(),
+          ),
+        ),
+        Bind.singleton<GetCollaboratorPerspectives>(
+          (i) => GetCollaboratorPerspectives(
+            contract: i<CollectiveSessionContract>(),
+          ),
+        ),
+        Bind.singleton<MoveIndividualPerspectivesAudioToCollectiveSpace>(
+          (i) => MoveIndividualPerspectivesAudioToCollectiveSpace(
+            contract: i<CollectiveSessionContract>(),
+          ),
+        ),
+        Bind.singleton<GetCollaboratorPerspectivesGetterStore>(
+          (i) => GetCollaboratorPerspectivesGetterStore(
+            logic: i<GetCollaboratorPerspectives>(),
+          ),
+        ),
+        Bind.singleton<
+            MoveIndividualPerspectivesAudioToCollectiveSpaceGetterStore>(
+          (i) => MoveIndividualPerspectivesAudioToCollectiveSpaceGetterStore(
+            logic: i<MoveIndividualPerspectivesAudioToCollectiveSpace>(),
+          ),
+        ),
+        Bind.singleton<GetCollaboratorPerspectivesStore>(
+          (i) => GetCollaboratorPerspectivesStore(
+            getterStore: i<GetCollaboratorPerspectivesGetterStore>(),
+          ),
+        ),
+        Bind.singleton<MoveIndividualPerspectivesAudioToCollectiveSpaceStore>(
+          (i) => MoveIndividualPerspectivesAudioToCollectiveSpaceStore(
+            getterStore: i<
+                MoveIndividualPerspectivesAudioToCollectiveSpaceGetterStore>(),
+          ),
+        ),
+        Bind.singleton<CollectiveSessionPhase1Coordinator>(
+          (i) => CollectiveSessionPhase1Coordinator(
+              audioPlayer: Modular.get<ChangeAudioPlayingStatusStore>(),
+              quadrantAPI: Modular.get<QuadrantAPI>(),
+              moveTheAudio:
+                  i<MoveIndividualPerspectivesAudioToCollectiveSpaceStore>(),
+              getCollaboratorPerspectives:
+                  i<GetCollaboratorPerspectivesStore>()),
+        ),
+      ];
 
   @override
   List<ChildRoute> get routes => [
--- packages/client/lib/app/modules/collective_session/presentation/mobx/coordinators/collective_session_phase1_coordinator.dart
+++ packages/client/lib/app/modules/collective_session/presentation/mobx/coordinators/collective_session_phase1_coordinator.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/modules/audio_player/mobx/mobx.dart';
 import 'package:nokhte/app/modules/collective_session/presentation/mobx/mobx.dart';
 part 'collective_session_phase1_coordinator.g.dart';
 
@@ -9,12 +10,17 @@ class CollectiveSessionPhase1Coordinator = _CollectiveSessionPhase1CoordinatorBa
 
 abstract class _CollectiveSessionPhase1CoordinatorBase
     extends BaseQuadrantAPIReceiver with Store {
+  final ChangeAudioPlayingStatusStore audioPlayer;
   final MoveIndividualPerspectivesAudioToCollectiveSpaceStore moveTheAudio;
+  final GetCollaboratorPerspectivesStore getCollaboratorPerspectives;
 
   _CollectiveSessionPhase1CoordinatorBase({
     required super.quadrantAPI,
+    required this.audioPlayer,
     required this.moveTheAudio,
+    required this.getCollaboratorPerspectives,
   });
+
   @override
   List<Object> get props => [];
 }
