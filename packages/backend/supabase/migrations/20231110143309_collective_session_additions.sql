a3bb7caeed87618f7a52d093110040808a9807e3
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-13 12:38:55 -0600

    forgot rls for collective sessions

--- packages/backend/supabase/migrations/20231110143309_collective_session_additions.sql
+++ packages/backend/supabase/migrations/20231110143309_collective_session_additions.sql
@@ -17,3 +17,26 @@ alter table "public"."collective_sessions" add constraint "collective_sessions_c
 alter table "public"."collective_sessions" add constraint "collective_sessions_collaborator_two_individual_session_m_check" CHECK (is_valid_individual_session_metadata_jsonb(collaborator_two_individual_session_metadata)) not valid;
 
 alter table "public"."collective_sessions" validate constraint "collective_sessions_collaborator_two_individual_session_m_check";
+
+create policy "Can Only Read Documents That They Are Co-Authors Of"
+on "public"."collective_sessions"
+as permissive
+for select
+to authenticated
+using (((auth.uid() = collaborator_one_uid) OR (auth.uid() = collaborator_two_uid)));
+
+
+create policy "authenticated users can insert"
+on "public"."collective_sessions"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "they can only update rows they are collaborators on"
+on "public"."collective_sessions"
+as permissive
+for delete
+to authenticated
+using (((auth.uid() = collaborator_one_uid) OR (auth.uid() = collaborator_two_uid)));
