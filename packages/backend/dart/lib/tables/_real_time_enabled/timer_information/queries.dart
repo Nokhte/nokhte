6d0acd18ecfa84d9d745d01bc31d16886d1543dc
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-17 10:08:47 -0600

    made base timer information queries class

---
+++ packages/backend/dart/lib/tables/_real_time_enabled/timer_information/queries.dart
@@ -0,0 +1,5 @@
+import 'package:nokhte_backend/tables/_real_time_enabled/shared/shared.dart';
+
+class TimerInformationQueries extends CollaborativeQueries {
+  TimerInformationQueries({required super.supabase});
+}
