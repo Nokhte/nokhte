b54ae4c1bcf2e4271def7bcec26f2c5223d2c5e4
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-17 10:11:12 -0600

    added table constant fields for timer_information

--- packages/backend/dart/lib/tables/_real_time_enabled/timer_information/queries.dart
+++ packages/backend/dart/lib/tables/_real_time_enabled/timer_information/queries.dart
@@ -1,5 +1,10 @@
 import 'package:nokhte_backend/tables/_real_time_enabled/shared/shared.dart';
 
 class TimerInformationQueries extends CollaborativeQueries {
+  static const tableName = "timer_information";
+  static const isOnline = "is_online";
+  static const timerIsRunning = "timer_is_running";
+  static const timeRemainingInMilliseconds = "time_remaining_in_milliseconds";
+
   TimerInformationQueries({required super.supabase});
 }
