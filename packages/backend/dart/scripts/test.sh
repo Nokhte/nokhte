b0d7e339fe234f265926464196af099ee258e996
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-17 13:06:37 -0600

    End edge functions after their test

--- packages/backend/dart/scripts/test.sh
+++ packages/backend/dart/scripts/test.sh
@@ -1,6 +1,7 @@
 flutter test test/01_user_name_table_test.dart
 flutter test test/02_collaborator_phrases_table_test.dart
 flutter test test/03_synchronous_search_test.dart
+kill $SERVER_PID
 flutter test test/04_fetch_agora_token_test.dart
 flutter test test/05_existing_collaborations_table_test.dart
 flutter test test/06_solo_sharable_documents_table_test.dart
