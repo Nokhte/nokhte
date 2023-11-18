f43bf06d1583ee62fc82d52a1d63691995050a01
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-18 06:55:30 -0600

    add create timer method in remote source

--- packages/client/lib/app/core/modules/timer/data/sources/timer_remote_source.dart
+++ packages/client/lib/app/core/modules/timer/data/sources/timer_remote_source.dart
@@ -1,5 +1,26 @@
+import 'package:nokhte/app/core/modules/timer/domain/logic/create_timer.dart';
+import 'package:nokhte_backend/tables/timer_information.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 abstract class TimerRemoteSource {
-  Future<List> createTimer();
+  Future<List> createTimer(CreateTimerParams params);
 // Future<> ();
 // Future<> ();
+}
+
+class TimerRemoteSourceImpl implements TimerRemoteSource {
+  final SupabaseClient supabase;
+  final TimerInformationQueries queries;
+  final TimerInformationStreams stream;
+
+  TimerRemoteSourceImpl({
+    required this.supabase,
+  })  : queries = TimerInformationQueries(supabase: supabase),
+        stream = TimerInformationStreams(supabase: supabase);
+
+  @override
+  Future<List> createTimer(CreateTimerParams params) async =>
+      await queries.createNewTimer(
+        timerLengthInMilliseconds: params.toMilliseconds(),
+      );
 }
