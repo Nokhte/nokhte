dc40aabaf81d9207bc9a00de3576d6235ee148dd
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-18 10:12:17 -0600

    fixed class name from Markdown Time to Timer

--- packages/client/lib/app/core/modules/timer/domain/logic/markdown_the_timer.dart
+++ packages/client/lib/app/core/modules/timer/domain/logic/markdown_the_timer.dart
@@ -1,11 +1,11 @@
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/modules/timer/domain/domain.dart';
 
-class MarkdownTheTime
+class MarkdownTheTimer
     implements AbstractFutureLogic<TimerMarkdownStatusEntity, NoParams> {
   final TimerContract contract;
 
-  MarkdownTheTime({required this.contract});
+  MarkdownTheTimer({required this.contract});
 
   @override
   call(params) async => await contract.markdownTheTimer(params);
