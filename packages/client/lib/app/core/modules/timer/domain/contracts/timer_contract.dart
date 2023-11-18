0d6924c63f1e5f54672e9d631d19e7e52fb6f599
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-18 06:54:37 -0600

    reflected create timer param changes in contract

--- packages/client/lib/app/core/modules/timer/domain/contracts/timer_contract.dart
+++ packages/client/lib/app/core/modules/timer/domain/contracts/timer_contract.dart
@@ -1,11 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:nokhte/app/core/error/failure.dart';
-// import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/modules/timer/domain/domain.dart';
+import 'package:nokhte/app/core/modules/timer/domain/logic/create_timer.dart';
 
 abstract class TimerContract {
-  Future<Either<Failure, TimerCreationStatusEntity>> createTimer(params);
-  // Future<Either<Failure, returnEntity>> name(NoParams params);
-  // Future<Either<Failure, returnEntity>> name(NoParams params);
-  // Future<Either<Failure, returnEntity>> name(NoParams params);
+  Future<Either<Failure, TimerCreationStatusEntity>> createTimer(
+    CreateTimerParams params,
+  );
 }
