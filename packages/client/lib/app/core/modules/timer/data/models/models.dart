5a80c6ad71fe5ae4e1039d0e50337f38fbe76fc1
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-18 09:22:36 -0600

    update models barrel to include stream entity

--- packages/client/lib/app/core/modules/timer/data/models/models.dart
+++ packages/client/lib/app/core/modules/timer/data/models/models.dart
@@ -1,4 +1,5 @@
 export 'timer_creation_status_model.dart';
 export 'presence_update_status_model.dart';
 export 'timer_deletion_status_model.dart';
+export 'timer_information_stream_model.dart';
 export 'timer_markdown_status_model.dart';
