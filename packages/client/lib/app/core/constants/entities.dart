d9f929491677d8cd1309667f36e882202eb848d5
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-18 10:15:43 -0600

    add timer module constant entities for mobx stores

--- packages/client/lib/app/core/constants/entities.dart
+++ packages/client/lib/app/core/constants/entities.dart
@@ -7,6 +7,7 @@ import 'package:nokhte/app/core/modules/fetch_the_purpose/domain/domain.dart';
 import 'package:nokhte/app/core/modules/get_current_perspectives/domain/domain.dart';
 import 'package:nokhte/app/core/modules/local_speech_to_text/constants/constants.dart';
 import 'package:nokhte/app/core/modules/scheduling/domain/domain.dart';
+import 'package:nokhte/app/core/modules/timer/domain/domain.dart';
 import 'package:nokhte/app/modules/collective_session/domain/entities/entities.dart';
 import 'package:nokhte/app/modules/home/domain/entities/entities.dart';
 import 'package:nokhte/app/modules/individual_session/domain/domain.dart';
@@ -17,6 +18,7 @@ import 'package:nokhte/app/core/modules/local_speech_to_text/domain/domain.dart'
 import 'package:nokhte/app/core/modules/solo_doc/domain/domain.dart';
 import 'package:nokhte/app/modules/p2p_perspectives_session/domain/domain.dart';
 import 'package:nokhte_backend/tables/phrase_components.dart';
+import 'package:nokhte_backend/tables/timer_information.dart';
 import 'package:nokhte_backend/tables/working_collaborative_documents.dart';
 import 'package:nokhte_backend/tables/working_collaborative_scheduling.dart';
 import 'package:nokhte_backend/tables/working_perspectives_positioning.dart';
@@ -212,4 +214,27 @@ class DefaultEntities {
               hasDoneThePerspectivesSession: false,
             ),
           );
+
+  static Either<Failure, PresenceUpdateStatusEntity>
+      get presenceUpdateStatusEntity =>
+          const Right(PresenceUpdateStatusEntity(isUpdated: false));
+
+  static Either<Failure, TimerDeletionStatusEntity>
+      get timerDeletionStatusEntity =>
+          const Right(TimerDeletionStatusEntity(isDeleted: false));
+
+  static Stream<PresenceAndTimeRemaining> get timerInformationStream =>
+      const Stream.empty();
+
+  static Either<Failure, TimerMarkdownStatusEntity>
+      get timerMarkdownStatusEntity =>
+          const Right(TimerMarkdownStatusEntity(isMarkedDown: false));
+
+  static Either<Failure, TimerRunningUpdateStatusEntity>
+      get timerRunningUpdateStatusEntity =>
+          const Right(TimerRunningUpdateStatusEntity(isUpdated: false));
+
+  static Either<Failure, TimerCreationStatusEntity>
+      get timerCreationStatusEntity =>
+          const Right(TimerCreationStatusEntity(isCreated: false));
 }
