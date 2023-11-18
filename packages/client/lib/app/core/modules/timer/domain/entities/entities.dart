77bfb4ef05a4902fe9bbcebcf1be7a9cac0bf3ea
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-18 09:10:40 -0600

    entities barrel file updates for new entities

--- packages/client/lib/app/core/modules/timer/domain/entities/entities.dart
+++ packages/client/lib/app/core/modules/timer/domain/entities/entities.dart
@@ -1,3 +1,5 @@
 export 'timer_creation_status_entity.dart';
 export 'presence_update_status_entity.dart';
 export 'timer_runnning_update_status_entity.dart';
+export 'timer_deletion_status_entity.dart';
+export 'timer_markdown_status_entity.dart';
