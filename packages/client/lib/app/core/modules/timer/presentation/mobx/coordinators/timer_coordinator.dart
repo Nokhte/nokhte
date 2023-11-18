368c8a52439a09781cfabda549fd1b9aee26cb3e
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-18 10:24:37 -0600

    add timer coordinator

---
+++ packages/client/lib/app/core/modules/timer/presentation/mobx/coordinators/timer_coordinator.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/modules/timer/presentation/presentation.dart';
+part 'timer_coordinator.g.dart';
+
+class TimerCoordinator = _TimerCoordinatorBase with _$TimerCoordinator;
+
+abstract class _TimerCoordinatorBase extends Equatable with Store {
+  final CreateTimerStore createTimer;
+  final DeleteTheTimerStore deleteTheTimer;
+  final GetTimerInformationStreamStore getTimeInfoStream;
+  final MarkdownTheTimerStore markdownTheTimer;
+  final UpdatePresenceStore updatePresence;
+  final UpdateTimerRunningStatusStore updateTimerRunningStatus;
+
+  _TimerCoordinatorBase({
+    required this.createTimer,
+    required this.deleteTheTimer,
+    required this.getTimeInfoStream,
+    required this.markdownTheTimer,
+    required this.updatePresence,
+    required this.updateTimerRunningStatus,
+  });
+
+  @override
+  List<Object> get props => [];
+}
