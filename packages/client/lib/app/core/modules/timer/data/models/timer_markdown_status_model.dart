4d23f8b37e323036ec9d37f6da0ec6ee42debdb6
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-18 09:09:17 -0600

    add timer markdown model

---
+++ packages/client/lib/app/core/modules/timer/data/models/timer_markdown_status_model.dart
@@ -0,0 +1,5 @@
+import 'package:nokhte/app/core/modules/timer/domain/domain.dart';
+
+class TimerMarkdownStatusModel extends TimerMarkdownStatusEntity {
+  const TimerMarkdownStatusModel({required super.isMarkedDown});
+}
