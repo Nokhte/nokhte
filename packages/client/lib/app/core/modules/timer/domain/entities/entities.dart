8effaf6f26f82675f0970225a09938a3fcbcd601
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-18 10:13:42 -0600

    change timer running status entity file in barrel

--- packages/client/lib/app/core/modules/timer/domain/entities/entities.dart
+++ packages/client/lib/app/core/modules/timer/domain/entities/entities.dart
@@ -1,6 +1,6 @@
 export 'timer_creation_status_entity.dart';
 export 'presence_update_status_entity.dart';
-export 'timer_runnning_update_status_entity.dart';
+export 'timer_running_update_status_entity.dart';
 export 'timer_deletion_status_entity.dart';
 export 'timer_markdown_status_entity.dart';
 export 'timer_information_stream_entity.dart';
