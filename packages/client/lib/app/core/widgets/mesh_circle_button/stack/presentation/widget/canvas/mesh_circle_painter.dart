83743e0a0050a4386d0c0aecea2b24e6549499b3
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-20 12:58:30 -0600

    added glow abilities to mesh painter

--- packages/client/lib/app/core/widgets/mesh_circle_button/stack/presentation/widget/canvas/mesh_circle_painter.dart
+++ packages/client/lib/app/core/widgets/mesh_circle_button/stack/presentation/widget/canvas/mesh_circle_painter.dart
@@ -3,14 +3,19 @@ import 'package:flutter/material.dart';
 class MeshCirclePainter extends CustomPainter {
   Path path;
   late Paint myPaint;
+  late Paint theGlowPaint;
   Color firstGradientColor;
   Color secondGradientColor;
+  Color glowColor;
+  double glowOpacity;
 
   MeshCirclePainter(
     this.path,
     Size size, {
     required this.firstGradientColor,
     required this.secondGradientColor,
+    required this.glowColor,
+    required this.glowOpacity,
   }) {
     path.getBounds();
     myPaint = Paint()
@@ -27,10 +32,17 @@ class MeshCirclePainter extends CustomPainter {
     myPaint.style = PaintingStyle.stroke;
     myPaint.strokeWidth = 5.0;
     myPaint.color = Colors.white;
+
+    theGlowPaint = Paint();
+    if (glowOpacity != 0) {
+      theGlowPaint.maskFilter = const MaskFilter.blur(BlurStyle.normal, 10.0);
+      theGlowPaint.color = glowColor.withOpacity(glowOpacity);
+    }
   }
 
   @override
   void paint(Canvas canvas, Size size) {
+    canvas.drawPath(path, theGlowPaint);
     canvas.drawPath(path, myPaint);
   }
 
