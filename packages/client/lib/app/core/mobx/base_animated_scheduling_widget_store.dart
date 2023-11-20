9c807b30f5f9ec066341be718b2c4361a38c0639
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-20 15:41:01 -0600

    extended base scheduling w/ custom animated store

--- packages/client/lib/app/core/mobx/base_animated_scheduling_widget_store.dart
+++ packages/client/lib/app/core/mobx/base_animated_scheduling_widget_store.dart
@@ -1,57 +1,49 @@
-// ignore_for_file: must_be_immutable, library_private_types_in_public_api, missing_override_of_must_be_overridden
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
 // * Mobx Import
-import 'package:meta/meta.dart';
 import 'package:mobx/mobx.dart';
 // * Equatable Import
-import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/mobx/base_custom_animated_widget_store.dart';
 // * Mobx Codegen Inclusion
-part 'base_scheduling_widget_store.g.dart';
+part 'base_animated_scheduling_widget_store.g.dart';
 
-class BaseSchedulingWidgetStore<ArrayType, InitParams,
-        IsATimeParams> = _BaseSchedulingWidgetStoreBase<ArrayType, InitParams, IsATimeParams>
-    with _$BaseSchedulingWidgetStore<ArrayType, InitParams, IsATimeParams>;
+class BaseAnimatedSchedulingWidgetStore<ArrayType, InitParams,
+        IsATimeParams> = _BaseAnimatedSchedulingWidgetStoreBase<ArrayType, InitParams, IsATimeParams>
+    with
+        _$BaseAnimatedSchedulingWidgetStore<ArrayType, InitParams,
+            IsATimeParams>;
 
-abstract class _BaseSchedulingWidgetStoreBase<ArrayType, InitParams,
-    IsATimeParams> extends Equatable with Store {
+abstract class _BaseAnimatedSchedulingWidgetStoreBase<ArrayType, InitParams,
+    IsATimeParams> extends BaseCustomAnimatedWidgetStore with Store {
   @observable
   List<ArrayType> startingGrad = [];
 
   @observable
   List<ArrayType> endingGrad = [];
 
-  @mustBeOverridden
   @action
   void isADuskTime(IsATimeParams params) {}
 
-  @mustBeOverridden
   @action
   void isAMorningTime(IsATimeParams params) {}
 
-  @mustBeOverridden
   @action
   void isADayTime(IsATimeParams params) {}
 
-  @mustBeOverridden
   @action
   void isAEveningTime(IsATimeParams params) {}
 
-  @mustBeOverridden
   @action
   void initDuskCallback(InitParams params) {}
 
-  @mustBeOverridden
   @action
   void initMorningCallback(InitParams params) {}
 
-  @mustBeOverridden
   @action
   void initDayCallback(InitParams params) {}
 
-  @mustBeOverridden
   @action
   void initEveningCallback(InitParams params) {}
 
-  @mustBeOverridden
   @action
   initTimeShift({required DateTime pastTime, required DateTime newTime}) {}
 
