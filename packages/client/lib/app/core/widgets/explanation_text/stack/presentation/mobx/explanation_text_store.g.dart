db1f1abee46194d18ce4e712db68e3645a45951c
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-20 14:12:43 -0600

    add explanation_text_store w/ basic functionality

---
+++ packages/client/lib/app/core/widgets/explanation_text/stack/presentation/mobx/explanation_text_store.g.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'explanation_text_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$ExplanationTextStore on _ExplanationTextStoreBase, Store {
+  late final _$showWidgetAtom =
+      Atom(name: '_ExplanationTextStoreBase.showWidget', context: context);
+
+  @override
+  bool get showWidget {
+    _$showWidgetAtom.reportRead();
+    return super.showWidget;
+  }
+
+  @override
+  set showWidget(bool value) {
+    _$showWidgetAtom.reportWrite(value, super.showWidget, () {
+      super.showWidget = value;
+    });
+  }
+
+  late final _$_ExplanationTextStoreBaseActionController =
+      ActionController(name: '_ExplanationTextStoreBase', context: context);
+
+  @override
+  dynamic widgetConstructor() {
+    final _$actionInfo = _$_ExplanationTextStoreBaseActionController
+        .startAction(name: '_ExplanationTextStoreBase.widgetConstructor');
+    try {
+      return super.widgetConstructor();
+    } finally {
+      _$_ExplanationTextStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic toggleWidgetVisibility() {
+    final _$actionInfo = _$_ExplanationTextStoreBaseActionController
+        .startAction(name: '_ExplanationTextStoreBase.toggleWidgetVisibility');
+    try {
+      return super.toggleWidgetVisibility();
+    } finally {
+      _$_ExplanationTextStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+showWidget: ${showWidget}
+    ''';
+  }
+}
