6e5937d9a94f033302dabc9547fc6a067485e728
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-20 12:58:48 -0600

    add glow capabilities in painter params

--- packages/client/lib/app/core/widgets/mesh_circle_button/stack/presentation/widget/mesh_circle_button.dart
+++ packages/client/lib/app/core/widgets/mesh_circle_button/stack/presentation/widget/mesh_circle_button.dart
@@ -5,6 +5,7 @@ import 'package:nokhte/app/core/canvas_widget_utils/canvas_widget_utils.dart';
 import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/shared/constants/svg_animation_constants.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 import 'canvas/mesh_circle_painter.dart';
 
@@ -23,31 +24,40 @@ class MeshCircleButton extends StatelessWidget {
       return AnimatedOpacity(
         opacity: trackerStore.showWidget ? 1 : 0,
         duration: Seconds.get(1),
-        child: SizedBox(
-          width: size.width,
-          height: size.height,
-          child: CustomPaint(
-            painter: MeshCirclePainter(
-              SvgAnimtionConstants.circlePath,
-              size,
-              firstGradientColor: Colors.white,
-              secondGradientColor: Colors.white,
-            ),
-            child: ClipPath(
-              clipper: CustomPathClipper(SvgAnimtionConstants.circlePath),
-              child: AnimatedMeshGradient(
-                controller: trackerStore.meshGradientController,
-                colors: const [
-                  Color(0xFF88F4FC),
-                  Color(0xFF0BB7FC),
-                  Color(0xFF6FE6C9),
-                  Color(0xFF67EEAC),
-                ],
-                options: AnimatedMeshGradientOptions(),
-              ),
-            ),
-          ),
-        ),
+        child: CustomAnimationBuilder(
+            tween: trackerStore.movie,
+            control: trackerStore.control,
+            duration: trackerStore.movie.duration,
+            builder: (context, value, child) {
+              trackerStore.setCurrentGlowColor(value.get('color'));
+              return SizedBox(
+                width: size.width,
+                height: size.height,
+                child: CustomPaint(
+                  painter: MeshCirclePainter(
+                    SvgAnimtionConstants.circlePath,
+                    size,
+                    firstGradientColor: Colors.white,
+                    secondGradientColor: Colors.white,
+                    glowColor: value.get('color'),
+                    glowOpacity: value.get('opacity'),
+                  ),
+                  child: ClipPath(
+                    clipper: CustomPathClipper(SvgAnimtionConstants.circlePath),
+                    child: AnimatedMeshGradient(
+                      controller: trackerStore.meshGradientController,
+                      colors: const [
+                        Color(0xFF88F4FC),
+                        Color(0xFF0BB7FC),
+                        Color(0xFF6FE6C9),
+                        Color(0xFF67EEAC),
+                      ],
+                      options: AnimatedMeshGradientOptions(),
+                    ),
+                  ),
+                ),
+              );
+            }),
       );
     });
   }
