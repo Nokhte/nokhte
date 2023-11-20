36bd637394768ab51761d11e039c2dc982b7e7a8
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-20 06:22:10 -0600

    add appState listener in base timer store

--- packages/client/lib/app/core/mobx/base_times_up_store.dart
+++ packages/client/lib/app/core/mobx/base_times_up_store.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 // * Mobx Import
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 // * Equatable Import
 import 'package:equatable/equatable.dart';
@@ -25,7 +26,24 @@ abstract class _BaseTimesUpStoreBase extends Equatable with Store {
   });
 
   @observable
+  AppLifecycleState appState = AppLifecycleState.resumed;
+
+  @observable
   bool isFirstTimeStartingMovie = true;
+
+  appStateListener() => reaction((p0) => appState, (p0) async {
+        switch (p0) {
+          case AppLifecycleState.resumed:
+            await timer.setOnlineStatus(true);
+          case AppLifecycleState.inactive:
+            await timer.setOnlineStatus(false);
+          default:
+            break;
+        }
+      });
+
+  @action
+  setAppState(AppLifecycleState newState) => appState = newState;
 
   @action
   initOrPauseTimesUp(bool shouldRun) {
