7819f2dc9a1b7dd42cc114758f16d37b263a897c
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-21 09:27:26 -0600

    update test in commensurate w/ supabase injection

--- packages/backend/dart/test/02_collaborator_phrases_table_test.dart
+++ packages/backend/dart/test/02_collaborator_phrases_table_test.dart
@@ -1,50 +1,21 @@
 // ignore_for_file: file_names
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:nokhte_backend/constants/constants.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:nokhte_backend/tables/collaborator_phrases.dart';
 
+import 'shared/shared.dart';
+
 void main() {
-  late SupabaseClient supabase;
-  late String? currentUserUID;
-  late String? otherUserUID;
+  final tSetup = CommonCollaborativeTestFunctions();
+  late CollaboratorPhraseQueries user1Queries;
 
   setUpAll(() async {
-    supabase = SupabaseClientConfigConstants.supabase;
-    final userIDResults = await UserSetupConstants.getUIDs();
-    currentUserUID = userIDResults.first;
-    otherUserUID = userIDResults[1];
-    await SignIn.user1(supabase: supabase);
+    await tSetup.setUp(shouldMakeCollaboration: false);
+    user1Queries = CollaboratorPhraseQueries(supabase: tSetup.user1Supabase);
   });
 
   test("Should be able to read their row", () async {
-    final collaboratorPhrasRes = await CollaboratorPhraseQueries.getUserInfo(
-      supabase: supabase,
-      userUID: currentUserUID,
-    );
-    expect(collaboratorPhrasRes, isNotEmpty);
-  });
-  test("Shouldn't be able to read someone else's row", () async {
-    final collaboratorPhrasRes = await CollaboratorPhraseQueries.getUserInfo(
-      supabase: supabase,
-      userUID: otherUserUID,
-    );
-    expect(collaboratorPhrasRes, isEmpty);
-  });
-
-  test("Shouldn't be able to insert another row", () async {
-    try {
-      final failedInsertRes = await supabase
-          .from('collaborator_phrases')
-          .insert({
-        "collaborator_phrase": "abandoned acid",
-        "noun_id": "1",
-        "adjective_id": "1"
-      });
-      expect(failedInsertRes, isA<PostgrestException>());
-    } catch (e) {
-      expect(e, isA<PostgrestException>());
-    }
+    final res = await user1Queries.getUserInfo();
+    expect(res, isNotEmpty);
   });
 }
