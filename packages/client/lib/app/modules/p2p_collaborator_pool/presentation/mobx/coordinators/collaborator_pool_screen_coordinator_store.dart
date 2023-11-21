4f15fb4e01147551963888491de34b748763e949
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-21 07:22:15 -0600

    moved startRotating text to screen constructor
    this is advantageous to the extent that it runs once on the
    construction of the screen and not when users go in & out so that
    bug behavior should be no more

--- packages/client/lib/app/modules/p2p_collaborator_pool/presentation/mobx/coordinators/collaborator_pool_screen_coordinator_store.dart
+++ packages/client/lib/app/modules/p2p_collaborator_pool/presentation/mobx/coordinators/collaborator_pool_screen_coordinator_store.dart
@@ -4,10 +4,10 @@ import 'package:mobx/mobx.dart';
 import 'package:equatable/equatable.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/beach_widgets/shared/shared.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/p2p_collaborator_pool/presentation/mobx/mobx.dart';
 
-import '../../../../../core/widgets/beach_widgets/shared/types/types.dart';
 part 'collaborator_pool_screen_coordinator_store.g.dart';
 
 class CollaboratorPoolScreenCoordinatorStore = _CollaboratorPoolScreenCoordinatorStoreBase
@@ -61,6 +61,7 @@ abstract class _CollaboratorPoolScreenCoordinatorStoreBase extends Equatable
 
   @action
   screenConstructorCallback() {
+    fadingTextStore.startRotatingText(Seconds.get(0));
     beachWavesStore.initiateTimesUp(
       timerLength: Seconds.get(45),
     );
