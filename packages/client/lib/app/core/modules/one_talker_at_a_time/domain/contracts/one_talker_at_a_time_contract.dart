a95f4026603119ad7d9ac14fce5fea1fe6fd9838
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-21 12:23:14 -0600

    added constract interface for one talker module

---
+++ packages/client/lib/app/core/modules/one_talker_at_a_time/domain/contracts/one_talker_at_a_time_contract.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:nokhte/app/core/error/failure.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/modules/one_talker_at_a_time/domain/domain.dart';
+
+abstract class OneTalkerAtATimeContract {
+  Future<Either<Failure, CheckIfCollaboratorisTalkingEntity>>
+      checkIfCollaboratorIsTalking(NoParams params);
+  Future<Either<Failure, WhoIsTalkingUpdateStatusEntity>> updateWhoIsTalking(
+    WhoIsTalkingUpdateOptions params,
+  );
+}
