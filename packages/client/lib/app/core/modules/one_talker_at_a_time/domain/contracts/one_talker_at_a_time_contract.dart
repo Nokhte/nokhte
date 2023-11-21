cf3e1bd477a38683b771bb41b4e44db41bc8677e
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-21 12:41:22 -0600

    fixed typo with 'i' in the contract interface

--- packages/client/lib/app/core/modules/one_talker_at_a_time/domain/contracts/one_talker_at_a_time_contract.dart
+++ packages/client/lib/app/core/modules/one_talker_at_a_time/domain/contracts/one_talker_at_a_time_contract.dart
@@ -4,7 +4,7 @@ import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/modules/one_talker_at_a_time/domain/domain.dart';
 
 abstract class OneTalkerAtATimeContract {
-  Future<Either<Failure, CheckIfCollaboratorisTalkingEntity>>
+  Future<Either<Failure, CheckIfCollaboratorIsTalkingEntity>>
       checkIfCollaboratorIsTalking(NoParams params);
   Future<Either<Failure, WhoIsTalkingUpdateStatusEntity>> updateWhoIsTalking(
     WhoIsTalkingUpdateParams params,
