05eb4ae3d7df61f87a436fc01496099877392e37
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-22 12:08:05 -0600

    delayed navigation migration completed (relatively)

--- packages/client/lib/app/modules/p2p_collaborator_pool/presentation/mobx/coordinators/collaborator_pool_screen_coordinator_store.dart
+++ packages/client/lib/app/modules/p2p_collaborator_pool/presentation/mobx/coordinators/collaborator_pool_screen_coordinator_store.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
-import 'package:equatable/equatable.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
 import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/beach_widgets/shared/shared.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
@@ -13,40 +13,36 @@ part 'collaborator_pool_screen_coordinator_store.g.dart';
 class CollaboratorPoolScreenCoordinatorStore = _CollaboratorPoolScreenCoordinatorStoreBase
     with _$CollaboratorPoolScreenCoordinatorStore;
 
-abstract class _CollaboratorPoolScreenCoordinatorStoreBase extends Equatable
-    with Store {
+abstract class _CollaboratorPoolScreenCoordinatorStoreBase
+    extends BaseCoordinator with Store {
   final ExitCollaboratorPoolStore exitCollaboratorPoolStore;
   final CancelCollaboratorStreamStore cancelStreamStore;
   final GetCollaboratorSearchStatusStore getCollaboratorSearchStatusStore;
-  final BeachWavesTrackerStore beachWavesStore;
   final FadeInAndChangeColorTextStore fadeInAndColorTextStore;
 
   _CollaboratorPoolScreenCoordinatorStoreBase({
     required this.exitCollaboratorPoolStore,
     required this.cancelStreamStore,
     required this.getCollaboratorSearchStatusStore,
-    required this.beachWavesStore,
+    required super.beachWaves,
     required this.fadeInAndColorTextStore,
   });
-  // so click a bunch to see where it comes from
 
   @action
   beachWavesMovieStatusListener() =>
-      reaction((p0) => beachWavesStore.movieStatus, (p0) {
-        print(
-            "what's happening here?? ${beachWavesStore.movieStatus} ${beachWavesStore.movieMode}");
-        if (beachWavesStore.movieStatus == MovieStatus.finished &&
-            beachWavesStore.movieMode == BeachWaveMovieModes.timesUp) {
-          beachWavesStore.initiateBackToOceanDive();
+      reaction((p0) => beachWaves.movieStatus, (p0) {
+        if (beachWaves.movieStatus == MovieStatus.inProgress &&
+            beachWaves.movieMode == BeachWaveMovieModes.timesUp) {
+          delayedNavigation(() => beachWaves.initiateBackToOceanDive());
           exitCollaboratorPoolStore(NoParams());
           cancelStreamStore(NoParams());
-        } else if (beachWavesStore.movieStatus == MovieStatus.finished &&
-            beachWavesStore.movieMode == BeachWaveMovieModes.backToOceanDive) {
-          Modular.to.navigate('/p2p_collaborator_pool/');
-        } else if (beachWavesStore.movieStatus == MovieStatus.finished &&
-            beachWavesStore.movieMode == BeachWaveMovieModes.backToTheDepths) {
-          print("did the nav to purpose session work??");
-          Modular.to.navigate('/p2p_purpose_session/');
+        } else if (beachWaves.movieStatus == MovieStatus.inProgress &&
+            beachWaves.movieMode == BeachWaveMovieModes.backToOceanDive) {
+          delayedNavigation(
+              () => Modular.to.navigate('/p2p_collaborator_pool/'));
+        } else if (beachWaves.movieStatus == MovieStatus.inProgress &&
+            beachWaves.movieMode == BeachWaveMovieModes.backToTheDepths) {
+          delayedNavigation(() => Modular.to.navigate('/p2p_purpose_session/'));
         }
       });
 
@@ -57,26 +53,17 @@ abstract class _CollaboratorPoolScreenCoordinatorStoreBase extends Equatable
           final Stopwatch stopwatch = Stopwatch();
           stopwatch.start();
           if (value == true) {
-            print("did this piece run??");
-            // if (stopwatch.elapsedMilliseconds < 1000) {
             Future.delayed(Seconds.get(2), () {
-              beachWavesStore.teeUpBackToTheDepths();
+              beachWaves.teeUpBackToTheDepths();
               fadeInAndColorTextStore.teeUpFadeOut();
             });
-            // } else {
-            // beachWavesStore.teeUpBackToTheDepths();
-            // fadeInAndColorTextStore.teeUpFadeOut();
-            // ship this in the morning and see if this fixes the issue
-            // also click around alot to see how we can counter act the problem
-            // of the transition not happening sometimes
-            // }
           }
         });
       });
 
   @action
   screenConstructorCallback() {
-    beachWavesStore.initiateTimesUp(
+    beachWaves.initiateTimesUp(
       timerLength: Seconds.get(45),
     );
     getCollaboratorSearchStatusStore();
