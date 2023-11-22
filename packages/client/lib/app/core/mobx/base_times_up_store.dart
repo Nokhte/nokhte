8a773d7684e935a9079256cf4dcbafead66afc2e
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-22 12:04:46 -0600

    extended BaseCoordinator for simplicity sake

--- packages/client/lib/app/core/mobx/base_times_up_store.dart
+++ packages/client/lib/app/core/mobx/base_times_up_store.dart
@@ -4,24 +4,23 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 // * Equatable Import
-import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
 import 'package:nokhte/app/core/modules/timer/presentation/mobx/mobx.dart';
 import 'package:nokhte/app/core/types/seconds.dart';
-import 'package:nokhte/app/core/widgets/mobx.dart';
 import 'package:simple_animations/simple_animations.dart';
 // * Mobx Codegen Inclusion
 part 'base_times_up_store.g.dart';
 
 class BaseTimesUpStore = _BaseTimesUpStoreBase with _$BaseTimesUpStore;
 
-abstract class _BaseTimesUpStoreBase extends Equatable with Store {
+abstract class _BaseTimesUpStoreBase extends BaseCoordinator with Store {
   final TimerCoordinator timer;
-  final BeachWavesTrackerStore beachWaves;
+  // final BeachWavesTrackerStore beachWaves;
   final Duration productionTimerLength;
 
   _BaseTimesUpStoreBase({
     required this.timer,
-    required this.beachWaves,
+    required super.beachWaves,
     required this.productionTimerLength,
   });
 
--- packages/client/lib/app/core/mobx/mobx.dart
+++ packages/client/lib/app/core/mobx/mobx.dart
@@ -1,6 +1,7 @@
 export './base_future_store.dart';
 export './base_mobx_db_store.dart';
 export 'base_times_up_store.dart';
+export 'base_coordinator.dart';
 export './base_quadrant_api_receiver.dart';
 export './error.dart';
 export './store_state.dart';
