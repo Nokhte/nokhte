37a5fba2752fab72e614ba516138342eebb48af4
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-23 07:26:40 -0600

    fix listeners to work with new nav architecture

--- packages/client/lib/app/modules/p2p_collaborator_pool/presentation/mobx/coordinators/collaborator_pool_screen_coordinator_store.dart
+++ packages/client/lib/app/modules/p2p_collaborator_pool/presentation/mobx/coordinators/collaborator_pool_screen_coordinator_store.dart
@@ -1,4 +1,7 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
@@ -26,50 +29,101 @@ abstract class _CollaboratorPoolScreenCoordinatorStoreBase
     required this.getCollaboratorSearchStatusStore,
     required super.beachWaves,
     required this.fadeInAndColorTextStore,
-  });
+  }) {
+    // searchStatusListener();
+    // beachWavesMovieStatusListener();
+    // final duration = kDebugMode ? Seconds.get(10) : Seconds.get(45);
+    // beachWaves.initiateTimesUp(
+    //   timerLength: duration,
+    // );
+  }
 
-  @action
+  // @action
   beachWavesMovieStatusListener() =>
       reaction((p0) => beachWaves.movieStatus, (p0) {
-        if (beachWaves.movieStatus == MovieStatus.inProgress &&
+        print(
+            "HEYYYYYYYYY IS THIS WORKING???? ${beachWaves.movieStatus} ${beachWaves.movieMode} ${beachWaves.movie.duration}");
+        if (beachWaves.movieStatus == MovieStatus.finished &&
             beachWaves.movieMode == BeachWaveMovieModes.timesUp) {
-          delayedNavigation(() => beachWaves.initiateBackToOceanDive());
+          // Future.delayed(Seconds.get(0), () {
+          beachWaves.initiateBackToOceanDive();
           exitCollaboratorPoolStore(NoParams());
           cancelStreamStore(NoParams());
-        } else if (beachWaves.movieStatus == MovieStatus.inProgress &&
+          // });
+        } else if (beachWaves.movieStatus == MovieStatus.finished &&
             beachWaves.movieMode == BeachWaveMovieModes.backToOceanDive) {
-          delayedNavigation(
-              () => Modular.to.navigate('/p2p_collaborator_pool/'));
-        } else if (beachWaves.movieStatus == MovieStatus.inProgress &&
+          // delayedNavigation(() =>
+          Modular.to.navigate('/p2p_collaborator_pool/');
+          // );
+        } else if (beachWaves.movieStatus == MovieStatus.finished &&
             beachWaves.movieMode == BeachWaveMovieModes.backToTheDepths) {
-          delayedNavigation(() => Modular.to.navigate('/p2p_purpose_session/'));
+          // delayedNavigation(
+          // () =>
+          Modular.to.navigate('/p2p_purpose_session/');
+          // );
         }
       });
 
-  @action
+  // @action
   searchStatusListener() =>
       reaction((p0) => getCollaboratorSearchStatusStore.searchStatus, (p0) {
         p0.listen((value) {
-          final Stopwatch stopwatch = Stopwatch();
-          stopwatch.start();
-          if (value == true) {
+          print("value $value");
+          if (value) {
+            print("did the first reaction run search status? $value?");
             Future.delayed(Seconds.get(2), () {
               beachWaves.teeUpBackToTheDepths();
               fadeInAndColorTextStore.teeUpFadeOut();
             });
           }
+          // else if (!value) {
+          //   delayedNavigation(() {
+          //     if (!value &&
+          //         beachWaves.movieMode == BeachWaveMovieModes.timesUp) {
+          //       beachWaves.initiateBackToOceanDive();
+          //       exitCollaboratorPoolStore(NoParams());
+          //       cancelStreamStore(NoParams());
+          //     }
+          //   });
+          // }
         });
       });
 
   @action
   screenConstructorCallback() {
+    // final duration = kDebugMode ? Seconds.get(10) : Seconds.get(45);
+    final duration = kDebugMode ? Seconds.get(10) : Seconds.get(45);
     beachWaves.initiateTimesUp(
-      timerLength: Seconds.get(45),
+      timerLength: duration,
     );
     getCollaboratorSearchStatusStore();
     searchStatusListener();
     beachWavesMovieStatusListener();
+    // await Future.delayed(Seconds.get(0), () async {
+    // Timer.run(() {
+    // beachWaves.initiateTimesUp(
+    //   timerLength: duration,
+    // );
+    // });
+    // });
   }
+
+  // @action
+  // screenConstructorCallback() {
+  //   beachWaves.initiateTimesUp(
+  //     timerLength: Seconds.get(45),
+  //   );
+  //   getCollaboratorSearchStatusStore();
+  //   searchStatusListener();
+  //   beachWavesMovieStatusListener();
+  // }
+
+  // @action
+  // separateFunction(duration) => Future.delayed(
+  //     Seconds.get(0, milli: 1),
+  //     () => beachWaves.initiateTimesUp(
+  //           timerLength: duration,
+  //         ));
 
   @override
   List<Object> get props => [];
