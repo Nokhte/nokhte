149a0eee085787f20148a420ffff2dd86a278de2
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-24 11:22:12 -0600

    update spelling with correction

--- packages/client/lib/app/core/modules/update_existing_collaborations/domain/contracts/update_existing_collaborations_contract.dart
+++ packages/client/lib/app/core/modules/update_existing_collaborations/domain/contracts/update_existing_collaborations_contract.dart
@@ -2,9 +2,8 @@ import 'package:dartz/dartz.dart';
 import 'package:nokhte/app/core/error/failure.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/modules/update_existing_collaborations/domain/domain.dart';
-import 'package:nokhte/app/core/modules/update_existing_collaborations/domain/entities/collaboration_activation_status_entity.dart';
 
-abstract class UpdateExistingCollaborationsConstract {
+abstract class UpdateExistingCollaborationsContract {
   Future<Either<Failure, CollaborationConsecrationStatusEntity>>
       consecrateTheCollaboration(NoParams params);
   Future<Either<Failure, CollaborationActivationStatusEntity>>
