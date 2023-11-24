f37476202a468f4462122e11622b54253bebab80
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-24 14:11:01 -0600

    add update entry to barrel

--- packages/client/lib/app/core/modules/update_existing_collaborations/mobx/main/main.dart
+++ packages/client/lib/app/core/modules/update_existing_collaborations/mobx/main/main.dart
@@ -1,2 +1,3 @@
 export './consecrate_the_collaboration_store.dart';
 export './update_collaboration_activation_status_store.dart';
+export './update_individual_collaborator_entry_status_store.dart';
