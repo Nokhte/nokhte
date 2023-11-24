1b8231c04b3f8e9496ce979dff0d74558314d362
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-24 14:01:38 -0600

    modify model to searchAndEntry stream

--- packages/client/lib/app/modules/p2p_collaborator_pool/data/models/collaborator_search_status_model.dart
+++ packages/client/lib/app/modules/p2p_collaborator_pool/data/models/collaborator_search_status_model.dart
@@ -1,5 +1,6 @@
 import 'package:nokhte/app/modules/p2p_collaborator_pool/domain/entities/entities.dart';
 
 class CollaboratorSearchStatusModel extends CollaboratorSearchStatusEntity {
-  const CollaboratorSearchStatusModel({required super.isFound});
+  const CollaboratorSearchStatusModel(
+      {required super.searchAndEntryStatusStream});
 }
