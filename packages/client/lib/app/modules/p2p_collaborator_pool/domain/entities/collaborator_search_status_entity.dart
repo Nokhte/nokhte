b17cd1de4ff011784c81d175fb2b14a2804a1cd8
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-24 14:01:00 -0600

    change to searchAndEntryStatus stream from bool

--- packages/client/lib/app/modules/p2p_collaborator_pool/domain/entities/collaborator_search_status_entity.dart
+++ packages/client/lib/app/modules/p2p_collaborator_pool/domain/entities/collaborator_search_status_entity.dart
@@ -1,10 +1,12 @@
 import 'package:equatable/equatable.dart';
+import 'package:nokhte_backend/tables/existing_collaborations.dart';
 
 class CollaboratorSearchStatusEntity extends Equatable {
-  final Stream<bool> isFound;
+  final Stream<CollaboratorSearchAndEntryStatus> searchAndEntryStatusStream;
 
-  const CollaboratorSearchStatusEntity({required this.isFound});
+  const CollaboratorSearchStatusEntity(
+      {required this.searchAndEntryStatusStream});
 
   @override
-  List<Object> get props => [isFound];
+  List<Object> get props => [searchAndEntryStatusStream];
 }
