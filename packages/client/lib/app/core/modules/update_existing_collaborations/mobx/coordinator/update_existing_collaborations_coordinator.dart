c6cb80fd98f5ce5db76029fd99c115e3ab276c03
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-24 11:26:24 -0600

    add coordinator

---
+++ packages/client/lib/app/core/modules/update_existing_collaborations/mobx/coordinator/update_existing_collaborations_coordinator.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+// * Mobx Import
+import 'package:mobx/mobx.dart';
+// * Equatable Import
+import 'package:equatable/equatable.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:nokhte/app/core/error/failure.dart';
+import 'package:nokhte/app/core/modules/update_existing_collaborations/mobx/main/main.dart';
+// import 'package:nokhte/app/core/modules/update_existing_collaborations/mobx/mobx.dart';
+// * Mobx Codegen Inclusion
+part 'update_existing_collaborations_coordinator.g.dart';
+
+class UpdateExistingCollaborationsCoordinator = _UpdateExistingCollaborationsCoordinatorBase
+    with _$UpdateExistingCollaborationsCoordinator;
+
+abstract class _UpdateExistingCollaborationsCoordinatorBase extends Equatable
+    with Store {
+  final ConsecrateTheCollaborationStore consecrateTheCollaboration;
+  final UpdateCollaborationActivationStatusStore
+      updateCollaborationActivationStatus;
+
+  _UpdateExistingCollaborationsCoordinatorBase({
+    required this.consecrateTheCollaboration,
+    required this.updateCollaborationActivationStatus,
+  });
+  @override
+  List<Object> get props => [
+// some items
+      ];
+}
---
+++ packages/client/lib/app/core/modules/update_existing_collaborations/mobx/mobx.dart
@@ -0,0 +1,2 @@
+export './coordinator/update_existing_collaborations_coordinator.dart';
+export './main/main.dart';
