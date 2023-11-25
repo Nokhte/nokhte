13c5ebf311e8b5ae5e46d78df90f2f86d6be8e6d
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-25 07:35:58 -0600

    add string comparison and helper methods

--- packages/core/lib/custom_control_structures/string_comparisons.dart
+++ packages/core/lib/custom_control_structures/string_comparisons.dart
@@ -1,11 +1,30 @@
+import 'package:nokhte_core/types/types.dart';
+
 class StringComparison {
-  static isCollaboratorTwo({
+  static isCollaborator({
+    required CollaboratorOptions collaboratorOptions,
     required String input,
     required Function callback,
     required elseReturnVal,
   }) async {
-    if (input == "collaborator_two") {
-      return await callback();
+    switch (collaboratorOptions) {
+      case CollaboratorOptions.one:
+        executeCallbackIfStringEquals(
+            callback, input, "collaborator_one", elseReturnVal);
+      case CollaboratorOptions.two:
+        executeCallbackIfStringEquals(
+            callback, input, "collaborator_two", elseReturnVal);
+    }
+  }
+
+  static executeCallbackIfStringEquals(
+    Function callback,
+    String input,
+    String comparisonString,
+    elseReturnVal,
+  ) async {
+    if (input == comparisonString) {
+      await callback();
     } else {
       return elseReturnVal;
     }
