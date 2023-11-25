cd3ae0142261af64d2ad193c02c9f6700c165dcb
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-25 12:53:13 -0600

    add remote source & worksapce changes

---
+++ packages/client/lib/app/core/modules/abort_purpose_session_artifacts/data/sources/abort_purpose_session_artifacts_remote_source.dart
@@ -0,0 +1,42 @@
+import 'package:nokhte/app/core/modules/timer/data/data.dart';
+import 'package:nokhte_backend/tables/existing_collaborations.dart';
+import 'package:nokhte_backend/tables/solo_sharable_documents.dart';
+import 'package:nokhte_backend/tables/working_collaborative_documents.dart';
+
+abstract class AbortPurposeSessionArtifactsRemoteSource {
+  Future<void> abortTheCollaboration();
+  Future<void> deleteSoloDocuments();
+  Future<void> deleteWorkingCollaborativeDocument();
+  // Future<List> deleteTimer(); not needed
+}
+
+abstract class AbortPurposeSessionArtifactsRemoteSourceImpl
+    extends TimerRemoteSourceImpl
+    implements AbortPurposeSessionArtifactsRemoteSource {
+  final ExistingCollaborationsQueries existingCollaborationsQueries;
+  final SoloSharableDocumentQueries soloSharableDocumentQueries;
+  final WorkingCollaborativeDocumentsQueries
+      workingCollaborativeDocumentsQueries;
+  AbortPurposeSessionArtifactsRemoteSourceImpl({required super.supabase})
+      : existingCollaborationsQueries =
+            ExistingCollaborationsQueries(supabase: supabase),
+        soloSharableDocumentQueries =
+            SoloSharableDocumentQueries(supabase: supabase),
+        workingCollaborativeDocumentsQueries =
+            WorkingCollaborativeDocumentsQueries(supabase: supabase);
+
+  @override
+  Future<void> abortTheCollaboration() async =>
+      await existingCollaborationsQueries.abortUnConsecratedTheCollaboration();
+  @override
+  Future<void> deleteSoloDocuments() async =>
+      await soloSharableDocumentQueries.deleteDocument();
+  @override
+  Future<void> deleteWorkingCollaborativeDocument() async =>
+      await workingCollaborativeDocumentsQueries.deleteThedoc();
+}
+
+// 1. existing_collaborations
+// 2. solo_sharable_documents
+// 3. timer_information
+// 4. working_collaborative_documents
