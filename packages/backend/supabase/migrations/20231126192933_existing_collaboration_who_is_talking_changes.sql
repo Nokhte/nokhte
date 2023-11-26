73b1c42153281bb21ff943218ba9ceb582dc2abf
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-26 14:09:03 -0600

    modified name to who is talking to talking_queue

--- packages/backend/supabase/migrations/20231126192933_existing_collaboration_who_is_talking_changes.sql
+++ packages/backend/supabase/migrations/20231126192933_existing_collaboration_who_is_talking_changes.sql
@@ -1,5 +1,5 @@
 alter table "public"."existing_collaborations" drop constraint "existing_collaborations_who_is_talking_fkey";
 
-alter table "public"."existing_collaborations" alter column "who_is_talking" set not null;
+alter table "public"."existing_collaborations" drop column "who_is_talking";
 
-alter table "public"."existing_collaborations" alter column "who_is_talking" set data type uuid[] using "who_is_talking"::uuid[]; 
+alter table "public"."existing_collaborations" add column "talking_queue" uuid[] not null default '{}'::uuid[];
