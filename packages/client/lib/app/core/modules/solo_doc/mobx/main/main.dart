d9668943cc7fb252853f371462f73670dd49a367
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-27 07:10:19 -0600

    delete create solo doc store + test + main.dart barrel

--- packages/client/lib/app/core/modules/solo_doc/mobx/main/main.dart
+++
@@ -1,5 +0,0 @@
-export './create_solo_doc_store.dart';
-export './get_solo_doc_store.dart';
-export './seal_solo_doc_store.dart';
-export './share_solo_doc_store.dart';
-export './submit_solo_doc_store.dart';
