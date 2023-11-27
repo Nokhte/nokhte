8514127409d669b4d5356e3d265398c7e9f82c3a
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-27 06:30:34 -0600

    move to root & rename coordinatorStore to coordinator

---
+++ packages/client/lib/app/core/modules/collaborative_doc/mobx/collaborative_doc_coordinator.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/modules/collaborative_doc/domain/domain.dart';
+import 'get_collaborative_doc_content_store.dart';
+part 'collaborative_doc_coordinator.g.dart';
+
+class CollaborativeDocCoordinator = _CollaborativeDocCoordinatorBase
+    with _$CollaborativeDocCoordinator;
+
+abstract class _CollaborativeDocCoordinatorBase extends Equatable with Store {
+  final UpdateCommitDesireStatus updateCommitDesire;
+  final UpdateUserDelta updateDelta;
+  final UpdateUserPresence updatePresence;
+  final CreateCollaborativeDoc createDoc;
+  final GetCollaborativeDocContentStore getContent;
+  final GetCollaboratorDocInfo getCollaboratorInfo;
+  final UpdateCollaborativeDoc updateDoc;
+
+  _CollaborativeDocCoordinatorBase({
+    required this.updateDoc,
+    required this.createDoc,
+    required this.getContent,
+    required this.getCollaboratorInfo,
+    required this.updateDelta,
+    required this.updatePresence,
+    required this.updateCommitDesire,
+  });
+  @override
+  List<Object> get props => [];
+}
