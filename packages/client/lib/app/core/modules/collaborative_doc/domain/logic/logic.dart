04abd31970fd97d43f775b05ea47836110ec1ecb
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-27 06:33:51 -0600

    add missing logic & reorder in alphabetical order

--- packages/client/lib/app/core/modules/collaborative_doc/domain/logic/logic.dart
+++ packages/client/lib/app/core/modules/collaborative_doc/domain/logic/logic.dart
@@ -1,6 +1,7 @@
+export './create_collaborative_doc.dart';
 export './get_collaborative_doc_content.dart';
 export 'get_collaborator_doc_info.dart';
-export './create_collaborative_doc.dart';
 export './update_collaborative_doc.dart';
+export './update_commit_desire_status.dart';
 export './update_user_delta.dart';
 export './update_user_presence.dart';
