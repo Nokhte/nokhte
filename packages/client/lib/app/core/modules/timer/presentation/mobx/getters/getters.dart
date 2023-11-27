cad0d3d644a60c302b244f5d15cca0b7d3868462
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-27 07:25:46 -0600

    purge getter stores & their barrel

--- packages/client/lib/app/core/modules/timer/presentation/mobx/getters/getters.dart
+++
@@ -1,6 +0,0 @@
-export 'create_timer_getter_store.dart';
-export 'delete_the_timer_getter_store.dart';
-export 'get_timer_information_stream_getter_store.dart';
-export 'markdown_the_timer_getter_store.dart';
-export 'update_presence_getter_store.dart';
-export 'update_timer_running_status_getter_store.dart';
