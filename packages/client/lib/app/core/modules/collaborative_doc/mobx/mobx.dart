480f74e1fb1d763a150bad44bd553f0933b2f8e2
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-27 06:32:41 -0600

    add get_collaborative_doc_content_store to root
    it's the only necessary mobx store as it stores the state of the stream

---
+++ packages/client/lib/app/core/modules/collaborative_doc/mobx/mobx.dart
@@ -0,0 +1,2 @@
+export 'collaborative_doc_coordinator.dart';
+export './get_collaborative_doc_content_store.dart';
---
+++ packages/client/test/app/core/modules/collaborative_doc/mobx/get_collaborative_doc_content_store_test.dart
@@ -0,0 +1,51 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:nokhte/app/core/constants/failure_constants.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/modules/collaborative_doc/mobx/mobx.dart';
+import '../constants/entities/entities.dart';
+import '../fixtures/collaborative_doc_mock_gen.mocks.dart';
+
+void main() {
+  late MockMGetCollaborativeDocContent mockLogic;
+  late GetCollaborativeDocContentStore shareSoloDocStore;
+  final tParams = NoParams();
+
+  setUp(() {
+    mockLogic = MockMGetCollaborativeDocContent();
+    shareSoloDocStore = GetCollaborativeDocContentStore(
+      logic: mockLogic,
+    );
+  });
+
+  group("call", () {
+    test("✅ Success Case: should update accordingly if state is passed",
+        () async {
+      when(mockLogic(NoParams())).thenAnswer(
+        (_) async => ConstantCollaborativeDocContentEntity.wrappedSuccessCase,
+      );
+      await shareSoloDocStore(tParams);
+      shareSoloDocStore.docContent.listen((value) {
+        expect(value.content, "content");
+        expect(value.currentUserUID, "lastEditedBy");
+        expect(value.lastEditedBy, "lastEditedBy");
+      });
+      expect(shareSoloDocStore.errorMessage, "");
+    });
+    test("❌ Success Case: should update accordingly if failure is passed",
+        () async {
+      when(mockLogic(NoParams())).thenAnswer(
+        (_) async => Left(FailureConstants.dbFailure),
+      );
+      await shareSoloDocStore(tParams);
+      shareSoloDocStore.docContent.listen((value) {
+        expect(value.content, "");
+        expect(value.currentUserUID, "");
+        expect(value.lastEditedBy, "");
+      });
+      expect(
+          shareSoloDocStore.errorMessage, FailureConstants.genericFailureMsg);
+    });
+  });
+}
