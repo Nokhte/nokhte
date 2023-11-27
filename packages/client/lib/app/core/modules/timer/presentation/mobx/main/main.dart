08ea8d8ddbade35075850c453c74e02d53c12f8c
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-27 07:27:34 -0600

    remove get timer information stream

--- packages/client/lib/app/core/modules/timer/presentation/mobx/main/main.dart
+++
@@ -1,6 +0,0 @@
-export 'create_timer_store.dart';
-export 'delete_the_timer_store.dart';
-export 'get_timer_information_stream_store.dart';
-export 'markdown_the_timer_store.dart';
-export 'update_presence_store.dart';
-export 'update_timer_running_status_store.dart';
