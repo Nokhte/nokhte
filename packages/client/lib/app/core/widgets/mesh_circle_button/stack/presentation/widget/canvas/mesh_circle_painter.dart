707f26b1ae212005fbe408c68bdfc30b9f69638e
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-26 18:15:34 -0600

    add x & y scale and translate fields & scaling
    this is for the glow due to the requirement for the glow to scale
    and as a result the path needed to be scaled with Matrices

--- packages/client/lib/app/core/widgets/mesh_circle_button/stack/presentation/widget/canvas/mesh_circle_painter.dart
+++ packages/client/lib/app/core/widgets/mesh_circle_button/stack/presentation/widget/canvas/mesh_circle_painter.dart
@@ -4,10 +4,15 @@ class MeshCirclePainter extends CustomPainter {
   Path path;
   late Paint myPaint;
   late Paint theGlowPaint;
+  double xScaleglow;
+  double yGlowScale;
+  double xGlowXTranslate;
+  double yGlowYTranslate;
   Color firstGradientColor;
   Color secondGradientColor;
   Color glowColor;
   double glowOpacity;
+  late Path somePath;
 
   MeshCirclePainter(
     this.path,
@@ -16,6 +21,10 @@ class MeshCirclePainter extends CustomPainter {
     required this.secondGradientColor,
     required this.glowColor,
     required this.glowOpacity,
+    required this.xScaleglow,
+    required this.yGlowScale,
+    required this.xGlowXTranslate,
+    required this.yGlowYTranslate,
   }) {
     path.getBounds();
     myPaint = Paint()
@@ -28,13 +37,22 @@ class MeshCirclePainter extends CustomPainter {
       ]).createShader(
         path.getBounds(),
       );
+    final scalingMatrix = Matrix4(
+      xScaleglow, 0, 0, 0, //
+      0, yGlowScale, 0, 0, //
+      0, 0, 1, 0, //
+      xGlowXTranslate, yGlowYTranslate, 0, 1, //
+    );
 
+    somePath = path.transform(scalingMatrix.storage);
+    // somePath = path;
     myPaint.style = PaintingStyle.stroke;
     myPaint.strokeWidth = 5.0;
     myPaint.color = Colors.white;
 
     theGlowPaint = Paint();
-    if (glowOpacity != 0) {
+    if (glowOpacity > 0) {
+      print(glowOpacity);
       theGlowPaint.maskFilter = const MaskFilter.blur(BlurStyle.normal, 10.0);
       theGlowPaint.color = glowColor.withOpacity(glowOpacity);
     }
@@ -42,7 +60,9 @@ class MeshCirclePainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawPath(path, theGlowPaint);
+    if (glowOpacity > 0) {
+      canvas.drawPath(somePath, theGlowPaint);
+    }
     canvas.drawPath(path, myPaint);
   }
 
