339cfa176eb31c1423d19cb139db1deac58b52d0
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-27 07:08:28 -0600

    rm getter stores & their tests

--- packages/client/lib/app/core/modules/solo_doc/mobx/getters/getters.dart
+++
@@ -1,5 +0,0 @@
-export './create_solo_doc_getter_store.dart';
-export 'get_solo_doc_getter_store.dart';
-export './seal_solo_doc_getter_store.dart';
-export './share_solo_doc_getter_store.dart';
-export 'submit_solo_doc_getter_store.dart';
