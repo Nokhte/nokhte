fa7b7af51c1467990741fd20b8489ecc554e67da
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-27 07:14:17 -0600

    rename from coordinatorStore to coordinator

--- packages/client/lib/app/core/modules/solo_doc/mobx/coordinator/solo_doc_coordinator.dart
+++ packages/client/lib/app/core/modules/solo_doc/mobx/coordinator/solo_doc_coordinator.dart
@@ -3,19 +3,18 @@ import 'package:equatable/equatable.dart';
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/modules/solo_doc/domain/domain.dart';
 import 'package:nokhte/app/core/modules/solo_doc/mobx/main/get_solo_doc_store.dart';
-part 'solo_doc_coordinator_store.g.dart';
+part 'solo_doc_coordinator.g.dart';
 
-class SoloDocCoordinatorStore = _SoloDocCoordinatorStoreBase
-    with _$SoloDocCoordinatorStore;
+class SoloDocCoordinator = _SoloDocCoordinatorBase with _$SoloDocCoordinator;
 
-abstract class _SoloDocCoordinatorStoreBase extends Equatable with Store {
+abstract class _SoloDocCoordinatorBase extends Equatable with Store {
   final CreateSoloDoc createSoloDoc;
   final GetSoloDocStore getSoloDoc;
   final SealSoloDoc sealSoloDoc;
   final ShareSoloDoc shareSoloDoc;
   final SubmitSoloDoc submitSoloDoc;
 
-  _SoloDocCoordinatorStoreBase({
+  _SoloDocCoordinatorBase({
     required this.createSoloDoc,
     required this.getSoloDoc,
     required this.sealSoloDoc,
