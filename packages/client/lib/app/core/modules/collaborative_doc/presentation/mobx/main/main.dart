cd61f9d9d50790c9486a04f81b0ed75dd1ecede5
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-27 06:28:24 -0600

    purge the every main store except stream based one (get_doc_content)

--- packages/client/lib/app/core/modules/collaborative_doc/presentation/mobx/main/main.dart
+++
@@ -1,7 +0,0 @@
-export './get_collaborative_doc_content_store.dart';
-export './get_collaborator_doc_info_store.dart';
-export './create_collaborative_doc_store.dart';
-export './update_collaborative_doc_store.dart';
-export './update_user_delta_store.dart';
-export './update_user_presence_store.dart';
-export 'update_commit_desire_status_store.dart';
