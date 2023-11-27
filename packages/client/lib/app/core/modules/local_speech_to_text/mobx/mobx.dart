0febee738b6cabad389794987badf560304331e2
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-27 06:53:15 -0600

    rm getter exports & refactor coordinator export

--- packages/client/lib/app/core/modules/local_speech_to_text/mobx/mobx.dart
+++ packages/client/lib/app/core/modules/local_speech_to_text/mobx/mobx.dart
@@ -1,3 +1,2 @@
-export './getters/getters.dart';
 export './main/main.dart';
-export './coordinators/local_speech_to_text_coordinator_store.dart';
+export 'coordinators/local_speech_to_text_coordinator.dart';
