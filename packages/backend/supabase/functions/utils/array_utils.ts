3e5dd35c2d00e9fbe750154af39ea444a5296fe7
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-28 14:15:03 -0600

    add isEmptyOrNull function to edge functions

--- packages/backend/supabase/functions/utils/array_utils.ts
+++ packages/backend/supabase/functions/utils/array_utils.ts
@@ -1,4 +1,8 @@
 // deno-lint-ignore-file no-explicit-any
 export function isNotEmptyOrNull(arr: Array<any> | null): boolean {
-    return arr === null ? false : arr.length !== 0;
+  return arr === null ? false : arr.length !== 0;
+}
+
+export function isEmptyOrNull(array: Array<any> | null): boolean {
+  return array === null || array.length === 0;
 }
