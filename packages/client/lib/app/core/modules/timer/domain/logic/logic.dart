52a957f65a7ae0e688f9917571f4663452ea0d02
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-28 11:35:28 -0600

    timer module barrel updates

--- packages/client/lib/app/core/modules/timer/domain/logic/logic.dart
+++ packages/client/lib/app/core/modules/timer/domain/logic/logic.dart
@@ -2,6 +2,6 @@ export 'create_timer.dart';
 export 'delete_the_timer.dart';
 export 'get_timer_information_stream.dart';
 export 'logic.dart';
-export 'markdown_the_timer.dart';
+export 'mark_timer_as_complete.dart';
 export 'update_presence.dart';
 export 'update_timer_running_status.dart';
