9dfc435175a35089656249ae94e5ba1e620b2b42
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-28 11:35:53 -0600

    rm timer markdown entity, model & logic

--- packages/client/lib/app/core/modules/timer/domain/entities/timer_markdown_status_entity.dart
+++
@@ -1,6 +0,0 @@
-import 'package:nokhte/app/core/entities/default_db_status_entity.dart';
-
-class TimerMarkdownStatusEntity extends DefaultDBStatusEntity {
-  const TimerMarkdownStatusEntity({required bool isMarkedDown})
-      : super(isSent: isMarkedDown);
-}
