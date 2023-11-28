f40926a83af5355f57a82e4ed581d59c45e027c1
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-28 15:28:52 -0600

    change default values to true for presence

---
+++ packages/backend/supabase/migrations/20231128211138_timer_information_additions.sql
@@ -0,0 +1,3 @@
+alter table "public"."timer_information" alter column "collaborator_one_is_online" set default true;
+
+alter table "public"."timer_information" alter column "collaborator_two_is_online" set default true;
