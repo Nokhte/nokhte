02618487842a5fce836dcd954a1fcdf0007c2142
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-29 08:29:05 -0600

    rm type checking block, too ceremonial

--- packages/client/test/app/core/widgets/beach_waves/stack/presentation/movie_stores/on_shore_movie_store_test.dart
+++ packages/client/test/app/core/widgets/beach_waves/stack/presentation/movie_stores/on_shore_movie_store_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nokhte/app/core/types/seconds.dart';
-import 'package:nokhte/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/on_shore/on_shore_movie_store.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
 
 void main() {
@@ -8,18 +8,6 @@ void main() {
 
   setUp(() {
     testStore = OnShoreMovieStore();
-  });
-
-  group("type checking", () {
-    test("movie is a MovieTween", () {
-      expect(testStore.movie, isA<MovieTween>());
-    });
-    test("control is a Control", () {
-      expect(testStore.control, isA<Control>());
-    });
-    test("currentWaterYPosition is a double", () {
-      expect(testStore.currentWaterYPosition, isA<double>());
-    });
   });
 
   group("initial values", () {
