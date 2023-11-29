996aeefa82885fe56783f44ca064d08d874eaddd
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-29 10:23:46 -0600

    add  new smart beach waves widget

---
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/widgets/new_smart_beach_waves.dart
@@ -0,0 +1,73 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:nokhte/app/core/widgets/beach_widgets/shared/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
+import 'canvas/beach_waves_painter.dart';
+
+class NewSmartBeachWaves extends StatelessWidget {
+  final NewBeachWavesStore store;
+  const NewSmartBeachWaves({
+    super.key,
+    required this.store,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(builder: (context) {
+      return Opacity(
+        opacity: store.currentStore.showWidget ? 1 : 0,
+        child: CustomAnimationBuilder(
+          tween: store.currentMovie,
+          duration: store.currentMovie.duration,
+          control: store.currentControl,
+          onStarted: () => store.currentStore.startAndResetStopWatch(),
+          builder: (context, value, child) {
+            final currentAnimationValues =
+                GetCurrentWaterAnimation.values(value);
+            if (store.movieMode == BeachWaveMovieModes.oceanDiveSetup) {
+              store.setMovieMode(BeachWaveMovieModes.oceanDive);
+              store.currentStore.initMovie(currentAnimationValues.first);
+            } else if (store.movieMode ==
+                BeachWaveMovieModes.backToTheDepthsSetup) {
+              //   store.teeUpBackToTheDepthsValues(colorGradientsList: [
+              //     currentAnimationValues[1],
+              //     currentAnimationValues[2],
+              //     currentAnimationValues[3],
+              //     currentAnimationValues[4],
+              //     currentAnimationValues[5],
+              //     currentAnimationValues[6],
+              //     currentAnimationValues[7],
+              //     currentAnimationValues[8],
+              //   ]);
+            }
+            return CustomPaint(
+              painter: BeachWavesPainter(
+                waterValue: currentAnimationValues.first,
+                firstGradientColor: currentAnimationValues[1],
+                secondGradientColor: currentAnimationValues[2],
+                thirdGradientColor: currentAnimationValues[3],
+                fourthGradientColor: currentAnimationValues[4],
+                fifthGradientColor: currentAnimationValues[5],
+                sixthGradientColor: currentAnimationValues[6],
+                seventhGradientColor: currentAnimationValues[7],
+                eighthGradientColor: currentAnimationValues[8],
+                firstGradientStop: currentAnimationValues[9],
+                secondGradientStop: currentAnimationValues[10],
+                thirdGradientStop: currentAnimationValues[11],
+                fourthGradientStop: currentAnimationValues[12],
+                fifthGradientStop: currentAnimationValues[13],
+                sixthGradientStop: currentAnimationValues[14],
+                seventhGradientStop: currentAnimationValues[15],
+                eighthGradientStop: currentAnimationValues[16],
+              ),
+              size: MediaQuery.of(context).size,
+            );
+          },
+        ),
+      );
+    });
+  }
+}
