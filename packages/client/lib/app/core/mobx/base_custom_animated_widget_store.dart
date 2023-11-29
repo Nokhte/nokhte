a9d2c8d9bbc2ea74674af815a716d4bad4fc4ec1
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-29 10:24:42 -0600

    rm commented code

--- packages/client/lib/app/core/mobx/base_custom_animated_widget_store.dart
+++ packages/client/lib/app/core/mobx/base_custom_animated_widget_store.dart
@@ -1,47 +1,3 @@
-// // ignore_for_file: must_be_immutable, library_private_types_in_public_api
-// import 'package:mobx/mobx.dart';
-// // * Equatable Import
-// import 'package:equatable/equatable.dart';
-// import 'package:nokhte/app/core/types/types.dart';
-// import 'package:simple_animations/simple_animations.dart';
-// // * Mobx Codegen Inclusion
-// part 'base_custom_animated_widget_store.g.dart';
-
-// class BaseCustomAnimatedWidgetStore = _BaseCustomAnimatedWidgetStoreBase
-//     with _$BaseCustomAnimatedWidgetStore;
-
-// abstract class _BaseCustomAnimatedWidgetStoreBase extends Equatable with Store {
-//   @observable
-//   MovieTween movie = MovieTween();
-
-//   @observable
-//   Control control = Control.stop;
-
-//   @observable
-//   bool showWidget = true;
-
-//   @observable
-//   MovieStatus movieStatus = MovieStatus.idle;
-
-//   @observable
-//   String stopwatchMillseconds = "";
-
-//   @action
-//   toggleWidgetVisibility() => showWidget = !showWidget;
-
-//   @action
-//   void setMovie(MovieTween newMovie) => movie = newMovie;
-
-//   @action
-//   void setControl(Control newControl) => control = newControl;
-
-//   @action
-//   setMovieStatus(MovieStatus newMovieStatus) => movieStatus = newMovieStatus;
-
-//   @override
-//   List<Object> get props => [];
-// }
-
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 // * Mobx Import
 import 'dart:async';
@@ -54,10 +10,11 @@ import 'package:simple_animations/simple_animations.dart';
 // * Mobx Codegen Inclusion
 part 'base_custom_animated_widget_store.g.dart';
 
-class BaseCustomAnimatedWidgetStore = _BaseCustomAnimatedWidgetStoreBase
-    with _$BaseCustomAnimatedWidgetStore;
+class BaseCustomAnimatedWidgetStore<T> = _BaseCustomAnimatedWidgetStoreBase<T>
+    with _$BaseCustomAnimatedWidgetStore<T>;
 
-abstract class _BaseCustomAnimatedWidgetStoreBase extends Equatable with Store {
+abstract class _BaseCustomAnimatedWidgetStoreBase<T> extends Equatable
+    with Store {
   _BaseCustomAnimatedWidgetStoreBase() {
     controlListener();
     stopwatchListener();
@@ -114,10 +71,14 @@ abstract class _BaseCustomAnimatedWidgetStoreBase extends Equatable with Store {
   @action
   setMovieStatus(MovieStatus newMovieStatus) => movieStatus = newMovieStatus;
 
+  // @action
+  // onCompleted() {
+  //   print("did on completed run???");
+  //   setMovieStatus(MovieStatus.finished);
+  // }
+
   @action
-  onCompleted() {
-    setMovieStatus(MovieStatus.finished);
-  }
+  initMovie(T param) {}
 
   stopwatchListener() => reaction((p0) => stopwatchMillseconds, (p0) {
         if ((movie.duration.inMilliseconds - int.parse(p0)) < 1000) {
--- packages/client/lib/app/core/mobx/base_custom_animated_widget_store.g.dart
+++ packages/client/lib/app/core/mobx/base_custom_animated_widget_store.g.dart
@@ -8,8 +8,8 @@ part of 'base_custom_animated_widget_store.dart';
 
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
-mixin _$BaseCustomAnimatedWidgetStore
-    on _BaseCustomAnimatedWidgetStoreBase, Store {
+mixin _$BaseCustomAnimatedWidgetStore<T>
+    on _BaseCustomAnimatedWidgetStoreBase<T>, Store {
   late final _$localStopwatchAtom = Atom(
       name: '_BaseCustomAnimatedWidgetStoreBase.localStopwatch',
       context: context);
@@ -176,11 +176,11 @@ mixin _$BaseCustomAnimatedWidgetStore
   }
 
   @override
-  dynamic onCompleted() {
+  dynamic initMovie(T param) {
     final _$actionInfo = _$_BaseCustomAnimatedWidgetStoreBaseActionController
-        .startAction(name: '_BaseCustomAnimatedWidgetStoreBase.onCompleted');
+        .startAction(name: '_BaseCustomAnimatedWidgetStoreBase.initMovie');
     try {
-      return super.onCompleted();
+      return super.initMovie(param);
     } finally {
       _$_BaseCustomAnimatedWidgetStoreBaseActionController
           .endAction(_$actionInfo);
