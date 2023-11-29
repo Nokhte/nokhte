b3d7e7656095a8543c281a7706e606405e650282
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-29 15:38:39 -0600

    add times up end to ocean dive store + movie

---
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/times_up_end_to_ocean_dive/times_up_end_to_ocean_dive.dart
@@ -0,0 +1,2 @@
+export 'times_up_end_to_ocean_dive_movie.dart';
+export 'times_up_end_to_ocean_dive_movie_store.dart';
---
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/times_up_end_to_ocean_dive/times_up_end_to_ocean_dive_movie_store.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+// * Mobx Import
+import 'package:mobx/mobx.dart';
+// * Equatable Import
+import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+import 'times_up_end_to_ocean_dive_movie.dart';
+// * Mobx Codegen Inclusion
+part 'times_up_end_to_ocean_dive_movie_store.g.dart';
+
+class TimesUpEndToOceanDiveMovieStore = _TimesUpEndToOceanDiveMovieStoreBase
+    with _$TimesUpEndToOceanDiveMovieStore;
+
+abstract class _TimesUpEndToOceanDiveMovieStoreBase
+    extends BaseCustomAnimatedWidgetStore<Duration> with Store {
+  _TimesUpEndToOceanDiveMovieStoreBase() {
+    movie = TimesUpEndToOceanDiveMovie.movie;
+  }
+
+  @override
+  initMovie(Duration param) {
+    control = Control.playFromStart;
+  }
+}
