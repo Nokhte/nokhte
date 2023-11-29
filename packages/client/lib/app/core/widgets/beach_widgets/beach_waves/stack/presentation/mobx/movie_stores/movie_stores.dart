98bcf00f6aa9bf6adfbce20717b2962dfe2f3a8a
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-29 13:56:53 -0600

    update barrel

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/movie_stores.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/movie_stores.dart
@@ -1,3 +1,4 @@
 export 'ocean_dive_to_on_shore/ocean_dive_to_on_shore.dart';
 export 'on_shore/on_shore.dart';
 export 'on_shore_to_ocean_dive/on_shore_to_ocean_dive.dart';
+export './suspended_at_ocean_dive/suspended_at_ocean_dive.dart';
