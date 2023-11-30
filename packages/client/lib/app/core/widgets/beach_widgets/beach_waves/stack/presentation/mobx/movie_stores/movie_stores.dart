4cad82be23502415fd35c3ae56ff3b8eb0bca713
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-30 07:30:43 -0600

    add export

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/movie_stores.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/movie_stores.dart
@@ -5,3 +5,4 @@ export 'on_shore_to_ocean_dive/on_shore_to_ocean_dive.dart';
 export './times_up/times_up.dart';
 export './times_up_dynamic_point_to_the_depths/times_up_dynamic_point_to_the_depths.dart';
 export './suspended_at_ocean_dive/suspended_at_ocean_dive.dart';
+export './suspended_at_the_depths/suspended_at_the_depths.dart';
