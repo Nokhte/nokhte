9926503df612fa5e4440508951473c6c4e5bc9c1
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-30 08:22:59 -0600

    export new movie from movie_stores barrel

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/movie_stores.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/movie_stores.dart
@@ -6,3 +6,4 @@ export './times_up/times_up.dart';
 export './times_up_dynamic_point_to_the_depths/times_up_dynamic_point_to_the_depths.dart';
 export './suspended_at_ocean_dive/suspended_at_ocean_dive.dart';
 export './suspended_at_the_depths/suspended_at_the_depths.dart';
+export './times_up_end_to_the_depths/times_up_end_to_the_depths.dart';
