f2cd802653fd2b4d53c9184aa5431df816b75671
Sonny Vesali <sonny.vesali@protonmail.com>
2023-11-30 17:39:01 -0600

    rm un-needed import

--- packages/client/lib/app/modules/p2p_purpose_session/presentation/mobx/coordinators/p2p_purpose_phase1_coordinator.dart
+++ packages/client/lib/app/modules/p2p_purpose_session/presentation/mobx/coordinators/p2p_purpose_phase1_coordinator.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
