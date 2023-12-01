9790aebe9c68fef03bb7e8c392ac1343dc0eea62
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-01 07:15:41 -0600

    logic barrel update

--- packages/client/lib/app/core/modules/existing_collaborations/domain/logic/logic.dart
+++ packages/client/lib/app/core/modules/existing_collaborations/domain/logic/logic.dart
@@ -1,3 +1,4 @@
 export 'consecrate_the_collaboration.dart';
 export 'update_collaboration_activation_status.dart';
 export 'update_individual_collaborator_entry_status.dart';
+export './check_if_user_has_the_question.dart';
