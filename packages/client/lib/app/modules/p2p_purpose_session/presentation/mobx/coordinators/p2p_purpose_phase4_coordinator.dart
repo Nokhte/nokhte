ce9f2f47ae6e7e088cb1064aff179b4effc17326
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-01 13:18:17 -0600

    clean up comments  & inject widgets & rm ui code

--- packages/client/lib/app/modules/p2p_purpose_session/presentation/mobx/coordinators/p2p_purpose_phase4_coordinator.dart
+++ packages/client/lib/app/modules/p2p_purpose_session/presentation/mobx/coordinators/p2p_purpose_phase4_coordinator.dart
@@ -1,6 +1,4 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
-import 'package:flutter/foundation.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
@@ -11,73 +9,38 @@ import 'package:nokhte/app/core/modules/solo_doc/mobx/mobx.dart';
 import 'package:nokhte/app/core/modules/timer/domain/logic/logic.dart';
 import 'package:nokhte/app/core/modules/abort_purpose_session_artifacts/mobx/mobx.dart';
 import 'package:nokhte/app/core/modules/timer/mobx/mobx.dart';
-import 'package:nokhte/app/core/types/types.dart';
-// import 'package:nokhte/app/core/widgets/beach_widgets/shared/types/types.dart';
-import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:nokhte/app/modules/p2p_purpose_session/presentation/mobx/widget_coordinators/p2p_purpose_phase4_widgets_coordinator.dart';
 part 'p2p_purpose_phase4_coordinator.g.dart';
 
 class P2PPurposePhase4Coordinator = _P2PPurposePhase4CoordinatorBase
     with _$P2PPurposePhase4Coordinator;
 
-abstract class _P2PPurposePhase4CoordinatorBase extends BaseTimesUpStore
+abstract class _P2PPurposePhase4CoordinatorBase extends BaseCoordinator
     with Store {
-  final SmartFadingAnimatedTextTrackerStore fadingText;
   final AbortPurposeSessionArtifactsStore abortPurposeSessionArtifactsStore;
   final SoloDocCoordinator soloDoc;
   final TimerCoordinator timer;
-  final BeachWavesTrackerStore beachWaves;
+  final P2PPurposePhase4WidgetsCoordinator widgets;
 
   _P2PPurposePhase4CoordinatorBase({
     required this.timer,
-    required this.beachWaves,
-    required super.newBeachWaves,
+    required this.widgets,
     required this.abortPurposeSessionArtifactsStore,
-    required this.fadingText,
     required this.soloDoc,
-  }) : super(productionTimerLength: const Duration(minutes: 5)) {
-    // beachWavesMovieStatusListener();
-  }
-
-  final duration = kDebugMode ? Seconds.get(20) : const Duration(minutes: 5);
-
-  // @action
-  // beachWavesMovieStatusListener() =>
-  //     reaction((p0) => beachWaves.movieStatus, (p0) {
-  //       print("phase 4 ${beachWaves.movieStatus} ${beachWaves.movieMode}");
-  //       if (beachWaves.movieMode == BeachWaveMovieModes.timesUp) {
-  //         if (beachWaves.movieStatus == MovieStatus.finished) {
-  //           beachWaves.teeUpBackToTheDepths();
-  //           Future.delayed(Seconds.get(3), () async {
-  //             await timer.markTimerAsComplete(NoParams());
-  //           });
-  //         } else if (beachWaves.movieStatus == MovieStatus.inProgress) {
-  //           delayedNavigation(() {
-  //             beachWaves.teeUpBackToTheDepths();
-  //             Future.delayed(Seconds.get(3), () async {
-  //               await timer.markTimerAsComplete(NoParams());
-  //             });
-  //           });
-  //         }
-  //       }
-  //     });
+  });
 
   @action
-  cleanUpAndTransition() async {
-    timer.deleteTheTimer(NoParams());
-    fadingText.fadeTheTextOut();
-    Modular.to.navigate('/p2p_purpose_session/phase-5/');
-  }
+  cleanUpAndTransition() async => await timer.deleteTheTimer(NoParams());
 
   @action
   screenConstructor() async {
-    fadingText.startRotatingText(Seconds.get(0));
-    beachWaves.initiateSuspendedAtTheDepths();
-    beachWaves.initiateTimesUp(timerLength: duration);
-
-    fadingText.moveToNextMessage();
+    await soloDoc.getSoloDoc(const GetSoloDocParams(getCollaboratorsDoc: true));
+    widgets.beachWavesMovieStatusWatcher(
+        onTimesUpComplete: cleanUpAndTransition);
+    widgets.constructor(collaboratorsDocContent: soloDoc.getSoloDoc.docContent);
     await timer.setupAndStreamListenerActivation(
       const CreateTimerParams(timerLengthInMinutes: 5),
-      timerUICallback: initOrPauseTimesUp,
+      timerUICallback: widgets.initOrPauseTimesUp,
       onBothCollaboratorTimersCompleted: cleanUpAndTransition,
     );
     await timer.updateTimerRunningStatus(true);
@@ -90,30 +53,5 @@ abstract class _P2PPurposePhase4CoordinatorBase extends BaseTimesUpStore
         ),
       ),
     );
-    fadingText.fadeTheTextIn();
-    await soloDoc
-        .getSoloDoc(
-          const GetSoloDocParams(
-            getCollaboratorsDoc: true,
-          ),
-        )
-        .then((value) => Future.delayed(Seconds.get(2), () {
-              fadingText.togglePause();
-            }));
-    fadingText.setMainMessage(
-      index: 4,
-      thePhrase: soloDoc.getSoloDoc.docContent,
-    );
-
-    // delayedNavigation(() {
-    beachWaves.teeUpBackToTheDepths();
-    Future.delayed(Seconds.get(3), () async {
-      cleanUpAndTransition();
-      // await timer.markTimerAsComplete(NoParams());
-    });
-    // });
   }
-
-  @override
-  List<Object> get props => [];
 }
