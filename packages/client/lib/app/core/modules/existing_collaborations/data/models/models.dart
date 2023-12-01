9e83a8e748dfda2d8d781e13bbd5b6b350671787
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-01 07:15:50 -0600

    add who_has_the_question_model

--- packages/client/lib/app/core/modules/existing_collaborations/data/models/models.dart
+++ packages/client/lib/app/core/modules/existing_collaborations/data/models/models.dart
@@ -1,3 +1,4 @@
 export 'collaboration_activation_status_model.dart';
 export 'collaboration_consecration_status_model.dart';
 export 'individual_collaborator_entry_status_model.dart';
+export 'who_has_the_question_model.dart';
