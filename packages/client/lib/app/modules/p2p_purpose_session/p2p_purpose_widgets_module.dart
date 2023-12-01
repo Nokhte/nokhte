9056934efdf695378badc2d92b010cd94bb6be65
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-01 05:47:36 -0600

    added phase 3 to widget binds

--- packages/client/lib/app/modules/p2p_purpose_session/p2p_purpose_widgets_module.dart
+++ packages/client/lib/app/modules/p2p_purpose_session/p2p_purpose_widgets_module.dart
@@ -93,6 +93,14 @@ class P2PPurposeWidgetsModule extends Module {
           ),
           export: true,
         ),
+        Bind.singleton<P2PPurposePhase3WidgetsCoordinator>(
+          (i) => P2PPurposePhase3WidgetsCoordinator(
+            newBeachWaves: Modular.get<NewBeachWavesStore>(),
+            textEditor: Modular.get<SoloTextEditorTrackerStore>(),
+            fadingText: i<SmartFadingAnimatedTextTrackerStore>(),
+          ),
+          export: true,
+        ),
         Bind.singleton<P2PPurposePhase6WidgetsCoordinator>(
           (i) => P2PPurposePhase6WidgetsCoordinator(
             schedulingDelta: i<SchedulingDeltaStore>(),
