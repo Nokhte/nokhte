cfe217258fca8dcaa4fe7f7aa0f651dd7ad2a68c
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-01 07:20:45 -0600

    rm duplicate logic + barrel mention

--- packages/client/lib/app/core/modules/voice_call/domain/logic/logic.dart
+++ packages/client/lib/app/core/modules/voice_call/domain/logic/logic.dart
@@ -4,5 +4,4 @@ export 'join_call.dart';
 export 'leave_call.dart';
 export 'mute_local_audio.dart';
 export 'unmute_local_audio.dart';
-export 'check_if_user_has_the_question.dart';
 export 'get_channel_id.dart';
