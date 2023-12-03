8a53962a8fbbebbd62b685b36ec288a040780724
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-03 16:29:45 -0600

    💥 add individual content & consolidate last edit 1. instead of content it's now:     - collaborator_one_content     - collaborator_two_content 2. and instead of having     - String currentUserUID     - String lastEditedBy    it is now consolidated into     - lastEditWasTheUser

--- packages/backend/dart/lib/tables/_real_time_enabled/working_collaborative_documents/types/doc_info_content.dart
+++ packages/backend/dart/lib/tables/_real_time_enabled/working_collaborative_documents/types/doc_info_content.dart
@@ -1,22 +1,22 @@
 import 'package:equatable/equatable.dart';
 
 class DocInfoContent extends Equatable {
-  final String content;
-  final String currentUserUID;
-  final String lastEditedBy;
+  final bool lastEditWasTheUser;
+  final String usersContent;
+  final String collaboratorsContent;
   final bool collaboratorsCommitDesireStatus;
   final bool userCommitDesireStatus;
   final bool documentCommitStatus;
 
   DocInfoContent({
-    required this.content,
-    required this.lastEditedBy,
-    required this.currentUserUID,
+    required this.usersContent,
+    required this.collaboratorsContent,
+    required this.lastEditWasTheUser,
     required this.collaboratorsCommitDesireStatus,
     required this.documentCommitStatus,
     required this.userCommitDesireStatus,
   });
 
   @override
-  List<Object> get props => [content, lastEditedBy];
+  List<Object> get props => [];
 }
