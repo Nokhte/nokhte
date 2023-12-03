7befe32819986d3361879f4fcbad1230ee064179
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-03 16:51:18 -0600

    ✅ working_collaborative_document test suite

--- packages/backend/dart/test/08_working_collaborative_documents_test.dart
+++ packages/backend/dart/test/08_working_collaborative_documents_test.dart
@@ -9,9 +9,7 @@ import 'shared/shared.dart';
 void main() {
   final tSetup = CommonCollaborativeTestFunctions();
   late WorkingCollaborativeDocumentsQueries user1WorkingQueries;
-  late WorkingCollaborativeDocumentsQueries user2WorkingQueries;
   late WorkingCollaborativeDocumentsQueries adminWorkingQueries;
-  late FinishedCollaborativeP2PPurposeDocumentsQueries user1FinishedQueries;
   late FinishedCollaborativeP2PPurposeDocumentsQueries adminFinishedQueries;
   late String usersCollaboratorNumber;
   late String collaboratorsNumber;
@@ -28,8 +26,6 @@ void main() {
     await tSetup.setUp();
     user1WorkingQueries =
         WorkingCollaborativeDocumentsQueries(supabase: tSetup.user1Supabase);
-    user2WorkingQueries =
-        WorkingCollaborativeDocumentsQueries(supabase: tSetup.user2Supabase);
 
     await user1WorkingQueries.figureOutActiveCollaboratorInfoIfNotDoneAlready();
     usersCollaboratorNumber =
@@ -38,10 +34,6 @@ void main() {
     collaboratorsNumber =
         user1WorkingQueries.collaboratorInfo.theCollaboratorsNumber;
     collaboratorsUID = user1WorkingQueries.collaboratorInfo.theCollaboratorsUID;
-
-    user1FinishedQueries = FinishedCollaborativeP2PPurposeDocumentsQueries(
-      supabase: tSetup.user1Supabase,
-    );
     adminFinishedQueries = FinishedCollaborativeP2PPurposeDocumentsQueries(
       supabase: tSetup.supabaseAdmin,
     );
@@ -153,41 +145,4 @@ void main() {
       expect(usersDocContent, tContent);
     });
   });
-
-  group("streams", () {
-    setUp(
-      () async => await user1WorkingQueries.createCollaborativeDocument(
-          docType: 'purpose'),
-    );
-  });
-
-  // test("User Should be able to create, edit & commit a document", () async {
-  //   final res = await user1WorkingQueries.createCollaborativeDocument(
-  //       docType: 'purpose');
-  //   expect(res.first["collaborator_one_uid"], tSetup.firstUserUID);
-  //   expect(res.first["doc_type"], "purpose");
-  //   expect(res.first["collaborator_two_uid"], tSetup.secondUserUID);
-  //   expect(res.first["content"], isEmpty);
-  //   expect(res.first["collaborator_one_delta"], -1);
-  //   expect(res.first["collaborator_two_delta"], -1);
-  //   expect(res.first["collaborator_one_is_active"], false);
-  //   expect(res.first["collaborator_two_is_active"], false);
-  //   final stream =
-  //       WorkingCollaborativeDocumentsStreams(supabase: tSetup.user1Supabase);
-  //   stream.docContentStream().listen((value) {
-  //     expect(value.content, "newContent");
-  //     expect(value.lastEditedBy, tSetup.firstUserUID);
-  //     expect(value.currentUserUID, tSetup.firstUserUID);
-  //   });
-  //   await user1WorkingQueries.updateUsersDocContent(
-  //     newContent: "newContent",
-  //   );
-  //   await user1WorkingQueries.updateCommitDesireStatus(wantsToCommit: true);
-  //   await user2WorkingQueries.updateCommitDesireStatus(wantsToCommit: true);
-  //   final res2 = await user1FinishedQueries.getDocInfo(docType: 'purpose');
-  //   expect(res2.first["collaborator_one_uid"], tSetup.firstUserUID);
-  //   expect(res2.first["collaborator_two_uid"], tSetup.secondUserUID);
-  //   expect(res2.first["content"], "newContent");
-  //   expect(res2.first["doc_type"], "purpose");
-  // });
 }
