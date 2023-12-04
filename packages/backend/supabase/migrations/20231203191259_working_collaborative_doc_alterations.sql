46f5c17467eccc426f25a3efdc6facce5d4cca8e
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-04 13:41:47 -0600

    ⏪ working_collaborative_documents back to monolith

--- packages/backend/supabase/migrations/20231203191259_working_collaborative_doc_alterations.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."working_collaborative_documents" drop column "content";
-
-alter table "public"."working_collaborative_documents" add column "collaborator_one_content" text not null default ''::text;
-
-alter table "public"."working_collaborative_documents" add column "collaborator_two_content" text not null default ''::text;
