9546615975f8b31f2b003b774579898538126137
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-04 14:01:55 -0600

    🚨 un-used import + function body

--- packages/client/lib/app/modules/p2p_purpose_session/presentation/views/p2p_purpose6_schedule_next_meeting.dart
+++ packages/client/lib/app/modules/p2p_purpose_session/presentation/views/p2p_purpose6_schedule_next_meeting.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:nokhte/app/core/widgets/scheduling_delta/stack/stack.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/p2p_purpose_session/presentation/mobx/mobx.dart';
 
 class P2PPupose6ScheduleNextMeeting extends StatefulWidget {
   final P2PPurposePhase6CoordinatorStore coordinator;
-  P2PPupose6ScheduleNextMeeting({
+  const P2PPupose6ScheduleNextMeeting({
     super.key,
     required this.coordinator,
-  }) {}
+  });
 
   @override
   State<P2PPupose6ScheduleNextMeeting> createState() =>
