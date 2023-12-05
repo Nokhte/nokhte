76c000a7910367318b797755e00dc6e791a8eab6
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-05 11:35:08 -0600

    ✨ base_collaborative_doc_db_store added

---
+++ packages/client/lib/app/core/mobx/base_collaborative_doc_db_store.dart
@@ -0,0 +1,85 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+// * Mobx Import
+import 'package:mobx/mobx.dart';
+// * Equatable Import
+import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/modules/collaborative_doc/domain/domain.dart';
+import 'package:nokhte/app/core/modules/collaborative_doc/mobx/mobx.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/mobx.dart';
+// * Mobx Codegen Inclusion
+part 'base_collaborative_doc_db_store.g.dart';
+
+class BaseCollaborativeDocDBStore = _BaseCollaborativeDocDBStoreBase
+    with _$BaseCollaborativeDocDBStore;
+
+abstract class _BaseCollaborativeDocDBStoreBase extends BaseCoordinator
+    with Store {
+  final CollaborativeDocCoordinator collaborativeDocDB;
+  final BaseCollaborativeTextEditorStore widgets;
+  final String docType;
+  final SwipeDetector swipe;
+
+  _BaseCollaborativeDocDBStoreBase({
+    required this.swipe,
+    required this.collaborativeDocDB,
+    required this.docType,
+    required this.widgets,
+  });
+
+  initCollaborativeDocListeners(Function(String) onCommitted) {
+    widgets.collaborativeDocListener(
+      collaborativeDocDB.getContent.docContent,
+      updateTheDoc: updateTheDoc,
+      onCommitted: onCommitted,
+      revertAffirmativeCommitDesire: revertAffirmativeCommitDesire,
+      updateCommitStatusToAffirmative: updateCommitStatusToAffirmative,
+    );
+    gestureListener();
+    widgets.userTextControllerListener(
+      collaborativeDocDB: collaborativeDocDB,
+    );
+  }
+
+  updateTheDoc(String newContent) async {
+    await collaborativeDocDB.updateDoc(
+      UpdateCollaborativeDocParams(
+        newContent: newContent,
+      ),
+    );
+  }
+
+  @action
+  moveToFinishedDocs(String docContent) async {
+    await collaborativeDocDB.moveToFinishedDocs(
+      MoveToFinishedDocsParams(
+        docContent: docContent,
+        docType: docType,
+      ),
+    );
+  }
+
+  revertAffirmativeCommitDesire() async =>
+      await collaborativeDocDB.updateCommitDesire(
+          const UpdateCommitDesireStatusParams(wantsToCommit: false));
+
+  @action
+  updateCommitStatusToAffirmative() async {
+    await collaborativeDocDB.updateCommitDesire(
+      const UpdateCommitDesireStatusParams(wantsToCommit: true),
+    );
+    widgets.updateCommitStatusToAffirmative();
+  }
+
+  gestureListener() => reaction((p0) => swipe.directionsType, (p0) async {
+        switch (p0) {
+          case GestureDirections.up:
+            await updateCommitStatusToAffirmative();
+          default:
+            break;
+        }
+      });
+
+  @override
+  List<Object> get props => [];
+}
---
+++ packages/client/lib/app/core/mobx/base_collaborative_doc_db_store.g.dart
@@ -0,0 +1,38 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'base_collaborative_doc_db_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$BaseCollaborativeDocDBStore on _BaseCollaborativeDocDBStoreBase, Store {
+  late final _$moveToFinishedDocsAsyncAction = AsyncAction(
+      '_BaseCollaborativeDocDBStoreBase.moveToFinishedDocs',
+      context: context);
+
+  @override
+  Future moveToFinishedDocs(String docContent) {
+    return _$moveToFinishedDocsAsyncAction
+        .run(() => super.moveToFinishedDocs(docContent));
+  }
+
+  late final _$updateCommitStatusToAffirmativeAsyncAction = AsyncAction(
+      '_BaseCollaborativeDocDBStoreBase.updateCommitStatusToAffirmative',
+      context: context);
+
+  @override
+  Future updateCommitStatusToAffirmative() {
+    return _$updateCommitStatusToAffirmativeAsyncAction
+        .run(() => super.updateCommitStatusToAffirmative());
+  }
+
+  @override
+  String toString() {
+    return '''
+
+    ''';
+  }
+}
