cb800aaf99f24212083528ba7d553e4bf33f336d
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-05 09:47:04 -0600

    ✨ move_to_finished_docs added to logic barrel

--- packages/client/lib/app/core/modules/collaborative_doc/domain/logic/logic.dart
+++ packages/client/lib/app/core/modules/collaborative_doc/domain/logic/logic.dart
@@ -1,4 +1,5 @@
 export 'create_collaborative_doc.dart';
+export 'move_to_finished_docs.dart';
 export 'get_collaborative_doc_content.dart';
 export 'get_collaborator_doc_info.dart';
 export 'update_collaborative_doc.dart';
