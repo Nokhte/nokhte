f69c6e09380ed826fda4d63f90552b0993c9308f
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 08:12:02 -0600

    🔥 solo_doc module

--- packages/client/lib/app/core/modules/solo_doc/domain/entities/solo_doc_sharing_status_entity.dart
+++
@@ -1,6 +0,0 @@
-import 'package:nokhte/app/core/entities/default_db_status_entity.dart';
-
-class SoloDocSharingStatusEntity extends DefaultDBStatusEntity {
-  const SoloDocSharingStatusEntity({required isShared})
-      : super(isSent: isShared);
-}
