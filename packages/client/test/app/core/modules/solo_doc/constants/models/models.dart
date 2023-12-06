cfb723bacee9816a4bbc6e935db9c78139b59bb3
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 08:24:55 -0600

    🔥 solo_doc_module tests

--- packages/client/test/app/core/modules/solo_doc/constants/models/models.dart
+++
@@ -1,5 +0,0 @@
-export 'solo_doc_content_model.dart';
-export 'solo_doc_submission_status_model.dart';
-export 'solo_doc_creation_status_model.dart';
-export 'solo_doc_sealing_status_model.dart';
-export 'solo_doc_sharing_status_model.dart';
