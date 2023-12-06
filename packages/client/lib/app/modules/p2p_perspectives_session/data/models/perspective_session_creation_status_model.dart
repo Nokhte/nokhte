b7885573c9882cbca21bf5e85996bb1399794fd2
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 08:23:09 -0600

    🔥 p2p_perspectives_session module

--- packages/client/lib/app/modules/p2p_perspectives_session/data/models/perspective_session_creation_status_model.dart
+++
@@ -1,6 +0,0 @@
-import 'package:nokhte/app/modules/p2p_perspectives_session/domain/domain.dart';
-
-class PerspectiveSessionCreationStatusModel
-    extends PerspectiveSessionCreationStatusEntity {
-  const PerspectiveSessionCreationStatusModel({required super.isCreated});
-}
