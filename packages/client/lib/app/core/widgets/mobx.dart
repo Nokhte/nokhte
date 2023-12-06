8903c82428e5fc3bbb2a65c7599a514cf8f425d7
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 08:34:00 -0600

    🔥 base_perspectives_widgets_store & mentions

--- packages/client/lib/app/core/widgets/mobx.dart
+++ packages/client/lib/app/core/widgets/mobx.dart
@@ -1,4 +1,3 @@
-export 'text_editors/mobx.dart';
 export 'beach_widgets/beach_waves/stack/presentation/mobx/beach_waves_tracker_store.dart';
 export 'beach_widgets/beach_horizon_water/stack/presentation/mobx/beach_horizon_water_tracker_store.dart';
 export 'smart_fading_animated_text/stack/presentation/mobx/smart_fading_animated_text_tracker_store.dart';
