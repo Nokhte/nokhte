17317566d0cfdf7edda62b23519d58c52461767c
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 08:03:13 -0600

    🔥 audio player module

--- packages/client/lib/app/core/modules/audio_player/data/models/audio_playing_status_model.dart
+++
@@ -1,5 +0,0 @@
-import 'package:nokhte/app/core/modules/audio_player/domain/domain.dart';
-
-class AudioPlayingStatusModel extends AudioPlayingStatusEntity {
-  const AudioPlayingStatusModel({required super.isPlaying});
-}
