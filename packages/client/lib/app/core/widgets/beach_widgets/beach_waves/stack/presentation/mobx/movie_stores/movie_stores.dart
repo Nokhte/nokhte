ae30742747ed37bd136028c9f27aa69b5f5e6116
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 10:34:26 -0600

    ✨ missing movie to the export barrel

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/movie_stores.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/movie_stores.dart
@@ -7,4 +7,5 @@ export './times_up/times_up.dart';
 export './times_up_dynamic_point_to_the_depths/times_up_dynamic_point_to_the_depths.dart';
 export './suspended_at_ocean_dive/suspended_at_ocean_dive.dart';
 export './suspended_at_the_depths/suspended_at_the_depths.dart';
+export './times_up_end_to_ocean_dive/times_up_end_to_ocean_dive.dart';
 export './times_up_end_to_the_depths/times_up_end_to_the_depths.dart';
