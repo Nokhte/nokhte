452401fcba8b2cab509c30b8afcc4a88da495b8e
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 14:25:27 -0600

    🔥 old lists & ✨ new login list

--- packages/client/lib/app/core/widgets/smart_text/stack/constants/data/messages_data.dart
+++ packages/client/lib/app/core/widgets/smart_text/stack/constants/data/messages_data.dart
@@ -2,129 +2,31 @@ import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widget_constants.dart';
 
 class MessagesData {
-  static List<RotatingTextData> get homeList => [
+  static List<RotatingTextData> get loginList => [
         RotatingTextData(
-            mainMessage: "Schedule A Time With Your Collaborator",
-            subMessage: "",
-            pauseHere: false,
-            unlockGesture: Gestures.none,
-            extraDelayTime: Seconds.get(2),
-            mainMessageFontSize: 25.0,
-            subMessageFontSize: 15.0),
-        RotatingTextData(
-            mainMessage: "Double Tap",
-            subMessage: "",
-            pauseHere: true,
-            unlockGesture: Gestures.doubleTap,
-            extraDelayTime: Seconds.get(0),
-            mainMessageFontSize: 25.0,
-            subMessageFontSize: 15.0),
-        RotatingTextData(
-            mainMessage: "",
-            subMessage: "\n tap to copy",
-            pauseHere: true,
-            unlockGesture: Gestures.tap,
-            extraDelayTime: Seconds.get(0),
-            mainMessageFontSize: 25.0,
-            subMessageFontSize: 15.0),
-        RotatingTextData(
-            mainMessage: "Share Your Word With Your Collaborator",
-            subMessage: "",
-            pauseHere: false,
-            unlockGesture: Gestures.none,
-            extraDelayTime: Seconds.get(1, milli: 500),
-            mainMessageFontSize: 25.0,
-            subMessageFontSize: 15.0),
-        RotatingTextData(
-            mainMessage: "Swipe Up When You Are Both Ready",
-            subMessage: "",
-            pauseHere: false,
-            unlockGesture: Gestures.none,
-            extraDelayTime: Seconds.get(1),
-            mainMessageFontSize: 25.0,
-            subMessageFontSize: 15.0),
-      ];
-  static List<RotatingTextData> get hasACollaboratorHomeList => [
-        RotatingTextData(
-            mainMessage: "Double Tap",
-            subMessage: "",
-            pauseHere: true,
-            unlockGesture: Gestures.doubleTap,
-            extraDelayTime: Seconds.get(0),
-            mainMessageFontSize: 25.0,
-            subMessageFontSize: 15.0),
-        RotatingTextData(
-            mainMessage: "",
-            subMessage: "\n tap to copy",
-            pauseHere: true,
-            unlockGesture: Gestures.tap,
-            extraDelayTime: Seconds.get(0),
-            mainMessageFontSize: 25.0,
-            subMessageFontSize: 15.0),
-      ];
-  static List<RotatingTextData> get speakTheCollaboratorPhraseList => [
-        RotatingTextData(
-            mainMessage: "Speak Your Collaborator's Phrase",
-            subMessage: "",
-            pauseHere: true,
-            unlockGesture: Gestures.hold,
-            extraDelayTime: Seconds.get(0),
-            mainMessageFontSize: 25.0,
-            subMessageFontSize: 15.0),
-      ];
-  static List<RotatingTextData> get p2pPurposeSession => [
-        RotatingTextData(
-            mainMessage: "One of you has a question to start the conversation",
-            subMessage: "",
-            pauseHere: true,
-            unlockGesture: Gestures.none,
-            extraDelayTime: Seconds.get(0),
-            mainMessageFontSize: 25.0,
-            subMessageFontSize: 15.0),
-        RotatingTextData(
-            mainMessage: "?",
-            subMessage: "",
-            pauseHere: true,
-            unlockGesture: Gestures.none,
-            extraDelayTime: Seconds.get(0),
-            mainMessageFontSize: 25.0,
-            subMessageFontSize: 15.0),
-        RotatingTextData(
-          mainMessage:
-              "Write one path of collective creation that stuck out to you.",
-          subMessage: "",
-          pauseHere: true,
+          initialFadeInDelay: Seconds.get(1),
+          mainMessage: "Howdy",
           unlockGesture: Gestures.none,
-          extraDelayTime: Seconds.get(0),
-          mainMessageFontSize: 25.0,
-          subMessageFontSize: 15.0,
+          onScreenTime: Seconds.get(2),
         ),
         RotatingTextData(
-          mainMessage: "Look At Your Partners proposal.",
-          subMessage: "",
-          pauseHere: true,
+          initialFadeInDelay: Seconds.get(2),
+          mainMessage: "The Point Of The App Is",
           unlockGesture: Gestures.none,
-          extraDelayTime: Seconds.get(0),
-          mainMessageFontSize: 25.0,
-          subMessageFontSize: 15.0,
+          onScreenTime: Seconds.get(2),
         ),
         RotatingTextData(
-          mainMessage: ".",
-          subMessage: "",
-          pauseHere: true,
+          initialFadeInDelay: Seconds.get(2),
+          mainMessage: "To Collectively Uplift Ideas Into Reality",
           unlockGesture: Gestures.none,
-          extraDelayTime: Seconds.get(0),
-          mainMessageFontSize: 25.0,
-          subMessageFontSize: 15.0,
+          onScreenTime: Seconds.get(2),
         ),
         RotatingTextData(
-          mainMessage: "Now Make Your Collective Path Together.",
-          subMessage: "",
+          initialFadeInDelay: Seconds.get(2),
+          mainMessage: "Tap",
+          unlockGesture: Gestures.tap,
           pauseHere: true,
-          unlockGesture: Gestures.none,
-          extraDelayTime: Seconds.get(0),
-          mainMessageFontSize: 25.0,
-          subMessageFontSize: 15.0,
+          onScreenTime: Seconds.get(2),
         ),
       ];
 }
