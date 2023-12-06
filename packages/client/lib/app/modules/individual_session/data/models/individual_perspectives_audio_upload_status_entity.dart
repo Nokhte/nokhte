3d888a5565fc7e991b901f1228886c15e29f20b7
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 08:05:24 -0600

    🔥 individual_session module

--- packages/client/lib/app/modules/individual_session/data/models/individual_perspectives_audio_upload_status_entity.dart
+++
@@ -1,7 +0,0 @@
-import 'package:nokhte/app/modules/individual_session/domain/entities/individual_perspectives_audio_upload_status_entity.dart';
-
-class IndividualPerspectivesAudioUploadStatusModel
-    extends IndividualPerspectivesAudioUploadStatusEntity {
-  const IndividualPerspectivesAudioUploadStatusModel(
-      {required super.isUploaded});
-}
