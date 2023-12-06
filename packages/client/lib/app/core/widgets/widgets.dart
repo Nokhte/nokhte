0686a634c5b3919653e58ab373496d369a38abec
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 08:31:51 -0600

    🔥 mesh_circle_button widget

--- packages/client/lib/app/core/widgets/widgets.dart
+++ packages/client/lib/app/core/widgets/widgets.dart
@@ -1,7 +1,6 @@
 export 'beach_widgets/beach_widgets.dart';
 export 'smart_fading_animated_text/smart_fading_animated_text.dart';
 export 'gesture_pill/gesture_pill.dart';
-export 'mesh_circle_button/mesh_circle_button.dart';
 export 'text_editors/text_editors.dart';
 export 'perspectives_map/perspectives_map.dart';
 export 'gestures/gestures.dart';
