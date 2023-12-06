990f935329339f15ad59ce2eda2d10160df4404a
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 10:02:19 -0600

    🔥 update export to reflect beach wave store rename

--- packages/client/lib/app/core/widgets/mobx.dart
+++ packages/client/lib/app/core/widgets/mobx.dart
@@ -1,4 +1,4 @@
-export 'beach_widgets/beach_waves/stack/presentation/mobx/beach_waves_tracker_store.dart';
+export 'beach_widgets/beach_waves/stack/presentation/mobx/beach_waves_store.dart';
 export 'beach_widgets/beach_horizon_water/stack/presentation/mobx/beach_horizon_water_tracker_store.dart';
 export 'smart_fading_animated_text/stack/presentation/mobx/smart_fading_animated_text_tracker_store.dart';
 export 'gestures/stack/presentation/mobx/swipe_detector.dart';
