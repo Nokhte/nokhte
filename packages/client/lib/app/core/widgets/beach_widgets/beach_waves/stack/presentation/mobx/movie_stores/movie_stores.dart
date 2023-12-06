79de8dcc85131f7d669a5d7a78369c60d91c2960
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 10:01:27 -0600

    ✨ export black out sub store

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/movie_stores.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/movie_stores.dart
@@ -1,3 +1,4 @@
+export './black_out/black_out.dart';
 export 'ocean_dive_to_on_shore/ocean_dive_to_on_shore.dart';
 export 'ocean_dive_to_times_up_start/ocean_dive_to_times_up_start.dart';
 export 'on_shore/on_shore.dart';
