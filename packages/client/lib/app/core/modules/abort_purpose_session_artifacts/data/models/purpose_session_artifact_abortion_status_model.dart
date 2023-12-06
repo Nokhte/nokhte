842945bb74c6b8d0c5ca619e1a549b6c9da39fff
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 08:02:55 -0600

    🔥 abort_purpose_session_artifacts module

--- packages/client/lib/app/core/modules/abort_purpose_session_artifacts/data/models/purpose_session_artifact_abortion_status_model.dart
+++
@@ -1,6 +0,0 @@
-import 'package:nokhte/app/core/modules/abort_purpose_session_artifacts/domain/domain.dart';
-
-class PurposeSessionArtifactAbortionStatusModel
-    extends PurposeSessionArtifactAbortionStatusEntity {
-  const PurposeSessionArtifactAbortionStatusModel({required super.isAborted});
-}
