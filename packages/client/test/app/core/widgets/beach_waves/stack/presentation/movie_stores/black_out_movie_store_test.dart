ee67ce9ad1e20dff8a8dbf30006637ebed9ca4ff
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 10:18:21 -0600

    ✅ add init movie test

--- packages/client/test/app/core/widgets/beach_waves/stack/presentation/movie_stores/black_out_movie_store_test.dart
+++ packages/client/test/app/core/widgets/beach_waves/stack/presentation/movie_stores/black_out_movie_store_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/types/seconds.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
@@ -18,5 +19,10 @@ void main() {
     test("control should be mirror", () {
       expect(testStore.control, Control.stop);
     });
+  });
+
+  test("initMovie", () {
+    testStore.initMovie(NoParams());
+    expect(testStore.control, Control.playFromStart);
   });
 }
