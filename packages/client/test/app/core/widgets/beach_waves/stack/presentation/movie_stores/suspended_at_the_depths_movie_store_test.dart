944cbdb1d203e9fd810f27a6f0ca5fbceae1a12c
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 10:28:33 -0600

    ✅ suspended_at_the_depths_movie_store_test added

---
+++ packages/client/test/app/core/widgets/beach_waves/stack/presentation/movie_stores/suspended_at_the_depths_movie_store_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+void main() {
+  late SuspendedAtTheDepthsMovieStore testStore;
+
+  setUp(() {
+    testStore = SuspendedAtTheDepthsMovieStore();
+  });
+
+  group("initial values", () {
+    test("movie duration should be the ocean dive duration", () {
+      expect(testStore.movie.duration, Seconds.get(1));
+    });
+
+    test("control should be control.stop", () {
+      expect(testStore.control, Control.stop);
+    });
+  });
+}
