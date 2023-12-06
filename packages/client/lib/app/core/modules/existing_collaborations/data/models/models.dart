8c58c3d0f27e54a2e1a0442fab835a312dea01d0
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 08:08:09 -0600

    🔥 existing_collaborations module

--- packages/client/lib/app/core/modules/existing_collaborations/data/models/models.dart
+++
@@ -1,4 +0,0 @@
-export 'collaboration_activation_status_model.dart';
-export 'collaboration_consecration_status_model.dart';
-export 'individual_collaborator_entry_status_model.dart';
-export 'who_has_the_question_model.dart';
--- packages/client/lib/app/core/modules/existing_collaborations/data/models/who_has_the_question_model.dart
+++
@@ -1,7 +0,0 @@
-import 'package:nokhte/app/core/modules/existing_collaborations/domain/domain.dart';
-
-class WhoHasTheQuestionModel extends WhoHasTheQuestionEntity {
-  const WhoHasTheQuestionModel({
-    required super.userHasTheQuestion,
-  });
-}
