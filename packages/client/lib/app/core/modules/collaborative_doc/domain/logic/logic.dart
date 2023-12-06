a06ccde5ba4ceb2f01af24cd51db18e464cc1c8c
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-06 08:03:34 -0600

    🔥 collaborative_doc module

--- packages/client/lib/app/core/modules/collaborative_doc/domain/logic/logic.dart
+++
@@ -1,8 +0,0 @@
-export 'create_collaborative_doc.dart';
-export 'move_to_finished_docs.dart';
-export 'get_collaborative_doc_content.dart';
-export 'get_collaborator_doc_info.dart';
-export 'update_collaborative_doc.dart';
-export 'update_commit_desire_status.dart';
-export 'update_user_delta.dart';
-export 'update_user_presence.dart';
