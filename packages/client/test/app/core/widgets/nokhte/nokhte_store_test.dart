abf01113908a924bd7ef482884d7a9e61605dd3b
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-07 11:39:46 -0600

    ✨ nokhte_store & associated test suite

---
+++ packages/client/test/app/core/widgets/nokhte/nokhte_store_test.dart
@@ -0,0 +1,29 @@
+import 'package:fake_async/fake_async.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/nokhte/stack/presentation/mobx/nokhte_store.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+void main() {
+  late NokhteStore testStore;
+
+  setUp(() {
+    testStore = NokhteStore();
+  });
+
+  group("initial values", () {
+    test("movie", () {
+      expect(testStore.movie.duration, Seconds.get(2));
+    });
+  });
+
+  group("actions", () {
+    test('setPositionMovie', () {
+      fakeAsync((async) {
+        testStore.setPositionMovie(Offset.zero, Offset.zero);
+        async.elapse(Seconds.get(0, milli: 190));
+        expect(testStore.control, Control.playFromStart);
+      });
+    });
+  });
+}
