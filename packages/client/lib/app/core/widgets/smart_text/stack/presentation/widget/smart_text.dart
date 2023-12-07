09ca57bfccda26470e29e802a420576e265cf77f
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-07 08:20:37 -0600

    ♻️ smart_text to use simple_animations instead of animated_opacity

--- packages/client/lib/app/core/widgets/smart_text/stack/presentation/widget/smart_text.dart
+++ packages/client/lib/app/core/widgets/smart_text/stack/presentation/widget/smart_text.dart
@@ -3,8 +3,9 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/mobx.dart';
+import 'package:nokhte/app/core/widgets/widget_constants.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 class SmartText extends HookWidget {
   final SmartTextStore store;
@@ -16,32 +17,36 @@ class SmartText extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return Observer(builder: (context) {
-      return AnimatedOpacity(
-        opacity: store.showWidget ? 1 : 0,
-        onEnd: store.onOpacityTransitionComplete(),
-        duration: Seconds.get(0, milli: 500), // Adjust fade duration
-        child: Padding(
-          padding: const EdgeInsets.only(left: 50.0, right: 50.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              PlatformText(
-                store.currentMainText,
-                textAlign: TextAlign.center,
-                style: GoogleFonts.kantumruyPro(
-                  fontSize: store.currentMainTextFontSize,
-                  color: Colors.white,
+      return CustomAnimationBuilder(
+        tween: FadeInAndOutMovie.movie,
+        control: store.control,
+        onCompleted: () => store.onOpacityTransitionComplete(),
+        duration: FadeInAndOutMovie.movie.duration, // Adjust fade duration
+        builder: (context, value, child) => Opacity(
+          opacity: value.get('opacity'),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 50.0, right: 50.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                PlatformText(
+                  store.currentMainText,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.kantumruyPro(
+                    fontSize: store.currentMainTextFontSize,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              PlatformText(
-                store.currentSubText,
-                textAlign: TextAlign.center,
-                style: GoogleFonts.kantumruyPro(
-                  fontSize: store.currentSubTextFontSize,
-                  color: Colors.white,
+                PlatformText(
+                  store.currentSubText,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.kantumruyPro(
+                    fontSize: store.currentSubTextFontSize,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       );
