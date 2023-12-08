264a12f7966c55bbbd1b82e8ea9f109f90fcac60
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-08 16:08:56 -0600

    🆙 gestures_module from v5 => v6

--- packages/client/lib/app/core/widgets/widget_modules/gestures_module.dart
+++ packages/client/lib/app/core/widgets/widget_modules/gestures_module.dart
@@ -3,18 +3,17 @@ import 'package:nokhte/app/core/widgets/widgets.dart';
 
 class GesturesModule extends Module {
   @override
-  List<Bind> get binds => [
-        Bind.factory<HoldDetector>(
-          (i) => HoldDetector(),
-          export: true,
-        ),
-        Bind.factory<SwipeDetector>(
-          (i) => SwipeDetector(),
-          export: true,
-        ),
-        Bind.factory<TapDetector>(
-          (i) => TapDetector(),
-          export: true,
-        ),
-      ];
+  exportedBinds(i) {
+    i.add<HoldDetector>(
+      () => HoldDetector(),
+    );
+
+    i.add<SwipeDetector>(
+      () => SwipeDetector(),
+    );
+
+    i.add<TapDetector>(
+      () => TapDetector(),
+    );
+  }
 }
