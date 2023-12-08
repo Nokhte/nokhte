ef499ca2e9084f4b73d326b9837225b9f4f2bd6d
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-08 16:18:13 -0600

    ✨ export beach wave shared types

--- packages/client/lib/app/core/widgets/widget_constants.dart
+++ packages/client/lib/app/core/widgets/widget_constants.dart
@@ -1,3 +1,4 @@
 export 'smart_text/stack/constants/constants.dart';
 export 'beach_widgets/beach_waves/stack/movies/movies.dart';
+export 'beach_widgets/shared/types/types.dart';
 export 'smart_text/stack/constants/types/rotating_text_data.dart';
