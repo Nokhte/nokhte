bc3bc44a20481c5b093f402306813736d15afc79
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-08 16:19:34 -0600

    ✨ add coordinator exports

--- packages/client/lib/app/modules/authentication/presentation/mobx/main/main.dart
+++ packages/client/lib/app/modules/authentication/presentation/mobx/main/main.dart
@@ -1,3 +1,2 @@
 export 'sign_in_with_auth_provider_store.dart';
 export 'get_auth_state_store.dart';
-export '../coordinator/login_screen_coordinator.dart';
--- packages/client/lib/app/modules/authentication/presentation/mobx/mobx.dart
+++ packages/client/lib/app/modules/authentication/presentation/mobx/mobx.dart
@@ -1,2 +1,2 @@
 export 'main/main.dart';
-export './coordinator/login_screen_coordinator.dart';
+export './coordinators/coordinators.dart';
