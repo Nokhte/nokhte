d3c704d92a312a5a2fe069f6fb91e56626a1592c
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-09 17:33:25 -0600

    🔥 un-neccessary test in core

--- packages/core/test/core_test.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:core/custom_control_structures.dart';
-// import 'package:test/test.dart';
-
-// void main() {
-//   test('calculate', () {
-//     expect(calculate(), 42);
-//   });
-// }
