3d940dfbccc43535d497be4781c1d16343444c26
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-10 07:14:17 -0600

    ✨ double comparison extension for readability

---
+++ packages/client/lib/app/core/extensions/double.dart
@@ -0,0 +1,6 @@
+extension DoubleComparisons on double {
+  bool isLessThan(double value) => this < value;
+  bool isGreaterThan(double value) => this > value;
+  bool isLessThanOrEqualTo(double value) => this <= value;
+  bool isGreaterThanOrEqualTo(double value) => this >= value;
+}
