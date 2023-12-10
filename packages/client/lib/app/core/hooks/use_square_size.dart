21ea8ed32f4d58f67b6c4ea870a6ac83c722eab5
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-10 07:21:57 -0600

    ♻️ use_square_size improve readability & condense declarations

--- packages/client/lib/app/core/hooks/use_square_size.dart
+++ packages/client/lib/app/core/hooks/use_square_size.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nokhte/app/core/extensions/extensions.dart';
+import 'package:nokhte/app/core/hooks/hooks.dart';
 
 Size useSquareSize({
   required double relativeLength,
 }) {
-  final context = useContext();
-  double width = MediaQuery.of(context).size.width;
-  var padding = MediaQuery.of(context).padding;
-  double unrefinedHeight = MediaQuery.of(context).size.height;
-  double height = unrefinedHeight - padding.top - padding.bottom;
-  double desiredSize =
-      width < height ? width * relativeLength : height * relativeLength;
+  final screenSizeData = useScreenSizeData(),
+      unrefinedHeight = screenSizeData.height,
+      width = screenSizeData.width,
+      topPadding = screenSizeData.padding.top,
+      bottomPadding = screenSizeData.padding.bottom,
+      refinedHeight = unrefinedHeight - topPadding - bottomPadding;
+
+  double desiredSize = width.isLessThan(refinedHeight)
+      ? width * relativeLength
+      : refinedHeight * relativeLength;
   return Size(desiredSize, desiredSize);
 }
