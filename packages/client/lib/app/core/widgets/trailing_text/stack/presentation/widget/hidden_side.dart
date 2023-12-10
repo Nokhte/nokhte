ff3c91280b681055a270af5cc2d4e61ee1906be9
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-10 09:21:33 -0600

    ✨ hidden_side widget to be used in trailing_text

---
+++ packages/client/lib/app/core/widgets/trailing_text/stack/presentation/widget/hidden_side.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+
+class HiddenSide extends StatelessWidget {
+  final TextRevealDirection textRevealDirection;
+  final Color hiddenSideColor;
+  final Size size;
+  const HiddenSide({
+    super.key,
+    required this.textRevealDirection,
+    required this.hiddenSideColor,
+    required this.size,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return textRevealDirection == TextRevealDirection.fromLeft
+        ? Row(
+            children: [
+              Expanded(child: Container()),
+              Container(
+                width: size.width,
+                height: size.height,
+                color: hiddenSideColor,
+              ),
+            ],
+          )
+        : Row(
+            children: [
+              Expanded(child: Container()),
+              Container(
+                width: size.width,
+                height: size.height,
+                color: hiddenSideColor,
+              ),
+            ],
+          );
+  }
+}
