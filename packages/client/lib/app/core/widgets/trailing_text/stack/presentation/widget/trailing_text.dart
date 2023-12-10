9b40bb55ba7ca666349af57508ef89c7fd9b685d
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-10 05:51:00 -0600

    🚧 base trailing_text impl

--- packages/client/lib/app/core/widgets/trailing_text/stack/presentation/widget/trailing_text.dart
+++ packages/client/lib/app/core/widgets/trailing_text/stack/presentation/widget/trailing_text.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/trailing_text/stack/presentation/mobx/trailing_text_store.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 class TrailingText extends StatelessWidget {
   final TrailingTextStore store;
@@ -9,7 +13,21 @@ class TrailingText extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  Widget build(BuildContext context) => Observer(
+        builder: (context) => AnimatedOpacity(
+          opacity: 1,
+          duration: Seconds.get(0, milli: 500),
+          child: CustomAnimationBuilder(
+            tween: store.movie,
+            duration: store.movie.duration,
+            control: store.control,
+            builder: (context, value, child) => Row(
+              children: [
+                PlatformText("something"),
+                Container(),
+              ],
+            ),
+          ),
+        ),
+      );
 }
