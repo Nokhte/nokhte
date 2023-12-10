d06f2fdd74bd146d70a3e70ce39cd6b25f7393f9
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-10 13:50:10 -0600

    ♻️ play to initMovie

--- packages/client/lib/app/core/widgets/trailing_text/stack/presentation/mobx/trailing_text_store.dart
+++ packages/client/lib/app/core/widgets/trailing_text/stack/presentation/mobx/trailing_text_store.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
-import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/trailing_text/stack/movies/reveal_text_movie.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
@@ -10,8 +10,8 @@ part 'trailing_text_store.g.dart';
 
 class TrailingTextStore = _TrailingTextStoreBase with _$TrailingTextStore;
 
-abstract class _TrailingTextStoreBase extends BaseCustomAnimatedWidgetStore
-    with Store {
+abstract class _TrailingTextStoreBase
+    extends BaseCustomAnimatedWidgetStore<NoParams> with Store {
   @observable
   Size textSize = Size.zero;
 
@@ -22,8 +22,10 @@ abstract class _TrailingTextStoreBase extends BaseCustomAnimatedWidgetStore
   }) {
     textSize = newTextSize;
     setMovie(RevealTextMovie.getMovie(newTextRevealDirection, textSize.width));
-    Future.delayed(Seconds.get(2), () {
-      setControl(Control.playFromStart);
-    });
+  }
+
+  @override
+  initMovie(NoParams params) {
+    setControl(Control.playFromStart);
   }
 }
