d58cf546c679e396c29b12937df16b35fb0d7856
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-10 09:20:49 -0600

    ♻️ to useScreenSizeData from useScreenSize

--- packages/client/lib/app/core/hooks/use_screen_size_data.dart
+++ packages/client/lib/app/core/hooks/use_screen_size_data.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:nokhte/app/core/types/types.dart';
 
-ScreenSize useScreenSizeData() {
+ScreenSizeData useScreenSizeData() {
   final context = useContext(),
       width = MediaQuery.of(context).size.width,
       height = MediaQuery.of(context).size.height,
       padding = MediaQuery.of(context).padding;
 
-  return ScreenSize(
+  return ScreenSizeData(
     height: height,
     width: width,
     padding: padding,
