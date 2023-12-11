a3d183937a93061e266eb1f1193a38e28b27914a
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-11 09:46:56 -0600

    ✨ stack barrel instead of stack sub-exports

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/beach_waves.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/beach_waves.dart
@@ -1,4 +1,1 @@
-export 'stack/presentation/widgets/widgets.dart';
-export 'stack/presentation/mobx/mobx.dart';
-export 'stack/movies/movies.dart';
-export 'stack/utils/utils.dart';
+export 'stack/stack.dart';
