d2016d7e3645c40dc18d955e9345c7fb048f49a2
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-11 12:07:11 -0600

    ✨ export water from top in movie_stores barrel

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/movie_stores.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/movie_stores.dart
@@ -10,3 +10,4 @@ export './suspended_at_ocean_dive/suspended_at_ocean_dive.dart';
 export './suspended_at_the_depths/suspended_at_the_depths.dart';
 export './times_up_end_to_ocean_dive/times_up_end_to_ocean_dive.dart';
 export './times_up_end_to_the_depths/times_up_end_to_the_depths.dart';
+export './water_from_top_to_on_shore/water_from_top_to_on_shore.dart';
