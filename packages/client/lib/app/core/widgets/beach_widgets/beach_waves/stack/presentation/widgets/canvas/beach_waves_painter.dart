d919f1052fa61ba286bfdf7d52d7d807ea6f7b59
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-11 09:25:00 -0600

    ♻️ to color & stops list, list field deps & more readable

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/widgets/canvas/beach_waves_painter.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/widgets/canvas/beach_waves_painter.dart
@@ -2,76 +2,16 @@ import 'package:flutter/material.dart';
 
 class BeachWavesPainter extends CustomPainter {
   final double waterValue;
-  final Color firstGradientColor;
-  final Color secondGradientColor;
-  final Color thirdGradientColor;
-  final Color fourthGradientColor;
-  final Color fifthGradientColor;
-  final Color sixthGradientColor;
-  final Color seventhGradientColor;
-  final Color eighthGradientColor;
-  final double firstGradientStop;
-  final double secondGradientStop;
-  final double thirdGradientStop;
-  final double fourthGradientStop;
-  final double fifthGradientStop;
-  final double sixthGradientStop;
-  final double seventhGradientStop;
-  final double eighthGradientStop;
+  final List<Color> colorsList;
+  final List<double> stopsList;
 
   BeachWavesPainter({
     required this.waterValue,
-    required this.firstGradientColor,
-    required this.secondGradientColor,
-    required this.thirdGradientColor,
-    required this.fourthGradientColor,
-    required this.fifthGradientColor,
-    required this.sixthGradientColor,
-    required this.seventhGradientColor,
-    required this.eighthGradientColor,
-    required this.firstGradientStop,
-    required this.secondGradientStop,
-    required this.thirdGradientStop,
-    required this.fourthGradientStop,
-    required this.fifthGradientStop,
-    required this.sixthGradientStop,
-    required this.seventhGradientStop,
-    required this.eighthGradientStop,
+    required this.colorsList,
+    required this.stopsList,
   });
 
-  @override
-  void paint(Canvas canvas, Size size) {
-    const waveCount = 3;
-    const waveAmplitude = 20.0;
-
-    final waveGradient = LinearGradient(
-      colors: [
-        firstGradientColor,
-        secondGradientColor,
-        thirdGradientColor,
-        fourthGradientColor,
-        fifthGradientColor,
-        sixthGradientColor,
-        seventhGradientColor,
-        eighthGradientColor,
-      ],
-      stops: [
-        firstGradientStop,
-        secondGradientStop,
-        thirdGradientStop,
-        fourthGradientStop,
-        fifthGradientStop,
-        sixthGradientStop,
-        seventhGradientStop,
-        eighthGradientStop,
-      ],
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-    );
-    final waterPaint = Paint()
-      ..shader = waveGradient
-          .createShader(Rect.fromLTWH(0, 0, size.width, size.height));
-
+  paintSand(Canvas canvas, Size size) {
     final sandGrandient = Paint()
       ..shader = const LinearGradient(
         colors: [
@@ -84,6 +24,22 @@ class BeachWavesPainter extends CustomPainter {
           Rect.fromLTWH(0, size.height * 0.75, size.width, size.height * 0.25));
 
     canvas.drawRect(Offset.zero & size, sandGrandient);
+    //
+  }
+
+  paintWater(Canvas canvas, Size size) {
+    const waveCount = 3;
+    const waveAmplitude = 20.0;
+
+    final waveGradient = LinearGradient(
+      colors: colorsList,
+      stops: stopsList,
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+    );
+    final waterPaint = Paint()
+      ..shader = waveGradient
+          .createShader(Rect.fromLTWH(0, 0, size.width, size.height));
 
     final phase = waterValue;
 
@@ -105,6 +61,12 @@ class BeachWavesPainter extends CustomPainter {
         ..close();
       canvas.drawPath(path, waterPaint);
     }
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    paintSand(canvas, size);
+    paintWater(canvas, size);
   }
 
   @override
