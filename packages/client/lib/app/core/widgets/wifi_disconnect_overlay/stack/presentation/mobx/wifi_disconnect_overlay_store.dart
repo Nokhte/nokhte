f7669fd53de804c15a2be36c78be9b0b5d8a7e56
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-13 12:34:33 -0600

    💉 connectivity store into wifi widget store

--- packages/client/lib/app/core/widgets/wifi_disconnect_overlay/stack/presentation/mobx/wifi_disconnect_overlay_store.dart
+++ packages/client/lib/app/core/widgets/wifi_disconnect_overlay/stack/presentation/mobx/wifi_disconnect_overlay_store.dart
@@ -1,10 +1,17 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/modules/connectivity/mobx/get_on_connectivity_changed_store.dart';
 part 'wifi_disconnect_overlay_store.g.dart';
 
 class WifiDisconnectOverlayStore = _WifiDisconnectOverlayStoreBase
     with _$WifiDisconnectOverlayStore;
 
 abstract class _WifiDisconnectOverlayStoreBase
-    extends BaseCustomAnimatedWidgetStore with Store {}
+    extends BaseCustomAnimatedWidgetStore with Store {
+  final GetOnConnectivityChangedStore getOnConnectivityChanged;
+
+  _WifiDisconnectOverlayStoreBase({
+    required this.getOnConnectivityChanged,
+  });
+}
