5db7abc58b5520cba9b4afdea08445c5130ad012
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-13 09:16:46 -0600

    🚚 rename module to LegacyConnectivity

--- packages/client/lib/app/core/modules/legacy_connectivity/legacy_connectivity_module.dart
+++ packages/client/lib/app/core/modules/legacy_connectivity/legacy_connectivity_module.dart
@@ -1,10 +1,10 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-class ConnectivityModule extends Module {
+class LegacyConnectivityModule extends Module {
   final Connectivity connectivityInstance;
 
-  ConnectivityModule({required this.connectivityInstance});
+  LegacyConnectivityModule({required this.connectivityInstance});
 
   @override
   binds(i) => [
