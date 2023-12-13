b7a13be90ce11ff8d9d9bfbf22da065527b8bdcf
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-13 12:33:45 -0600

    🛢️ widget exports updated

--- packages/client/lib/app/core/widgets/widgets.dart
+++ packages/client/lib/app/core/widgets/widgets.dart
@@ -4,3 +4,4 @@ export 'gesture_pill/gesture_pill.dart';
 export 'nokhte/nokhte.dart';
 export 'smart_text/smart_text.dart';
 export 'trailing_text/trailing_text.dart';
+export './wifi_disconnect_overlay/wifi_disconnect_overlay.dart';
