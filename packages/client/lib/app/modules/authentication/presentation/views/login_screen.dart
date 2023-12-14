517c70bbe4bd8b8fcca5ba59064ffe7d0c55050d
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-14 11:33:27 -0600

    ♻️ to include back in all the auth widgets

--- packages/client/lib/app/modules/authentication/presentation/views/login_screen.dart
+++ packages/client/lib/app/modules/authentication/presentation/views/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nokhte/app/core/constants/constants.dart';
 import 'package:nokhte/app/core/hooks/use_center_offset.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/authentication/presentation/presentation.dart';
@@ -51,45 +52,45 @@ class LoginScreen extends HookWidget with WidgetsBindingObserver {
                       store: coordinator.widgets.layer2BeachWaves,
                     ),
                   ),
-                  // SizedBox(
-                  //   width: MediaQuery.of(context).size.width,
-                  //   height: MediaQuery.of(context).size.height,
-                  //   child: TrailingText(
-                  //     fontStyle: Fonts.kantumruy(
-                  //       fontSize: 25.0,
-                  //     ),
-                  //     gradientList: List.filled(2, Colors.white),
-                  //     store: coordinator.widgets.bottomTrailingText,
-                  //     textRevealDirection: TextRevealDirection.fromLeft,
-                  //     textContent: "Swipe Up",
-                  //   ),
-                  // ),
-                  // SizedBox(
-                  //   width: MediaQuery.of(context).size.width,
-                  //   height: MediaQuery.of(context).size.height,
-                  //   child: TrailingText(
-                  //     fontStyle: Fonts.kantumruy(
-                  //       fontSize: 25.0,
-                  //     ),
-                  //     gradientList: Gradients.skyMint,
-                  //     store: coordinator.widgets.topTrailingText,
-                  //     textRevealDirection: TextRevealDirection.fromRight,
-                  //     textContent: "To Login",
-                  //     additionalPadding: const EdgeInsets.only(bottom: 280),
-                  //   ),
-                  // ),
-                  // Center(
-                  //   child: SmartText(
-                  //     store: coordinator.widgets.smartTextStore,
-                  //   ),
-                  // ),
-                  // SizedBox(
-                  //   width: MediaQuery.of(context).size.width,
-                  //   height: MediaQuery.of(context).size.height,
-                  //   child: Nokhte(
-                  //     store: coordinator.widgets.nokhte,
-                  //   ),
-                  // ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: TrailingText(
+                      fontStyle: Fonts.kantumruy(
+                        fontSize: 25.0,
+                      ),
+                      gradientList: List.filled(2, Colors.white),
+                      store: coordinator.widgets.bottomTrailingText,
+                      textRevealDirection: TextRevealDirection.fromLeft,
+                      textContent: "Swipe Up",
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: TrailingText(
+                      fontStyle: Fonts.kantumruy(
+                        fontSize: 25.0,
+                      ),
+                      gradientList: Gradients.skyMint,
+                      store: coordinator.widgets.topTrailingText,
+                      textRevealDirection: TextRevealDirection.fromRight,
+                      textContent: "To Login",
+                      additionalPadding: const EdgeInsets.only(bottom: 280),
+                    ),
+                  ),
+                  Center(
+                    child: SmartText(
+                      store: coordinator.widgets.smartTextStore,
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: Nokhte(
+                      store: coordinator.widgets.nokhte,
+                    ),
+                  ),
                 ],
               ),
             ),
