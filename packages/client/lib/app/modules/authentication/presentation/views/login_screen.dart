99b174f27f932d4aee9478f9c7c02f305377f1b7
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-14 06:36:57 -0600

    ♻️ to include wifi-disconnect overlay

--- packages/client/lib/app/modules/authentication/presentation/views/login_screen.dart
+++ packages/client/lib/app/modules/authentication/presentation/views/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:nokhte/app/core/constants/constants.dart';
 import 'package:nokhte/app/core/hooks/use_center_offset.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/authentication/presentation/presentation.dart';
@@ -30,66 +29,69 @@ class LoginScreen extends HookWidget with WidgetsBindingObserver {
     return Builder(builder: (context) {
       return Scaffold(
         resizeToAvoidBottomInset: false,
-        body: Tap(
-          trackerStore: coordinator.tap,
-          child: Swipe(
-            trackerStore: coordinator.swipe,
-            child: Stack(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  child: BeachWaves(
-                    store: coordinator.widgets.layer1BeachWaves,
-                  ),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  child: BeachWaves(
-                    store: coordinator.widgets.layer2BeachWaves,
-                  ),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  child: TrailingText(
-                    fontStyle: Fonts.kantumruy(
-                      fontSize: 25.0,
+        body: WifiDisconnectOverlay(
+          store: coordinator.widgets.wifiDisconnectOverlay,
+          child: Tap(
+            trackerStore: coordinator.tap,
+            child: Swipe(
+              trackerStore: coordinator.swipe,
+              child: Stack(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: BeachWaves(
+                      store: coordinator.widgets.layer1BeachWaves,
                     ),
-                    gradientList: List.filled(2, Colors.white),
-                    store: coordinator.widgets.bottomTrailingText,
-                    textRevealDirection: TextRevealDirection.fromLeft,
-                    textContent: "Swipe Up",
                   ),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  child: TrailingText(
-                    fontStyle: Fonts.kantumruy(
-                      fontSize: 25.0,
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: BeachWaves(
+                      store: coordinator.widgets.layer2BeachWaves,
                     ),
-                    gradientList: Gradients.skyMint,
-                    store: coordinator.widgets.topTrailingText,
-                    textRevealDirection: TextRevealDirection.fromRight,
-                    textContent: "To Login",
-                    additionalPadding: const EdgeInsets.only(bottom: 280),
                   ),
-                ),
-                Center(
-                  child: SmartText(
-                    store: coordinator.widgets.smartTextStore,
-                  ),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  child: Nokhte(
-                    store: coordinator.widgets.nokhte,
-                  ),
-                ),
-              ],
+                  // SizedBox(
+                  //   width: MediaQuery.of(context).size.width,
+                  //   height: MediaQuery.of(context).size.height,
+                  //   child: TrailingText(
+                  //     fontStyle: Fonts.kantumruy(
+                  //       fontSize: 25.0,
+                  //     ),
+                  //     gradientList: List.filled(2, Colors.white),
+                  //     store: coordinator.widgets.bottomTrailingText,
+                  //     textRevealDirection: TextRevealDirection.fromLeft,
+                  //     textContent: "Swipe Up",
+                  //   ),
+                  // ),
+                  // SizedBox(
+                  //   width: MediaQuery.of(context).size.width,
+                  //   height: MediaQuery.of(context).size.height,
+                  //   child: TrailingText(
+                  //     fontStyle: Fonts.kantumruy(
+                  //       fontSize: 25.0,
+                  //     ),
+                  //     gradientList: Gradients.skyMint,
+                  //     store: coordinator.widgets.topTrailingText,
+                  //     textRevealDirection: TextRevealDirection.fromRight,
+                  //     textContent: "To Login",
+                  //     additionalPadding: const EdgeInsets.only(bottom: 280),
+                  //   ),
+                  // ),
+                  // Center(
+                  //   child: SmartText(
+                  //     store: coordinator.widgets.smartTextStore,
+                  //   ),
+                  // ),
+                  // SizedBox(
+                  //   width: MediaQuery.of(context).size.width,
+                  //   height: MediaQuery.of(context).size.height,
+                  //   child: Nokhte(
+                  //     store: coordinator.widgets.nokhte,
+                  //   ),
+                  // ),
+                ],
+              ),
             ),
           ),
         ),
