a82735b70fc636be6f1de0e40b508bac8ed24fca
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-16 14:59:12 -0600

    ✨ int comparisons extension added

---
+++ packages/client/lib/app/core/extensions/int.dart
@@ -0,0 +1,6 @@
+extension IntComparisons on int {
+  bool isLessThan(int value) => this < value;
+  bool isGreaterThan(int value) => this > value;
+  bool isLessThanOrEqualTo(int value) => this <= value;
+  bool isGreaterThanOrEqualTo(int value) => this >= value;
+}
