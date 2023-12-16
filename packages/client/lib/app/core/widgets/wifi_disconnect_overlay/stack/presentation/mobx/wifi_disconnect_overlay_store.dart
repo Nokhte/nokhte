71518b1ec0364b947374e79062573ffa99edae48
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-16 11:07:46 -0600

    ✨♻️ wifi_disconnect_overlay add loop for ripple out part of the movie

--- packages/client/lib/app/core/widgets/wifi_disconnect_overlay/stack/presentation/mobx/wifi_disconnect_overlay_store.dart
+++ packages/client/lib/app/core/widgets/wifi_disconnect_overlay/stack/presentation/mobx/wifi_disconnect_overlay_store.dart
@@ -1,9 +1,10 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
 import 'package:nokhte/app/core/modules/connectivity/mobx/get_on_connectivity_changed_store.dart';
 import 'package:nokhte/app/core/types/types.dart';
-import 'package:nokhte/app/core/widgets/wifi_disconnect_overlay/stack/constants/movies/wifi_ripple_movie.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
 part 'wifi_disconnect_overlay_store.g.dart';
 
@@ -22,6 +23,13 @@ abstract class _WifiDisconnectOverlayStoreBase
     onCompletedReactor();
     toggleWidgetVisibility();
   }
+
+  @observable
+  WifiDisconnectMovieModes movieMode = WifiDisconnectMovieModes.initial;
+
+  @action
+  setMovieMode(WifiDisconnectMovieModes newMovieModes) =>
+      movieMode = newMovieModes;
 
   connectionReactor({
     required Function onConnected,
@@ -46,18 +54,53 @@ abstract class _WifiDisconnectOverlayStoreBase
         toggleWidgetVisibility();
       }
       onConnected();
-      setControl(Control.playReverse);
+      // initRemoveTheCircle();
+      // setControl(Control.playReverse);
     } else {
-      setControl(Control.play);
+      // setControl(Control.play);
+      initPlaceTheCircle();
       onDisconnected();
     }
   }
 
+  initPlaceTheCircle() {
+    setMovieStatus(MovieStatus.inProgress);
+    setMovie(PlaceTheCircleMovie.movie);
+    setControl(Control.playFromStart);
+    setMovieMode(WifiDisconnectMovieModes.placeTheCircle);
+  }
+
+  initLoopMovie() {
+    setMovieStatus(MovieStatus.inProgress);
+    setMovie(WifiRippleMovie.movie);
+    setMovieMode(WifiDisconnectMovieModes.rippleLoop);
+    setControl(Control.playFromStart);
+  }
+
+  @observable
+  int rippleCount = 0;
+
+  @action
+  incrementRippleCount() => rippleCount++;
+
+  @action
+  resetRippleCount() => rippleCount = 0;
+
   onCompletedReactor() => reaction((p0) => movieStatus, (p0) {
-        if (p0 == MovieStatus.finished && pastControl == Control.play) {
-          if (!showWidget) {
-            toggleWidgetVisibility();
+        if (p0 == MovieStatus.finished &&
+            movieMode == WifiDisconnectMovieModes.placeTheCircle) {
+          initLoopMovie();
+        } else if (p0 == MovieStatus.finished &&
+            movieMode == WifiDisconnectMovieModes.rippleLoop) {
+          if (rippleCount < 2) {
+            initLoopMovie();
+            incrementRippleCount();
           }
         }
+        // if (p0 == MovieStatus.finished && pastControl == Control.play) {
+        //   if (!showWidget) {
+        //     toggleWidgetVisibility();
+        //   }
+        // }
       });
 }
--- packages/client/lib/app/core/widgets/wifi_disconnect_overlay/stack/presentation/mobx/wifi_disconnect_overlay_store.g.dart
+++ packages/client/lib/app/core/widgets/wifi_disconnect_overlay/stack/presentation/mobx/wifi_disconnect_overlay_store.g.dart
@@ -9,9 +9,52 @@ part of 'wifi_disconnect_overlay_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$WifiDisconnectOverlayStore on _WifiDisconnectOverlayStoreBase, Store {
+  late final _$movieModeAtom =
+      Atom(name: '_WifiDisconnectOverlayStoreBase.movieMode', context: context);
+
+  @override
+  WifiDisconnectMovieModes get movieMode {
+    _$movieModeAtom.reportRead();
+    return super.movieMode;
+  }
+
+  @override
+  set movieMode(WifiDisconnectMovieModes value) {
+    _$movieModeAtom.reportWrite(value, super.movieMode, () {
+      super.movieMode = value;
+    });
+  }
+
+  late final _$rippleCountAtom = Atom(
+      name: '_WifiDisconnectOverlayStoreBase.rippleCount', context: context);
+
+  @override
+  int get rippleCount {
+    _$rippleCountAtom.reportRead();
+    return super.rippleCount;
+  }
+
+  @override
+  set rippleCount(int value) {
+    _$rippleCountAtom.reportWrite(value, super.rippleCount, () {
+      super.rippleCount = value;
+    });
+  }
+
   late final _$_WifiDisconnectOverlayStoreBaseActionController =
       ActionController(
           name: '_WifiDisconnectOverlayStoreBase', context: context);
+
+  @override
+  dynamic setMovieMode(WifiDisconnectMovieModes newMovieModes) {
+    final _$actionInfo = _$_WifiDisconnectOverlayStoreBaseActionController
+        .startAction(name: '_WifiDisconnectOverlayStoreBase.setMovieMode');
+    try {
+      return super.setMovieMode(newMovieModes);
+    } finally {
+      _$_WifiDisconnectOverlayStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
 
   @override
   dynamic attuneWidgetsBasedOnConnection(
@@ -29,9 +72,33 @@ mixin _$WifiDisconnectOverlayStore on _WifiDisconnectOverlayStoreBase, Store {
   }
 
   @override
+  dynamic incrementRippleCount() {
+    final _$actionInfo =
+        _$_WifiDisconnectOverlayStoreBaseActionController.startAction(
+            name: '_WifiDisconnectOverlayStoreBase.incrementRippleCount');
+    try {
+      return super.incrementRippleCount();
+    } finally {
+      _$_WifiDisconnectOverlayStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic resetRippleCount() {
+    final _$actionInfo = _$_WifiDisconnectOverlayStoreBaseActionController
+        .startAction(name: '_WifiDisconnectOverlayStoreBase.resetRippleCount');
+    try {
+      return super.resetRippleCount();
+    } finally {
+      _$_WifiDisconnectOverlayStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
   String toString() {
     return '''
-
+movieMode: ${movieMode},
+rippleCount: ${rippleCount}
     ''';
   }
 }
