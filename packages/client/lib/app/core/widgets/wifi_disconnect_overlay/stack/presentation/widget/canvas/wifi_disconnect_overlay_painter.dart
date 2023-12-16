64216eee70ec5b91a1fa48e8f382cb9afe416285
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-16 15:06:33 -0600

    ✨ wifi_disconnect_overlay: agency for circle outline radii

--- packages/client/lib/app/core/widgets/wifi_disconnect_overlay/stack/presentation/widget/canvas/wifi_disconnect_overlay_painter.dart
+++ packages/client/lib/app/core/widgets/wifi_disconnect_overlay/stack/presentation/widget/canvas/wifi_disconnect_overlay_painter.dart
@@ -3,24 +3,40 @@ import 'package:flutter/material.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 
 class WifiDisconnectOverlayPainter extends CustomPainter {
-  final OpacityAndRadius circle, firstArc, secondArc, thirdArc;
+  final OpacityAndRadius circle,
+      firstArc,
+      secondArc,
+      thirdArc,
+      firstCircleOutline,
+      secondCircleOutline,
+      thirdCircleOutline;
 
   WifiDisconnectOverlayPainter({
     required this.circle,
     required this.firstArc,
     required this.secondArc,
     required this.thirdArc,
+    required this.firstCircleOutline,
+    required this.secondCircleOutline,
+    required this.thirdCircleOutline,
   });
 
   drawArc(
-      Canvas canvas, Size size, double radius, double opacity, Offset center) {
+    Canvas canvas,
+    Size size,
+    Offset center, {
+    required double arcRadius,
+    required double circleRadius,
+    required double arcOpacity,
+    required double circleOpacity,
+  }) {
     Paint arcPaint = Paint()
-      ..color = Colors.white.withOpacity(opacity)
+      ..color = Colors.white.withOpacity(arcOpacity)
       ..strokeCap = StrokeCap.square
       ..strokeWidth = 3
       ..style = PaintingStyle.stroke;
     canvas.drawArc(
-      Rect.fromCircle(center: center, radius: radius - 1),
+      Rect.fromCircle(center: center, radius: arcRadius - 1),
       pi + .83,
       (pi / 6) + 1,
       false,
@@ -28,10 +44,10 @@ class WifiDisconnectOverlayPainter extends CustomPainter {
     );
 
     Paint circlePaint = Paint()
-      ..color = Colors.white.withOpacity(opacity / 2)
+      ..color = Colors.white.withOpacity(circleOpacity)
       ..style = PaintingStyle.stroke
       ..strokeWidth = 1;
-    canvas.drawCircle(center, radius, circlePaint);
+    canvas.drawCircle(center, circleRadius, circlePaint);
   }
 
   drawCenterCircle(
@@ -44,9 +60,33 @@ class WifiDisconnectOverlayPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final center = Offset(size.width / 2, size.height / 2);
-    drawArc(canvas, size, firstArc.radius, firstArc.opacity, center);
-    drawArc(canvas, size, secondArc.radius, secondArc.opacity, center);
-    drawArc(canvas, size, thirdArc.radius, thirdArc.opacity, center);
+    drawArc(
+      canvas,
+      size,
+      center,
+      arcOpacity: firstArc.opacity,
+      arcRadius: firstArc.radius,
+      circleOpacity: firstCircleOutline.opacity,
+      circleRadius: firstCircleOutline.radius,
+    );
+    drawArc(
+      canvas,
+      size,
+      center,
+      arcOpacity: secondArc.opacity,
+      arcRadius: secondArc.radius,
+      circleOpacity: secondCircleOutline.opacity,
+      circleRadius: secondCircleOutline.radius,
+    );
+    drawArc(
+      canvas,
+      size,
+      center,
+      arcOpacity: thirdArc.opacity,
+      arcRadius: thirdArc.radius,
+      circleOpacity: thirdCircleOutline.opacity,
+      circleRadius: thirdCircleOutline.radius,
+    );
     drawCenterCircle(canvas, size, circle.radius, circle.opacity, center);
   }
 
