65aeeb72cbfd1a617f5a13b54264f6a3fcc13aab
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-16 15:02:22 -0600

    ✨ static string props to maintain consistency across movies

---
+++ packages/client/lib/app/core/widgets/wifi_disconnect_overlay/stack/constants/types/circle_movie_props.dart
@@ -0,0 +1,17 @@
+class CircleMovieProps {
+  static String get circleRadius => "circleRadius";
+  static String get circleOpacity => 'circleOpacity';
+  static String get blur => "blur";
+  static String get arc1Radius => "arc1Radius";
+  static String get circle1Radius => 'circle1Radius';
+  static String get arc2Radius => 'arc2Radius';
+  static String get circle2Radius => 'circle2Radius';
+  static String get arc3Radius => 'arc3Radius';
+  static String get circle3Radius => 'circle3Radius';
+  static String get arc2Opacity => 'arc2Opacity';
+  static String get arc1Opacity => 'arc1Opacity';
+  static String get arc3Opacity => 'arc3Opacity';
+  static String get circle2Opacity => 'circle2Opacity';
+  static String get circle1Opacity => 'circle1Opacity';
+  static String get circle3Opacity => 'circle3Opacity';
+}
