8d4a769978cd04305067e44ccbec0945cffa5f18
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-16 15:05:55 -0600

    ♻️to use static props ✨ border ripple functionality

--- packages/client/lib/app/core/widgets/wifi_disconnect_overlay/stack/constants/movies/wifi_ripple_movie.dart
+++ packages/client/lib/app/core/widgets/wifi_disconnect_overlay/stack/constants/movies/wifi_ripple_movie.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
 
 class WifiRippleMovie {
@@ -9,28 +10,56 @@ class WifiRippleMovie {
       end: Seconds.get(0, milli: 500),
     )
         .tween(
-          'circleRadius',
+          CircleMovieProps.circleRadius,
           Tween<double>(
             begin: 4.0,
             end: 4.0,
           ),
         )
         .tween(
-          'blur',
+          CircleMovieProps.circle1Radius,
+          Tween<double>(
+            begin: 0,
+            end: 15.0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.circle2Radius,
+          Tween<double>(
+            begin: 0,
+            end: 0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.circle3Radius,
+          Tween<double>(
+            begin: 0,
+            end: 0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.blur,
           Tween<double>(
             begin: 10.0,
             end: 10.0,
           ),
         )
         .tween(
-          'arc1Radius',
+          CircleMovieProps.arc1Radius,
           Tween<double>(
             begin: 0.0,
             end: 15.0,
           ),
         )
         .tween(
-          'arc1Opacity',
+          CircleMovieProps.arc1Opacity,
+          Tween<double>(
+            begin: 0.0,
+            end: 1.0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.circle1Opacity,
           Tween<double>(
             begin: 0.0,
             end: 1.0,
@@ -41,28 +70,35 @@ class WifiRippleMovie {
       end: Seconds.get(1),
     )
         .tween(
-          'circleOpacity',
+          CircleMovieProps.circleOpacity,
           Tween<double>(
             begin: 1.0,
             end: 0.25,
           ),
         )
         .tween(
-          'arc1Radius',
+          CircleMovieProps.arc1Radius,
           Tween<double>(
             begin: 15.0,
             end: 30.0,
           ),
         )
         .tween(
-          'arc2Radius',
+          CircleMovieProps.arc2Radius,
           Tween<double>(
             begin: 0.0,
             end: 15.0,
           ),
         )
         .tween(
-          'arc2Opacity',
+          CircleMovieProps.arc2Opacity,
+          Tween<double>(
+            begin: 1.0,
+            end: 1.0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.circle2Opacity,
           Tween<double>(
             begin: 1.0,
             end: 1.0,
@@ -73,68 +109,120 @@ class WifiRippleMovie {
       end: Seconds.get(1, milli: 500),
     )
         .tween(
-          'circleOpacity',
+          CircleMovieProps.circleOpacity,
           Tween<double>(
             begin: 0.25,
             end: 0.0,
           ),
         )
         .tween(
-          'arc1Radius',
+          CircleMovieProps.arc1Radius,
           Tween<double>(
             begin: 30.0,
             end: 45.0,
           ),
         )
         .tween(
-          'arc2Radius',
+          CircleMovieProps.arc2Radius,
           Tween<double>(
             begin: 15.0,
             end: 30.0,
           ),
         )
         .tween(
-          'arc3Radius',
+          CircleMovieProps.arc3Radius,
           Tween<double>(
             begin: 0,
             end: 15.0,
           ),
         )
         .tween(
-          'arc3Opacity',
+          CircleMovieProps.arc3Opacity,
+          Tween<double>(
+            begin: 0.0,
+            end: 1.0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.circle3Opacity,
           Tween<double>(
             begin: 0.0,
             end: 1.0,
           ),
         )
     ..scene(
+      begin: Seconds.get(0, milli: 500),
+      end: Seconds.get(3),
+    )
+        .tween(
+          CircleMovieProps.circle1Radius,
+          Tween<double>(
+            begin: 15.0,
+            end: 120.0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.circle1Opacity,
+          Tween<double>(
+            begin: 1.0,
+            end: 0.0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.arc1Opacity,
+          Tween<double>(
+            begin: 1.0,
+            end: 0.0,
+          ),
+        )
+    ..scene(
+      begin: Seconds.get(1),
+      end: Seconds.get(3, milli: 500),
+    )
+        .tween(
+          CircleMovieProps.circle2Radius,
+          Tween<double>(
+            begin: 15.0,
+            end: 120.0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.circle2Opacity,
+          Tween<double>(
+            begin: 1.0,
+            end: 0.0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.arc2Opacity,
+          Tween<double>(
+            begin: 1.0,
+            end: 0.0,
+          ),
+        )
+    ..scene(
       begin: Seconds.get(1, milli: 500),
-      end: Seconds.get(1, milli: 800),
-    ).tween(
-      'arc1Opacity',
-      Tween<double>(
-        begin: 1.0,
-        end: 0,
-      ),
+      end: Seconds.get(4),
     )
-    ..scene(
-      begin: Seconds.get(1, milli: 800),
-      end: Seconds.get(2, milli: 100),
-    ).tween(
-      'arc2Opacity',
-      Tween<double>(
-        begin: 1.0,
-        end: 0,
-      ),
-    )
-    ..scene(
-      begin: Seconds.get(2, milli: 100),
-      end: Seconds.get(2, milli: 400),
-    ).tween(
-      'arc3Opacity',
-      Tween<double>(
-        begin: 1.0,
-        end: 0,
-      ),
-    );
+        .tween(
+          CircleMovieProps.circle3Radius,
+          Tween<double>(
+            begin: 15.0,
+            end: 120.0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.circle3Opacity,
+          Tween<double>(
+            begin: 1.0,
+            end: 0.0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.arc3Opacity,
+          Tween<double>(
+            begin: 1.0,
+            end: 0.0,
+          ),
+        );
 }
