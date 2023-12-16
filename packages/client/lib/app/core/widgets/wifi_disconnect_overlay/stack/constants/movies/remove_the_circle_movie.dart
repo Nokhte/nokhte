4d3bb1c48af5342437c7130e4d30195a26edc6bb
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-16 15:04:05 -0600

    ✨ remove the circle movie to do proper ripples

--- packages/client/lib/app/core/widgets/wifi_disconnect_overlay/stack/constants/movies/remove_the_circle_movie.dart
+++ packages/client/lib/app/core/widgets/wifi_disconnect_overlay/stack/constants/movies/remove_the_circle_movie.dart
@@ -1,46 +1,137 @@
 import 'package:flutter/material.dart';
 import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
 
 class RemoveTheCircleMovie {
   static MovieTween get movie => MovieTween()
     ..scene(
       begin: Seconds.get(0),
-      end: Seconds.get(1),
+      end: Seconds.get(1, milli: 500),
     )
         .tween(
-          'circleOpacity',
+          CircleMovieProps.circleRadius,
           Tween<double>(
-            begin: 0.0,
-            end: -1,
+            begin: 4.0,
+            end: 15.0,
           ),
         )
         .tween(
-          'arc1Radius',
-          Tween<double>(
-            begin: 45.0,
-            end: -1,
-          ),
-        )
-        .tween(
-          'arc2Radius',
-          Tween<double>(
-            begin: 30.0,
-            end: -1,
-          ),
-        )
-        .tween(
-          'arc3Radius',
-          Tween<double>(
-            begin: 15.0,
-            end: -1,
-          ),
-        )
-        .tween(
-          'arc3Opacity',
+          CircleMovieProps.circleOpacity,
           Tween<double>(
             begin: 1.0,
-            end: -1,
+            end: 1.0,
           ),
-        );
+        )
+        .tween(
+          CircleMovieProps.blur,
+          Tween<double>(
+            begin: 10.0,
+            end: 10.0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.circle1Radius,
+          Tween<double>(
+            begin: 0,
+            end: 0.0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.arc2Radius,
+          Tween<double>(
+            begin: 0,
+            end: 0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.arc2Opacity,
+          Tween<double>(
+            begin: 0,
+            end: 0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.arc3Radius,
+          Tween<double>(
+            begin: 0,
+            end: 0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.arc3Opacity,
+          Tween<double>(
+            begin: 0,
+            end: 0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.circle2Radius,
+          Tween<double>(
+            begin: 0,
+            end: 0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.circle2Opacity,
+          Tween<double>(
+            begin: 0,
+            end: 0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.circle3Opacity,
+          Tween<double>(
+            begin: 0,
+            end: 0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.circle3Radius,
+          Tween<double>(
+            begin: 0,
+            end: 0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.arc1Radius,
+          Tween<double>(
+            begin: 0.0,
+            end: 0.0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.arc1Opacity,
+          Tween<double>(
+            begin: 0.0,
+            end: 0.0,
+          ),
+        )
+        .tween(
+          CircleMovieProps.circle1Opacity,
+          Tween<double>(
+            begin: 0.0,
+            end: 0.0,
+          ),
+        )
+    ..scene(
+      begin: Seconds.get(1, milli: 500),
+      end: Seconds.get(2),
+    ).tween(
+      CircleMovieProps.circleOpacity,
+      Tween<double>(
+        begin: 1.0,
+        end: 0.0,
+      ),
+    )
+    ..scene(
+      begin: Seconds.get(2),
+      end: Seconds.get(2, milli: 500),
+    ).tween(
+      CircleMovieProps.blur,
+      Tween<double>(
+        begin: 10.0,
+        end: 0.0,
+      ),
+    );
 }
