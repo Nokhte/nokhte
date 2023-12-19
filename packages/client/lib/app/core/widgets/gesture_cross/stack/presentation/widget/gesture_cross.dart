ccac7499c284cfeb818b96676cf532bb3d8b9026
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-19 16:52:46 -0600

    3️⃣💄 gesture_cross padding & painter compat updates

--- packages/client/lib/app/core/widgets/gesture_cross/stack/presentation/widget/gesture_cross.dart
+++ packages/client/lib/app/core/widgets/gesture_cross/stack/presentation/widget/gesture_cross.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: library_private_types_in_public_api
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:nokhte/app/core/hooks/hooks.dart';
 import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/gesture_cross/stack/types/colors_and_stops.dart';
 import 'package:nokhte/app/core/widgets/shared/constants/svg_animation_constants.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
@@ -10,7 +12,7 @@ import 'canvas/gesture_cross_painter.dart';
 
 class GestureCross extends StatelessWidget {
   final Size size;
-  final GesturePillStore stateTrackerStore;
+  final GestureCrossStore stateTrackerStore;
   const GestureCross({
     super.key,
     required this.size,
@@ -33,21 +35,46 @@ class GestureCross extends StatelessWidget {
             width: size.width,
             height: size.height,
             child: Padding(
-              padding: const EdgeInsets.only(left: 20.0),
+              padding: const EdgeInsets.only(left: 10.0),
               child: CustomPaint(
                 painter: GestureCrossPainter(
                   SvgAnimtionConstants.crossPath,
                   size,
                   SvgAnimtionConstants.crossPath.getBounds(),
-                  centerCircleOpacity: value.get('center circle opacity'),
-                  firstGradientColor: value.get('first gradient color'),
-                  secondGradientColor: value.get('second gradient color'),
-                  upperCircleLinearGradient: [
-                    value.get('top circle color 1'),
-                    value.get('top circle color 2'),
+                  crossGradient: ColorsAndStops(colors: [
+                    const Color(0xFF0A98FF),
+                    Colors.white.withOpacity(0)
+                  ], stops: const [
+                    0,
+                    .5
+                  ]),
+                  circleInformation: [
+                    //bottom
+                    const CircleInformation(
+                      colorOrGradient: Left(Colors.white),
+                      offset: Offset(0, 12),
+                    ),
+                    // center
+                    const CircleInformation(
+                      colorOrGradient: Left(Colors.white),
+                      offset: Offset(0, 35),
+                    ),
+                    // left
+                    const CircleInformation(
+                      colorOrGradient: Left(Colors.white),
+                      offset: Offset(23, 35),
+                    ),
+                    // right
+                    const CircleInformation(
+                      colorOrGradient: Left(Colors.white),
+                      offset: Offset(-23, 35),
+                    ),
+                    // top
+                    const CircleInformation(
+                      colorOrGradient: Left(Colors.white),
+                      offset: Offset(0, 57),
+                    ),
                   ],
-                  centerCircleAnimationConstant:
-                      value.get('center circle constant'),
                 ),
               ),
             ),
