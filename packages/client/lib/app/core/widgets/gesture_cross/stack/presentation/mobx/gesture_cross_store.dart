e1cce344f591dc855ba9438a54ba99db1fefbe01
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-19 17:59:27 -0600

    3️⃣👔 gesture_cross store logic cleared out + constants

--- packages/client/lib/app/core/widgets/gesture_cross/stack/presentation/mobx/gesture_cross_store.dart
+++ packages/client/lib/app/core/widgets/gesture_cross/stack/presentation/mobx/gesture_cross_store.dart
@@ -1,41 +1,53 @@
-// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api, prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/base_custom_animated_widget_store.dart';
-import 'package:simple_animations/simple_animations.dart';
+import 'package:nokhte/app/core/widgets/shared/constants/svg_animation_constants.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 part 'gesture_cross_store.g.dart';
 
 class GestureCrossStore = _GestureCrossStoreBase with _$GestureCrossStore;
 
 abstract class _GestureCrossStoreBase extends BaseCustomAnimatedWidgetStore
     with Store {
-  MovieTween topCircleAnimationMovie = MovieTween();
+  final Path path = SvgAnimtionConstants.crossPath;
+  final Rect bounds = SvgAnimtionConstants.crossPath.getBounds();
 
   @observable
-  bool wantToFadeOut = true;
-
-  @action
-  setFadeOut(bool newFadeStatus) {
-    wantToFadeOut = newFadeStatus;
-  }
-
-  @action
-  setPillAnimationControl(Control newControl) {
-    pillController = newControl;
-  }
-
-  @action
-  setPillMovie(MovieTween newMovie) {
-    movie = newMovie;
-  }
-
-  @observable
-  Control pillController = Control.stop;
-
-  @action
-  onAnimationCompleted() {
-    if (wantToFadeOut) showWidget = false;
-  }
-
-  @override
-  List<Object> get props => [];
+  ObservableList<CircleInformation> circleInformation = ObservableList.of(
+    [
+      //bottom
+      CircleInformation(
+        colorOrGradient: Left(Colors.white.withOpacity(.25)),
+        offset: CircleOffsets.bottom,
+      ),
+      // center
+      CircleInformation(
+        colorOrGradient: Right(ColorsAndStops(
+          colors: [Colors.white],
+          stops: [0],
+        )),
+        offset: CircleOffsets.center,
+      ),
+      // left
+      CircleInformation(
+        colorOrGradient: Left(Colors.white.withOpacity(.25)),
+        offset: CircleOffsets.left,
+      ),
+      // right
+      CircleInformation(
+        colorOrGradient: Left(Colors.white.withOpacity(.25)),
+        offset: CircleOffsets.right,
+      ),
+      // top
+      CircleInformation(
+        colorOrGradient: Right(ColorsAndStops(
+          colors: [Color(0xFF4CD7FC), Color(0xFF6AEAB9)],
+          stops: [.1, .6],
+        )),
+        offset: CircleOffsets.top,
+      ),
+    ],
+  );
 }
--- packages/client/lib/app/core/widgets/gesture_cross/stack/presentation/mobx/gesture_cross_store.g.dart
+++ packages/client/lib/app/core/widgets/gesture_cross/stack/presentation/mobx/gesture_cross_store.g.dart
@@ -9,90 +9,26 @@ part of 'gesture_cross_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$GestureCrossStore on _GestureCrossStoreBase, Store {
-  late final _$wantToFadeOutAtom =
-      Atom(name: '_GestureCrossStoreBase.wantToFadeOut', context: context);
+  late final _$circleInformationAtom =
+      Atom(name: '_GestureCrossStoreBase.circleInformation', context: context);
 
   @override
-  bool get wantToFadeOut {
-    _$wantToFadeOutAtom.reportRead();
-    return super.wantToFadeOut;
+  ObservableList<CircleInformation> get circleInformation {
+    _$circleInformationAtom.reportRead();
+    return super.circleInformation;
   }
 
   @override
-  set wantToFadeOut(bool value) {
-    _$wantToFadeOutAtom.reportWrite(value, super.wantToFadeOut, () {
-      super.wantToFadeOut = value;
+  set circleInformation(ObservableList<CircleInformation> value) {
+    _$circleInformationAtom.reportWrite(value, super.circleInformation, () {
+      super.circleInformation = value;
     });
-  }
-
-  late final _$pillControllerAtom =
-      Atom(name: '_GestureCrossStoreBase.pillController', context: context);
-
-  @override
-  Control get pillController {
-    _$pillControllerAtom.reportRead();
-    return super.pillController;
-  }
-
-  @override
-  set pillController(Control value) {
-    _$pillControllerAtom.reportWrite(value, super.pillController, () {
-      super.pillController = value;
-    });
-  }
-
-  late final _$_GestureCrossStoreBaseActionController =
-      ActionController(name: '_GestureCrossStoreBase', context: context);
-
-  @override
-  dynamic setFadeOut(bool newFadeStatus) {
-    final _$actionInfo = _$_GestureCrossStoreBaseActionController.startAction(
-        name: '_GestureCrossStoreBase.setFadeOut');
-    try {
-      return super.setFadeOut(newFadeStatus);
-    } finally {
-      _$_GestureCrossStoreBaseActionController.endAction(_$actionInfo);
-    }
-  }
-
-  @override
-  dynamic setPillAnimationControl(Control newControl) {
-    final _$actionInfo = _$_GestureCrossStoreBaseActionController.startAction(
-        name: '_GestureCrossStoreBase.setPillAnimationControl');
-    try {
-      return super.setPillAnimationControl(newControl);
-    } finally {
-      _$_GestureCrossStoreBaseActionController.endAction(_$actionInfo);
-    }
-  }
-
-  @override
-  dynamic setPillMovie(MovieTween newMovie) {
-    final _$actionInfo = _$_GestureCrossStoreBaseActionController.startAction(
-        name: '_GestureCrossStoreBase.setPillMovie');
-    try {
-      return super.setPillMovie(newMovie);
-    } finally {
-      _$_GestureCrossStoreBaseActionController.endAction(_$actionInfo);
-    }
-  }
-
-  @override
-  dynamic onAnimationCompleted() {
-    final _$actionInfo = _$_GestureCrossStoreBaseActionController.startAction(
-        name: '_GestureCrossStoreBase.onAnimationCompleted');
-    try {
-      return super.onAnimationCompleted();
-    } finally {
-      _$_GestureCrossStoreBaseActionController.endAction(_$actionInfo);
-    }
   }
 
   @override
   String toString() {
     return '''
-wantToFadeOut: ${wantToFadeOut},
-pillController: ${pillController}
+circleInformation: ${circleInformation}
     ''';
   }
 }
