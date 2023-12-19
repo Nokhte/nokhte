73ac0d24574b75c4e51ad37e8594f9907d8ffb7b
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-19 16:56:36 -0600

    5️⃣👔 add Home Screen Widgets Coordinator just move logic from constructor over here

---
+++ packages/client/lib/app/modules/home/presentation/mobx/coordinators/home_screen_widgets_coordinator.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/widgets/widget_constants.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+part 'home_screen_widgets_coordinator.g.dart';
+
+class HomeScreenWidgetsCoordinator = _HomeScreenWidgetsCoordinatorBase
+    with _$HomeScreenWidgetsCoordinator;
+
+abstract class _HomeScreenWidgetsCoordinatorBase extends Equatable with Store {
+  final BeachWavesStore beachWaves;
+  final WifiDisconnectOverlayStore wifiDisconnectOverlay;
+  final GestureCrossStore gestureCross;
+  final SmartTextStore smartText;
+
+  _HomeScreenWidgetsCoordinatorBase({
+    required this.beachWaves,
+    required this.wifiDisconnectOverlay,
+    required this.gestureCross,
+    required this.smartText,
+  });
+
+  @action
+  constructor() {
+    beachWaves.setMovieMode(BeachWaveMovieModes.onShore);
+    wifiDisconnectOverlay.connectionReactor(
+      onConnected: () {},
+      onDisconnected: () {},
+    );
+    //
+  }
+
+  @override
+  List<Object> get props => [];
+}
---
+++ packages/client/lib/app/modules/home/presentation/mobx/coordinators/home_screen_widgets_coordinator.g.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_screen_widgets_coordinator.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$HomeScreenWidgetsCoordinator
+    on _HomeScreenWidgetsCoordinatorBase, Store {
+  late final _$_HomeScreenWidgetsCoordinatorBaseActionController =
+      ActionController(
+          name: '_HomeScreenWidgetsCoordinatorBase', context: context);
+
+  @override
+  dynamic constructor() {
+    final _$actionInfo = _$_HomeScreenWidgetsCoordinatorBaseActionController
+        .startAction(name: '_HomeScreenWidgetsCoordinatorBase.constructor');
+    try {
+      return super.constructor();
+    } finally {
+      _$_HomeScreenWidgetsCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+
+    ''';
+  }
+}
