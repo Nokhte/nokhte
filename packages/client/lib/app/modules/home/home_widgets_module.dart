edcbb31a2b2150b285df995b21a6a8a96c823323
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-20 14:17:46 -0600

    1️⃣👔 inject NokhteBlur into home screen widget coordinator

--- packages/client/lib/app/modules/home/home_widgets_module.dart
+++ packages/client/lib/app/modules/home/home_widgets_module.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:nokhte/app/core/widgets/modules.dart';
+import 'package:nokhte/app/core/widgets/nokhte_blur/stack/presentation/presentation.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/home/presentation/mobx/coordinators/home_screen_widgets_coordinator.dart';
 
@@ -21,8 +22,12 @@ class HomeWidgetsModule extends Module {
     i.addSingleton<SpotlightHaloStore>(
       () => SpotlightHaloStore(),
     );
+    i.addSingleton<NokhteBlurStore>(
+      () => NokhteBlurStore(),
+    );
     i.addSingleton<HomeScreenWidgetsCoordinator>(
       () => HomeScreenWidgetsCoordinator(
+        nokhteBlur: i<NokhteBlurStore>(),
         spotlightHalo: i<SpotlightHaloStore>(),
         smartText: i<SmartTextStore>(),
         wifiDisconnectOverlay: i<WifiDisconnectOverlayStore>(),
