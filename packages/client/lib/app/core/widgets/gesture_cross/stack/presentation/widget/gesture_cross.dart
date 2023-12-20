8ebf4d1b1e7b7cc3fb3a17665a432b453184b1a9
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-19 18:06:51 -0600

    2️⃣️☯️ simplify name of store + move painter fields to store 1. mainly for cleanliness purposes

--- packages/client/lib/app/core/widgets/gesture_cross/stack/presentation/widget/gesture_cross.dart
+++ packages/client/lib/app/core/widgets/gesture_cross/stack/presentation/widget/gesture_cross.dart
@@ -1,33 +1,31 @@
 // ignore_for_file: library_private_types_in_public_api
-import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:nokhte/app/core/hooks/hooks.dart';
 import 'package:nokhte/app/core/types/types.dart';
-import 'package:nokhte/app/core/widgets/shared/constants/svg_animation_constants.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
 import 'canvas/gesture_cross_painter.dart';
 
 class GestureCross extends StatelessWidget {
   final Size size;
-  final GestureCrossStore stateTrackerStore;
+  final GestureCrossStore store;
   const GestureCross({
     super.key,
     required this.size,
-    required this.stateTrackerStore,
+    required this.store,
   });
 
   @override
   Widget build(BuildContext context) {
     return Observer(
       builder: (context) => CustomAnimationBuilder(
-        tween: stateTrackerStore.movie,
-        duration: stateTrackerStore.movie.duration,
-        control: stateTrackerStore.pillController,
-        onCompleted: () => stateTrackerStore.onAnimationCompleted(),
+        tween: store.movie,
+        duration: store.movie.duration,
+        control: store.control,
+        onCompleted: () => store.onCompleted(),
         builder: (context, value, child) => AnimatedOpacity(
-          opacity: useWidgetOpacity(stateTrackerStore.showWidget),
+          opacity: useWidgetOpacity(store.showWidget),
           duration: Seconds.get(1),
           child: Container(
             alignment: Alignment.topLeft,
@@ -37,9 +35,9 @@ class GestureCross extends StatelessWidget {
               padding: const EdgeInsets.only(left: 10.0),
               child: CustomPaint(
                 painter: GestureCrossPainter(
-                  SvgAnimtionConstants.crossPath,
+                  store.path,
+                  store.bounds,
                   size,
-                  SvgAnimtionConstants.crossPath.getBounds(),
                   crossGradient: ColorsAndStops(colors: [
                     const Color(0xFF0A98FF),
                     Colors.white.withOpacity(0)
@@ -47,33 +45,7 @@ class GestureCross extends StatelessWidget {
                     0,
                     .5
                   ]),
-                  circleInformation: [
-                    //bottom
-                    const CircleInformation(
-                      colorOrGradient: Left(Colors.white),
-                      offset: Offset(0, 12),
-                    ),
-                    // center
-                    const CircleInformation(
-                      colorOrGradient: Left(Colors.white),
-                      offset: Offset(0, 35),
-                    ),
-                    // left
-                    const CircleInformation(
-                      colorOrGradient: Left(Colors.white),
-                      offset: Offset(23, 35),
-                    ),
-                    // right
-                    const CircleInformation(
-                      colorOrGradient: Left(Colors.white),
-                      offset: Offset(-23, 35),
-                    ),
-                    // top
-                    const CircleInformation(
-                      colorOrGradient: Left(Colors.white),
-                      offset: Offset(0, 57),
-                    ),
-                  ],
+                  circleInformation: store.circleInformation,
                 ),
               ),
             ),
