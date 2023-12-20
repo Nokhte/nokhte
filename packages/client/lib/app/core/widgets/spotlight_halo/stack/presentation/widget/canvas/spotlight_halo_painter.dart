1f5f28f217088f3631f5cdfec0e37759e0ec3fe7
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-20 09:41:46 -0600

    2️⃣️💄 paint a Circle in spotlight_halo_painter

--- packages/client/lib/app/core/widgets/spotlight_halo/stack/presentation/widget/canvas/spotlight_halo_painter.dart
+++ packages/client/lib/app/core/widgets/spotlight_halo/stack/presentation/widget/canvas/spotlight_halo_painter.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 
 class SpotlightHaloPainter extends CustomPainter {
   @override
-  void paint(Canvas canvas, Size size) {}
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+    final circlePaint = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 5.0;
+    canvas.drawCircle(center, 43.0, circlePaint);
+  }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
