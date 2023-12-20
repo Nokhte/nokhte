d93b5383292b6569191782ba1ee2f865e2e5d6a7
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-20 06:30:02 -0600

    1️⃣👔 extend BaseCustomAnimatedWidget in spotlight_halo_store

--- packages/client/lib/app/core/widgets/spotlight_halo/stack/presentation/mobx/spotlight_halo_store.dart
+++ packages/client/lib/app/core/widgets/spotlight_halo/stack/presentation/mobx/spotlight_halo_store.dart
@@ -1,11 +1,12 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
-import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/mobx/base_custom_animated_widget_store.dart';
 part 'spotlight_halo_store.g.dart';
 
 class SpotlightHaloStore = _SpotlightHaloStoreBase with _$SpotlightHaloStore;
 
-abstract class _SpotlightHaloStoreBase extends Equatable with Store {
+abstract class _SpotlightHaloStoreBase extends BaseCustomAnimatedWidgetStore
+    with Store {
   @override
   List<Object> get props => [];
 }
