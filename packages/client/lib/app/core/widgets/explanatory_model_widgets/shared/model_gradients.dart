a997ca9460776eb54e3f8436f640074d60defd97
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-21 08:32:27 -0600

    2️⃣️💬 add ModelGradients for user & collaborator colors

---
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/shared/model_gradients.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ModelGradients {
+  static List<Color> get userGradient =>
+      const [Color(0xFF5A7FFF), Color(0xFF5AFFB0)];
+  static List<Color> get collaboratorGradient =>
+      const [Color(0xFFFF5A5A), Color(0xFFFFE096)];
+}
