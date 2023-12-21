c3a46c2d1b3c4c0d48a404ae2035b72f17b61362
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-21 08:43:35 -0600

    5️⃣💄 add gradient_cirle skeleton implementation

---
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/gradient_circle/stack/presentation/presentation.dart
@@ -0,0 +1,2 @@
+export 'mobx/gradient_circle_store.dart';
+export 'widget/gradient_circle.dart';
---
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/gradient_circle/stack/presentation/widget/canvas/gradient_circle_painter.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class GradientCirclePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {}
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
