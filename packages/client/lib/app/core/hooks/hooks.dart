c681d9222b953a280bf34f802e27e00ecef51f0c
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-21 10:53:26 -0600

    1️⃣🛢️ export use full screen size hook

--- packages/client/lib/app/core/hooks/hooks.dart
+++ packages/client/lib/app/core/hooks/hooks.dart
@@ -1,5 +1,6 @@
 export 'use_square_size.dart';
 export 'use_center_offset.dart';
 export 'use_widget_opacity.dart';
+export 'use_full_screen_size.dart';
 export 'use_screen_size_data.dart';
 export 'use_text_size.dart';
