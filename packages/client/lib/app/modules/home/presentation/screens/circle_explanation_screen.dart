1f92acfaf9250f9e70f39ac5045edb64481199ba
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-22 13:46:04 -0600

    3️⃣💄 add circle_explanation_screen to home module screens

---
+++ packages/client/lib/app/modules/home/presentation/screens/circle_explanation_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:nokhte/app/modules/home/presentation/mobx/coordinators/circle_explanation_coordinator.dart';
+
+class CircleExplanationScreen extends StatelessWidget {
+  final CircleExplanationCoordinator coordinator;
+  const CircleExplanationScreen({
+    super.key,
+    required this.coordinator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
