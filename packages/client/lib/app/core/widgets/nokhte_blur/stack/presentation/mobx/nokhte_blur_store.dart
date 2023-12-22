be26125096175c8bf27167ef0bbdfaa1e98329b3
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-22 13:50:01 -0600

    1️⃣🧮 add movie status setting in nokhte_blur init + reverse methods

--- packages/client/lib/app/core/widgets/nokhte_blur/stack/presentation/mobx/nokhte_blur_store.dart
+++ packages/client/lib/app/core/widgets/nokhte_blur/stack/presentation/mobx/nokhte_blur_store.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/nokhte_blur/stack/stack.dart';
 import 'package:simple_animations/simple_animations.dart';
 part 'nokhte_blur_store.g.dart';
@@ -15,11 +16,13 @@ abstract class _NokhteBlurStoreBase extends BaseCustomAnimatedWidgetStore
 
   @action
   init() {
+    setMovieStatus(MovieStatus.inProgress);
     setControl(Control.playFromStart);
   }
 
   @action
   reverse() {
+    setMovieStatus(MovieStatus.inProgress);
     setControl(Control.playReverseFromEnd);
   }
 }
