7e369cc914945aefcf093929b17e85b5f40f5bd1
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-22 12:06:16 -0600

    1️⃣🎨 rename TrackerStore field to store in Tap

--- packages/client/lib/app/core/widgets/gestures/stack/presentation/widgets/tap.dart
+++ packages/client/lib/app/core/widgets/gestures/stack/presentation/widgets/tap.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:nokhte/app/core/widgets/gestures/stack/presentation/mobx/tap_detector.dart';
 
 class Tap extends StatelessWidget {
-  final TapDetector trackerStore;
+  final TapDetector store;
   final Widget child;
   const Tap({
     super.key,
-    required this.trackerStore,
+    required this.store,
     required this.child,
   });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => trackerStore.onTap(),
-      onTapDown: (details) => trackerStore.onTapDown(details.globalPosition),
+      onTap: () => store.onTap(),
+      onTapDown: (details) => store.onTapDown(details.globalPosition),
       child: child,
     );
   }
