ffe6f8d548a5260b40e807e1f5ffad84771c4e16
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-22 08:22:58 -0600

    1️⃣🛢️ add shared barrel to explanatory_model_widgets barrel

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/explanatory_model_widgets.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/explanatory_model_widgets.dart
@@ -1,5 +1,5 @@
 export 'accompanying_text/accompanying_text.dart';
 export './clock_widgets/clock_widgets.dart';
 export './gradient_circle/gradient_circle.dart';
-export './shared/model_gradients.dart';
+export './shared/shared.dart';
 export 'coordinators/coordinators.dart';
