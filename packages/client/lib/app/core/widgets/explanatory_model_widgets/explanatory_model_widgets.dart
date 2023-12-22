3a8cea4908d8e4f85c35d2b6cc310edc94d9ce61
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-21 19:27:00 -0600

    1️⃣🛢️ add coorindators export in explanatory_model_widgets

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/explanatory_model_widgets.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/explanatory_model_widgets.dart
@@ -2,3 +2,4 @@ export 'accompanying_text/accompanying_text.dart';
 export './clock_widgets/clock_widgets.dart';
 export './gradient_circle/gradient_circle.dart';
 export './shared/model_gradients.dart';
+export 'coordinators/coordinators.dart';
