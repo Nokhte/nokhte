7eb0b063a402294e79fdd471e750bd6b32422904
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-22 14:42:24 -0600

    2️⃣️🧮 add widget stores circle_explanation_widgets_coordinator

--- packages/client/lib/app/modules/home/presentation/mobx/coordinators/widgets/circle_explanation_widgets_coordinator.dart
+++ packages/client/lib/app/modules/home/presentation/mobx/coordinators/widgets/circle_explanation_widgets_coordinator.dart
@@ -1,8 +1,7 @@
-// make a mobx store based on the name of the file
-
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 part 'circle_explanation_widgets_coordinator.g.dart';
 
 class CircleExplanationWidgetsCoordinator = _CircleExplanationWidgetsCoordinatorBase
@@ -10,6 +9,22 @@ class CircleExplanationWidgetsCoordinator = _CircleExplanationWidgetsCoordinator
 
 abstract class _CircleExplanationWidgetsCoordinatorBase extends Equatable
     with Store {
+  final NokhteBlurStore nokhteBlur;
+  final BeachWavesStore beachWaves;
+  final WifiDisconnectOverlayStore wifiDisconnectOverlay;
+  final SmartTextStore smartText;
+  final GestureCrossStore gestureCross;
+  final CircleExplanationModelCoordinator circleModel;
+
+  _CircleExplanationWidgetsCoordinatorBase({
+    required this.nokhteBlur,
+    required this.beachWaves,
+    required this.wifiDisconnectOverlay,
+    required this.smartText,
+    required this.gestureCross,
+    required this.circleModel,
+  });
+
   @override
   List<Object> get props => [];
 }
