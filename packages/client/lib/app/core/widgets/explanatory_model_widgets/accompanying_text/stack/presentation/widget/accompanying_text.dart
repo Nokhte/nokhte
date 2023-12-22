9ab663fb6cbc39e652f19bfb77f5a9c639d58515
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-22 08:24:08 -0600

    3️⃣💄 accompanying_text: add bottom padding + gradient abilities

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/accompanying_text/stack/presentation/widget/accompanying_text.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/accompanying_text/stack/presentation/widget/accompanying_text.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 import 'package:nokhte/app/core/hooks/hooks.dart';
 import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
@@ -7,19 +9,41 @@ import 'package:simple_animations/simple_animations.dart';
 
 class AccompanyingText extends StatelessWidget {
   final AccompanyingTextStore store;
-  const AccompanyingText({
+  final double bottomPadding;
+  final String content;
+  final ModelGradientOptions gradient;
+  const AccompanyingText(
+    this.content, {
     super.key,
     required this.store,
+    required this.gradient,
+    this.bottomPadding = 0.0,
   });
   @override
   Widget build(BuildContext context) => Observer(
-      builder: (context) => CustomAnimationBuilder(
-            tween: store.movie,
-            duration: store.movie.duration,
-            control: store.control,
-            builder: (context, value, child) => AnimatedOpacity(
+        builder: (context) => FullScreen(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedOpacity(
                 opacity: useWidgetOpacity(store.showWidget),
                 duration: Seconds.get(0),
-                child: const Placeholder()),
-          ));
+                child: GradientText(
+                  content,
+                  colors: gradient == ModelGradientOptions.user
+                      ? ModelGradients.userGradient
+                      : ModelGradients.collaboratorGradient,
+                  style: GoogleFonts.jost(fontSize: 23.0),
+                  gradientDirection: GradientDirection.ttb,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  bottom: bottomPadding,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
 }
