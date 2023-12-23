36c37295c268362d612eff2d5acca977235658b0
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-23 17:52:55 -0600

    3️⃣💬 add ClockFaceMovieProperties

---
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/clock_face/stack/constants/types/clock_face_movie_properties.dart
@@ -0,0 +1,33 @@
+class ClockFaceMovieProperties {
+  static String get hourMarkLength => 'hourMarkLength';
+  // 12
+  static String get twelveLineOneRotation => 'twelveLineOneRotation';
+  static String get twelveLineOneTranslation => 'twelveLineOneTranslation';
+  static String get twelveLineTwoRotation => 'twelveLineTwoRotation';
+  static String get twelveLineTwoTranslation => 'twelveLineTwoTranslation';
+  static String get twelveLineThreeRotation => 'twelveLineThreeRotation';
+  static String get twelveLineThreeTranslation => 'twelveLineThreeTranslation';
+  static String get twelveLineFourRotation => 'twelveLineFourRotation';
+  static String get twelveLineFourTranslation => 'twelveLineFourTranslation';
+  // 3
+  static String get threeLineOneRotation => 'twelveLineOneRotation';
+  static String get threeLineOneTranslation => 'twelveLineOneTranslation';
+  static String get threeLineTwoRotation => 'twelveLineTwoRotation';
+  static String get threeLineTwoTranslation => 'twelveLineTwoTranslation';
+  static String get threeLineThreeRotation => 'twelveLineThreeRotation';
+  static String get threeLineThreeTranslation => 'twelveLineThreeTranslation';
+  // 6
+  static String get sixLineOneRotation => 'twelveLineOneRotation';
+  static String get sixLineOneTranslation => 'twelveLineOneTranslation';
+  static String get sixLineTwoRotation => 'twelveLineTwoRotation';
+  static String get sixLineTwoTranslation => 'twelveLineTwoTranslation';
+  static String get sixLineThreeRotation => 'twelveLineThreeRotation';
+  static String get sixLineThreeTranslation => 'twelveLineThreeTranslation';
+  // 9
+  static String get nineLineOneRotation => 'twelveLineOneRotation';
+  static String get nineLineOneTranslation => 'twelveLineOneTranslation';
+  static String get nineLineTwoRotation => 'twelveLineTwoRotation';
+  static String get nineLineTwoTranslation => 'twelveLineTwoTranslation';
+  static String get nineLineThreeRotation => 'twelveLineThreeRotation';
+  static String get nineLineThreeTranslation => 'twelveLineThreeTranslation';
+}
