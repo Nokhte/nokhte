60abc91b19d610b2eb3caba89bf3a557d3d8c063
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-23 11:54:35 -0600

    5️⃣🧮 add circle_explanation_model_coordinator

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/coordinators/circle_explanation_model_coordinator.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/coordinators/circle_explanation_model_coordinator.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 part 'circle_explanation_model_coordinator.g.dart';
 
@@ -20,6 +21,70 @@ abstract class _CircleExplanationModelCoordinatorBase extends Equatable
     required this.userCircle,
     required this.userAccompanyingText,
   });
+
+  @action
+  toggleAllWidgetVisibilities() {
+    collaboratorAccompanyingText.toggleWidgetVisibility();
+    collaboratorCircle.toggleWidgetVisibility();
+    userCircle.toggleWidgetVisibility();
+    userAccompanyingText.toggleWidgetVisibility();
+  }
+
+  @action
+  constructor() {
+    toggleAllWidgetVisibilities();
+    initReactors();
+  }
+
+  @observable
+  bool hasCompletedExplanation = false;
+
+  @action
+  toggleHasCompletedExplanation() =>
+      hasCompletedExplanation = !hasCompletedExplanation;
+
+  @action
+  initExplanation() => userAccompanyingText.toggleWidgetVisibility();
+
+  @action
+  showUserCircle() => userCircle.toggleWidgetVisibility();
+
+  @action
+  initReactors() {
+    userAccompanyingTextReactor();
+    userCircleReactor();
+    collaboratorAccompanyingTextReactor();
+    collaboratorCircleReactor();
+  }
+
+  userAccompanyingTextReactor() =>
+      reaction((p0) => userAccompanyingText.hasFadedIn, (p0) {
+        if (p0) {
+          userCircle.toggleWidgetVisibility();
+        }
+      });
+
+  userCircleReactor() => reaction((p0) => userCircle.hasFadedIn, (p0) {
+        if (p0) {
+          collaboratorAccompanyingText.toggleWidgetVisibility();
+        }
+      });
+  collaboratorAccompanyingTextReactor() =>
+      reaction((p0) => collaboratorAccompanyingText.hasFadedIn, (p0) {
+        if (p0) {
+          collaboratorCircle.toggleWidgetVisibility();
+        }
+      });
+
+  collaboratorCircleReactor() =>
+      reaction((p0) => collaboratorCircle.hasFadedIn, (p0) {
+        if (p0) {
+          Future.delayed(Seconds.get(2), () {
+            toggleAllWidgetVisibilities();
+            toggleHasCompletedExplanation();
+          });
+        }
+      });
 
   @override
   List<Object> get props => [];
--- packages/client/lib/app/core/widgets/explanatory_model_widgets/coordinators/circle_explanation_model_coordinator.g.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/coordinators/circle_explanation_model_coordinator.g.dart
@@ -10,10 +10,112 @@ part of 'circle_explanation_model_coordinator.dart';
 
 mixin _$CircleExplanationModelCoordinator
     on _CircleExplanationModelCoordinatorBase, Store {
+  late final _$hasCompletedExplanationAtom = Atom(
+      name: '_CircleExplanationModelCoordinatorBase.hasCompletedExplanation',
+      context: context);
+
+  @override
+  bool get hasCompletedExplanation {
+    _$hasCompletedExplanationAtom.reportRead();
+    return super.hasCompletedExplanation;
+  }
+
+  @override
+  set hasCompletedExplanation(bool value) {
+    _$hasCompletedExplanationAtom
+        .reportWrite(value, super.hasCompletedExplanation, () {
+      super.hasCompletedExplanation = value;
+    });
+  }
+
+  late final _$_CircleExplanationModelCoordinatorBaseActionController =
+      ActionController(
+          name: '_CircleExplanationModelCoordinatorBase', context: context);
+
+  @override
+  dynamic toggleAllWidgetVisibilities() {
+    final _$actionInfo =
+        _$_CircleExplanationModelCoordinatorBaseActionController.startAction(
+            name:
+                '_CircleExplanationModelCoordinatorBase.toggleAllWidgetVisibilities');
+    try {
+      return super.toggleAllWidgetVisibilities();
+    } finally {
+      _$_CircleExplanationModelCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic constructor() {
+    final _$actionInfo =
+        _$_CircleExplanationModelCoordinatorBaseActionController.startAction(
+            name: '_CircleExplanationModelCoordinatorBase.constructor');
+    try {
+      return super.constructor();
+    } finally {
+      _$_CircleExplanationModelCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic toggleHasCompletedExplanation() {
+    final _$actionInfo =
+        _$_CircleExplanationModelCoordinatorBaseActionController.startAction(
+            name:
+                '_CircleExplanationModelCoordinatorBase.toggleHasCompletedExplanation');
+    try {
+      return super.toggleHasCompletedExplanation();
+    } finally {
+      _$_CircleExplanationModelCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic initExplanation() {
+    final _$actionInfo =
+        _$_CircleExplanationModelCoordinatorBaseActionController.startAction(
+            name: '_CircleExplanationModelCoordinatorBase.initExplanation');
+    try {
+      return super.initExplanation();
+    } finally {
+      _$_CircleExplanationModelCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic showUserCircle() {
+    final _$actionInfo =
+        _$_CircleExplanationModelCoordinatorBaseActionController.startAction(
+            name: '_CircleExplanationModelCoordinatorBase.showUserCircle');
+    try {
+      return super.showUserCircle();
+    } finally {
+      _$_CircleExplanationModelCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic initReactors() {
+    final _$actionInfo =
+        _$_CircleExplanationModelCoordinatorBaseActionController.startAction(
+            name: '_CircleExplanationModelCoordinatorBase.initReactors');
+    try {
+      return super.initReactors();
+    } finally {
+      _$_CircleExplanationModelCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+hasCompletedExplanation: ${hasCompletedExplanation}
     ''';
   }
 }
