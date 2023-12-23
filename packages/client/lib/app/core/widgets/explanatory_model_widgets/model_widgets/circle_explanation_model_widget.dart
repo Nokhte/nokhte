2e45bdd6362b5a57e07e34711f445b1a517a4a97
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-23 11:59:23 -0600

    2️⃣️💄 add circle_explanation_model_widget implementation

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/model_widgets/circle_explanation_model_widget.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/model_widgets/circle_explanation_model_widget.dart
@@ -2,14 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 
 class CircleExplanationModelWidget extends StatelessWidget {
-  final CircleExplanationModelCoordinator coordinator;
+  final CircleExplanationModelCoordinator store;
   const CircleExplanationModelWidget({
     super.key,
-    required this.coordinator,
+    required this.store,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FullScreen(
+      child: Stack(
+        children: [
+          AccompanyingText(
+            "You:",
+            store: store.userAccompanyingText,
+            gradient: ModelGradientOptions.user,
+            bottomPadding: 625.0,
+          ),
+          GradientCircle(
+            store: store.userCircle,
+            gradient: ModelGradientOptions.user,
+            bottomPadding: 500.0,
+          ),
+          AccompanyingText(
+            "Collaborator:",
+            store: store.collaboratorAccompanyingText,
+            gradient: ModelGradientOptions.collaborator,
+            bottomPadding: 300.0,
+          ),
+          GradientCircle(
+            store: store.collaboratorCircle,
+            gradient: ModelGradientOptions.collaborator,
+            bottomPadding: 175.0,
+          )
+        ],
+      ),
+    );
   }
 }
