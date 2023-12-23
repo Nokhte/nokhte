f5bee99ec4df91fcade46f1cb869e40b9fb0f6ae
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-23 11:55:22 -0600

    3️⃣✅ add circle_explanation_model_coordinator test

---
+++ packages/client/test/app/core/widgets/explanatory_model_widgets/circle_explanation_model_coordinator_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import './explanatory_model_widgets_mock_gen.mocks.dart';
+
+void main() {
+  late MockGradientCircleStore userCircle;
+  late MockGradientCircleStore collaboratorCircle;
+  late MockAccompanyingTextStore userText;
+  late MockAccompanyingTextStore collaboratorText;
+  late CircleExplanationModelCoordinator testStore;
+
+  setUp(() {
+    userCircle = MockGradientCircleStore();
+    collaboratorCircle = MockGradientCircleStore();
+    userText = MockAccompanyingTextStore();
+    collaboratorText = MockAccompanyingTextStore();
+    testStore = CircleExplanationModelCoordinator(
+        collaboratorCircle: collaboratorCircle,
+        collaboratorAccompanyingText: collaboratorText,
+        userCircle: userCircle,
+        userAccompanyingText: userText);
+  });
+
+  group("initial values", () {
+    test("hasCompletedExplanation", () {
+      expect(testStore.hasCompletedExplanation, false);
+    });
+  });
+
+  group("actions", () {
+    test("toggleAllWidgetVisibilites", () {
+      testStore.toggleAllWidgetVisibilities();
+      expect(userText.showWidget, false);
+      expect(collaboratorText.showWidget, false);
+      expect(userCircle.showWidget, false);
+      expect(collaboratorCircle.showWidget, false);
+    });
+
+    test("toggleHasCompletedExplanation", () {
+      testStore.toggleHasCompletedExplanation();
+      expect(testStore.hasCompletedExplanation, true);
+    });
+  });
+}
