4521ac11fc23e9b24f35a018118ba91ff1af4a11
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-23 15:37:00 -0600

    5️⃣🔥 remove problematic app state reactor + test (fails in production)

--- packages/client/lib/app/core/mobx/base_coordinator.dart
+++ packages/client/lib/app/core/mobx/base_coordinator.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
-import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:equatable/equatable.dart';
 part 'base_coordinator.g.dart';
@@ -7,27 +6,6 @@ part 'base_coordinator.g.dart';
 class BaseCoordinator = _BaseCoordinatorBase with _$BaseCoordinator;
 
 abstract class _BaseCoordinatorBase extends Equatable with Store {
-  @observable
-  AppLifecycleState appState = AppLifecycleState.resumed;
-
-  foregroundAndBackgroundStateReactor({
-    required Function resumedCallback,
-    required Function inactiveCallback,
-    required Function detachedCallback,
-  }) =>
-      reaction((p0) => appState, (p0) async {
-        switch (p0) {
-          case AppLifecycleState.resumed:
-            await resumedCallback();
-          case AppLifecycleState.inactive:
-            await inactiveCallback();
-          case AppLifecycleState.detached:
-            await detachedCallback();
-          default:
-            break;
-        }
-      });
-
   @observable
   bool disableAllTouchFeedback = false;
 
@@ -38,9 +16,6 @@ abstract class _BaseCoordinatorBase extends Equatable with Store {
   ifTouchIsNotDisabled(Function callback) {
     if (!disableAllTouchFeedback) callback();
   }
-
-  @action
-  setAppState(AppLifecycleState newState) => appState = newState;
 
   @override
   List<Object> get props => [];
--- packages/client/test/app/core/mobx/base_coordinator_test.dart
+++ packages/client/test/app/core/mobx/base_coordinator_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nokhte/app/core/mobx/base_coordinator.dart';
 
@@ -11,21 +10,12 @@ void main() {
     test("disableAllTouchFeedback", () {
       expect(testStore.disableAllTouchFeedback, false);
     });
-
-    test("appState", () {
-      expect(testStore.appState, AppLifecycleState.resumed);
-    });
   });
 
   group("actions", () {
     test("toggleDisableAllTouchFeedback", () {
       testStore.toggleDisableAllTouchFeedback();
       expect(testStore.disableAllTouchFeedback, true);
-    });
-
-    test("setAppState", () {
-      testStore.setAppState(AppLifecycleState.detached);
-      expect(testStore.appState, AppLifecycleState.detached);
     });
   });
 }
