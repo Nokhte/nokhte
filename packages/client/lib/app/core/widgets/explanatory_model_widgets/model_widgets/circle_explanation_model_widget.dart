ab15266dfd01585f3e8036ebaa723f9d77371d5b
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-23 06:57:47 -0600

    2️⃣️💄 add base model_widgets impl

---
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/model_widgets/circle_explanation_model_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+
+class CircleExplanationModelWidget extends StatelessWidget {
+  final CircleExplanationModelCoordinator coordinator;
+  const CircleExplanationModelWidget({
+    super.key,
+    required this.coordinator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
