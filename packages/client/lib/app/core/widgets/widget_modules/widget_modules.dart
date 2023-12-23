16f9aad18f130a32345fbd2f276c2f6f9c58515b
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-23 07:00:28 -0600

    1️⃣🚚 export time_alignment_module

--- packages/client/lib/app/core/widgets/widget_modules/widget_modules.dart
+++ packages/client/lib/app/core/widgets/widget_modules/widget_modules.dart
@@ -2,4 +2,4 @@ export 'beach_waves_module.dart';
 export 'circle_explanation_module.dart';
 export 'gestures_module.dart';
 export 'wifi_disconnect_overlay_module.dart';
-export './clock_model_module.dart';
+export 'time_alignment_model_module.dart';
