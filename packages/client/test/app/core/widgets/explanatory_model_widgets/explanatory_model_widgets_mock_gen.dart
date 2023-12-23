e7a7ae02eca0c1ddac6a0365c038fd3cfe2a4570
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-23 11:53:29 -0600

    5️⃣🤡 explanatory_model_widget mocks

---
+++ packages/client/test/app/core/widgets/explanatory_model_widgets/explanatory_model_widgets_mock_gen.dart
@@ -0,0 +1,8 @@
+import 'package:mockito/annotations.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+
+@GenerateNiceMocks([
+  MockSpec<GradientCircleStore>(),
+  MockSpec<AccompanyingTextStore>(),
+])
+void main() {}
