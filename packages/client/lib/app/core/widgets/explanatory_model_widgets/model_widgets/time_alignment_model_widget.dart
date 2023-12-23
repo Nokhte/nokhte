3f4792ed590901d214b5a532422b983aea33b812
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-23 07:17:39 -0600

    1️⃣💄 add store as dep to time_alignment_model_widget

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/model_widgets/time_alignment_model_widget.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/model_widgets/time_alignment_model_widget.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 
 class TimeAlignmentModelWidget extends StatelessWidget {
-  const TimeAlignmentModelWidget({super.key});
+  final TimeAlignmentModelCoordinator store;
+  const TimeAlignmentModelWidget({
+    super.key,
+    required this.store,
+  });
 
   @override
   Widget build(BuildContext context) {
