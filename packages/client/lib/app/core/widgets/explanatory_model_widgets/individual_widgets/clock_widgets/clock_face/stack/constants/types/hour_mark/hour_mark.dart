377347acbe8efba941c4009ad5d1a9ed0949c4a2
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-24 11:44:19 -0600

    5️⃣🛢️ update hour mark type barrel

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/clock_face/stack/constants/types/hour_mark/hour_mark.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/clock_face/stack/constants/types/hour_mark/hour_mark.dart
@@ -1,4 +1,4 @@
-export 'nine_hour_mark_properties.dart';
-export 'six_hour_mark_properties.dart';
-export 'three_hour_mark_properties.dart';
+export 'nine_properties.dart';
+export 'six_properties.dart';
+export 'three_properties.dart';
 export 'twelve_properties.dart';
