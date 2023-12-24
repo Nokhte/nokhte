341d82f6c66390a2d37536e89d9b3446f35357fe
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-23 18:00:59 -0600

    2️⃣️🧮 set the movie in the clockface store

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/clock_face/stack/presentation/mobx/clock_face_store.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/clock_face/stack/presentation/mobx/clock_face_store.dart
@@ -1,9 +1,19 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/base_custom_animated_widget_store.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
 part 'clock_face_store.g.dart';
 
 class ClockFaceStore = _ClockFaceStoreBase with _$ClockFaceStore;
 
 abstract class _ClockFaceStoreBase extends BaseCustomAnimatedWidgetStore
-    with Store {}
+    with Store {
+  _ClockFaceStoreBase() {
+    setMovie(SetUpTheClockfaceMovie.movie);
+    Future.delayed(Seconds.get(2), () {
+      setControl(Control.playFromStart);
+    });
+  }
+}
