f34c2738e5088e965440758392e1d8326ff2e957
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-26 06:43:06 -0600

    1️⃣🧮 set the movie in availability_sector_store constructor

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/presentation/mobx/availability_sectors_store.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/presentation/mobx/availability_sectors_store.dart
@@ -1,10 +1,15 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/base_custom_animated_widget_store.dart';
+import 'package:nokhte/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/constants/movies/movies.dart';
 part 'availability_sectors_store.g.dart';
 
 class AvailabilitySectorsStore = _AvailabilitySectorsStoreBase
     with _$AvailabilitySectorsStore;
 
 abstract class _AvailabilitySectorsStoreBase
-    extends BaseCustomAnimatedWidgetStore with Store {}
+    extends BaseCustomAnimatedWidgetStore with Store {
+  _AvailabilitySectorsStoreBase() {
+    setMovie(SetupAvailabilitySectorsMovies.movie);
+  }
+}
