631cad39f7afc722575d418c2861733870ecd21f
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-26 20:03:24 -0600

    2️⃣️🧮 add init move to clock face store

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/clock_face/stack/presentation/mobx/clock_face_store.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/clock_face/stack/presentation/mobx/clock_face_store.dart
@@ -1,14 +1,23 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/mobx/base_custom_animated_widget_store.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
 part 'clock_face_store.g.dart';
 
 class ClockFaceStore = _ClockFaceStoreBase with _$ClockFaceStore;
 
-abstract class _ClockFaceStoreBase extends BaseCustomAnimatedWidgetStore
-    with Store {
+abstract class _ClockFaceStoreBase
+    extends BaseCustomAnimatedWidgetStore<NoParams> with Store {
   _ClockFaceStoreBase() {
     setMovie(SetUpTheClockfaceMovie.movie);
+  }
+
+  @override
+  initMovie(param) {
+    setMovieStatus(MovieStatus.inProgress);
+    setControl(Control.playFromStart);
   }
 }
