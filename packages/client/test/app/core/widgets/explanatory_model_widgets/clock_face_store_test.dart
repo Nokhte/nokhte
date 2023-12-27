dbcd36f8251619ae22614ecddd7551047bfb7b29
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-26 20:03:35 -0600

    2️⃣️✅ add test for init movie on clock_face_store_test

--- packages/client/test/app/core/widgets/explanatory_model_widgets/clock_face_store_test.dart
+++ packages/client/test/app/core/widgets/explanatory_model_widgets/clock_face_store_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 void main() {
   late ClockFaceStore testStore;
@@ -12,6 +14,14 @@ void main() {
   group("initial values", () {
     test("movie", () {
       expect(testStore.movie.duration, Seconds.get(3));
+    });
+  });
+
+  group("actions", () {
+    test('initMovie', () {
+      testStore.initMovie(NoParams());
+      expect(testStore.control, Control.playFromStart);
+      expect(testStore.movieStatus, MovieStatus.inProgress);
     });
   });
 }
