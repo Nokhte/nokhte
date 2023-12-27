c256b91ed7b2c028b7b2dd3e139bff75cfe7d783
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-27 07:11:33 -0600

    1️⃣💬 add success gradient to model gradients

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/shared/model_gradients.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/shared/model_gradients.dart
@@ -5,4 +5,8 @@ class ModelGradients {
       const [Color(0xFF5A7FFF), Color(0xFF5AFFB0)];
   static List<Color> get collaboratorGradient =>
       const [Color(0xFFFF5A5A), Color(0xFFFFE096)];
+  static List<Color> get successGradient => const [
+        Color(0xFF37E05C),
+        Color(0xFF16FFCA),
+      ];
 }
