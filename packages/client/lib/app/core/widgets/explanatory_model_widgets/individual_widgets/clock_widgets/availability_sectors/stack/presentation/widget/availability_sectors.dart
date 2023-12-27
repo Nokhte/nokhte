88594b7a812183fbaf1acdd89851140bf6e43790
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-27 07:35:30 -0600

    2️⃣️🚚 use SectorAnimationUtils for painter params instead of literals

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/presentation/widget/availability_sectors.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/presentation/widget/availability_sectors.dart
@@ -16,115 +16,32 @@ class AvailabilitySectors extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Observer(
-      builder: (context) => AnimatedOpacity(
-            opacity: useWidgetOpacity(store.showWidget),
-            duration: Seconds.get(0, milli: 500),
-            child: CustomAnimationBuilder(
-              tween: store.movie,
-              duration: store.movie.duration,
-              control: store.control,
-              builder: (context, value, child) => FullScreen(
-                child: CustomPaint(
-                  painter: AvailabilitySectorsPainter(
-                    redSectorLengths: [
-                      value.get(
-                          AvailabilitySectorMovieProperties.redSector1Length),
-                      value.get(
-                          AvailabilitySectorMovieProperties.redSector2Length),
-                      value.get(
-                          AvailabilitySectorMovieProperties.redSector3Length),
-                    ],
-                    redSectorGradients: [
-                      [
-                        value.get(
-                          AvailabilitySectorMovieProperties.redSector1Color1,
-                        ),
-                        value.get(
-                          AvailabilitySectorMovieProperties.redSector1Color2,
-                        )
-                      ],
-                      [
-                        value.get(
-                          AvailabilitySectorMovieProperties.redSector2Color1,
-                        ),
-                        value.get(
-                          AvailabilitySectorMovieProperties.redSector2Color2,
-                        ),
-                      ],
-                      [
-                        value.get(
-                          AvailabilitySectorMovieProperties.redSector3Color1,
-                        ),
-                        value.get(
-                          AvailabilitySectorMovieProperties.redSector3Color2,
-                        )
-                      ]
-                    ],
-                    redSectorRadii: [
-                      value.get(
-                        AvailabilitySectorMovieProperties
-                            .redSector1RadiusScalingFactor,
-                      ),
-                      value.get(
-                        AvailabilitySectorMovieProperties
-                            .redSector2RadiusScalingFactor,
-                      ),
-                      value.get(
-                        AvailabilitySectorMovieProperties
-                            .redSector3RadiusScalingFactor,
-                      ),
-                    ],
-                    blueSectorLengths: [
-                      value.get(
-                          AvailabilitySectorMovieProperties.blueSector1Length),
-                      value.get(
-                          AvailabilitySectorMovieProperties.blueSector2Length),
-                      value.get(
-                          AvailabilitySectorMovieProperties.blueSector3Length),
-                    ],
-                    blueSectorGradients: [
-                      [
-                        value.get(
-                          AvailabilitySectorMovieProperties.blueSector1Color1,
-                        ),
-                        value.get(
-                          AvailabilitySectorMovieProperties.blueSector1Color2,
-                        )
-                      ],
-                      [
-                        value.get(
-                          AvailabilitySectorMovieProperties.blueSector2Color1,
-                        ),
-                        value.get(
-                          AvailabilitySectorMovieProperties.blueSector2Color2,
-                        ),
-                      ],
-                      [
-                        value.get(
-                          AvailabilitySectorMovieProperties.blueSector3Color1,
-                        ),
-                        value.get(
-                          AvailabilitySectorMovieProperties.blueSector3Color2,
-                        )
-                      ]
-                    ],
-                    blueSectorRadii: [
-                      value.get(
-                        AvailabilitySectorMovieProperties
-                            .blueSector1RadiusScalingFactor,
-                      ),
-                      value.get(
-                        AvailabilitySectorMovieProperties
-                            .blueSector2RadiusScalingFactor,
-                      ),
-                      value.get(
-                        AvailabilitySectorMovieProperties
-                            .blueSector3RadiusScalingFactor,
-                      ),
-                    ],
+        builder: (context) => AnimatedOpacity(
+          opacity: useWidgetOpacity(store.showWidget),
+          duration: Seconds.get(0, milli: 500),
+          child: CustomAnimationBuilder(
+            tween: store.movie,
+            duration: store.movie.duration,
+            control: store.control,
+            builder: (context, value, child) => FullScreen(
+              child: CustomPaint(
+                painter: AvailabilitySectorsPainter(
+                  redSectorLengths: SectorAnimationUtils.getRedSectorLengths(
+                    value,
                   ),
+                  redSectorGradients:
+                      SectorAnimationUtils.getRedSectorGradients(value),
+                  redSectorRadii: SectorAnimationUtils.getRedSectorRadii(value),
+                  blueSectorLengths:
+                      SectorAnimationUtils.getBlueSectorLengths(value),
+                  blueSectorGradients:
+                      SectorAnimationUtils.getBlueSectorGradients(value),
+                  blueSectorRadii:
+                      SectorAnimationUtils.getBlueSectorRadii(value),
                 ),
               ),
             ),
-          ));
+          ),
+        ),
+      );
 }
