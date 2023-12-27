d4cbae9ae868d1025de9fcd0d6f9b3b1747f1e78
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-27 07:15:54 -0600

    1️⃣🛢️ update import in availability_sectors_store

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/presentation/mobx/availability_sectors_store.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/presentation/mobx/availability_sectors_store.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/base_custom_animated_widget_store.dart';
-import 'package:nokhte/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/constants/movies/movies.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 part 'availability_sectors_store.g.dart';
 
 class AvailabilitySectorsStore = _AvailabilitySectorsStoreBase
