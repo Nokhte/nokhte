1407f99f155ebd69591bb30c2fdedb6ed130d667
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-27 07:14:20 -0600

    4️⃣💫 accompany new painter complexity in setup sectors movie

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/constants/movies/setup_availability_sectors_movie.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/constants/movies/setup_availability_sectors_movie.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
 
 class SetupAvailabilitySectorsMovies {
@@ -8,11 +9,183 @@ class SetupAvailabilitySectorsMovies {
     ..scene(
       begin: Seconds.get(0),
       end: Seconds.get(3),
+    )
+        .tween(
+          AvailabilitySectorMovieProperties.blueSector1Length,
+          Tween<double>(
+            begin: pi,
+            end: 3.4,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.blueSector2Length,
+          Tween<double>(
+            begin: pi,
+            end: 3.4,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.blueSector3Length,
+          Tween<double>(
+            begin: pi,
+            end: 3.4,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.redSector1Length,
+          Tween<double>(
+            begin: pi,
+            end: 3.4,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.redSector2Length,
+          Tween<double>(
+            begin: pi,
+            end: 3.4,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.redSector3Length,
+          Tween<double>(
+            begin: pi,
+            end: 3.4,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.blueSector1Color1,
+          ColorTween(
+            begin: ModelGradients.userGradient.first,
+            end: ModelGradients.userGradient.first,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.blueSector1Color2,
+          ColorTween(
+            begin: ModelGradients.userGradient[1],
+            end: ModelGradients.userGradient[1],
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.blueSector2Color1,
+          ColorTween(
+            begin: ModelGradients.userGradient.first,
+            end: ModelGradients.userGradient.first,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.blueSector2Color2,
+          ColorTween(
+            begin: ModelGradients.userGradient[1],
+            end: ModelGradients.userGradient[1],
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.blueSector3Color1,
+          ColorTween(
+            begin: ModelGradients.userGradient.first,
+            end: ModelGradients.userGradient.first,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.blueSector3Color2,
+          ColorTween(
+            begin: ModelGradients.userGradient[1],
+            end: ModelGradients.userGradient[1],
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.redSector1Color1,
+          ColorTween(
+            begin: ModelGradients.collaboratorGradient.first,
+            end: ModelGradients.collaboratorGradient.first,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.redSector1Color2,
+          ColorTween(
+            begin: ModelGradients.collaboratorGradient[1],
+            end: ModelGradients.collaboratorGradient[1],
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.redSector2Color1,
+          ColorTween(
+            begin: ModelGradients.collaboratorGradient.first,
+            end: ModelGradients.collaboratorGradient.first,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.redSector2Color2,
+          ColorTween(
+            begin: ModelGradients.collaboratorGradient[1],
+            end: ModelGradients.collaboratorGradient[1],
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.redSector3Color1,
+          ColorTween(
+            begin: ModelGradients.collaboratorGradient.first,
+            end: ModelGradients.collaboratorGradient.first,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.redSector3Color2,
+          ColorTween(
+            begin: ModelGradients.collaboratorGradient[1],
+            end: ModelGradients.collaboratorGradient[1],
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.blueSector1RadiusScalingFactor,
+          Tween<double>(
+            begin: 1,
+            end: 1,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.blueSector2RadiusScalingFactor,
+          Tween<double>(
+            begin: 1,
+            end: 1,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.blueSector3RadiusScalingFactor,
+          Tween<double>(
+            begin: 1,
+            end: 1,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.redSector1RadiusScalingFactor,
+          Tween<double>(
+            begin: 1,
+            end: 1,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.redSector2RadiusScalingFactor,
+          Tween<double>(
+            begin: 1,
+            end: 1,
+          ),
+        )
+        .tween(
+          AvailabilitySectorMovieProperties.redSector3RadiusScalingFactor,
+          Tween<double>(
+            begin: 1,
+            end: 1,
+          ),
+        )
+    ..scene(
+      begin: Seconds.get(3),
+      end: Seconds.get(4),
     ).tween(
-      "sectorLength",
+      AvailabilitySectorMovieProperties.blueSector2RadiusScalingFactor,
       Tween<double>(
-        begin: pi,
-        end: 3.4,
+        begin: 1,
+        end: .97,
       ),
     );
 }
