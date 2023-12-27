2e98e178808fcdc6e3a9fe724110b157fb9976b9
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-27 13:26:06 -0600

    2️⃣️🧮 add init movie + init join & fade out in availability_sectors_store

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/presentation/mobx/availability_sectors_store.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/presentation/mobx/availability_sectors_store.dart
@@ -1,15 +1,43 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/mobx/base_custom_animated_widget_store.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
 part 'availability_sectors_store.g.dart';
 
 class AvailabilitySectorsStore = _AvailabilitySectorsStoreBase
     with _$AvailabilitySectorsStore;
 
 abstract class _AvailabilitySectorsStoreBase
-    extends BaseCustomAnimatedWidgetStore with Store {
+    extends BaseCustomAnimatedWidgetStore<NoParams> with Store {
   _AvailabilitySectorsStoreBase() {
     setMovie(SetupAvailabilitySectorsMovies.movie);
+  }
+
+  @observable
+  AvailabilitySectorMovieModes movieMode = AvailabilitySectorMovieModes.initial;
+
+  @action
+  setMovieMode(AvailabilitySectorMovieModes newMovieMode) =>
+      movieMode = newMovieMode;
+
+  @override
+  initMovie(param) {
+    setMovieStatus(MovieStatus.inProgress);
+    setControl(Control.play);
+    setMovieMode(AvailabilitySectorMovieModes.setup);
+  }
+
+  @action
+  initJoinAndFadeOutMovie() {
+    if (movieStatus == MovieStatus.finished &&
+        movieMode == AvailabilitySectorMovieModes.setup) {
+      setMovieStatus(MovieStatus.inProgress);
+      setMovieMode(AvailabilitySectorMovieModes.joinAndFadeOut);
+      setMovie(JoinSectorsAndFadeOutMovie.movie);
+      setControl(Control.playFromStart);
+    }
   }
 }
