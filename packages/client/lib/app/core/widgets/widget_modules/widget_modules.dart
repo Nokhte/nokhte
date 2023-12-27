4d14ce870661bc5fb3489b4320476dc854a99dc2
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-27 13:30:54 -0600

    1️⃣🛢️ export smart_text_module from widget_modules

--- packages/client/lib/app/core/widgets/widget_modules/widget_modules.dart
+++ packages/client/lib/app/core/widgets/widget_modules/widget_modules.dart
@@ -3,3 +3,4 @@ export 'circle_explanation_module.dart';
 export 'gestures_module.dart';
 export 'wifi_disconnect_overlay_module.dart';
 export 'time_alignment_model_module.dart';
+export './smart_text_module.dart';
