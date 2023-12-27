ad172d8b249e4eadeacb2a335d49a4da0da0a45f
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-27 13:26:59 -0600

    2️⃣️✅ availability_sectors_store_test

--- packages/client/test/app/core/widgets/explanatory_model_widgets/availability_sectors_store_test.dart
+++ packages/client/test/app/core/widgets/explanatory_model_widgets/availability_sectors_store_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 void main() {
   late AvailabilitySectorsStore testStore;
@@ -10,6 +12,33 @@ void main() {
   group("initial values", () {
     test("movie", () {
       expect(testStore.movie.duration, Seconds.get(3));
+    });
+
+    test("movieMode", () {
+      expect(testStore.movieMode, AvailabilitySectorMovieModes.initial);
+    });
+  });
+
+  group("actions", () {
+    test("setMovieMode", () {
+      testStore.setMovieMode(AvailabilitySectorMovieModes.joinAndFadeOut);
+      expect(testStore.movieMode, AvailabilitySectorMovieModes.joinAndFadeOut);
+    });
+
+    test("initMovie", () {
+      testStore.initMovie(NoParams());
+      expect(testStore.control, Control.play);
+      expect(testStore.movieMode, AvailabilitySectorMovieModes.setup);
+    });
+
+    test("initJoinAndFadeOutMovie", () {
+      testStore.setMovieStatus(MovieStatus.finished);
+      testStore.setMovieMode(AvailabilitySectorMovieModes.setup);
+      testStore.initJoinAndFadeOutMovie();
+      expect(testStore.movieStatus, MovieStatus.inProgress);
+      expect(testStore.movieMode, AvailabilitySectorMovieModes.joinAndFadeOut);
+      expect(testStore.movie.duration, Seconds.get(4));
+      expect(testStore.control, Control.playFromStart);
     });
   });
 }
