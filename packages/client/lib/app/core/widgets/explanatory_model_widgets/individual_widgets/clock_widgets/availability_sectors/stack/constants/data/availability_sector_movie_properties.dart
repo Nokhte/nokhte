411530900454dc33978afa3bd8682da825a467e7
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-27 07:11:59 -0600

    3️⃣💬 add availability_sector_movie_properties this is mainly to accopmany complexity & minimize typo errors w/ a single source of truth for movie property names

---
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/constants/data/availability_sector_movie_properties.dart
@@ -0,0 +1,41 @@
+class AvailabilitySectorMovieProperties {
+  static String get blueSector1Length => "blueSector1Length";
+  static String get blueSector2Length => "blueSector2Length";
+  static String get blueSector3Length => "blueSector3Length";
+
+  static String get blueSector1RadiusScalingFactor =>
+      "blueSector1RadiusScalingFactor";
+  static String get blueSector2RadiusScalingFactor =>
+      "blueSector2RadiusScalingFactor";
+  static String get blueSector3RadiusScalingFactor =>
+      "blueSector3RadiusScalingFactor";
+
+  static String get blueSector1Color1 => 'blueSector1Color1';
+  static String get blueSector1Color2 => 'blueSector1Color2';
+
+  static String get blueSector2Color1 => 'blueSector2Color1';
+  static String get blueSector2Color2 => 'blueSector2Color2';
+
+  static String get blueSector3Color1 => 'blueSector3Color1';
+  static String get blueSector3Color2 => 'blueSector3Color2';
+
+  static String get redSector1Length => "redSector1Length";
+  static String get redSector2Length => "redSector2Length";
+  static String get redSector3Length => "redSector3Length";
+
+  static String get redSector1RadiusScalingFactor =>
+      "redSector1RadiusScalingFactor";
+  static String get redSector2RadiusScalingFactor =>
+      "redSector2RadiusScalingFactor";
+  static String get redSector3RadiusScalingFactor =>
+      "redSector3RadiusScalingFactor";
+
+  static String get redSector1Color1 => 'redSector1Color1';
+  static String get redSector1Color2 => 'redSector1Color2';
+
+  static String get redSector2Color1 => 'redSector2Color1';
+  static String get redSector2Color2 => 'redSector2Color2';
+
+  static String get redSector3Color1 => 'redSector3Color1';
+  static String get redSector3Color2 => 'redSector3Color2';
+}
