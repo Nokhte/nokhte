3ca2ec1113ca284038b00c98771fc0ce6ca5006f
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-26 20:23:05 -0600

    4️⃣🧮 home screen widgets coordinator overhaul overhaul includes     1. remove circleModel     2. add secondaryText proofs

--- packages/client/lib/app/modules/home/presentation/mobx/coordinators/home_screen_widgets_coordinator.dart
+++ packages/client/lib/app/modules/home/presentation/mobx/coordinators/home_screen_widgets_coordinator.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widget_constants.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 part 'home_screen_widgets_coordinator.g.dart';
@@ -10,31 +11,31 @@ class HomeScreenWidgetsCoordinator = _HomeScreenWidgetsCoordinatorBase
 
 abstract class _HomeScreenWidgetsCoordinatorBase extends Equatable with Store {
   final TimeAlignmentModelCoordinator timeModel;
-  final CircleExplanationModelCoordinator circleModel;
   final NokhteBlurStore nokhteBlur;
   final BeachWavesStore beachWaves;
   final WifiDisconnectOverlayStore wifiDisconnectOverlay;
   final GestureCrossStore gestureCross;
-  final SmartTextStore smartText;
+  final SmartTextStore primarySmartText;
+  final SmartTextStore secondarySmartText;
 
   _HomeScreenWidgetsCoordinatorBase({
     required this.timeModel,
-    required this.circleModel,
     required this.nokhteBlur,
     required this.beachWaves,
     required this.wifiDisconnectOverlay,
     required this.gestureCross,
-    required this.smartText,
+    required this.primarySmartText,
+    required this.secondarySmartText,
   });
 
   @action
   constructor() {
-    smartText.reset();
-    if (!smartText.showWidget) smartText.toggleWidgetVisibility();
-    smartText.setMessagesData(MessagesData.firstTimeHomeList);
-    smartText.startRotatingText();
+    primarySmartText.reset();
+    if (!primarySmartText.showWidget) primarySmartText.toggleWidgetVisibility();
+    primarySmartText.setMessagesData(MessagesData.firstTimeHomeList);
+    secondarySmartText.setMessagesData(MessagesData.firstTimeSecondaryHomeList);
+    primarySmartText.startRotatingText();
     beachWaves.setMovieMode(BeachWaveMovieModes.onShore);
-    circleModel.constructor();
     initReactors();
   }
 
@@ -49,10 +50,19 @@ abstract class _HomeScreenWidgetsCoordinatorBase extends Equatable with Store {
   }
 
   @observable
-  bool hasInitiatedBlur = false;
+  bool clockIsVisible = false;
 
   @observable
   bool isDisconnected = false;
+
+  @observable
+  bool hasInitiatedBlur = false;
+
+  @observable
+  bool secondaryTextIsInProgress = false;
+
+  @action
+  toggleClockIsVisible() => clockIsVisible = !clockIsVisible;
 
   @action
   toggleIsDisconnected() => isDisconnected = !isDisconnected;
@@ -60,28 +70,57 @@ abstract class _HomeScreenWidgetsCoordinatorBase extends Equatable with Store {
   @action
   toggleHasInitiatedBlur() => hasInitiatedBlur = !hasInitiatedBlur;
 
+  @action
+  toggleSecondaryTextIsInProgress() =>
+      secondaryTextIsInProgress = !secondaryTextIsInProgress;
+
   initReactors() {
-    smartTextReactor();
+    primarySmartTextReactor();
+    secondarySmartTextReactor();
     gestureCrossTapReactor();
     wifiDisconnectOverlay.connectionReactor(
       onConnected: onConnected,
       onDisconnected: onDisconnected,
     );
+    clockFaceAnimationStatusReactor();
   }
 
   gestureCrossTapReactor() => reaction((p0) => gestureCross.tapCount, (p0) {
         if (!isDisconnected) {
           if (!hasInitiatedBlur) {
             nokhteBlur.init();
-            smartText.startRotatingText(isResuming: true);
+            primarySmartText.startRotatingText(isResuming: true);
             toggleHasInitiatedBlur();
+          } else if (clockIsVisible && !secondaryTextIsInProgress) {
+            toggleSecondaryTextIsInProgress();
+            secondarySmartText.startRotatingText(isResuming: true);
           }
         }
       });
 
-  smartTextReactor() => reaction((p0) => smartText.currentIndex, (p0) {
+  primarySmartTextReactor() =>
+      reaction((p0) => primarySmartText.currentIndex, (p0) {
         if (p0 == 3) {
           timeModel.init();
+        }
+      });
+  secondarySmartTextReactor() =>
+      reaction((p0) => secondarySmartText.currentIndex, (p0) {
+        if (!secondaryTextIsInProgress) {
+          toggleSecondaryTextIsInProgress();
+        }
+        toggleSecondaryTextIsInProgress();
+        if (p0 == 2) {
+          secondarySmartText.reset();
+          secondarySmartText.startRotatingText();
+        }
+      });
+
+  clockFaceAnimationStatusReactor() =>
+      reaction((p0) => timeModel.clockFace.movieStatus, (p0) {
+        if (p0 == MovieStatus.finished) {
+          secondarySmartText.startRotatingText();
+          toggleClockIsVisible();
         }
       });
 
