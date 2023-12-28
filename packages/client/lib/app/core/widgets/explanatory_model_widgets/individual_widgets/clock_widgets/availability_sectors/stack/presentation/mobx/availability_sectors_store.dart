30c599cd29fc9c963bbdf3f0b90117af11f7567c
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-27 18:11:45 -0600

    2️⃣️🧮 add connectivity into availability_sectors_store mainly b/c the reaction version of this didn't really work so we had to find an alternative

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/presentation/mobx/availability_sectors_store.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/individual_widgets/clock_widgets/availability_sectors/stack/presentation/mobx/availability_sectors_store.dart
@@ -2,6 +2,7 @@
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/mobx/base_custom_animated_widget_store.dart';
+import 'package:nokhte/app/core/modules/connectivity/mobx/get_on_connectivity_changed_store.dart';
 import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
@@ -12,9 +13,15 @@ class AvailabilitySectorsStore = _AvailabilitySectorsStoreBase
 
 abstract class _AvailabilitySectorsStoreBase
     extends BaseCustomAnimatedWidgetStore<NoParams> with Store {
-  _AvailabilitySectorsStoreBase() {
+  final GetOnConnectivityChangedStore connectivity;
+  _AvailabilitySectorsStoreBase({
+    required this.connectivity,
+  }) {
     setMovie(SetupAvailabilitySectorsMovies.movie);
   }
+
+  @observable
+  bool isDisconnected = false;
 
   @observable
   AvailabilitySectorMovieModes movieMode = AvailabilitySectorMovieModes.initial;
@@ -32,12 +39,14 @@ abstract class _AvailabilitySectorsStoreBase
 
   @action
   initJoinAndFadeOutMovie() {
-    if (movieStatus == MovieStatus.finished &&
-        movieMode == AvailabilitySectorMovieModes.setup) {
-      setMovieStatus(MovieStatus.inProgress);
-      setMovieMode(AvailabilitySectorMovieModes.joinAndFadeOut);
-      setMovie(JoinSectorsAndFadeOutMovie.movie);
-      setControl(Control.playFromStart);
+    if (connectivity.isConnected) {
+      if (movieStatus == MovieStatus.finished &&
+          movieMode == AvailabilitySectorMovieModes.setup) {
+        setMovieStatus(MovieStatus.inProgress);
+        setMovieMode(AvailabilitySectorMovieModes.joinAndFadeOut);
+        setMovie(JoinSectorsAndFadeOutMovie.movie);
+        setControl(Control.playFromStart);
+      }
     }
   }
 }
