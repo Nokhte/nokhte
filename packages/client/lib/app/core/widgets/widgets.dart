1a8841fb912a7bd1bb23d9bee591cbb4ad696d10
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-31 07:07:05 -0600

    1️⃣🛢️ add gradient_tree_node to widgets exports

--- packages/client/lib/app/core/widgets/widgets.dart
+++ packages/client/lib/app/core/widgets/widgets.dart
@@ -4,6 +4,7 @@ export 'full_screen/full_screen.dart';
 export 'gesture_cross/gesture_cross.dart';
 export 'gesture_pill/gesture_pill.dart';
 export 'gestures/gestures.dart';
+export 'gradient_tree_node/gradient_tree_node.dart';
 export 'multi_hit_stack/multi_hit_stack.dart';
 export 'nokhte/nokhte.dart';
 export 'nokhte_blur/nokhte_blur.dart';
