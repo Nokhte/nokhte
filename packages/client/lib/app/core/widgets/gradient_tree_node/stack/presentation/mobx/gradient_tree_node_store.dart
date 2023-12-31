0c26fa9a241676da708e5e6fd3c17a30777504fc
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-31 06:20:43 -0600

    2️⃣️🧮 add gradient_tree_node_store

---
+++ packages/client/lib/app/core/widgets/gradient_tree_node/stack/presentation/mobx/gradient_tree_node_store.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
+part 'gradient_tree_node_store.g.dart';
+
+class GradientTreeNodeStore = _GradientTreeNodeStoreBase
+    with _$GradientTreeNodeStore;
+
+abstract class _GradientTreeNodeStoreBase extends BaseCustomAnimatedWidgetStore
+    with Store {}
