c56481c1efbddbdf481b30bee696ea0e663fb19c
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-31 07:00:15 -0600

    3️⃣💄 add gradient_tree_node_painter & tap functionality

---
+++ packages/client/lib/app/core/widgets/gradient_tree_node/stack/presentation/widget/canvas/gradient_tree_node_painter.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:nokhte/app/core/extensions/extensions.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:touchable/touchable.dart';
+
+class GradientTreeNodePainter extends CustomPainter {
+  final NokhteGradient gradient;
+  final BuildContext context;
+  final Function onTap;
+
+  GradientTreeNodePainter({
+    required this.gradient,
+    required this.context,
+    required this.onTap,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final touchableCanvas = TouchyCanvas(context, canvas);
+    final center = Offset(size.width.half(), size.height.half());
+
+    const double radius = 25.0;
+
+    final circleRect = Rect.fromCircle(center: center, radius: radius);
+
+    final circleStrokePaint = Paint()
+      ..color = Colors.white
+      ..strokeWidth = 2.0
+      ..style = PaintingStyle.stroke;
+
+    final gradientPaint = Paint()
+      ..shader = LinearGradient(
+        colors: gradient.colors,
+        stops: gradient.stops,
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+      ).createShader(circleRect);
+
+    touchableCanvas.drawCircle(
+      center,
+      radius,
+      gradientPaint,
+      onTapDown: (details) => onTap(),
+    );
+
+    touchableCanvas.drawCircle(
+      center,
+      radius,
+      circleStrokePaint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
