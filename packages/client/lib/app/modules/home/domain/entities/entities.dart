2a7ce1c93cf518f447aa216585ea826712f500c1
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-31 19:29:35 -0600

    1️⃣🛢️ home module entities exports

--- packages/client/lib/app/modules/home/domain/entities/entities.dart
+++ packages/client/lib/app/modules/home/domain/entities/entities.dart
@@ -1,3 +1,4 @@
+export 'collaboration_invitation_send_status_entity.dart';
 export 'collaborator_phrase_entity.dart';
 export 'name_creation_status_entity.dart';
 export 'existing_collaborations_info_entity.dart';
