e1f721af277324d682c14867665d41977b46438b
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-01 07:06:03 -0600

    1️⃣🛢️ update home model exports

--- packages/client/lib/app/modules/home/data/models/models.dart
+++ packages/client/lib/app/modules/home/data/models/models.dart
@@ -1,3 +1,4 @@
 export 'collaborator_phrase_model.dart';
 export 'name_creation_status_model.dart';
 export 'existing_collaborations_info_model.dart';
+export 'collaboration_invitation_send_status_model.dart';
