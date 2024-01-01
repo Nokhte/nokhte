e2ee1ff2bb26ab83d815166d095a656991571f3f
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-01 07:09:59 -0600

    1️⃣🛢️ main mobx store exports

--- packages/client/lib/app/modules/home/presentation/mobx/main/main.dart
+++ packages/client/lib/app/modules/home/presentation/mobx/main/main.dart
@@ -1,3 +1,4 @@
 export 'add_name_to_database_store.dart';
 export 'get_collaborator_phrase_store.dart';
 export 'get_existing_collaborations_info_store.dart';
+export 'share_collaboration_invitation_store.dart';
