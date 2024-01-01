e74314c2eb53e9872947dfeee83bf425b7fa5e81
Sonny Vesali <sonny.vesali@protonmail.com>
2023-12-31 19:29:09 -0600

    1️⃣🧮 add collaboration_invitation_send_status_entity (home module)

---
+++ packages/client/lib/app/modules/home/domain/entities/collaboration_invitation_send_status_entity.dart
@@ -0,0 +1,7 @@
+import 'package:nokhte/app/core/entities/default_db_status_entity.dart';
+
+class CollaborationInvitationSendStatusEntity extends DefaultDBStatusEntity {
+  const CollaborationInvitationSendStatusEntity({
+    required super.isSent,
+  });
+}
