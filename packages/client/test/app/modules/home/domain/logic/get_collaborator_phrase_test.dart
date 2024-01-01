07109acf46b7c7544651e74f4f34fc0f0a1f87a4
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-01 07:12:04 -0600

    3️⃣✅ get_collaborator_phrase_test

--- packages/client/test/app/modules/home/domain/logic/get_collaborator_phrase_test.dart
+++ packages/client/test/app/modules/home/domain/logic/get_collaborator_phrase_test.dart
@@ -2,15 +2,17 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:nokhte/app/core/constants/failure_constants.dart';
+import 'package:nokhte/app/core/error/failure.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/modules/home/domain/entities/entities.dart';
 import 'package:nokhte/app/modules/home/domain/logic/logic.dart';
-
-import '../../constants/entities/entities.dart';
 import '../../fixtures/home_stack_mock_gen.mocks.dart';
 
 void main() {
   late MockHomeContract mockContract;
   late GetCollaboratorPhrase logic;
+  const Either<Failure, CollaboratorPhraseEntity> tSuccess =
+      Right(CollaboratorPhraseEntity(collaboratorPhrase: "test test"));
 
   setUp(() {
     mockContract = MockHomeContract();
@@ -18,13 +20,12 @@ void main() {
   });
 
   test("✅ should pass the Status Entity from Contract ==> Logic", () async {
-    when(mockContract.getCollaboratorPhrase(NoParams())).thenAnswer(
-      (_) async => ConstantCollaboratorPhraseEntities.wrappedSuccessCase,
-    );
+    when(mockContract.getCollaboratorPhrase(NoParams()))
+        .thenAnswer((_) async => tSuccess);
 
     final result = await logic(NoParams());
 
-    expect(result, ConstantCollaboratorPhraseEntities.wrappedSuccessCase);
+    expect(result, tSuccess);
     verify(mockContract.getCollaboratorPhrase(NoParams()));
     verifyNoMoreInteractions(mockContract);
   });
