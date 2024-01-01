b04defe26adb1fd4e26670b63771cac8258c9d24
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-01 07:06:43 -0600

    2️⃣️🧮 add share_collaboration_invitation logic method

---
+++ packages/client/lib/app/modules/home/domain/logic/share_collaboration_invitation.dart
@@ -0,0 +1,15 @@
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/modules/home/domain/domain.dart';
+
+class ShareCollaborationInvitation
+    implements
+        AbstractFutureLogic<CollaborationInvitationSendStatusEntity, NoParams> {
+  final HomeContract contract;
+
+  ShareCollaborationInvitation({
+    required this.contract,
+  });
+
+  @override
+  call(params) async => await contract.shareCollaborationInvitation(params);
+}
