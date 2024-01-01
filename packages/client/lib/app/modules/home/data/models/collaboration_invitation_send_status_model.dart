0681da167d79a68c06cd01ab82e580cbea100f2c
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-01 07:04:41 -0600

    2️⃣️🧮 add collaboration_invitation_send_status_model

---
+++ packages/client/lib/app/modules/home/data/models/collaboration_invitation_send_status_model.dart
@@ -0,0 +1,18 @@
+import 'package:nokhte/app/modules/home/domain/domain.dart';
+import 'package:share_plus/share_plus.dart';
+
+class CollaborationInvitationSendStatusModel
+    extends CollaborationInvitationSendStatusEntity {
+  const CollaborationInvitationSendStatusModel({
+    required super.isSent,
+  });
+
+  factory CollaborationInvitationSendStatusModel.fromShareResult(
+      ShareResult res) {
+    if (res.status == ShareResultStatus.success) {
+      return const CollaborationInvitationSendStatusModel(isSent: true);
+    } else {
+      return const CollaborationInvitationSendStatusModel(isSent: false);
+    }
+  }
+}
