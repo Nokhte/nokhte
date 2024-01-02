78ef9ce2d918d62e99c24e7f771d65e8ecf48740
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-02 13:25:45 -0600

    2️⃣️🏷️ add collaboration_invitation_data_entity

---
+++ packages/client/lib/app/modules/home/domain/entities/collaboration_invitation_data_entity.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+
+class CollaborationInvitationDataEntity extends Equatable {
+  final String link;
+
+  const CollaborationInvitationDataEntity({
+    required this.link,
+  });
+
+  @override
+  List<Object> get props => [];
+}
