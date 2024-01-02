3f46d8c9db32f8754599aa8e8ff701f4b3486ee6
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-02 16:42:06 -0600

    2️⃣️🗃️ add update RLS on user_names table

--- packages/backend/supabase/migrations/20240102211534_user_names_additions.sql
+++ packages/backend/supabase/migrations/20240102211534_user_names_additions.sql
@@ -1,3 +1,11 @@
 alter table "public"."user_names" add column "has_gone_through_invitation_flow" boolean not null default false;
 
 alter table "public"."user_names" add column "has_sent_an_invitation" boolean not null default false;
+
+create policy "UPDATE: Users can update their own row"
+on "public"."user_names"
+as permissive
+for update
+to authenticated
+using ((uid = auth.uid()))
+with check (true);
