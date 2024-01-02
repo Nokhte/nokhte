9e6dbc4ceb765a455b6da22f873de6d99aa2a96b
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-02 13:46:03 -0600

    2️⃣️🏷️ add collaboration_invitation_data_model

---
+++ packages/client/lib/app/modules/home/data/models/collaboration_invitation_data_model.dart
@@ -0,0 +1,8 @@
+import 'package:nokhte/app/modules/home/domain/domain.dart';
+
+class CollaborationInvitationDataModel
+    extends CollaborationInvitationDataEntity {
+  const CollaborationInvitationDataModel({
+    required super.link,
+  });
+}
