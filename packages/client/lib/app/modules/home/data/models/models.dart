17605c62fd208e03642ccd0c2bd7255e7a0a8998
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-02 13:48:05 -0600

    1️⃣🛢️ home models: A-Z re-order + collaboration_invitation_model added

--- packages/client/lib/app/modules/home/data/models/models.dart
+++ packages/client/lib/app/modules/home/data/models/models.dart
@@ -1,4 +1,5 @@
-export 'collaborator_phrase_model.dart';
-export 'name_creation_status_model.dart';
-export 'existing_collaborations_info_model.dart';
+export 'collaboration_invitation_data_model.dart';
 export 'collaboration_invitation_send_status_model.dart';
+export 'collaborator_phrase_model.dart';
+export 'existing_collaborations_info_model.dart';
+export 'name_creation_status_model.dart';
