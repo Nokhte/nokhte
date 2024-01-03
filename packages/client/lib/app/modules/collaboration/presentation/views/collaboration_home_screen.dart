e39a1e2c0324083e4522cf106841c9da1a1be968
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-03 17:44:52 -0600

    1️⃣🧱 add base collaboration_home_screen impl

---
+++ packages/client/lib/app/modules/collaboration/presentation/views/collaboration_home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CollaborationHomeScreen extends StatelessWidget {
+  const CollaborationHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
