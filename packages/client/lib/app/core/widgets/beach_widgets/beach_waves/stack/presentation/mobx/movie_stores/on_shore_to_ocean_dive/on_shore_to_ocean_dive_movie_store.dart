6a84b952b58806cba8cfc3398f9beea21af5e147
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-03 17:45:59 -0600

    2️⃣🔥 un-needed scenes in on_shore_to_ocean_dive_movie

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/on_shore_to_ocean_dive/on_shore_to_ocean_dive_movie_store.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/on_shore_to_ocean_dive/on_shore_to_ocean_dive_movie_store.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
 part 'on_shore_to_ocean_dive_movie_store.g.dart';
@@ -21,6 +22,7 @@ abstract class _OnShoreToOceanDiveMovieStoreBase
   @override
   @action
   initMovie(double startingWaterPosition) {
+    setMovieStatus(MovieStatus.inProgress);
     movie = OnShoreToOceanDiveMovie.getOceanDiveMovie(
         startingWaterMovement: startingWaterPosition);
     control = Control.playFromStart;
