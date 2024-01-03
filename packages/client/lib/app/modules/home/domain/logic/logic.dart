214aed1a4499c1556951ca39a36a31d212fd0830
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-03 06:15:21 -0600

    1️⃣🛢️ add home logic exports

--- packages/client/lib/app/modules/home/domain/logic/logic.dart
+++ packages/client/lib/app/modules/home/domain/logic/logic.dart
@@ -2,4 +2,7 @@ export 'add_name_to_database.dart';
 export 'get_collaborator_phrase.dart';
 export 'get_existing_collaborations_info.dart';
 export 'get_invitation_url.dart';
+export 'get_user_info.dart';
 export 'share_collaboration_invitation.dart';
+export 'update_has_gone_through_invitation_flow.dart';
+export 'update_has_sent_an_invitation.dart';
