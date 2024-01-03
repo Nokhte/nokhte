4323e18bad8c6cbe80f9e696f89fe29f6100055c
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-03 06:28:05 -0600

    3️⃣👔 add userInfo methods to contract implementation 1. getUserInfo 2. updateHasGoneThroughInvitationFlow 3. updateHasSentAnInvitation

--- packages/client/lib/app/modules/home/data/contracts/home_contract_impl.dart
+++ packages/client/lib/app/modules/home/data/contracts/home_contract_impl.dart
@@ -90,4 +90,41 @@ class HomeContractImpl implements HomeContract {
       return Left(FailureConstants.internetConnectionFailure);
     }
   }
+
+  @override
+  Future<Either<Failure, UserJourneyInfoModel>> getUserInfo(
+      NoParams params) async {
+    if (await networkInfo.isConnected) {
+      final res = await remoteSource.getUserInfo();
+      return Right(UserJourneyInfoModel.fromSupabase(res));
+    } else {
+      return Left(FailureConstants.internetConnectionFailure);
+    }
+  }
+
+  @override
+  Future<Either<Failure, HasGoneThroughInvitationFlowUpdateStatusModel>>
+      updateHasGoneThroughInvitationFlow(
+          bool hasGoneThroughInvitationFlowParam) async {
+    if (await networkInfo.isConnected) {
+      final res = await remoteSource.updateHasGoneThroughInvitationFlow(
+          hasGoneThroughInvitationFlowParam);
+      return Right(
+          HasGoneThroughInvitationFlowUpdateStatusModel.fromSupabase(res));
+    } else {
+      return Left(FailureConstants.internetConnectionFailure);
+    }
+  }
+
+  @override
+  Future<Either<Failure, HasSentAnInvitationUpdateStatusModel>>
+      updateHasSentAnInvitation(bool hasSentAnInvitationParam) async {
+    if (await networkInfo.isConnected) {
+      final res = await remoteSource
+          .updateHasSentAnInvitation(hasSentAnInvitationParam);
+      return Right(HasSentAnInvitationUpdateStatusModel.fromSupabase(res));
+    } else {
+      return Left(FailureConstants.internetConnectionFailure);
+    }
+  }
 }
