3561f6ec7390c32da23b68dfd05f242f148ae9eb
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-03 17:45:06 -0600

    1️⃣🧮 add equals in int extension

--- packages/client/lib/app/core/extensions/int/int_comparison.dart
+++ packages/client/lib/app/core/extensions/int/int_comparison.dart
@@ -3,4 +3,5 @@ extension IntComparisons on int {
   bool isGreaterThan(int value) => this > value;
   bool isLessThanOrEqualTo(int value) => this <= value;
   bool isGreaterThanOrEqualTo(int value) => this >= value;
+  bool equals(int value) => this == value;
 }
