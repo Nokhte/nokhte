68242e33b5e1bb05c6d8c65227120d4f6a39870e
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-03 06:14:19 -0600

    2️⃣️👔 add update_has_gone_through_invitation_flow

---
+++ packages/client/lib/app/modules/home/domain/logic/update_has_gone_through_invitation_flow.dart
@@ -0,0 +1,15 @@
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app//modules/home/domain/domain.dart';
+
+class UpdateHasGoneThroughInvitationFlow
+    implements
+        AbstractFutureLogic<HasGoneThroughInvitationFlowUpdateStatusEntity,
+            bool> {
+  final HomeContract contract;
+
+  UpdateHasGoneThroughInvitationFlow({required this.contract});
+
+  @override
+  call(params) async =>
+      await contract.updateHasGoneThroughInvitationFlow(params);
+}
