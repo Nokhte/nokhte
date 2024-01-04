80841ac6302268f0c34595e19685ba351b70c444
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-04 15:06:12 -0600

    5️⃣🔥 remove un-needed complexity in stroke_cross_nokhte

--- packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/stroke_cross_nokhte/stack/presentation/widget/canvas/solid_color_cross_nokhte_painter.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class SolidColorCrossPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {}
-
-  @override
-  bool shouldRepaint(SolidColorCrossPainter oldDelegate) => true;
-}
