ad333d4c4ae3cc82790d0ee7b97c9d389cb6622a
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-04 17:46:43 -0600

    1️⃣🛢️ gesture_cross types exports

--- packages/client/lib/app/core/widgets/gesture_cross/gesture_cross/stack/types/types.dart
+++ packages/client/lib/app/core/widgets/gesture_cross/gesture_cross/stack/types/types.dart
@@ -1,2 +1,5 @@
-export 'gesture_cross_configuration.dart';
-export 'gesture_cross_nokhte_types.dart';
+export './gesture_cross_configuration.dart';
+export './gesture_cross_nokhte_types.dart';
+export './nokhte_gradient_config.dart';
+export './nokhte_gradient_types.dart';
+export './stroke_config.dart';
