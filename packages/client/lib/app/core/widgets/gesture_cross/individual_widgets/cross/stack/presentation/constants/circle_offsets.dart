c1de8ff46565d9e7a3b949802b8e608c7712bcb1
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-04 10:56:22 -0600

    2️⃣️💬 add CircleOffsets constants & remove old one

--- packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/cross/stack/presentation/constants/circle_offsets.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CircleOffsets {
-  static Offset get bottom => const Offset(0, 12);
-  static Offset get center => const Offset(0, 35);
-  static Offset get left => const Offset(23, 35);
-  static Offset get right => const Offset(-23, 35);
-  static Offset get top => const Offset(0, 57);
-}
