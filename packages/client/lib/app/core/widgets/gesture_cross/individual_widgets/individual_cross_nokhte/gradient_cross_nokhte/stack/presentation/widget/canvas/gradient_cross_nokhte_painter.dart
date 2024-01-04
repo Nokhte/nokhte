62272d1c0fc95a8a4e44e5d05a311b07c14adab0
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-04 10:54:00 -0600

    3️⃣💄 add gradient_cross_nokhte_painter

---
+++ packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/gradient_cross_nokhte/stack/presentation/widget/canvas/gradient_cross_nokhte_painter.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:nokhte/app/core/extensions/extensions.dart';
+
+class GradientCrossNokhtePainter extends CustomPainter {
+  final Size canvasSize;
+  final BuildContext context;
+  final List<Offset> offsets;
+  final List<List<Color>> gradients;
+  final List<List<double>> stops;
+
+  GradientCrossNokhtePainter({
+    required this.canvasSize,
+    required this.context,
+    required this.offsets,
+    required this.gradients,
+    required this.stops,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < offsets.length; i++) {
+      const radius = 4.50;
+      final center = Offset(
+        size.width.half() - offsets[i].dx,
+        size.height.half() - offsets[i].dy,
+      );
+      final Rect circleBounds = Rect.fromCircle(center: center, radius: radius);
+      final paint = Paint()
+        ..shader = LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: gradients[i],
+          stops: stops[i],
+        ).createShader(circleBounds);
+      canvas.drawCircle(center, radius, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(GradientCrossNokhtePainter oldDelegate) => true;
+}
