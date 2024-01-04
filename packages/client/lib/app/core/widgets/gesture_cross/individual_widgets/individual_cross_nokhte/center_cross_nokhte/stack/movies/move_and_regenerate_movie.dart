62161510d86ba612eb27df0d416ab9b6a7aa61e3
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-04 14:22:05 -0600

    5️⃣💫 add center_cross_nokhte movies

---
+++ packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/center_cross_nokhte/stack/movies/move_and_regenerate_movie.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+class MoveAndRegengerateMovie {
+  static MovieTween getMovie(Offset endDirection) => MovieTween()
+    ..scene(
+      begin: Seconds.get(0),
+      end: Seconds.get(1),
+    )
+        .tween(
+            'dx1',
+            Tween<double>(
+              begin: CircleOffsets.center.dx,
+              end: endDirection.dx,
+            ))
+        .tween(
+            'dy1',
+            Tween<double>(
+              begin: CircleOffsets.center.dy,
+              end: endDirection.dy,
+            ))
+        .tween(
+          'radii1',
+          Tween<double>(
+            begin: 4.5,
+            end: 4.5,
+          ),
+        )
+        .tween(
+            'dx2',
+            Tween<double>(
+              begin: CircleOffsets.center.dx,
+              end: CircleOffsets.center.dx,
+            ))
+        .tween(
+            'dy2',
+            Tween<double>(
+              begin: CircleOffsets.center.dy,
+              end: CircleOffsets.center.dy,
+            ))
+    ..scene(
+      begin: Seconds.get(1),
+      end: Seconds.get(2),
+    ).tween(
+      'radii2',
+      Tween<double>(
+        begin: 0,
+        end: 4.5,
+      ),
+    );
+}
---
+++ packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/center_cross_nokhte/stack/movies/movies.dart
@@ -0,0 +1,2 @@
+export 'move_and_regenerate_movie.dart';
+export 'resting_center_cross_movie.dart';
