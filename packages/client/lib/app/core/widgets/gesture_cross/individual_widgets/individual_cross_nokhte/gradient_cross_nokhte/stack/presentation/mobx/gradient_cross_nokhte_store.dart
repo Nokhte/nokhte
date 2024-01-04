5b1d82012def5ecbc32f89110044729a5f60c9a5
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-04 09:24:52 -0600

    1️⃣🧮gradientCrossNokhteStore: extend base animated widget store

--- packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/gradient_cross_nokhte/stack/presentation/mobx/gradient_cross_nokhte_store.dart
+++ packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/gradient_cross_nokhte/stack/presentation/mobx/gradient_cross_nokhte_store.dart
@@ -1,12 +1,10 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
-import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
 part 'gradient_cross_nokhte_store.g.dart';
 
 class GradientCrossNokhteStore = _GradientCrossNokhteStoreBase
     with _$GradientCrossNokhteStore;
 
-abstract class _GradientCrossNokhteStoreBase extends Equatable with Store {
-  @override
-  List<Object> get props => [];
-}
+abstract class _GradientCrossNokhteStoreBase
+    extends BaseCustomAnimatedWidgetStore with Store {}
