53b76cc3b39f5c257c19148d223207797a671747
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-04 14:18:47 -0600

    2️⃣️💄 add center_cross_nokhte_painter

--- packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/center_cross_nokhte/stack/presentation/widget/canvas/center_cross_nokhte_painter.dart
+++ packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/center_cross_nokhte/stack/presentation/widget/canvas/center_cross_nokhte_painter.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:nokhte/app/core/extensions/extensions.dart';
 
 class CenterCrossNokhtePainter extends CustomPainter {
+  final List<Offset> offsets;
+  final List<double> radii;
+
+  CenterCrossNokhtePainter({
+    required this.offsets,
+    required this.radii,
+  });
+
   @override
-  void paint(Canvas canvas, Size size) {}
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < offsets.length; i++) {
+      final center = Offset(
+        size.width.half() - offsets[i].dx,
+        size.height.half() - offsets[i].dy,
+      );
+      final paint = Paint()..color = Colors.white;
+      canvas.drawCircle(center, radii[i], paint);
+    }
+  }
 
   @override
   bool shouldRepaint(CenterCrossNokhtePainter oldDelegate) => true;
