7078896afbfa5aede35986c9410bcb7586785e31
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-04 09:28:58 -0600

    2️⃣🧮 add gesture_cross_module

---
+++ packages/client/lib/app/core/widgets/widget_modules/gesture_cross_module.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+
+class GestureCrossModule extends Module {
+  @override
+  void exportedBinds(Injector i) {
+    i.add<GradientCrossNokhteStore>(
+      () => GradientCrossNokhteStore(),
+    );
+    i.add<CrossStore>(
+      () => CrossStore(),
+    );
+    i.add<GestureCrossStore>(
+      () => GestureCrossStore(
+        cross: Modular.get<CrossStore>(),
+        gradientNokhte: Modular.get<GradientCrossNokhteStore>(),
+      ),
+    );
+  }
+}
