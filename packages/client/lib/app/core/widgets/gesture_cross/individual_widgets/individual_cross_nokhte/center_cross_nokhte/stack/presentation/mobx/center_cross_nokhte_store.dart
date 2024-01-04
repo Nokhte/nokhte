19e762c113c572f363a1433eafa4618ce158f967
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-04 14:20:36 -0600

    1️⃣🧮 add center_cross_nokhte_store

--- packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/center_cross_nokhte/stack/presentation/mobx/center_cross_nokhte_store.dart
+++ packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/center_cross_nokhte/stack/presentation/mobx/center_cross_nokhte_store.dart
@@ -1,12 +1,17 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
-import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 part 'center_cross_nokhte_store.g.dart';
 
 class CenterCrossNokhteStore = _CenterCrossNokhteStoreBase
     with _$CenterCrossNokhteStore;
 
-abstract class _CenterCrossNokhteStoreBase extends Equatable with Store {
+abstract class _CenterCrossNokhteStoreBase extends BaseCustomAnimatedWidgetStore
+    with Store {
+  _CenterCrossNokhteStoreBase() {
+    setMovie(RestingCenterCrossMovie.movie);
+  }
   @override
   List<Object> get props => [];
 }
