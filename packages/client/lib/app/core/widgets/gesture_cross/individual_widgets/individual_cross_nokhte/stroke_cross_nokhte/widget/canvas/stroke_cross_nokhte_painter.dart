84696db7d82d8bf9835ba568ad61d113bab08e60
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-04 15:08:27 -0600

    3️⃣💄 add stroke_cross_nokhte & painter

---
+++ packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/stroke_cross_nokhte/widget/canvas/stroke_cross_nokhte_painter.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:nokhte/app/core/extensions/extensions.dart';
+
+class StrokeCrossNokhtePainter extends CustomPainter {
+  final List<Offset> offsets;
+  final BuildContext context;
+
+  StrokeCrossNokhtePainter({
+    required this.offsets,
+    required this.context,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < offsets.length; i++) {
+      const radius = 4.50;
+      final center = Offset(
+        size.width.half() - offsets[i].dx,
+        size.height.half() - offsets[i].dy,
+      );
+      final paint = Paint()
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 1
+        ..color = Colors.white.withOpacity(.27);
+      canvas.drawCircle(center, radius, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(StrokeCrossNokhtePainter oldDelegate) => false;
+}
