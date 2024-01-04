1681357f83d50ce3512eb9aede71baf466d37ff3
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-04 14:19:36 -0600

    2️⃣️💄 add center_cross_nokhte

--- packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/center_cross_nokhte/stack/presentation/widget/center_cross_nokhte.dart
+++ packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/center_cross_nokhte/stack/presentation/widget/center_cross_nokhte.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
+import 'package:touchable/touchable.dart';
+
+import 'canvas/center_cross_nokhte_painter.dart';
 
 class CenterCrossNokhte extends StatelessWidget {
-  const CenterCrossNokhte({super.key});
+  final CenterCrossNokhteStore store;
+  const CenterCrossNokhte({
+    super.key,
+    required this.store,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Observer(
+      builder: (context) => CustomAnimationBuilder(
+        tween: store.movie,
+        duration: store.movie.duration,
+        control: store.control,
+        builder: (context, value, child) => SizedBox.expand(
+          child: CanvasTouchDetector(
+            gesturesToOverride: const [
+              GestureType.onTapDown,
+              GestureType.onTapUp,
+            ],
+            builder: (context) => CustomPaint(
+              painter: CenterCrossNokhtePainter(
+                radii: [
+                  value.get('radii1'),
+                  value.get('radii2'),
+                ],
+                offsets: [
+                  Offset(value.get('dx1'), value.get('dy1')),
+                  Offset(value.get('dx2'), value.get('dy2')),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
