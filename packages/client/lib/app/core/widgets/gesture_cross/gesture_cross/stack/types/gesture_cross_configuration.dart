3a290986e1db59e93c30435449ccf66a3784529d
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-04 17:58:29 -0600

    2️⃣️☯️ simplify gesture_cross_configuration api

--- packages/client/lib/app/core/widgets/gesture_cross/gesture_cross/stack/types/gesture_cross_configuration.dart
+++ packages/client/lib/app/core/widgets/gesture_cross/gesture_cross/stack/types/gesture_cross_configuration.dart
@@ -1,38 +1,43 @@
-import 'package:equatable/equatable.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 
-class GestureCrossConfiguration extends Equatable {
-  final GestureCrossNokhteTypes top;
-  final GestureCrossNokhteTypes bottom;
-  final GestureCrossNokhteTypes left;
-  final GestureCrossNokhteTypes right;
+class GestureCrossConfiguration {
+  final Either<StrokeConfig, NokhteGradientConfig> top;
+  final Either<StrokeConfig, NokhteGradientConfig> bottom;
+  final Either<StrokeConfig, NokhteGradientConfig> left;
+  final Either<StrokeConfig, NokhteGradientConfig> right;
+  List<int> gradientColorLengths = [];
 
-  const GestureCrossConfiguration({
-    this.top = GestureCrossNokhteTypes.stroke,
-    this.bottom = GestureCrossNokhteTypes.stroke,
-    this.left = GestureCrossNokhteTypes.stroke,
-    this.right = GestureCrossNokhteTypes.stroke,
+  List get directions => [top, bottom, left, right];
+
+  GestureCrossConfiguration({
+    this.top = const Left(StrokeConfig()),
+    this.bottom = const Left(StrokeConfig()),
+    this.left = const Left(StrokeConfig()),
+    this.right = const Left(StrokeConfig()),
   });
 
-  List<Offset> _getOffsetsForType(
-      GestureCrossNokhteTypes type, Offset defaultOffset) {
+  List<Offset> _getOffsetsForType(GestureCrossNokhteTypes type) {
     final List<Offset> offsets = [];
-
-    if (top == type) offsets.add(CircleOffsets.top);
-    if (bottom == type) offsets.add(CircleOffsets.bottom);
-    if (left == type) offsets.add(CircleOffsets.left);
-    if (right == type) offsets.add(CircleOffsets.right);
-
-    return offsets.isEmpty ? [defaultOffset] : offsets;
+    for (int i = 0; i < directions.length; i++) {
+      directions[i].fold((stroke) {
+        if (type == GestureCrossNokhteTypes.stroke) {
+          offsets.add(CircleOffsets.directions[i]);
+        }
+      }, (gradient) {
+        if (type == GestureCrossNokhteTypes.gradient) {
+          offsets.add(CircleOffsets.directions[i]);
+          gradientColorLengths.add(gradient.gradientLength);
+        }
+      });
+    }
+    return offsets;
   }
 
   List<Offset> getGradientOffsets() =>
-      _getOffsetsForType(GestureCrossNokhteTypes.gradient, CircleOffsets.top);
+      _getOffsetsForType(GestureCrossNokhteTypes.gradient);
 
   List<Offset> getStrokeOffsets() =>
-      _getOffsetsForType(GestureCrossNokhteTypes.stroke, CircleOffsets.top);
-
-  @override
-  List<Object> get props => [];
+      _getOffsetsForType(GestureCrossNokhteTypes.stroke);
 }
