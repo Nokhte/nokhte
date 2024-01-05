f56410d20de5532b98bb60311045ad369a82b324
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-04 18:02:16 -0600

    1️⃣🧮 gradient_cross_nokhte_store constructor

--- packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/gradient_cross_nokhte/stack/presentation/mobx/gradient_cross_nokhte_store.dart
+++ packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/gradient_cross_nokhte/stack/presentation/mobx/gradient_cross_nokhte_store.dart
@@ -1,8 +1,7 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
-import 'package:nokhte/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/gradient_cross_nokhte/stack/constants/constants.dart';
-import 'package:simple_animations/simple_animations.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 part 'gradient_cross_nokhte_store.g.dart';
 
 class GradientCrossNokhteStore = _GradientCrossNokhteStoreBase
@@ -11,7 +10,6 @@ class GradientCrossNokhteStore = _GradientCrossNokhteStoreBase
 abstract class _GradientCrossNokhteStoreBase
     extends BaseCustomAnimatedWidgetStore with Store {
   _GradientCrossNokhteStoreBase() {
-    setMovie(WaterGradientNokhteMovie.movie);
-    setControl(Control.mirror);
+    setMovie(VibrantBlueGradientNokhteMovie.movie);
   }
 }
