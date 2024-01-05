0c10c2099ebfbb2f02899b7d67b76fcfc90ed657
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-05 13:26:41 -0600

    3️⃣🔥 paintCirlces code in cross painter

--- packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/cross/stack/presentation/widget/canvas/cross_painter.dart
+++ packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/cross/stack/presentation/widget/canvas/cross_painter.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:nokhte/app/core/extensions/extensions.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:touchable/touchable.dart';
 
@@ -41,49 +40,10 @@ class CrossPainter extends CustomPainter {
     });
   }
 
-  paintCircles(
-    Canvas canvas,
-    Size size,
-    Offset center,
-    double radius,
-    List<CircleInformation> circleInformation,
-  ) {
-    for (final individualCircle in circleInformation) {
-      Paint circlePaint = Paint();
-      final circleY = (height) - individualCircle.offset.dy;
-      final circleX = (center.dx) - individualCircle.offset.dx;
-      final circleCenter = Offset(circleX, circleY);
-      final Rect circleBounds =
-          Rect.fromCircle(center: circleCenter, radius: radius);
-      individualCircle.colorOrGradient.fold((color) {
-        circlePaint = Paint()
-          ..color = color
-          ..style = PaintingStyle.stroke
-          ..strokeWidth = 1;
-      }, (gradient) {
-        circlePaint = Paint()
-          ..shader = LinearGradient(
-            begin: individualCircle.startingAlignment,
-            end: individualCircle.endingAlignment,
-            colors: gradient.colors,
-            stops: gradient.stops,
-          ).createShader(circleBounds);
-      });
-      canvas.drawCircle(
-        Offset(circleX, circleY),
-        radius,
-        circlePaint,
-      );
-    }
-  }
-
   @override
   void paint(Canvas canvas, Size size) {
     final touchableCanvas = TouchyCanvas(context, canvas);
     paintCross(touchableCanvas, size);
-    // final center = Offset(width.half(), height.half());
-    // const radius = 4.5;
-    // paintCircles(canvas, size, center, radius, circleInformation);
   }
 
   @override
