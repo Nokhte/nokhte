5f63224b5f2e83e489d80e69560a9d5807e7e50f
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-05 11:46:31 -0600

    1️⃣🛢️ coordinator exports (collaboration)

--- packages/client/lib/app/modules/collaboration/presentation/mobx/coordinators/coordinators.dart
+++ packages/client/lib/app/modules/collaboration/presentation/mobx/coordinators/coordinators.dart
@@ -1,2 +1,2 @@
-export 'collaboration_home_coordinator.dart';
-export 'collaboration_home_widgets_coordinator.dart';
+export 'collaboration_home_screen_coordinator.dart';
+export 'collaboration_home_screen_widgets_coordinator.dart';
