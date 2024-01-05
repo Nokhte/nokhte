209eb614434e18e67e1070009bada86a896a49aa
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-05 06:48:26 -0600

    3️⃣🧮 add collaboration & collaboration_widgets modules

--- packages/client/lib/app/modules/collaboration/collaboration_module.dart
+++ packages/client/lib/app/modules/collaboration/collaboration_module.dart
@@ -1,1 +1,32 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:nokhte/app/modules/collaboration/presentation/presentation.dart';
+import 'collaboration_widgets_module.dart';
 
+class CollaborationModule extends Module {
+  @override
+  List<Module> get imports => [
+        CollaborationWidgetsModule(),
+      ];
+
+  @override
+  void binds(Injector i) {
+    i.add<CollaborationHomeCoordinator>(
+      () => CollaborationHomeCoordinator(
+        widgets: Modular.get<CollaborationHomeWidgetsCoordinator>(),
+      ),
+    );
+    //
+  }
+
+  @override
+  void routes(RouteManager r) {
+    r.child(
+      '/',
+      transition: TransitionType.noTransition,
+      child: (context) => CollaborationHomeScreen(
+        coordinator: Modular.get<CollaborationHomeCoordinator>(),
+      ),
+    );
+    //
+  }
+}
--- packages/client/lib/app/modules/collaboration/collaboration_widgets_module.dart
+++ packages/client/lib/app/modules/collaboration/collaboration_widgets_module.dart
@@ -1,1 +1,30 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:nokhte/app/core/widgets/modules.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:nokhte/app/modules/collaboration/presentation/presentation.dart';
 
+class CollaborationWidgetsModule extends Module {
+  @override
+  List<Module> get imports => [
+        BeachWavesModule(),
+        WifiDisconnectOverlayModule(),
+        SmartTextModule(),
+        GestureCrossModule(),
+      ];
+  @override
+  void exportedBinds(Injector i) {
+    i.add<GradientTreeNodeStore>(
+      () => GradientTreeNodeStore(),
+    );
+    i.add<CollaborationHomeWidgetsCoordinator>(
+      () => CollaborationHomeWidgetsCoordinator(
+        gestureCross: Modular.get<GestureCrossStore>(),
+        beachWaves: Modular.get<BeachWavesStore>(),
+        gradientTreeNode: i<GradientTreeNodeStore>(),
+        smartText: Modular.get<SmartTextStore>(),
+        wifiDisconnectOverlay: Modular.get<WifiDisconnectOverlayStore>(),
+      ),
+    );
+    //
+  }
+}
