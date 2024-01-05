72f45ec3494acc551a72ba6cf91869721e95cdcc
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-05 15:32:12 -0600

    2️⃣✅ home_screen_coordinator

--- packages/client/test/app/modules/home/presentation/mobx/coordinator/home_screen_coordinator_test.dart
+++ packages/client/test/app/modules/home/presentation/mobx/coordinator/home_screen_coordinator_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/modules/user_information/mobx/mobx.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/home/presentation/presentation.dart';
 
@@ -12,28 +13,39 @@ void main() {
   late MockGetExistingCollaborationsInfoStore mockGetExistingCollaborationInfo;
   late MockHomeScreenWidgetsCoordinator mockWidgets;
   late HomeScreenCoordinator testStore;
+  late UserInformationCoordinator mockUserInformation;
 
   setUp(() {
     mockAddNameToDatabase = MockAddNameToDatabaseStore();
     mockGetCollaboratorPhrase = MockGetCollaboratorPhraseStore();
     mockGetExistingCollaborationInfo = MockGetExistingCollaborationsInfoStore();
     mockWidgets = MockHomeScreenWidgetsCoordinator();
+    mockUserInformation = UserInformationCoordinator(
+      getUserInfo: MockGetUserInfoStore(),
+      updateHasGoneThroughInvitationFlow:
+          MockUpdateHasGoneThroughInvitationFlowStore(),
+      updateHasSentAnInvitation: MockUpdateHasSentAnInvitationStore(),
+    );
 
     testStore = HomeScreenCoordinator(
       swipe: SwipeDetector(),
       addNameToDatabaseStore: mockAddNameToDatabase,
       getExistingCollaborationInfo: mockGetExistingCollaborationInfo,
       getCollaboratorPhraseStore: mockGetCollaboratorPhrase,
-      userInformation: MockUserInformationCoordinator(),
+      userInformation: mockUserInformation,
       widgets: mockWidgets,
     );
   });
 
   group("actions", () {
     test("constructor", () async {
+      when(mockUserInformation.getUserInfo.hasGoneThroughInvitationFlow)
+          .thenAnswer((_) => false);
       await testStore.constructor();
       verify(mockWidgets.constructor());
       verify(mockGetExistingCollaborationInfo(NoParams()));
+      verify(testStore.userInformation.getUserInfo(NoParams()));
+      verify(testStore.widgets.invitationFlowConstructor());
       verify(mockAddNameToDatabase(NoParams()));
       verify(mockGetCollaboratorPhrase(NoParams()));
     });
