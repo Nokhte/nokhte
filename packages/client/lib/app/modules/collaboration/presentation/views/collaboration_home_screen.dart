00d9c7fd8b5e8885147c67b010931124b22ea75e
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-05 06:52:43 -0600

    3️⃣💄 add collaboration_home_screen widgets

--- packages/client/lib/app/modules/collaboration/presentation/views/collaboration_home_screen.dart
+++ packages/client/lib/app/modules/collaboration/presentation/views/collaboration_home_screen.dart
@@ -1,10 +1,54 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nokhte/app/core/hooks/hooks.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:nokhte/app/modules/collaboration/presentation/presentation.dart';
 
-class CollaborationHomeScreen extends StatelessWidget {
-  const CollaborationHomeScreen({super.key});
+class CollaborationHomeScreen extends HookWidget {
+  final CollaborationHomeCoordinator coordinator;
+  const CollaborationHomeScreen({
+    super.key,
+    required this.coordinator,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = useSquareSize(relativeLength: .20);
+    useEffect(() {
+      coordinator.constructor();
+      return null;
+    }, []);
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: MultiHitStack(
+        children: [
+          FullScreen(
+              child: BeachWaves(
+            store: coordinator.widgets.beachWaves,
+          )),
+          Center(
+              child: SmartText(
+            store: coordinator.widgets.smartText,
+            bottomPadding: 180,
+          )),
+          GestureCross(
+            config: GestureCrossConfiguration(
+              bottom: Right(
+                NokhteGradientConfig(
+                  gradientType: NokhteGradientTypes.water,
+                ),
+              ),
+            ),
+            size: size,
+            store: coordinator.widgets.gestureCross,
+          ),
+          WifiDisconnectOverlay(
+            store: coordinator.widgets.wifiDisconnectOverlay,
+          ),
+        ],
+      ),
+      // ),
+    );
   }
 }
