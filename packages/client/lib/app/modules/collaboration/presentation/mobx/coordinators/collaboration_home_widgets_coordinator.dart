818e67e460fffc108c60641f4a567e18e18ca630
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-05 06:37:55 -0600

    3️⃣🧮 collaboration_home_widgets_coordinator update w/ constructor

--- packages/client/lib/app/modules/collaboration/presentation/mobx/coordinators/collaboration_home_widgets_coordinator.dart
+++ packages/client/lib/app/modules/collaboration/presentation/mobx/coordinators/collaboration_home_widgets_coordinator.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/widgets/widget_constants.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 part 'collaboration_home_widgets_coordinator.g.dart';
 
@@ -13,13 +14,23 @@ abstract class _CollaborationHomeWidgetsCoordinatorBase extends Equatable
   final GradientTreeNodeStore gradientTreeNode;
   final SmartTextStore smartText;
   final WifiDisconnectOverlayStore wifiDisconnectOverlay;
+  final GestureCrossStore gestureCross;
 
   _CollaborationHomeWidgetsCoordinatorBase({
     required this.beachWaves,
+    required this.gestureCross,
     required this.gradientTreeNode,
     required this.smartText,
     required this.wifiDisconnectOverlay,
   });
+
+  @action
+  constructor() {
+    beachWaves.setMovieMode(BeachWaveMovieModes.suspendedAtOceanDive);
+    gestureCross.setCollaborationHomeScreen();
+    gradientTreeNode.toggleWidgetVisibility();
+    smartText.setMessagesData(MessagesData.empty);
+  }
 
   @override
   List<Object> get props => [];
