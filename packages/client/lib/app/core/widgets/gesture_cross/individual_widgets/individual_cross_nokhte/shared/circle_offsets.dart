4b05e122d54ff710dc22999a63b159d84c80c01c
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-05 17:07:47 -0600

    1️⃣💄 fix circle offsets

--- packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/shared/circle_offsets.dart
+++ packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/shared/circle_offsets.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class CircleOffsets {
-  static Offset get center => const Offset(.5, 4);
+  static Offset get center => const Offset(-1, 4);
 
   static Offset get right => const Offset(-25, 4);
-  static Offset get left => const Offset(25, 4);
+  static Offset get left => const Offset(23, 4);
 
-  static Offset get top => const Offset(.5, 28);
-  static Offset get bottom => const Offset(.5, -20);
+  static Offset get top => const Offset(-1, 28);
+  static Offset get bottom => const Offset(-1, -20);
 
   static List<Offset> get directions => [top, bottom, left, right];
 }
