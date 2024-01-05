c320bb4f03932878b35b4238fb12069d50fe29c9
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-05 13:07:51 -0600

    2️⃣️🧮 flesh out collaboration_home_screen_coordinator

--- packages/client/lib/app/modules/collaboration/presentation/mobx/coordinators/collaboration_home_screen_coordinator.dart
+++ packages/client/lib/app/modules/collaboration/presentation/mobx/coordinators/collaboration_home_screen_coordinator.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/modules/user_information/mobx/mobx.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/collaboration/presentation/presentation.dart';
 part 'collaboration_home_screen_coordinator.g.dart';
 
@@ -12,17 +16,67 @@ abstract class _CollaborationHomeScreenCoordinatorBase extends BaseCoordinator
   final CollaborationHomeScreenWidgetsCoordinator widgets;
   final GetInvitationURLStore getInvitationURL;
   final ShareCollaborationInvitationStore shareCollaborationInvitation;
+  final UserInformationCoordinator userInformation;
+  final SwipeDetector swipe;
 
   _CollaborationHomeScreenCoordinatorBase({
     required this.getInvitationURL,
     required this.shareCollaborationInvitation,
     required this.widgets,
+    required this.userInformation,
+    required this.swipe,
   });
 
+  @observable
+  bool isNavigatingAway = false;
+
   @action
-  constructor() {
+  toggleIsNavigatingAway() => isNavigatingAway = !isNavigatingAway;
+
+  @action
+  constructor() async {
     widgets.constructor();
+
+    widgets.initReactors(onGradientTreeNodeTap, onFlowCompleted);
+    await getInvitationURL(NoParams());
+    initReactors();
   }
+
+  @action
+  onGradientTreeNodeTap() => ifTouchIsNotDisabled(() {
+        shareCollaborationInvitation(getInvitationURL.link);
+      });
+
+  @action
+  onFlowCompleted() => userInformation.updateHasGoneThroughInvitationFlow(true);
+
+  initReactors() {
+    swipeReactor();
+    shareInvitationReactor();
+  }
+
+  shareInvitationReactor() =>
+      reaction((p0) => shareCollaborationInvitation.isShared, (p0) {
+        if (p0) {
+          userInformation.updateHasSentAnInvitation(true);
+          widgets.toggleInvitationIsSent();
+        }
+      });
+
+  swipeReactor() => reaction((p0) => swipe.directionsType, (p0) {
+        if (!isNavigatingAway) {
+          switch (p0) {
+            case GestureDirections.down:
+              toggleIsNavigatingAway();
+              ifTouchIsNotDisabled(() {
+                widgets.onSwipeDown();
+              });
+              toggleDisableAllTouchFeedback();
+            default:
+              break;
+          }
+        }
+      });
 
   @override
   List<Object> get props => [];
--- packages/client/lib/app/modules/collaboration/presentation/mobx/coordinators/collaboration_home_screen_coordinator.g.dart
+++ packages/client/lib/app/modules/collaboration/presentation/mobx/coordinators/collaboration_home_screen_coordinator.g.dart
@@ -10,17 +10,71 @@ part of 'collaboration_home_screen_coordinator.dart';
 
 mixin _$CollaborationHomeScreenCoordinator
     on _CollaborationHomeScreenCoordinatorBase, Store {
+  late final _$isNavigatingAwayAtom = Atom(
+      name: '_CollaborationHomeScreenCoordinatorBase.isNavigatingAway',
+      context: context);
+
+  @override
+  bool get isNavigatingAway {
+    _$isNavigatingAwayAtom.reportRead();
+    return super.isNavigatingAway;
+  }
+
+  @override
+  set isNavigatingAway(bool value) {
+    _$isNavigatingAwayAtom.reportWrite(value, super.isNavigatingAway, () {
+      super.isNavigatingAway = value;
+    });
+  }
+
+  late final _$constructorAsyncAction = AsyncAction(
+      '_CollaborationHomeScreenCoordinatorBase.constructor',
+      context: context);
+
+  @override
+  Future constructor() {
+    return _$constructorAsyncAction.run(() => super.constructor());
+  }
+
   late final _$_CollaborationHomeScreenCoordinatorBaseActionController =
       ActionController(
           name: '_CollaborationHomeScreenCoordinatorBase', context: context);
 
   @override
-  dynamic constructor() {
+  dynamic toggleIsNavigatingAway() {
     final _$actionInfo =
         _$_CollaborationHomeScreenCoordinatorBaseActionController.startAction(
-            name: '_CollaborationHomeScreenCoordinatorBase.constructor');
+            name:
+                '_CollaborationHomeScreenCoordinatorBase.toggleIsNavigatingAway');
     try {
-      return super.constructor();
+      return super.toggleIsNavigatingAway();
+    } finally {
+      _$_CollaborationHomeScreenCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic onGradientTreeNodeTap() {
+    final _$actionInfo =
+        _$_CollaborationHomeScreenCoordinatorBaseActionController.startAction(
+            name:
+                '_CollaborationHomeScreenCoordinatorBase.onGradientTreeNodeTap');
+    try {
+      return super.onGradientTreeNodeTap();
+    } finally {
+      _$_CollaborationHomeScreenCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic onFlowCompleted() {
+    final _$actionInfo =
+        _$_CollaborationHomeScreenCoordinatorBaseActionController.startAction(
+            name: '_CollaborationHomeScreenCoordinatorBase.onFlowCompleted');
+    try {
+      return super.onFlowCompleted();
     } finally {
       _$_CollaborationHomeScreenCoordinatorBaseActionController
           .endAction(_$actionInfo);
@@ -30,7 +84,7 @@ mixin _$CollaborationHomeScreenCoordinator
   @override
   String toString() {
     return '''
-
+isNavigatingAway: ${isNavigatingAway}
     ''';
   }
 }
