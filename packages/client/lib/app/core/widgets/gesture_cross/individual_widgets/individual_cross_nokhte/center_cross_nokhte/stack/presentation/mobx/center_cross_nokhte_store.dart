1b2aaa1c6d99431d1487bd7ff578f690233871e5
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-04 19:03:00 -0600

    1️⃣🧮 add initMoveAndRegenerate to center_cross_nokhte_store

--- packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/center_cross_nokhte/stack/presentation/mobx/center_cross_nokhte_store.dart
+++ packages/client/lib/app/core/widgets/gesture_cross/individual_widgets/individual_cross_nokhte/center_cross_nokhte/stack/presentation/mobx/center_cross_nokhte_store.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
 part 'center_cross_nokhte_store.g.dart';
 
 class CenterCrossNokhteStore = _CenterCrossNokhteStoreBase
@@ -12,6 +14,13 @@ abstract class _CenterCrossNokhteStoreBase extends BaseCustomAnimatedWidgetStore
   _CenterCrossNokhteStoreBase() {
     setMovie(RestingCenterCrossMovie.movie);
   }
+
+  @action
+  initMoveAndRegenerate(Offset endDirection) {
+    setMovie(MoveAndRegengerateMovie.getMovie(endDirection));
+    setControl(Control.playFromStart);
+  }
+
   @override
   List<Object> get props => [];
 }
