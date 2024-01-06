dedd227198338f8bcd9a72651ba3e1a7b19e252d
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-06 12:34:48 -0600

    2️⃣🧮 add update_wants_to_repeat_invitation_flow_store

---
+++ packages/client/lib/app/core/modules/user_information/mobx/main/update_wants_to_repeat_invitation_flow_store.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:dartz/dartz.dart';
+import 'package:nokhte/app/core/modules/user_information/domain/domain.dart';
+part 'update_wants_to_repeat_invitation_flow_store.g.dart';
+
+class UpdateWantsToRepeatInvitationFlowStore = _UpdateWantsToRepeatInvitationFlowStoreBase
+    with _$UpdateWantsToRepeatInvitationFlowStore;
+
+abstract class _UpdateWantsToRepeatInvitationFlowStoreBase
+    extends BaseMobxDBStore<bool, WantsToRepeatInvitationFlowUpdateStatusEntity>
+    with Store {
+  @observable
+  bool isUpdated = false;
+
+  final UpdateWantsToRepeatInvitationFlow logic;
+  _UpdateWantsToRepeatInvitationFlowStoreBase({required this.logic});
+
+  @observable
+  BaseFutureStore<WantsToRepeatInvitationFlowUpdateStatusEntity> futureStore =
+      BaseFutureStore(
+    baseEntity: Right(WantsToRepeatInvitationFlowUpdateStatusEntity.initial),
+    entityFutureParam: ObservableFuture(
+      Future.value(
+          Right(WantsToRepeatInvitationFlowUpdateStatusEntity.initial)),
+    ),
+  );
+
+  @override
+  void stateOrErrorUpdater(result) {
+    result.fold((failure) {
+      errorMessage = mapFailureToMessage(failure);
+      state = StoreState.initial;
+    }, (updateStatusEntity) {
+      isUpdated = updateStatusEntity.isSent;
+    });
+  }
+
+  @override
+  @action
+  Future<void> call(params) async {
+    state = StoreState.loading;
+    futureStore.entityOrFailureFuture = ObservableFuture(logic(params));
+    futureStore.unwrappedEntityOrFailure =
+        await futureStore.entityOrFailureFuture;
+    stateOrErrorUpdater(futureStore.unwrappedEntityOrFailure);
+    state = StoreState.loaded;
+  }
+}
