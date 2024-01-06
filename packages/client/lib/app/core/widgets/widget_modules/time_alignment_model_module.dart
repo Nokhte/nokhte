e1e7ed63ab38c38c0d6db21551b02d54949b6c69
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-06 10:15:20 -0600

    1️⃣🔥time_alignment_model_module: remove connectivity binds & imports

--- packages/client/lib/app/core/widgets/widget_modules/time_alignment_model_module.dart
+++ packages/client/lib/app/core/widgets/widget_modules/time_alignment_model_module.dart
@@ -1,22 +1,14 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:nokhte/app/core/modules/connectivity/connectivity_module.dart';
-import 'package:nokhte/app/core/modules/connectivity/mobx/get_on_connectivity_changed_store.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 
 class TimeAlignmentModelModule extends Module {
-  @override
-  List<Module> get imports => [
-        ConnectivityModule(),
-      ];
   @override
   void exportedBinds(i) {
     i.addSingleton<ClockFaceStore>(
       () => ClockFaceStore(),
     );
     i.add<AvailabilitySectorsStore>(
-      () => AvailabilitySectorsStore(
-        connectivity: Modular.get<GetOnConnectivityChangedStore>(),
-      ),
+      () => AvailabilitySectorsStore(),
     );
     i.add<TimeAlignmentModelCoordinator>(
       () => TimeAlignmentModelCoordinator(
