2a3e7f7bcd186d1b8e85a0a08bae855a8c8c7440
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-06 14:27:56 -0600

    2️⃣💄 add animated opacity in time_model

--- packages/client/lib/app/core/widgets/explanatory_model_widgets/coordinators/time_alignment_model_coordinator.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/coordinators/time_alignment_model_coordinator.dart
@@ -1,14 +1,15 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
-import 'package:equatable/equatable.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/mobx/base_custom_animated_widget_store.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 part 'time_alignment_model_coordinator.g.dart';
 
 class TimeAlignmentModelCoordinator = _TimeAlignmentModelCoordinatorBase
     with _$TimeAlignmentModelCoordinator;
 
-abstract class _TimeAlignmentModelCoordinatorBase extends Equatable with Store {
+abstract class _TimeAlignmentModelCoordinatorBase
+    extends BaseCustomAnimatedWidgetStore with Store {
   final ClockFaceStore clockFace;
   final AvailabilitySectorsStore availabilitySectors;
 
--- packages/client/lib/app/core/widgets/explanatory_model_widgets/model_widgets/time_alignment_model_widget.dart
+++ packages/client/lib/app/core/widgets/explanatory_model_widgets/model_widgets/time_alignment_model_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:nokhte/app/core/hooks/hooks.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 
 class TimeAlignmentModelWidget extends StatelessWidget {
@@ -10,16 +13,22 @@ class TimeAlignmentModelWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FullScreen(
-        child: MultiHitStack(
-      children: [
-        ClockFace(
-          store: store.clockFace,
-        ),
-        AvailabilitySectors(
-          store: store.availabilitySectors,
-        ),
-      ],
-    ));
+    return Observer(builder: (context) {
+      return AnimatedOpacity(
+        opacity: useWidgetOpacity(store.showWidget),
+        duration: Seconds.get(0, milli: 500),
+        child: FullScreen(
+            child: MultiHitStack(
+          children: [
+            ClockFace(
+              store: store.clockFace,
+            ),
+            AvailabilitySectors(
+              store: store.availabilitySectors,
+            ),
+          ],
+        )),
+      );
+    });
   }
 }
