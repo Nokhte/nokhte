77e3e00cdaa3e1e4345063cff0f10729af5d412d
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-06 12:36:47 -0600

    🔥1️⃣ user_information_contract_impl: remove un-needed import

--- packages/client/lib/app/core/modules/user_information/data/contracts/user_information_contract_impl.dart
+++ packages/client/lib/app/core/modules/user_information/data/contracts/user_information_contract_impl.dart
@@ -2,7 +2,6 @@ import 'package:dartz/dartz.dart';
 import 'package:nokhte/app/core/constants/failure_constants.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/modules/user_information/data/data.dart';
-import 'package:nokhte/app/core/modules/user_information/data/models/wants_to_repeat_invitation_flow_update_status_model.dart';
 import 'package:nokhte/app/core/modules/user_information/domain/domain.dart';
 import 'package:nokhte/app/core/network/network_info.dart';
 
