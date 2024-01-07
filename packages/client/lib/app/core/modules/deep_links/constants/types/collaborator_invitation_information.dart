d0b81172ceb6b477421af3a2b067365320186e58
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-07 17:53:26 -0600

    3️⃣🏷️ add collaborator_invitation_information

---
+++ packages/client/lib/app/core/modules/deep_links/constants/types/collaborator_invitation_information.dart
@@ -0,0 +1,34 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_branch_sdk/flutter_branch_sdk.dart';
+import 'package:nokhte/app/core/modules/deep_links/constants/constants.dart';
+
+class CollaboratorInvitationInformation extends Equatable {
+  final String uid;
+  final String imageURL =
+      "https://gitlab.com/nokhte/nokhte/-/raw/development/assets/logo.png";
+  final String firstName;
+  const CollaboratorInvitationInformation({
+    required this.firstName,
+    required this.uid,
+  });
+
+  BranchUniversalObjectAndLinkProperties getBranchLinkProperties() {
+    final branchObject = BranchUniversalObject(
+      canonicalIdentifier: "${DeepLinkPrefixes.collaboratorCode}/$uid",
+      title: "Collaborate With $firstName",
+      imageUrl: imageURL,
+    );
+    final linkProperties = BranchLinkProperties(
+      alias: branchObject.canonicalIdentifier,
+      feature: "collaboration",
+      stage: "new invitation",
+    );
+    return BranchUniversalObjectAndLinkProperties(
+      universalObject: branchObject,
+      linkProperties: linkProperties,
+    );
+  }
+
+  @override
+  List<Object?> get props => [firstName, uid];
+}
