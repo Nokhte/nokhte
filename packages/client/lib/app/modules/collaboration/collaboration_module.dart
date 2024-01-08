2ff7d749968adaf24667b88be66e6f23f831ed5a
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-07 18:06:17 -0600

    3️⃣☯️ collaboration_module: simplify & update binds

--- packages/client/lib/app/modules/collaboration/collaboration_module.dart
+++ packages/client/lib/app/modules/collaboration/collaboration_module.dart
@@ -1,14 +1,12 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:nokhte/app/core/modules/deep_links/deep_links_module.dart';
+import 'package:nokhte/app/core/modules/deep_links/mobx/mobx.dart';
 import 'package:nokhte/app/core/modules/user_information/mobx/mobx.dart';
 import 'package:nokhte/app/core/modules/user_information/user_information_module.dart';
-import 'package:nokhte/app/core/network/network_info.dart';
 import 'package:nokhte/app/core/widgets/modules.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
-import 'package:nokhte/app/modules/collaboration/data/data.dart';
 import 'package:nokhte/app/modules/collaboration/presentation/presentation.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'collaboration_widgets_module.dart';
-import 'domain/domain.dart';
 
 class CollaborationModule extends Module {
   @override
@@ -16,52 +14,19 @@ class CollaborationModule extends Module {
         CollaborationWidgetsModule(),
         UserInformationModule(),
         GesturesModule(),
+        DeepLinksModule(),
       ];
 
   @override
   void binds(Injector i) {
-    i.add<CollaborationRemoteSourceImpl>(
-      () => CollaborationRemoteSourceImpl(
-        supabase: Modular.get<SupabaseClient>(),
-      ),
-    );
-    i.add<CollaborationContractImpl>(
-      () => CollaborationContractImpl(
-        remoteSource: i<CollaborationRemoteSourceImpl>(),
-        networkInfo: Modular.get<NetworkInfoImpl>(),
-      ),
-    );
-    i.addSingleton<GetInvitationURL>(
-      () => GetInvitationURL(
-        contract: i<CollaborationContractImpl>(),
-      ),
-    );
-
-    i.addSingleton<ShareCollaborationInvitation>(
-      () => ShareCollaborationInvitation(
-        contract: i<CollaborationContractImpl>(),
-      ),
-    );
-    i.addSingleton<GetInvitationURLStore>(
-      () => GetInvitationURLStore(
-        logic: i<GetInvitationURL>(),
-      ),
-    );
-    i.addSingleton<ShareCollaborationInvitationStore>(
-      () => ShareCollaborationInvitationStore(
-        logic: i<ShareCollaborationInvitation>(),
-      ),
-    );
     i.add<CollaborationHomeScreenCoordinator>(
       () => CollaborationHomeScreenCoordinator(
         swipe: Modular.get<SwipeDetector>(),
-        getInvitationURL: i<GetInvitationURLStore>(),
-        shareCollaborationInvitation: i<ShareCollaborationInvitationStore>(),
+        deepLinks: Modular.get<DeepLinksCoordinator>(),
         widgets: Modular.get<CollaborationHomeScreenWidgetsCoordinator>(),
         userInformation: i<UserInformationCoordinator>(),
       ),
     );
-    //
   }
 
   @override
