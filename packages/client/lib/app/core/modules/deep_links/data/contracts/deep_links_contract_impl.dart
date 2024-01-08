8472106fb47e2c4cd591e705409ae8df5289b931
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-07 18:00:26 -0600

    3️⃣🧮 add deep_links_contract_impl

---
+++ packages/client/lib/app/core/modules/deep_links/data/contracts/deep_links_contract_impl.dart
@@ -0,0 +1,46 @@
+import 'package:dartz/dartz.dart';
+import 'package:nokhte/app/core/constants/failure_constants.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/modules/deep_links/data/data.dart';
+import 'package:nokhte/app/core/modules/deep_links/domain/domain.dart';
+import 'package:nokhte/app/core/network/network_info.dart';
+
+class DeepLinksContractImpl implements DeepLinksContract {
+  final DeepLinksRemoteSource remoteSource;
+  final NetworkInfo networkInfo;
+
+  DeepLinksContractImpl({
+    required this.remoteSource,
+    required this.networkInfo,
+  });
+
+  @override
+  getLatestOpenedDeeplink(NoParams params) async {
+    if (await networkInfo.isConnected) {
+      final res = await remoteSource.getLatestOpenedDeepLink();
+      return Right(DeepLinkInfoModel.fromBranch(res));
+    } else {
+      return Left(FailureConstants.internetConnectionFailure);
+    }
+  }
+
+  @override
+  getDeepLinkURL(GetDeepLinkURLParams params) async {
+    if (await networkInfo.isConnected) {
+      final res = await remoteSource.getDeepLinkURL(params);
+      return Right(DeepLinkUrlEntity(link: res.result));
+    } else {
+      return Left(FailureConstants.internetConnectionFailure);
+    }
+  }
+
+  @override
+  sendDeepLink(String params) async {
+    if (await networkInfo.isConnected) {
+      final res = await remoteSource.sendDeepLink(params);
+      return Right(DeepLinkSendStatusModel.fromShareResult(res));
+    } else {
+      return Left(FailureConstants.internetConnectionFailure);
+    }
+  }
+}
