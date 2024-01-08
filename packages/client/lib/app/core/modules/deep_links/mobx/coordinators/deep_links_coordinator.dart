15235be30eb140e4c244352afaa415f6d5eeed7d
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-07 18:02:13 -0600

    2️⃣🧮 add deep_links_coordinator

---
+++ packages/client/lib/app/core/modules/deep_links/mobx/coordinators/deep_links_coordinator.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/modules/deep_links/mobx/mobx.dart';
+part 'deep_links_coordinator.g.dart';
+
+class DeepLinksCoordinator = _DeepLinksCoordinatorBase
+    with _$DeepLinksCoordinator;
+
+abstract class _DeepLinksCoordinatorBase extends Equatable with Store {
+  final GetDeepLinkURLStore getDeepLinkURL;
+  final GetLatestOpenedDeepLinkStore getLatestOpenedDeepLink;
+  final SendDeepLinkStore sendDeepLink;
+
+  _DeepLinksCoordinatorBase({
+    required this.getDeepLinkURL,
+    required this.getLatestOpenedDeepLink,
+    required this.sendDeepLink,
+  });
+
+  @override
+  List<Object> get props => [];
+}
