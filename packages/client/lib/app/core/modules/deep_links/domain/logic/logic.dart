c99e43de82516d1d59290a9ffc29fcaf746624ec
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-08 19:33:50 -0600

    1️⃣💬 update deep_links exports

--- packages/client/lib/app/core/modules/deep_links/domain/logic/logic.dart
+++ packages/client/lib/app/core/modules/deep_links/domain/logic/logic.dart
@@ -1,4 +1,4 @@
 export 'listen_for_opened_deep_link.dart';
-export "interpret_deep_link.dart";
 export 'get_deep_link_url.dart';
 export "send_deep_link.dart";
+export "interpret_collaborator_code_deep_link.dart";
