230348df207eee937c8dac8cb87074e0791154aa
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-09 16:13:03 -0600

    3️⃣🧮 add collaborator_pool_screen_coordinator

---
+++ packages/client/lib/app/modules/collaboration/presentation/mobx/coordinators/collaborator_pool/collaborator_pool_screen_coordinator.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/modules/collaboration/presentation/presentation.dart';
+import 'collaborator_pool_screen_widgets_coordinator.dart';
+part 'collaborator_pool_screen_coordinator.g.dart';
+
+class CollaboratorPoolScreenCoordinator = _CollaboratorPoolScreenCoordinatorBase
+    with _$CollaboratorPoolScreenCoordinator;
+
+abstract class _CollaboratorPoolScreenCoordinatorBase extends BaseCoordinator
+    with Store {
+  final CollaboratorPoolScreenWidgetsCoordinator widgets;
+  final CancelCollaboratorSearchStreamStore cancelCollaboratorSearchStream;
+  final ExitCollaboratorPoolStore exitCollaboratorPool;
+
+  _CollaboratorPoolScreenCoordinatorBase({
+    required this.widgets,
+    required this.cancelCollaboratorSearchStream,
+    required this.exitCollaboratorPool,
+  });
+}
