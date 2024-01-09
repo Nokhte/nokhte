af0f155d05d7124691fcc70dddfdd8a96b3ad3aa
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-09 14:56:21 -0600

    1️⃣🔥home_screen: un-needed import

--- packages/client/lib/app/modules/collaboration/presentation/views/collaboration_home_screen.dart
+++ packages/client/lib/app/modules/collaboration/presentation/views/collaboration_home_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:nokhte/app/core/hooks/hooks.dart';
 import 'package:nokhte/app/core/types/types.dart';
-import 'package:nokhte/app/core/widgets/gradient_tree_node/stack/constants/tree_node_gradients.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/collaboration/presentation/presentation.dart';
 
