a7a6b1b4cd1163dea3d7966983ce2313a4d11321
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-09 09:59:22 -0600

    3️⃣🧮 collaboration_remote_source

---
+++ packages/client/lib/app/modules/collaboration/data/sources/collaboration_remote_source.dart
@@ -0,0 +1,48 @@
+import 'package:nokhte_backend/tables/existing_collaborations.dart';
+import 'package:nokhte_backend/edge_functions/edge_functions.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract class CollaborationRemoteSource {
+  Future<FunctionResponse> enterThePool(String collaboratorUID);
+
+  Future<FunctionResponse> exitThePool();
+
+  Stream<CollaboratorSearchAndEntryStatus> getCollaboratorSearchStatus();
+
+  bool cancelStream();
+}
+
+class CollaborationRemoteSourceImpl implements CollaborationRemoteSource {
+  final SupabaseClient supabase;
+  final String currentUserUID;
+  final ExistingCollaborationsStream existingCollaborationsStream;
+  final InitiateCollaboratorSearch initiateCollaboratorSearch;
+  final EndCollaboratorSearch endCollaboratorSearch;
+
+  CollaborationRemoteSourceImpl({
+    required this.supabase,
+  })  : initiateCollaboratorSearch =
+            InitiateCollaboratorSearch(supabase: supabase),
+        endCollaboratorSearch = EndCollaboratorSearch(supabase: supabase),
+        existingCollaborationsStream =
+            ExistingCollaborationsStream(supabase: supabase),
+        currentUserUID = supabase.auth.currentUser?.id ?? '';
+
+  @override
+  enterThePool(String collaboratorUID) async =>
+      await initiateCollaboratorSearch.invoke(collaboratorUID);
+
+  @override
+  Future<FunctionResponse> exitThePool() async =>
+      await endCollaboratorSearch.invoke();
+
+  @override
+  Stream<CollaboratorSearchAndEntryStatus> getCollaboratorSearchStatus() =>
+      existingCollaborationsStream.getCollaboratorSearchAndEntryStatus();
+
+  @override
+  bool cancelStream() {
+    existingCollaborationsStream.cancelGetCollaboratorSearchStream();
+    return existingCollaborationsStream.collaboratorSearchListeningStatus;
+  }
+}
