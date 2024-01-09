9bc8a7ce575b5fa7e8446ba6d5ae8998824db2fa
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-09 08:25:44 -0600

    1️⃣🧮 add isEmptyOrNull array operator to array_utils

--- packages/backend/supabase/functions/utils/array_utils.ts
+++ packages/backend/supabase/functions/utils/array_utils.ts
@@ -1,3 +1,7 @@
 export function isNotEmptyOrNull(arr: Array<any> | null): boolean {
   return arr === null ? false : arr.length !== 0;
 }
+
+export function isEmptyOrNull(arr: Array<any> | null): boolean {
+  return arr === null ? true : arr.length === 0;
+}
