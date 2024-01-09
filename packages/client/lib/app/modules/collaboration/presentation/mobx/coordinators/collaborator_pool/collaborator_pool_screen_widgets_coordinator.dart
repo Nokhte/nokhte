7f6925f18c8878867fc5b48df9049fd33b141337
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-09 16:13:19 -0600

    2️⃣🧮 collaborator_pool_screen_widgets_coordinator

---
+++ packages/client/lib/app/modules/collaboration/presentation/mobx/coordinators/collaborator_pool/collaborator_pool_screen_widgets_coordinator.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+part 'collaborator_pool_screen_widgets_coordinator.g.dart';
+
+class CollaboratorPoolScreenWidgetsCoordinator = _CollaboratorPoolScreenWidgetsCoordinatorBase
+    with _$CollaboratorPoolScreenWidgetsCoordinator;
+
+abstract class _CollaboratorPoolScreenWidgetsCoordinatorBase extends Equatable
+    with Store {
+  final BeachWavesStore beachWaves;
+  final WifiDisconnectOverlayStore wifiDisconnectOverlay;
+  // add waitingOnCollaborator text;
+
+  _CollaboratorPoolScreenWidgetsCoordinatorBase({
+    required this.beachWaves,
+    required this.wifiDisconnectOverlay,
+  });
+
+  @override
+  List<Object> get props => [];
+}
