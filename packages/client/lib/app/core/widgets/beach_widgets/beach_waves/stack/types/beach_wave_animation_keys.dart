49ebd0195e670763b0c4668d8c2942de478ec553
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-09 17:35:20 -0600

    2️⃣💬 add beach_wave_animation_keys

---
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/types/beach_wave_animation_keys.dart
@@ -0,0 +1,19 @@
+class BeachWaveAnimationKeys {
+  static const waterMovement = "waterMovement";
+  static const color1 = "color1";
+  static const color2 = "color2";
+  static const color3 = "color3";
+  static const color4 = "color4";
+  static const color5 = "color5";
+  static const color6 = "color6";
+  static const color7 = "color7";
+  static const color8 = "color8";
+  static const stop1 = "stop1";
+  static const stop2 = "stop2";
+  static const stop3 = "stop3";
+  static const stop4 = "stop4";
+  static const stop5 = "stop5";
+  static const stop6 = "stop6";
+  static const stop7 = "stop7";
+  static const stop8 = "stop8";
+}
