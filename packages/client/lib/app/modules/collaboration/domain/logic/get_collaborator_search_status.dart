c2b32dd3af9b6eb5f03f8fdc0a92af0c1709cc62
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-09 09:01:49 -0600

    2️⃣🧮 get_collaborator_search_status

---
+++ packages/client/lib/app/modules/collaboration/domain/logic/get_collaborator_search_status.dart
@@ -0,0 +1,13 @@
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/modules/collaboration/domain/domain.dart';
+import 'package:nokhte_backend/tables/existing_collaborations.dart';
+
+class GetCollaboratorSearchStatus extends AbstractFutureLogic<
+    Stream<CollaboratorSearchAndEntryStatus>, NoParams> {
+  final CollaborationContract contract;
+  GetCollaboratorSearchStatus({required this.contract});
+
+  @override
+  call(NoParams params) async =>
+      await contract.getCollaboratorSearchStatus(params);
+}
