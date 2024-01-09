8145076a0bec26ab892c8fdd2b6d47198180ad25
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-09 07:36:39 -0600

    2️⃣👷🏾‍♂️ update test.sh for ci tests

--- packages/backend/dart/scripts/test.sh
+++ packages/backend/dart/scripts/test.sh
@@ -1,13 +1,12 @@
-flutter test test/01_user_names_and_collaborator_phrase_tables_test.dart
-flutter test test/02_collaborator_phrases_table_test.dart
-flutter test test/03_synchronous_search_test.dart
-flutter test test/04_get_agora_token_test.dart
-flutter test test/05_existing_collaborations_table_test.dart
-flutter test test/06_solo_sharable_documents_table_test.dart
-flutter test test/07_finished_collaborative_documents_test.dart
-flutter test test/08_working_collaborative_documents_test.dart
-flutter test test/09_collaborative_scheduling_table_test.dart
-flutter test test/10_p2p_perspectives_tracking_table_test.dart
-# flutter test test/11_working_perspectives_positioning_table_test
-flutter test test/12_individual_sessions_and_perspectives_audio_storage_test.dart
-flutter test test/13_timer_information_table_test.dart
+flutter test test/01_user_names_table_test.dart
+flutter test test/02_synchronous_search_test.dart
+flutter test test/03_get_agora_token_test.dart
+flutter test test/04_existing_collaborations_table_test.dart
+flutter test test/05_solo_sharable_documents_table_test.dart
+flutter test test/06_finished_collaborative_documents_test.dart
+flutter test test/07_working_collaborative_documents_test.dart
+flutter test test/08_collaborative_scheduling_table_test.dart
+flutter test test/09_p2p_perspectives_tracking_table_test.dart
+# flutter test test/10_working_perspectives_positioning_table_test.dart
+flutter test test/11_individual_sessions_and_perspectives_audio_storage_test.dart
+flutter test test/12_timer_information_table_test.dart
