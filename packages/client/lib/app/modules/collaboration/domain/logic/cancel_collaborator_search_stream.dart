bf14a67bb53c47294430ae308405296bee8c3c25
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-09 09:01:31 -0600

    2️⃣🧮 cancel_collaborator_search_stream

---
+++ packages/client/lib/app/modules/collaboration/domain/logic/cancel_collaborator_search_stream.dart
@@ -0,0 +1,14 @@
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/modules/collaboration/domain/domain.dart';
+
+class CancelCollaboratorSearchStream extends AbstractFutureLogic<
+    CollaboratorStreamCancellationStatusEntity, NoParams> {
+  final CollaborationContract contract;
+
+  CancelCollaboratorSearchStream({
+    required this.contract,
+  });
+
+  @override
+  call(params) async => await contract.cancelCollaboratorSearchStream(params);
+}
