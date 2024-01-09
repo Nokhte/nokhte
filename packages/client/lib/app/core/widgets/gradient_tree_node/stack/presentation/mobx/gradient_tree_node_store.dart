ea8a380adbde12451b192f7f39bb112624e02785
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-09 14:49:25 -0600

    2️⃣🧮 add gradient_tree_node_store movie functionality

--- packages/client/lib/app/core/widgets/gradient_tree_node/stack/presentation/mobx/gradient_tree_node_store.dart
+++ packages/client/lib/app/core/widgets/gradient_tree_node/stack/presentation/mobx/gradient_tree_node_store.dart
@@ -1,16 +1,22 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/widgets/gradient_tree_node/stack/stack.dart';
+import 'package:simple_animations/simple_animations.dart';
 part 'gradient_tree_node_store.g.dart';
 
 class GradientTreeNodeStore = _GradientTreeNodeStoreBase
     with _$GradientTreeNodeStore;
 
-abstract class _GradientTreeNodeStoreBase extends BaseCustomAnimatedWidgetStore
-    with Store {
-  @observable
-  int tapCount = 0;
+abstract class _GradientTreeNodeStoreBase
+    extends BaseCustomAnimatedWidgetStore<NoParams> with Store {
+  _GradientTreeNodeStoreBase() {
+    setMovie(ChangeColorGlowAndMoveUpMovies.movie);
+  }
 
-  @action
-  incrementTapCount() => tapCount++;
+  @override
+  initMovie(param) {
+    setControl(Control.playFromStart);
+  }
 }
--- packages/client/lib/app/core/widgets/gradient_tree_node/stack/presentation/mobx/gradient_tree_node_store.g.dart
+++ packages/client/lib/app/core/widgets/gradient_tree_node/stack/presentation/mobx/gradient_tree_node_store.g.dart
@@ -9,40 +9,10 @@ part of 'gradient_tree_node_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$GradientTreeNodeStore on _GradientTreeNodeStoreBase, Store {
-  late final _$tapCountAtom =
-      Atom(name: '_GradientTreeNodeStoreBase.tapCount', context: context);
-
-  @override
-  int get tapCount {
-    _$tapCountAtom.reportRead();
-    return super.tapCount;
-  }
-
-  @override
-  set tapCount(int value) {
-    _$tapCountAtom.reportWrite(value, super.tapCount, () {
-      super.tapCount = value;
-    });
-  }
-
-  late final _$_GradientTreeNodeStoreBaseActionController =
-      ActionController(name: '_GradientTreeNodeStoreBase', context: context);
-
-  @override
-  dynamic incrementTapCount() {
-    final _$actionInfo = _$_GradientTreeNodeStoreBaseActionController
-        .startAction(name: '_GradientTreeNodeStoreBase.incrementTapCount');
-    try {
-      return super.incrementTapCount();
-    } finally {
-      _$_GradientTreeNodeStoreBaseActionController.endAction(_$actionInfo);
-    }
-  }
-
   @override
   String toString() {
     return '''
-tapCount: ${tapCount}
+
     ''';
   }
 }
