ec8d44fb6a9e4d5e1d3c0b88339bb191d8128441
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-09 16:14:37 -0600

    2️⃣💄 add collaborator_pool_screen

---
+++ packages/client/lib/app/modules/collaboration/presentation/views/collaborator_pool_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:nokhte/app/modules/collaboration/presentation/presentation.dart';
+
+class CollaboratorPoolScreen extends HookWidget {
+  final CollaboratorPoolScreenCoordinator coordinator;
+  const CollaboratorPoolScreen({
+    super.key,
+    required this.coordinator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          FullScreen(
+              child: BeachWaves(
+            store: coordinator.widgets.beachWaves,
+          )),
+          WifiDisconnectOverlay(
+            store: coordinator.widgets.wifiDisconnectOverlay,
+          ),
+        ],
+      ),
+    );
+  }
+}
