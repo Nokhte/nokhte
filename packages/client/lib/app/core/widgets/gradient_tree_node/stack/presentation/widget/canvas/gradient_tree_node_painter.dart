cdf436f4f31a648b2071ac02e38dc697f8d15ef2
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-09 14:55:33 -0600

    2️⃣💄 gradient_tree_node_painter: add offset,radius & stroke width params

--- packages/client/lib/app/core/widgets/gradient_tree_node/stack/presentation/widget/canvas/gradient_tree_node_painter.dart
+++ packages/client/lib/app/core/widgets/gradient_tree_node/stack/presentation/widget/canvas/gradient_tree_node_painter.dart
@@ -7,25 +7,30 @@ class GradientTreeNodePainter extends CustomPainter {
   final NokhteGradient gradient;
   final BuildContext context;
   final Function onTap;
+  final Offset offset;
+  final double radius;
+  final double strokeWidth;
 
   GradientTreeNodePainter({
     required this.gradient,
     required this.context,
     required this.onTap,
+    required this.offset,
+    required this.radius,
+    required this.strokeWidth,
   });
 
   @override
   void paint(Canvas canvas, Size size) {
     final touchableCanvas = TouchyCanvas(context, canvas);
-    final center = Offset(size.width.half() + 5, size.height.half());
-
-    const double radius = 25.0;
+    final center = Offset(
+        (size.width.half() + 5) + offset.dx, size.height.half() + offset.dy);
 
     final circleRect = Rect.fromCircle(center: center, radius: radius);
 
     final circleStrokePaint = Paint()
       ..color = Colors.white
-      ..strokeWidth = 2.0
+      ..strokeWidth = strokeWidth
       ..style = PaintingStyle.stroke;
 
     final gradientPaint = Paint()
