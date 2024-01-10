cd9318c0f0621974d11c17109fef22ab2570abbd
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-10 06:24:56 -0600

    2️⃣💫 vibrant_blue_gradient_to_times_up: change end colors to phase1

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/vibrant_blue_gradient_to_times_up/vibrant_blue_gradient_to_times_up_movie.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/vibrant_blue_gradient_to_times_up/vibrant_blue_gradient_to_times_up_movie.dart
@@ -9,7 +9,7 @@ class VibrantBlueGradientToTimesUpMovie {
   static MovieTween get movie => MovieTween()
     ..scene(
       begin: Seconds.get(0),
-      end: Seconds.get(1),
+      end: Seconds.get(2),
     )
         .tween(
           BeachWaveAnimationKeys.waterMovement,
@@ -22,112 +22,112 @@ class VibrantBlueGradientToTimesUpMovie {
           BeachWaveAnimationKeys.color1,
           ColorTween(
             begin: WaterColorsAndStops.vibrantBlueGrad.first.color,
-            end: WaterColorsAndStops.vibrantBlueGrad.first.color,
+            end: WaterColorsAndStops.timesUpPhase1.first.color,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.color2,
           ColorTween(
             begin: WaterColorsAndStops.vibrantBlueGrad[1].color,
-            end: WaterColorsAndStops.vibrantBlueGrad[1].color,
+            end: WaterColorsAndStops.timesUpPhase1[1].color,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.color3,
           ColorTween(
             begin: WaterColorsAndStops.vibrantBlueGrad[2].color,
-            end: WaterColorsAndStops.vibrantBlueGrad[2].color,
+            end: WaterColorsAndStops.timesUpPhase1[2].color,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.color4,
           ColorTween(
             begin: WaterColorsAndStops.vibrantBlueGrad[3].color,
-            end: WaterColorsAndStops.vibrantBlueGrad[3].color,
+            end: WaterColorsAndStops.timesUpPhase1[3].color,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.color5,
           ColorTween(
             begin: WaterColorsAndStops.vibrantBlueGrad[4].color,
-            end: WaterColorsAndStops.vibrantBlueGrad[4].color,
+            end: WaterColorsAndStops.timesUpPhase1[4].color,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.color6,
           ColorTween(
             begin: WaterColorsAndStops.vibrantBlueGrad[5].color,
-            end: WaterColorsAndStops.vibrantBlueGrad[5].color,
+            end: WaterColorsAndStops.timesUpPhase1[5].color,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.color7,
           ColorTween(
             begin: WaterColorsAndStops.vibrantBlueGrad[6].color,
-            end: WaterColorsAndStops.vibrantBlueGrad[6].color,
+            end: WaterColorsAndStops.timesUpPhase1[6].color,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.color8,
           ColorTween(
             begin: WaterColorsAndStops.vibrantBlueGrad[7].color,
-            end: WaterColorsAndStops.vibrantBlueGrad[7].color,
+            end: WaterColorsAndStops.timesUpPhase1[7].color,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.stop1,
           Tween<double>(
             begin: WaterColorsAndStops.vibrantBlueGrad.first.stop,
-            end: WaterColorsAndStops.vibrantBlueGrad.first.stop,
+            end: WaterColorsAndStops.timesUpPhase1.first.stop,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.stop2,
           Tween<double>(
             begin: WaterColorsAndStops.vibrantBlueGrad[1].stop,
-            end: WaterColorsAndStops.vibrantBlueGrad[1].stop,
+            end: WaterColorsAndStops.timesUpPhase1[1].stop,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.stop3,
           Tween<double>(
             begin: WaterColorsAndStops.vibrantBlueGrad[2].stop,
-            end: WaterColorsAndStops.vibrantBlueGrad[2].stop,
+            end: WaterColorsAndStops.timesUpPhase1[2].stop,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.stop4,
           Tween<double>(
             begin: WaterColorsAndStops.vibrantBlueGrad[3].stop,
-            end: WaterColorsAndStops.vibrantBlueGrad[3].stop,
+            end: WaterColorsAndStops.timesUpPhase1[3].stop,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.stop5,
           Tween<double>(
             begin: WaterColorsAndStops.vibrantBlueGrad[4].stop,
-            end: WaterColorsAndStops.vibrantBlueGrad[4].stop,
+            end: WaterColorsAndStops.timesUpPhase1[4].stop,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.stop6,
           Tween<double>(
             begin: WaterColorsAndStops.vibrantBlueGrad[5].stop,
-            end: WaterColorsAndStops.vibrantBlueGrad[5].stop,
+            end: WaterColorsAndStops.timesUpPhase1[5].stop,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.stop7,
           Tween<double>(
             begin: WaterColorsAndStops.vibrantBlueGrad[6].stop,
-            end: WaterColorsAndStops.vibrantBlueGrad[6].stop,
+            end: WaterColorsAndStops.timesUpPhase1[6].stop,
           ),
         )
         .tween(
           BeachWaveAnimationKeys.stop8,
           Tween<double>(
             begin: WaterColorsAndStops.vibrantBlueGrad[7].stop,
-            end: WaterColorsAndStops.vibrantBlueGrad[7].stop,
+            end: WaterColorsAndStops.timesUpPhase1[7].stop,
           ),
         );
 }
