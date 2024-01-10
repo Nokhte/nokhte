31975193684bb4fc750767b1676ae1f058129e0a
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-09 19:18:33 -0600

    2️⃣🛢️ adjust beach_waves exports

---
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/vibrant_blue_gradient_to_times_up/vibrant_blue_gradient_to_times_up.dart
@@ -0,0 +1,2 @@
+export "./vibrant_blue_gradient_to_times_up_movie_store.dart";
+export "./vibrant_blue_gradient_to_times_up_movie.dart";
--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/types/types.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/types/types.dart
@@ -1,1 +1,2 @@
 export './base_beach_wave_movie_store.dart';
+export "beach_wave_animation_keys.dart";
