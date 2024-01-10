642f0123852f3dee8ddea6b3c61d38de4b21b562
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-10 08:50:49 -0600

    2️⃣🧮collaborator_pool: add appLifecycleChange hook & gestureCross

--- packages/client/lib/app/modules/collaboration/presentation/views/collaborator_pool_screen.dart
+++ packages/client/lib/app/modules/collaboration/presentation/views/collaborator_pool_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nokhte/app/core/hooks/hooks.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/collaboration/presentation/presentation.dart';
 
@@ -16,14 +17,26 @@ class CollaboratorPoolScreen extends HookWidget {
       coordinator.constructor();
       return null;
     }, []);
+    useOnAppLifecycleStateChange(
+        (previous, current) => coordinator.onAppLifeCycleStateChange(
+              current,
+              onResumed: () => null,
+              onInactive: () => coordinator.widgets.onInactive(),
+            ));
+    final size = useSquareSize(relativeLength: .20);
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Stack(
+      body: MultiHitStack(
         children: [
           FullScreen(
               child: BeachWaves(
             store: coordinator.widgets.beachWaves,
           )),
+          GestureCross(
+            config: GestureCrossConfiguration(),
+            size: size,
+            store: coordinator.widgets.gestureCross,
+          ),
           WifiDisconnectOverlay(
             store: coordinator.widgets.wifiDisconnectOverlay,
           ),
