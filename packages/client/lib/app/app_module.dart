02db235f2381f352e4f495b55a613574044d7688
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-10 08:01:38 -0600

    1️⃣🧮app_module: init purpose_session module

--- packages/client/lib/app/app_module.dart
+++ packages/client/lib/app/app_module.dart
@@ -4,6 +4,7 @@ import 'package:nokhte/app/core/network/network_info.dart';
 import 'package:nokhte/app/modules/authentication/authentication_module.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:nokhte/app/modules/collaboration/collaboration_module.dart';
+import 'package:nokhte/app/modules/purpose_session/purpose_session_module.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'modules/home/home_module.dart';
@@ -30,5 +31,6 @@ class AppModule extends Module {
     r.module('/', module: AuthenticationModule());
     r.module('/home/', module: HomeModule());
     r.module('/collaboration/', module: CollaborationModule());
+    r.module('/purpose_session', module: PurposeSessionModule());
   }
 }
