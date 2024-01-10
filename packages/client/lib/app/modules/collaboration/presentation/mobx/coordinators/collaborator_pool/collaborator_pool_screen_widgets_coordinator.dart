28172ce3c156b6ed33b92ad400bb39576988f3e7
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-10 06:25:34 -0600

    2️⃣🧮 collaborator_pool_screen_widgets_coordinator: add beachWaveReactor

--- packages/client/lib/app/modules/collaboration/presentation/mobx/coordinators/collaborator_pool/collaborator_pool_screen_widgets_coordinator.dart
+++ packages/client/lib/app/modules/collaboration/presentation/mobx/coordinators/collaborator_pool/collaborator_pool_screen_widgets_coordinator.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/beach_widgets/shared/shared.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 part 'collaborator_pool_screen_widgets_coordinator.g.dart';
@@ -22,7 +24,19 @@ abstract class _CollaboratorPoolScreenWidgetsCoordinatorBase extends Equatable
   @action
   constructor() {
     beachWaves.setMovieMode(BeachWaveMovieModes.vibrantBlueGradientToTimesUp);
+    beachWaves.currentStore.initMovie(NoParams());
+    beachWavesMovieStatusReactor();
   }
+
+  beachWavesMovieStatusReactor() =>
+      reaction((p0) => beachWaves.movieStatus, (p0) {
+        if (p0 == MovieStatus.finished &&
+            beachWaves.movieMode ==
+                BeachWaveMovieModes.vibrantBlueGradientToTimesUp) {
+          beachWaves.setMovieMode(BeachWaveMovieModes.timesUp);
+          beachWaves.currentStore.initMovie(Seconds.get(63));
+        }
+      });
 
   @override
   List<Object> get props => [];
