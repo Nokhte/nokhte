a579fbfdeda49b2891d450a4a2040c41e91123d4
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-10 08:47:01 -0600

    2️⃣🧮 add base_widgets_coordinator

---
+++ packages/client/lib/app/core/mobx/base_widgets_coordinator.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:equatable/equatable.dart';
+part 'base_widgets_coordinator.g.dart';
+
+class BaseWidgetsCoordinator = _BaseWidgetsCoordinatorBase
+    with _$BaseWidgetsCoordinator;
+
+abstract class _BaseWidgetsCoordinatorBase extends Equatable with Store {
+  @observable
+  bool isDisconnected = false;
+
+  @action
+  toggleIsDisconnected() => isDisconnected = !isDisconnected;
+
+  @override
+  List<Object> get props => [];
+}
---
+++ packages/client/lib/app/core/mobx/base_widgets_coordinator.g.dart
@@ -0,0 +1,48 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'base_widgets_coordinator.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$BaseWidgetsCoordinator on _BaseWidgetsCoordinatorBase, Store {
+  late final _$isDisconnectedAtom = Atom(
+      name: '_BaseWidgetsCoordinatorBase.isDisconnected', context: context);
+
+  @override
+  bool get isDisconnected {
+    _$isDisconnectedAtom.reportRead();
+    return super.isDisconnected;
+  }
+
+  @override
+  set isDisconnected(bool value) {
+    _$isDisconnectedAtom.reportWrite(value, super.isDisconnected, () {
+      super.isDisconnected = value;
+    });
+  }
+
+  late final _$_BaseWidgetsCoordinatorBaseActionController =
+      ActionController(name: '_BaseWidgetsCoordinatorBase', context: context);
+
+  @override
+  dynamic toggleIsDisconnected() {
+    final _$actionInfo = _$_BaseWidgetsCoordinatorBaseActionController
+        .startAction(name: '_BaseWidgetsCoordinatorBase.toggleIsDisconnected');
+    try {
+      return super.toggleIsDisconnected();
+    } finally {
+      _$_BaseWidgetsCoordinatorBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+isDisconnected: ${isDisconnected}
+    ''';
+  }
+}
