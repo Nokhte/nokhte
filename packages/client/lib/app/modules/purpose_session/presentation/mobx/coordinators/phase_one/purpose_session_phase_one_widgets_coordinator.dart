9cc9f4e293fef0a96746a14bc7337b524086d986
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-12 08:00:40 -0600

    2️⃣🧮 add purpose_session_phase_one_widgets_coordinator

---
+++ packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase_one/purpose_session_phase_one_widgets_coordinator.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/widgets/widget_constants.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+part 'purpose_session_phase_one_widgets_coordinator.g.dart';
+
+class PurposeSessionPhaseOneWidgetsCoordinator = _PurposeSessionPhaseOneWidgetsCoordinatorBase
+    with _$PurposeSessionPhaseOneWidgetsCoordinator;
+
+abstract class _PurposeSessionPhaseOneWidgetsCoordinatorBase
+    extends BaseWidgetsCoordinator with Store {
+  final BeachWavesStore beachWaves;
+  final WifiDisconnectOverlayStore wifiDisconnectOverlay;
+  final SmartTextStore primarySmartText;
+  final SmartTextStore secondarySmartText;
+
+  _PurposeSessionPhaseOneWidgetsCoordinatorBase({
+    required this.beachWaves,
+    required this.wifiDisconnectOverlay,
+    required this.primarySmartText,
+    required this.secondarySmartText,
+  });
+
+  @action
+  constructor() {
+    beachWaves.setMovieMode(BeachWaveMovieModes.suspendedAtTheDepths);
+    primarySmartText.setMessagesData(MessagesData.empty);
+    secondarySmartText.setMessagesData(MessagesData.empty);
+  }
+}
