212e576fea6c28f2522e25fb7a107a7efc7460ec
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-12 10:23:55 -0600

    1️⃣🧮 add voiceCallCoordinator to purpose_session_phase_one_coordinator

--- packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase_one/purpose_session_phase_one_coordinator.dart
+++ packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase_one/purpose_session_phase_one_coordinator.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/base_coordinator.dart';
+import 'package:nokhte/app/core/modules/voice_call/mobx/mobx.dart';
 import 'purpose_session_phase_one_widgets_coordinator.dart';
 part 'purpose_session_phase_one_coordinator.g.dart';
 
@@ -9,10 +10,12 @@ class PurposeSessionPhaseOneCoordinator = _PurposeSessionPhaseOneCoordinatorBase
 
 abstract class _PurposeSessionPhaseOneCoordinatorBase extends BaseCoordinator
     with Store {
+  final VoiceCallCoordinator voiceCall;
   final PurposeSessionPhaseOneWidgetsCoordinator widgets;
 
   _PurposeSessionPhaseOneCoordinatorBase({
     required this.widgets,
+    required this.voiceCall,
   });
 
   @action
