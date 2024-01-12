48e5adef9cefcea008afa72d7fe11ac0e5d40a12
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-12 15:37:50 -0600

    1️⃣🔥 clean out un-needed imports

--- packages/client/lib/app/app_module.dart
+++ packages/client/lib/app/app_module.dart
@@ -1,8 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:nokhte/app/core/modules/legacy_connectivity/legacy_connectivity_module.dart';
-import 'package:nokhte/app/core/network/network_info.dart';
 import 'package:nokhte/app/modules/authentication/authentication_module.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:nokhte/app/modules/collaboration/collaboration_module.dart';
 import 'package:nokhte/app/modules/purpose_session/purpose_session_module.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
