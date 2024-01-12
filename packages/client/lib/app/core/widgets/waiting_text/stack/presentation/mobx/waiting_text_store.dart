ae470c98a03961c40ce79aa25d21093c3f8d7863
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-11 19:40:43 -0600

    2️⃣🧮 add waiting_text_store

--- packages/client/lib/app/core/widgets/waiting_text/stack/presentation/mobx/waiting_text_store.dart
+++ packages/client/lib/app/core/widgets/waiting_text/stack/presentation/mobx/waiting_text_store.dart
@@ -1,11 +1,16 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
-import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/widgets/waiting_text/stack/constants/movies/movies.dart';
+import 'package:simple_animations/simple_animations.dart';
 part 'waiting_text_store.g.dart';
 
 class WaitingTextStore = _WaitingTextStoreBase with _$WaitingTextStore;
 
-abstract class _WaitingTextStoreBase extends Equatable with Store {
-  @override
-  List<Object> get props => [];
+abstract class _WaitingTextStoreBase extends BaseCustomAnimatedWidgetStore
+    with Store {
+  _WaitingTextStoreBase() {
+    setMovie(WaitingTextStoneSkipMovie.movie);
+    setControl(Control.loop);
+  }
 }
