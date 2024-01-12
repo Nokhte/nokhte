9e83c9124fd528e9a6f0f923fd6767949e5a7cc9
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-12 08:02:37 -0600

    2️⃣🧮 add purpose_session_widgets_module

---
+++ packages/client/lib/app/modules/purpose_session/purpose_session_widgets_module.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:nokhte/app/core/widgets/widget_modules/widget_modules.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:nokhte/app/modules/purpose_session/presentation/mobx/coordinators/phase_one/phase_one.dart';
+
+class PurposeSessionWidgetsModule extends Module {
+  @override
+  List<Module> get imports => [
+        BeachWavesModule(),
+        WifiDisconnectOverlayModule(),
+        SmartTextModule(),
+      ];
+
+  @override
+  void exportedBinds(Injector i) {
+    i.add(
+      () => PurposeSessionPhaseOneWidgetsCoordinator(
+        beachWaves: Modular.get<BeachWavesStore>(),
+        wifiDisconnectOverlay: Modular.get<WifiDisconnectOverlayStore>(),
+        primarySmartText: Modular.get<SmartTextStore>(),
+        secondarySmartText: Modular.get<SmartTextStore>(),
+      ),
+    );
+  }
+}
