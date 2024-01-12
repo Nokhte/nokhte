c2636279105b322b01e0440c9fe0a7e377db3355
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-12 14:40:45 -0600

    3️⃣💥 existing_collabrations, remove has_entered columns & add other ones

---
+++ packages/backend/supabase/migrations/20240112200442_adjust_existing_collaborations.sql
@@ -0,0 +1,21 @@
+alter table "public"."existing_collaborations" drop column "collaborator_one_has_entered";
+
+alter table "public"."existing_collaborations" drop column "collaborator_two_has_entered";
+
+alter table "public"."existing_collaborations" add column "is_on_call" boolean[] not null default '{f,f}'::boolean[];
+
+alter table "public"."existing_collaborations" add column "is_online" boolean[] not null default '{f,f}'::boolean[];
+
+alter table "public"."existing_collaborations" add column "timer_init_status" boolean[] default '{f,f}'::boolean[];
+
+alter table "public"."existing_collaborations" add constraint "existing_collaborations_is_on_call_check" CHECK ((array_length(is_on_call, 1) = 2)) not valid;
+
+alter table "public"."existing_collaborations" validate constraint "existing_collaborations_is_on_call_check";
+
+alter table "public"."existing_collaborations" add constraint "existing_collaborations_is_online_check" CHECK ((array_length(is_online, 1) = 2)) not valid;
+
+alter table "public"."existing_collaborations" validate constraint "existing_collaborations_is_online_check";
+
+alter table "public"."existing_collaborations" add constraint "existing_collaborations_timer_init_status_check" CHECK ((array_length(timer_init_status, 1) = 2)) not valid;
+
+alter table "public"."existing_collaborations" validate constraint "existing_collaborations_timer_init_status_check";
