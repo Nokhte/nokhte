c4319a2987f718b88fbf18ee9393a1dc37b88841
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-12 10:19:31 -0600

    2️⃣♻️ legacy_connectivity_module to be self containted

--- packages/client/lib/app/core/modules/legacy_connectivity/legacy_connectivity_module.dart
+++ packages/client/lib/app/core/modules/legacy_connectivity/legacy_connectivity_module.dart
@@ -1,13 +1,15 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:nokhte/app/core/network/network_info.dart';
 
 class LegacyConnectivityModule extends Module {
-  final Connectivity connectivityInstance;
+  final Connectivity connectivityInstance = Connectivity();
 
-  LegacyConnectivityModule({required this.connectivityInstance});
+  LegacyConnectivityModule();
 
   @override
-  binds(i) {
+  exportedBinds(i) {
     i.addSingleton<Connectivity>(() => connectivityInstance);
+    i.addSingleton(() => NetworkInfoImpl(connectivityInstance));
   }
 }
