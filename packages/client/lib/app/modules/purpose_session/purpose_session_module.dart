4e5c14fd92481721e1c5e12bec4bddbfad1c00d7
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-12 08:02:51 -0600

    2️⃣🧮 update binds in purpose_session_module

--- packages/client/lib/app/modules/purpose_session/purpose_session_module.dart
+++ packages/client/lib/app/modules/purpose_session/purpose_session_module.dart
@@ -1,14 +1,25 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:nokhte/app/modules/purpose_session/data/data.dart';
-import 'package:nokhte/app/modules/purpose_session/presentation/views/purpose_session_phase1_greeter.dart';
+import 'package:nokhte/app/modules/purpose_session/presentation/mobx/coordinators/phase_one/phase_one.dart';
+import 'package:nokhte/app/modules/purpose_session/presentation/views/purpose_session_phase_one_greeter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'purpose_session_widgets_module.dart';
 
 class PurposeSessionModule extends Module {
+  @override
+  List<Module> get imports => [
+        PurposeSessionWidgetsModule(),
+      ];
   @override
   void binds(i) {
     i.add<PurposeSessionRemoteSourceImpl>(
       () => PurposeSessionRemoteSourceImpl(
         supabase: Modular.get<SupabaseClient>(),
+      ),
+    );
+    i.add<PurposeSessionPhaseOneCoordinator>(
+      () => PurposeSessionPhaseOneCoordinator(
+        widgets: Modular.get<PurposeSessionPhaseOneWidgetsCoordinator>(),
       ),
     );
   }
@@ -18,8 +29,8 @@ class PurposeSessionModule extends Module {
     r.child(
       '/',
       transition: TransitionType.noTransition,
-      child: (context) => PurposeSessionPhase1Greeter(
-        remoteSource: Modular.get<PurposeSessionRemoteSourceImpl>(),
+      child: (context) => PurposeSessionPhaseOneGreeter(
+        coordinator: Modular.get<PurposeSessionPhaseOneCoordinator>(),
       ),
     );
   }
