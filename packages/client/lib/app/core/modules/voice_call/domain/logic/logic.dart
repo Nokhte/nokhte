2c96e317422f1e50d8bd73c16e470ea4ac47cc2b
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-12 09:51:31 -0600

    2️⃣🛢️ update voice_call exports

--- packages/client/lib/app/core/modules/voice_call/domain/logic/logic.dart
+++ packages/client/lib/app/core/modules/voice_call/domain/logic/logic.dart
@@ -1,5 +1,4 @@
 export 'get_agora_token.dart';
-export 'instantiate_agora_sdk.dart';
 export 'join_call.dart';
 export 'leave_call.dart';
 export 'mute_local_audio.dart';
--- packages/client/lib/app/core/modules/voice_call/mobx/main/main.dart
+++ packages/client/lib/app/core/modules/voice_call/mobx/main/main.dart
@@ -1,5 +1,4 @@
-export 'agora_callbacks_store.dart';
 export 'get_agora_token_store.dart';
-export 'instantiate_agora_sdk_store.dart';
+export 'voice_call_status_store.dart';
 export 'voice_call_actions_store.dart';
 export 'get_channel_id_store.dart';
