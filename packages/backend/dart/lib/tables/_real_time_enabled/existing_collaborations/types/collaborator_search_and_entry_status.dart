73b6307a55418729edad724e6b6ef4ab98e7f7bd
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-12 15:24:02 -0600

    1️⃣🔥 remove has entered from collaborator_search_and_entry_status

--- packages/backend/dart/lib/tables/_real_time_enabled/existing_collaborations/types/collaborator_search_and_entry_status.dart
+++ packages/backend/dart/lib/tables/_real_time_enabled/existing_collaborations/types/collaborator_search_and_entry_status.dart
@@ -1,14 +1,12 @@
 import 'package:equatable/equatable.dart';
 
 class CollaboratorSearchAndEntryStatus extends Equatable {
-  final bool hasEntered;
   final bool hasFoundTheirCollaborator;
 
   CollaboratorSearchAndEntryStatus({
-    required this.hasEntered,
     required this.hasFoundTheirCollaborator,
   });
 
   @override
-  List<Object> get props => [hasEntered, hasFoundTheirCollaborator];
+  List<Object> get props => [hasFoundTheirCollaborator];
 }
