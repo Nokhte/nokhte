443f76e1fef4c4ae420a37886e8e3b5d4a16e7dd
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-13 09:11:57 -0600

    2️⃣🧮timeAllocator:add generateWithTransitionPeriods

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/utils/time_allocator.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/utils/time_allocator.dart
@@ -1,4 +1,3 @@
-
 import 'package:nokhte/app/core/widgets/beach_widgets/shared/shared.dart';
 
 class TimeAllocator {
@@ -7,13 +6,54 @@ class TimeAllocator {
     final List<CustomDuration> customDurations = [];
 
     for (int i = 0; i < numIncrements; i++) {
+      final timePeriod = _generateWithoutTransition(i, duration, numIncrements);
+      customDurations.add(timePeriod);
+    }
+
+    return customDurations;
+  }
+
+  static _generateWithoutTransition(
+    int i,
+    Duration duration,
+    int numIncrements,
+  ) {
+    final beginMillis = (i * duration.inMilliseconds ~/ numIncrements);
+    final endMillis = ((i + 1) * duration.inMilliseconds ~/ numIncrements);
+
+    final begin = Duration(milliseconds: beginMillis);
+    final end = Duration(milliseconds: endMillis);
+
+    return CustomDuration(begin: begin, end: end);
+  }
+
+  static List<CustomDuration> generateWithTransitionPeriods({
+    required Duration duration,
+    required int numIncrements,
+    required Duration transitionPeriod,
+  }) {
+    final List<CustomDuration> customDurations = [];
+    for (int i = 0; i < numIncrements; i++) {
       final beginMillis = (i * duration.inMilliseconds ~/ numIncrements);
-      final endMillis = ((i + 1) * duration.inMilliseconds ~/ numIncrements);
+      int endMillis;
+      if ((i + 1) < numIncrements) {
+        endMillis = ((i + 1) * duration.inMilliseconds ~/ numIncrements) -
+            transitionPeriod.inMilliseconds;
 
-      final begin = Duration(milliseconds: beginMillis);
-      final end = Duration(milliseconds: endMillis);
-
-      customDurations.add(CustomDuration(begin: begin, end: end));
+        final mainBeginPeriod = Duration(milliseconds: beginMillis);
+        final mainEndPeriod = Duration(milliseconds: endMillis);
+        customDurations
+            .add(CustomDuration(begin: mainBeginPeriod, end: mainEndPeriod));
+        final transitionEndPeriod = Duration(
+            milliseconds:
+                mainEndPeriod.inMilliseconds + transitionPeriod.inMilliseconds);
+        customDurations.add(
+            CustomDuration(begin: mainEndPeriod, end: transitionEndPeriod));
+      } else {
+        final transitionPeriod =
+            _generateWithoutTransition(i, duration, numIncrements);
+        customDurations.add(transitionPeriod);
+      }
     }
 
     return customDurations;
