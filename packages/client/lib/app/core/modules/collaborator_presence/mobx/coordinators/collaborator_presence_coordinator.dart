945f8c1a4a1d8da76f0d399984a9c4e2ecf24482
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-14 11:39:49 -0600

    3️⃣🧮 add collaborator_presence_coordinator

---
+++ packages/client/lib/app/core/modules/collaborator_presence/mobx/coordinators/collaborator_presence_coordinator.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/modules/collaborator_presence/mobx/mobx.dart';
+part 'collaborator_presence_coordinator.g.dart';
+
+class CollaboratorPresenceCoordinator = _CollaboratorPresenceCoordinatorBase
+    with _$CollaboratorPresenceCoordinator;
+
+abstract class _CollaboratorPresenceCoordinatorBase extends Equatable
+    with Store {
+  final UpdateOnCallStatusStore updateOnCallStatus;
+  final UpdateOnlineStatusStore updateOnlineStatus;
+  final UpdateTimerStatusStore updateTimerStatus;
+  final UpdateWhoIsTalkingStore updateWhoIsTalking;
+  final GetSessionMetadataStore getSessionMetadata;
+
+  _CollaboratorPresenceCoordinatorBase({
+    required this.getSessionMetadata,
+    required this.updateOnCallStatus,
+    required this.updateOnlineStatus,
+    required this.updateTimerStatus,
+    required this.updateWhoIsTalking,
+  });
+
+  @override
+  List<Object> get props => [];
+}
