e419be8298a975ec42cbc8cbbc50cead4f7d5a42
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-14 09:54:22 -0600

    1️⃣🔥main.dart: remove commented code

--- packages/client/lib/main.dart
+++ packages/client/lib/main.dart
@@ -34,14 +34,11 @@ void main() async {
     url: supabaseUrl,
     anonKey: supabaseAnonKey,
   );
-  // final supabaseClient = Supabase.instance.client;
 
   runApp(
     ModularApp(
       debugMode: true,
-      module: AppModule(
-          // supabase: supabaseClient,
-          ),
+      module: AppModule(),
       child: const AppWidget(),
     ),
   );
