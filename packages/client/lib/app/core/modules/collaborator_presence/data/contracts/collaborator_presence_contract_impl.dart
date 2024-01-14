374dd83785925fc4eaf88c64a3d7a81edd0d2010
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-14 11:38:13 -0600

    3️⃣🧮 add collaborator_presence_contract_impl

---
+++ packages/client/lib/app/core/modules/collaborator_presence/data/contracts/collaborator_presence_contract_impl.dart
@@ -0,0 +1,72 @@
+import 'package:dartz/dartz.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/modules/collaborator_presence/domain/domain.dart';
+import 'package:nokhte/app/core/modules/collaborator_presence/data/data.dart';
+import 'package:nokhte/app/core/network/network_info.dart';
+import 'package:nokhte/app/core/constants/failure_constants.dart';
+
+class CollaboratorPresenceContractImpl implements CollaboratorPresenceContract {
+  final CollaboratorPresenceRemoteSource remoteSource;
+  final NetworkInfo networkInfo;
+
+  CollaboratorPresenceContractImpl({
+    required this.remoteSource,
+    required this.networkInfo,
+  });
+
+  @override
+  updateOnCallStatus(UpdateOnCallStatusParams params) async {
+    if (await networkInfo.isConnected) {
+      final res = await remoteSource.updateOnCallStatus(params);
+      return Right(CallUpdateStatusModel.fromSupabase(res));
+    } else {
+      return Left(FailureConstants.internetConnectionFailure);
+    }
+  }
+
+  @override
+  updateOnlineStatus(UpdateOnlineStatusParams params) async {
+    if (await networkInfo.isConnected) {
+      final res = await remoteSource.updateOnlineStatus(params);
+      return Right(OnlineUpdateStatusModel.fromSupabase(res));
+    } else {
+      return Left(FailureConstants.internetConnectionFailure);
+    }
+  }
+
+  @override
+  updateTimerStatus(bool params) async {
+    if (await networkInfo.isConnected) {
+      final res = await remoteSource.updateTimerStatus(params);
+      return Right(TimerUpdateStatusModel.fromSupabase(res));
+    } else {
+      return Left(FailureConstants.internetConnectionFailure);
+    }
+  }
+
+  @override
+  updateWhoIsTalking(UpdateWhoIsTalkingParams params) async {
+    if (await networkInfo.isConnected) {
+      switch (params) {
+        case UpdateWhoIsTalkingParams.setUserAsTalker:
+          final res = await remoteSource.setUserAsCurrentTalker();
+          return Right(WhoIsTalkingUpdateStatusModel.fromSupabase(res));
+        case UpdateWhoIsTalkingParams.clearOut:
+          await remoteSource.clearTheCurrentTalker();
+          return const Right(WhoIsTalkingUpdateStatusModel(isUpdated: true));
+      }
+    } else {
+      return Left(FailureConstants.internetConnectionFailure);
+    }
+  }
+
+  @override
+  getSessionMetadata(NoParams params) async {
+    if (await networkInfo.isConnected) {
+      final res = remoteSource.getSessionMetadata();
+      return Right(res);
+    } else {
+      return Left(FailureConstants.internetConnectionFailure);
+    }
+  }
+}
