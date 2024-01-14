ab449b9cceec66e18e5011f119e09f8ac50f4b4c
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-14 11:40:41 -0600

    2️⃣🧮purpose_session_phase_one_coordinator: add presence & delete deps

--- packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase_one/purpose_session_phase_one_coordinator.dart
+++ packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase_one/purpose_session_phase_one_coordinator.dart
@@ -1,9 +1,10 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/base_coordinator.dart';
+import 'package:nokhte/app/core/modules/collaborator_presence/mobx/mobx.dart';
 import 'package:nokhte/app/core/modules/voice_call/mobx/mobx.dart';
 import 'package:nokhte/app/core/types/types.dart';
-import 'purpose_session_phase_one_widgets_coordinator.dart';
+import 'package:nokhte/app/modules/purpose_session/presentation/mobx/mobx.dart';
 part 'purpose_session_phase_one_coordinator.g.dart';
 
 class PurposeSessionPhaseOneCoordinator = _PurposeSessionPhaseOneCoordinatorBase
@@ -12,11 +13,15 @@ class PurposeSessionPhaseOneCoordinator = _PurposeSessionPhaseOneCoordinatorBase
 abstract class _PurposeSessionPhaseOneCoordinatorBase extends BaseCoordinator
     with Store {
   final VoiceCallCoordinator voiceCall;
+  final CollaboratorPresenceCoordinator collaboratorPresence;
+  final DeleteCollaborationArtifactsStore deleteCollaborationArtifacts;
   final PurposeSessionPhaseOneWidgetsCoordinator widgets;
 
   _PurposeSessionPhaseOneCoordinatorBase({
     required this.widgets,
     required this.voiceCall,
+    required this.collaboratorPresence,
+    required this.deleteCollaborationArtifacts,
   });
 
   @action
