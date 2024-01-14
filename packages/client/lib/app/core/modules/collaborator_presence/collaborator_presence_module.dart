11d9a1282e8fd8e1029cd3f34009d5dbc057c7bc
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-14 11:40:19 -0600

    3️⃣🧮 add collaborator_presence_module

---
+++ packages/client/lib/app/core/modules/collaborator_presence/collaborator_presence_module.dart
@@ -0,0 +1,88 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:nokhte/app/core/modules/supabase/supabase_module.dart';
+import 'package:nokhte/app/core/network/network_info.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'domain/domain.dart';
+import 'mobx/mobx.dart';
+import 'data/data.dart';
+
+class CollaboratorPresenceModule extends Module {
+  @override
+  List<Module> get imports => [
+        SupabaseModule(),
+      ];
+  @override
+  void exportedBinds(Injector i) {
+    i.add<CollaboratorPresenceRemoteSourceImpl>(
+      () => CollaboratorPresenceRemoteSourceImpl(
+        supabase: Modular.get<SupabaseClient>(),
+      ),
+    );
+    i.add<CollaboratorPresenceContractImpl>(
+      () => CollaboratorPresenceContractImpl(
+        remoteSource: i<CollaboratorPresenceRemoteSourceImpl>(),
+        networkInfo: Modular.get<NetworkInfoImpl>(),
+      ),
+    );
+    i.add<GetSessionMetadata>(
+      () => GetSessionMetadata(
+        contract: i<CollaboratorPresenceContractImpl>(),
+      ),
+    );
+    i.add<UpdateOnCallStatus>(
+      () => UpdateOnCallStatus(
+        contract: i<CollaboratorPresenceContractImpl>(),
+      ),
+    );
+    i.add<UpdateOnlineStatus>(
+      () => UpdateOnlineStatus(
+        contract: i<CollaboratorPresenceContractImpl>(),
+      ),
+    );
+    i.add<UpdateTimerStatus>(
+      () => UpdateTimerStatus(
+        contract: i<CollaboratorPresenceContractImpl>(),
+      ),
+    );
+    i.add<UpdateWhoIsTalking>(
+      () => UpdateWhoIsTalking(
+        contract: i<CollaboratorPresenceContractImpl>(),
+      ),
+    );
+    i.add<GetSessionMetadataStore>(
+      () => GetSessionMetadataStore(
+        logic: i<GetSessionMetadata>(),
+      ),
+    );
+
+    i.add<UpdateOnCallStatusStore>(
+      () => UpdateOnCallStatusStore(
+        logic: i<UpdateOnCallStatus>(),
+      ),
+    );
+    i.add<UpdateOnlineStatusStore>(
+      () => UpdateOnlineStatusStore(
+        logic: i<UpdateOnlineStatus>(),
+      ),
+    );
+    i.add<UpdateTimerStatusStore>(
+      () => UpdateTimerStatusStore(
+        logic: i<UpdateTimerStatus>(),
+      ),
+    );
+    i.add<UpdateWhoIsTalkingStore>(
+      () => UpdateWhoIsTalkingStore(
+        logic: i<UpdateWhoIsTalking>(),
+      ),
+    );
+    i.add<CollaboratorPresenceCoordinator>(
+      () => CollaboratorPresenceCoordinator(
+        getSessionMetadata: i<GetSessionMetadataStore>(),
+        updateOnCallStatus: i<UpdateOnCallStatusStore>(),
+        updateOnlineStatus: i<UpdateOnlineStatusStore>(),
+        updateTimerStatus: i<UpdateTimerStatusStore>(),
+        updateWhoIsTalking: i<UpdateWhoIsTalkingStore>(),
+      ),
+    );
+  }
+}
