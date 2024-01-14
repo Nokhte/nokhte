8dfeca5217affdb40b79a00b0fd33f92c4fd35da
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-14 11:34:35 -0600

    2️⃣🏷️ add collaborator_presence_contract

---
+++ packages/client/lib/app/core/modules/collaborator_presence/domain/contracts/collaborator_presence_contract.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:nokhte/app/core/error/failure.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/modules/collaborator_presence/domain/domain.dart';
+import 'package:nokhte_backend/tables/existing_collaborations.dart';
+
+abstract class CollaboratorPresenceContract {
+  Future<Either<Failure, OnlineUpdateStatusEntity>> updateOnlineStatus(
+      UpdateOnlineStatusParams params);
+  Future<Either<Failure, CallUpdateStatusEntity>> updateOnCallStatus(
+      UpdateOnCallStatusParams params);
+  Future<Either<Failure, WhoIsTalkingUpdateStatusEntity>> updateWhoIsTalking(
+      UpdateWhoIsTalkingParams params);
+  Future<Either<Failure, TimerUpdateStatusEntity>> updateTimerStatus(
+      bool params);
+  Future<Either<Failure, Stream<CollaborationSessionMetadata>>>
+      getSessionMetadata(NoParams params);
+}
