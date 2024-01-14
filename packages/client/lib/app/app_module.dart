c567205800c9c358b615b382ddbe763fc49c2ab6
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-14 06:21:14 -0600

    2️⃣♻️ app_module to import supabase module instead of constructor inject

--- packages/client/lib/app/app_module.dart
+++ packages/client/lib/app/app_module.dart
@@ -1,20 +1,15 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:nokhte/app/modules/authentication/authentication_module.dart';
-import 'package:nokhte/app/modules/collaboration/collaboration_module.dart';
-import 'package:nokhte/app/modules/purpose_session/purpose_session_module.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
+import 'core/modules/supabase/supabase_module.dart';
+import 'modules/authentication/authentication_module.dart';
+import 'modules/collaboration/collaboration_module.dart';
+import 'modules/purpose_session/purpose_session_module.dart';
 import 'modules/home/home_module.dart';
 
 class AppModule extends Module {
-  final SupabaseClient supabase;
-
-  AppModule({required this.supabase});
-
   @override
-  void binds(i) {
-    i.addSingleton(() => supabase);
-  }
+  List<Module> get imports => [
+        SupabaseModule(),
+      ];
 
   @override
   void routes(r) {
