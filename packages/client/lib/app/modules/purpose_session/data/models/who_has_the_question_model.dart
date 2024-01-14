2836ff5b2b64fa8b9258593b12337c2aa89fb07a
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-14 10:05:50 -0600

    1️⃣🏷️ add who_has_the_question_model

---
+++ packages/client/lib/app/modules/purpose_session/data/models/who_has_the_question_model.dart
@@ -0,0 +1,5 @@
+import 'package:nokhte/app/modules/purpose_session/domain/domain.dart';
+
+class WhoHasTheQuestionModel extends WhoHasTheQuestionEntity {
+  const WhoHasTheQuestionModel({required super.hasTheQuestion});
+}
