be2767b2bec196650dd6c698dbe538f579cc8149
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-14 20:10:30 -0600

    2️⃣🧮 update purpose_session_widgets_module

--- packages/client/lib/app/modules/purpose_session/purpose_session_widgets_module.dart
+++ packages/client/lib/app/modules/purpose_session/purpose_session_widgets_module.dart
@@ -2,6 +2,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:nokhte/app/core/widgets/widget_modules/widget_modules.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/purpose_session/presentation/mobx/coordinators/phase_one/phase_one.dart';
+import 'package:nokhte/app/modules/purpose_session/presentation/mobx/coordinators/phase_zero/purpose_session_phase_zero_widgets_coordinator.dart';
 
 class PurposeSessionWidgetsModule extends Module {
   @override
@@ -16,7 +17,14 @@ class PurposeSessionWidgetsModule extends Module {
     i.add<NokhteBlurStore>(
       () => NokhteBlurStore(),
     );
-    i.add(
+    i.add<PurposeSessionPhaseZeroWidgetsCoordinator>(
+      () => PurposeSessionPhaseZeroWidgetsCoordinator(
+        beachWaves: Modular.get<BeachWavesStore>(),
+        wifiDisconnectOverlay: Modular.get<WifiDisconnectOverlayStore>(),
+        primarySmartText: Modular.get<SmartTextStore>(),
+      ),
+    );
+    i.add<PurposeSessionPhaseOneWidgetsCoordinator>(
       () => PurposeSessionPhaseOneWidgetsCoordinator(
           beachWaves: Modular.get<BeachWavesStore>(),
           wifiDisconnectOverlay: Modular.get<WifiDisconnectOverlayStore>(),
