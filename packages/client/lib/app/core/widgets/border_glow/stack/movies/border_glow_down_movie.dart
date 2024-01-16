f7295c5714b086d9a7e5e84d1fae3613bfb8268b
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-16 06:20:49 -0600

    2️⃣💫 add border_glow up & down movies

---
+++ packages/client/lib/app/core/widgets/border_glow/stack/movies/border_glow_down_movie.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+class BorderGlowDownMovie {
+  static MovieTween getMovie({
+    required Color lastColor,
+    required double lastWidth,
+    required double lastBlur,
+  }) =>
+      MovieTween()
+        ..scene(
+          begin: Seconds.get(0),
+          end: Seconds.get(2),
+        )
+            .tween(
+              'color',
+              ColorTween(
+                begin: lastColor,
+                end: lastColor,
+              ),
+            )
+            .tween(
+              'width',
+              Tween<double>(
+                begin: lastWidth,
+                end: 0,
+              ),
+            )
+            .tween(
+              'blur',
+              Tween<double>(
+                begin: lastBlur,
+                end: 0,
+              ),
+            );
+}
---
+++ packages/client/lib/app/core/widgets/border_glow/stack/movies/border_glow_up_movie.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+class BorderGlowUpMovie {
+  static MovieTween get movie => MovieTween()
+    ..scene(
+      begin: Seconds.get(0),
+      end: Seconds.get(2),
+    )
+        .tween(
+          'width',
+          Tween<double>(
+            begin: 0,
+            end: 1.0,
+          ),
+        )
+        .tween(
+          'blur',
+          Tween<double>(
+            begin: 0,
+            end: 40.0,
+          ),
+        )
+        .tween(
+          'color',
+          ColorTween(
+            begin: Colors.transparent,
+            end: const Color(0x6305FF00),
+          ),
+        )
+    ..scene(
+      begin: Seconds.get(5),
+      end: Seconds.get(7),
+      // begin: Seconds.get(60),
+      // end: Seconds.get(62),
+    ).tween(
+      'color',
+      ColorTween(
+        begin: const Color(0x6305FF00),
+        end: const Color(0x80FAFF00),
+      ),
+    )
+    ..scene(
+      begin: Seconds.get(10),
+      end: Seconds.get(12),
+      // begin: Seconds.get(90),
+      // end: Seconds.get(92),
+    ).tween(
+      'color',
+      ColorTween(
+        begin: const Color(0x80FAFF00),
+        end: const Color(0x82FF0000),
+      ),
+    )
+    ..scene(
+      begin: Seconds.get(15),
+      end: Seconds.get(35),
+      // begin: Seconds.get(90),
+      // end: Seconds.get(120),
+    )
+        .tween(
+          'blur',
+          Tween<double>(
+            begin: 40,
+            end: 160.0,
+          ),
+        )
+        .tween(
+          'color',
+          ColorTween(
+            begin: const Color(0x82FF0000),
+            end: const Color(0x91FF0000),
+          ),
+        )
+    ..scene(
+      begin: Seconds.get(35),
+      end: Seconds.get(62),
+      // begin: Seconds.get(290),
+      // end: Seconds.get(300),
+    )
+        .tween(
+          'blur',
+          Tween<double>(
+            begin: 160.0,
+            end: 400.0,
+          ),
+        )
+        .tween(
+          'color',
+          ColorTween(
+            begin: const Color(0x91FF0000),
+            end: const Color(0xFFFF0000),
+          ),
+        );
+}
