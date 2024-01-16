424c5ccc39178276d988053dbc86923867c7b10e
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-16 08:09:33 -0600

    1️⃣🚚hold rename trackerStore to store

--- packages/client/lib/app/core/widgets/gestures/stack/presentation/widgets/hold.dart
+++ packages/client/lib/app/core/widgets/gestures/stack/presentation/widgets/hold.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:nokhte/app/core/widgets/gestures/stack/presentation/mobx/hold_detector.dart';
 
 class Hold extends StatelessWidget {
-  final HoldDetector trackerStore;
+  final HoldDetector store;
   final Widget child;
   const Hold({
     super.key,
-    required this.trackerStore,
+    required this.store,
     required this.child,
   });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onLongPressStart: (details) => trackerStore.onHold(),
-      onLongPressEnd: (details) => trackerStore.onLetGo(),
+      onLongPressStart: (details) => store.onHold(),
+      onLongPressEnd: (details) => store.onLetGo(),
       child: child,
     );
   }
