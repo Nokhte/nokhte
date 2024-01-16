9463c5eb819f3e262b0c6b5cda82a6f4e780bb79
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-16 06:19:29 -0600

    2️⃣💄 add border_glow widget

---
+++ packages/client/lib/app/core/widgets/border_glow/stack/presentation/widget/border_glow.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:nokhte/app/core/hooks/hooks.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
+import 'dart:ui';
+
+class BorderGlow extends HookWidget {
+  final BorderGlowStore store;
+  const BorderGlow({
+    super.key,
+    required this.store,
+  });
+
+  @override
+  Widget build(BuildContext context) => Observer(
+        builder: (context) => AnimatedOpacity(
+          onEnd: () => store.toggleHasFadedIn(),
+          opacity: useWidgetOpacity(store.showWidget),
+          duration: Seconds.get(1),
+          child: CustomAnimationBuilder(
+            tween: store.movie,
+            control: store.control,
+            duration: store.movie.duration,
+            onCompleted: () => store.onCompleted(),
+            builder: (context, value, child) {
+              store.setAnimationValues(
+                color: value.get('color'),
+                blur: value.get('blur'),
+                width: value.get('width'),
+              );
+              return Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: value.get('color'),
+                    width: value.get('width'),
+                  ),
+                ),
+                child: ClipPath(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: value.get('blur'),
+                      sigmaY: value.get('blur'),
+                    ),
+                    child: Container(),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      );
+}
