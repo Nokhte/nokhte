8c4d920ac60e4e0218a83ce58244113e9f8ef1b3
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-17 15:15:30 -0600

    2️⃣🧮 add get_room_information & remove the rest of the agora logic

--- packages/client/lib/app/core/modules/voice_call/domain/logic/logic.dart
+++ packages/client/lib/app/core/modules/voice_call/domain/logic/logic.dart
@@ -1,7 +1,1 @@
-export 'get_agora_token.dart';
-export 'join_call.dart';
-export "init_agora_sdk.dart";
-export 'leave_call.dart';
-export 'mute_local_audio.dart';
-export 'unmute_local_audio.dart';
-export 'get_channel_id.dart';
+export "get_room_information.dart";
