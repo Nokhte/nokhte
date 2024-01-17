cf682b8299239541550b565d48f10eda3558ab48
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-17 15:17:05 -0600

    2️⃣🧮 remove agora methods from the contract interface & impl

--- packages/client/lib/app/core/modules/voice_call/data/contracts/voice_call_contract_impl.dart
+++ packages/client/lib/app/core/modules/voice_call/data/contracts/voice_call_contract_impl.dart
@@ -1,9 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:nokhte/app/core/constants/failure_constants.dart';
-import 'package:nokhte/app/core/error/failure.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/network/network_info.dart';
-import 'package:nokhte/app/core/types/call_status.dart';
 import 'package:nokhte/app/core/modules/voice_call/data/data.dart';
 import 'package:nokhte/app/core/modules/voice_call/domain/domain.dart';
 
@@ -16,69 +14,18 @@ class VoiceCallContractImpl implements VoiceCallContract {
   });
 
   @override
-  Future<Either<Failure, AgoraCallTokenModel>> getAgoraToken({
-    required String channelName,
-  }) async {
+  getRoomInformation(NoParams params) async {
     if (await networkInfo.isConnected) {
-      final res = await remoteSource.getAgoraToken(channelName: channelName);
-      return Right(AgoraCallTokenModel.fromTokenServer(res));
+      final token = await remoteSource.getMeetingToken();
+      final meetingId = await remoteSource.getMeetingId();
+      final userUID = remoteSource.getUserUID();
+      return Right(RoomInformationModel(
+        token: token,
+        meetingId: meetingId,
+        userUID: userUID,
+      ));
     } else {
       return Left(FailureConstants.internetConnectionFailure);
     }
   }
-
-  @override
-  Future<Either<Failure, ChannelIdEntity>> getChannelId() async {
-    if (await networkInfo.isConnected) {
-      final res = await remoteSource.getCollaboratorInfo();
-      return Right(ChannelIdModel.fromSupabase(res));
-    } else {
-      return Left(FailureConstants.internetConnectionFailure);
-    }
-  }
-
-  @override
-  Future<Either<Failure, CallStatusModel>> joinCall(
-      String token, String channelId) async {
-    if (await networkInfo.isConnected) {
-      await remoteSource.joinCall(token: token, channelId: channelId);
-      return const Right(CallStatusModel(callStatus: CallStatus.joining));
-    } else {
-      return Left(FailureConstants.internetConnectionFailure);
-    }
-  }
-
-  @override
-  Future<Either<Failure, CallStatusModel>> leaveCall() async {
-    if (await networkInfo.isConnected) {
-      await remoteSource.leaveCall();
-      return const Right(CallStatusModel(callStatus: CallStatus.leaving));
-    } else {
-      return Left(FailureConstants.internetConnectionFailure);
-    }
-  }
-
-  @override
-  Future<Either<Failure, LocalAudioStreamStatusModel>> muteLocalAudio() async {
-    if (await networkInfo.isConnected) {
-      await remoteSource.muteLocalAudio();
-      return const Right(LocalAudioStreamStatusModel(isMuted: true));
-    } else {
-      return Left(FailureConstants.internetConnectionFailure);
-    }
-  }
-
-  @override
-  Future<Either<Failure, LocalAudioStreamStatusModel>>
-      unmuteLocalAudio() async {
-    if (await networkInfo.isConnected) {
-      await remoteSource.unmuteLocalAudio();
-      return const Right(LocalAudioStreamStatusModel(isMuted: false));
-    } else {
-      return Left(FailureConstants.internetConnectionFailure);
-    }
-  }
-
-  @override
-  initAgoraSdk(NoParams params) async => await remoteSource.initAgoraSdk();
 }
