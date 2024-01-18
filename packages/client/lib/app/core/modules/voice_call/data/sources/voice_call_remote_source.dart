fa24daec28e84a99896cfc491ddef64deb556ea6
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-18 06:28:14 -0600

    Revert "2️⃣🧮 update voice_call_remote_source to complete the agora migration"
    This reverts commit bda7664e034b874e8c9243842ab18539d7b9cc70.

--- packages/client/lib/app/core/modules/voice_call/data/sources/voice_call_remote_source.dart
+++ packages/client/lib/app/core/modules/voice_call/data/sources/voice_call_remote_source.dart
@@ -1,30 +1,96 @@
+import 'package:http/http.dart';
+import 'package:nokhte/app/core/utilities/utilities.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:nokhte_backend/token_server/token_server.dart';
 import 'package:nokhte_backend/tables/existing_collaborations.dart';
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 
 abstract class VoiceCallRemoteSource {
-  Future<String> getMeetingId();
+  Future<Response> getAgoraToken({required String channelName});
 
-  Future<String> getMeetingToken();
+  Future joinCall({required String token, required String channelId});
 
-  String getUserUID();
+  Future leaveCall();
+
+  Future muteLocalAudio();
+
+  Future unmuteLocalAudio();
+
+  Future<List<dynamic>> getCollaboratorInfo();
+
+  Future<RtcEngine> initAgoraSdk();
 }
 
 class VoiceCallRemoteSourceImpl implements VoiceCallRemoteSource {
   final SupabaseClient supabase;
-  final ExistingCollaborationsQueries queries;
+  final ExistingCollaborationsQueries existingCollaborationsQueries;
   final String currentUserUID;
+  final int currentAgoraUID;
+  final RtcEngine agoraEngine;
 
   VoiceCallRemoteSourceImpl({
     required this.supabase,
-  })  : currentUserUID = supabase.auth.currentUser?.id ?? '',
-        queries = ExistingCollaborationsQueries(supabase: supabase);
+  })  : agoraEngine = createAgoraRtcEngine(),
+        currentUserUID = supabase.auth.currentUser?.id ?? '',
+        currentAgoraUID = MiscAlgos.postgresUIDToInt(
+          supabase.auth.currentUser?.id ?? '',
+        ),
+        existingCollaborationsQueries =
+            ExistingCollaborationsQueries(supabase: supabase);
 
   @override
-  Future<String> getMeetingId() async => await queries.getMeetingId();
+  Future<RtcEngine> initAgoraSdk() async {
+    await agoraEngine.initialize(
+        const RtcEngineContext(appId: '050b22b688f44464b2533fac484c7300'));
+    await muteLocalAudio();
+    return agoraEngine;
+  }
 
   @override
-  Future<String> getMeetingToken() async => await queries.getMeetingToken();
+  Future<Response> getAgoraToken({
+    required String channelName,
+  }) async {
+    return await TokenServices.getAgoraToken(
+      currentUserUID: currentAgoraUID,
+      channelName: channelName,
+    );
+  }
 
   @override
-  String getUserUID() => currentUserUID;
+  Future<void> joinCall(
+      {required String token, required String channelId}) async {
+    ChannelMediaOptions options = const ChannelMediaOptions(
+      clientRoleType: ClientRoleType.clientRoleBroadcaster,
+    );
+    return await agoraEngine.joinChannel(
+      token: token,
+      channelId: channelId,
+      uid: currentAgoraUID,
+      options: options,
+    );
+  }
+
+  @override
+  Future<void> leaveCall() async {
+    return await agoraEngine.leaveChannel();
+  }
+
+  @override
+  Future<List> getCollaboratorInfo() async {
+    return [
+      await existingCollaborationsQueries.getCollaborations(
+          filterForIsActive: true),
+      currentUserUID
+    ];
+  }
+
+  @override
+  Future muteLocalAudio() async {
+    return await agoraEngine.muteLocalAudioStream(true);
+  }
+
+  @override
+  Future unmuteLocalAudio() async {
+    return await agoraEngine.muteLocalAudioStream(false);
+  }
 }
