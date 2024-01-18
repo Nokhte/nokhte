62d882c6fb74ec4a1ee566fa880ed859c0be317d
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-18 06:28:15 -0600

    Revert "2️⃣🧮 remove agora methods from the contract interface & impl"
    This reverts commit cf682b8299239541550b565d48f10eda3558ab48.

--- packages/client/lib/app/core/modules/voice_call/domain/contracts/voice_call_contract.dart
+++ packages/client/lib/app/core/modules/voice_call/domain/contracts/voice_call_contract.dart
@@ -1,9 +1,17 @@
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:dartz/dartz.dart';
 import 'package:nokhte/app/core/error/failure.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/modules/voice_call/domain/domain.dart';
 
 abstract class VoiceCallContract {
-  Future<Either<Failure, RoomInformationEntity>> getRoomInformation(
-      NoParams params);
+  Future<Either<Failure, AgoraCallTokenEntity>> getAgoraToken(
+      {required String channelName});
+  Future<Either<Failure, ChannelIdEntity>> getChannelId();
+  Future<Either<Failure, CallStatusEntity>> joinCall(
+      String token, String channelId);
+  Future<Either<Failure, CallStatusEntity>> leaveCall();
+  Future<Either<Failure, LocalAudioStreamStatusEntity>> muteLocalAudio();
+  Future<Either<Failure, LocalAudioStreamStatusEntity>> unmuteLocalAudio();
+  Future<RtcEngine> initAgoraSdk(NoParams params);
 }
