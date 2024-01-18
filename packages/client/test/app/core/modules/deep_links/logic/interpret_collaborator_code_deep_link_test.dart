cf93d5fecbe3d5324bfcf18098a1ca327c6008f1
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-18 17:24:34 -0600

    2️⃣✅ add test for interpret_collaborator_code_deep_link_test

---
+++ packages/client/test/app/core/modules/deep_links/logic/interpret_collaborator_code_deep_link_test.dart
@@ -0,0 +1,60 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nokhte/app/core/modules/deep_links/domain/domain.dart';
+
+void main() {
+  late InterpretCollaboratorCodeDeepLink tLogic;
+
+  setUp(() {
+    tLogic = InterpretCollaboratorCodeDeepLink();
+  });
+
+  group("call", () {
+    group("hasGoneThroughInvitationFlow", () {
+      test("params.userUID == params.collaboratorUID", () {
+        final res = tLogic(const InterpretCollaboratorCodeDeepLinkParams(
+          collaboratorUID: "userUID",
+          userUID: 'userUID',
+          hasGoneThroughInvitationFlow: true,
+          hasSentAnInvitation: true,
+          wantsToRepeatInvitationFlow: true,
+        ));
+        expect(res, const InterpretedDeepLinkEntity());
+      });
+
+      test("params.userUID != params.collaboratorUID", () {
+        final res = tLogic(const InterpretCollaboratorCodeDeepLinkParams(
+          collaboratorUID: "collaboratorUID",
+          userUID: 'userUID',
+          hasGoneThroughInvitationFlow: true,
+          hasSentAnInvitation: true,
+          wantsToRepeatInvitationFlow: true,
+        ));
+        expect(
+            res,
+            const InterpretedDeepLinkEntity(
+              path: "/collaboration/",
+              additionalMetadata: {
+                "hasSentAnInvitation": true,
+                "collaboratorUID": "collaboratorUID",
+              },
+            ));
+      });
+    });
+
+    test("!hasGoneThroughInvitationFlow", () {
+      final res = tLogic(const InterpretCollaboratorCodeDeepLinkParams(
+        collaboratorUID: "collaboratorUID",
+        userUID: 'userUID',
+        hasGoneThroughInvitationFlow: false,
+        hasSentAnInvitation: false,
+        wantsToRepeatInvitationFlow: false,
+      ));
+      expect(
+          res,
+          const InterpretedDeepLinkEntity(additionalMetadata: {
+            "hasSentAnInvitation": false,
+            "collaboratorUID": "collaboratorUID",
+          }));
+    });
+  });
+}
