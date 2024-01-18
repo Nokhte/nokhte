c33b40d7f037a6263a3579fe3f5d8b6be43e741b
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-18 16:30:11 -0600

    2️⃣✅ bulk up coverage in home_screen_coordinator_test

--- packages/client/test/app/modules/home/presentation/mobx/coordinator/home_screen_coordinator_test.dart
+++ packages/client/test/app/modules/home/presentation/mobx/coordinator/home_screen_coordinator_test.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: unused_local_variable
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/modules/user_information/mobx/mobx.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/home/presentation/presentation.dart';
@@ -39,5 +41,38 @@ void main() {
       userInformation: mockUserInformation,
       widgets: mockWidgets,
     );
+  });
+
+  group('constructor', () {
+    test("hasGoneThroughInvitationFlow", () async {
+      when(mockUserInformation.getUserInfo.hasGoneThroughInvitationFlow)
+          .thenAnswer((realInvocation) => true);
+      await testStore.constructor();
+      verify(mockWidgets.initReactors(testStore.repeatTheFlow));
+      verify(mockWidgets.constructor());
+      verify(testStore.deleteUnconsecratedCollaborations(NoParams()));
+      verify(mockUserInformation.getUserInfo(NoParams()));
+      verify(mockWidgets.postInvitationFlowConstructor());
+      verify(mockGetExistingCollaborationInfo(NoParams()));
+      verify(mockAddNameToDatabase(NoParams()));
+    });
+
+    test("!hasGoneThroughInvitationFlow", () async {
+      when(mockUserInformation.getUserInfo.hasGoneThroughInvitationFlow)
+          .thenAnswer((realInvocation) => false);
+      await testStore.constructor();
+      verify(mockWidgets.initReactors(testStore.repeatTheFlow));
+      verify(mockWidgets.constructor());
+      verify(testStore.deleteUnconsecratedCollaborations(NoParams()));
+      verify(mockUserInformation.getUserInfo(NoParams()));
+      verify(mockWidgets.invitationFlowConstructor());
+      verify(mockGetExistingCollaborationInfo(NoParams()));
+      verify(mockAddNameToDatabase(NoParams()));
+    });
+  });
+
+  test("repeatTheFlow", () async {
+    await testStore.repeatTheFlow();
+    verify(mockUserInformation.updateWantsToRepeatInvitationFlow(true));
   });
 }
