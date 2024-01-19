07867b5eae70c455f19bd278212ad452e63b48a9
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-19 12:50:40 -0600

    2️⃣♻️ send_deep_link (statusEntity => bool)

--- packages/client/lib/app/core/modules/deep_links/domain/logic/send_deep_link.dart
+++ packages/client/lib/app/core/modules/deep_links/domain/logic/send_deep_link.dart
@@ -1,8 +1,7 @@
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/modules/deep_links/domain/domain.dart';
 
-class SendDeepLink
-    implements AbstractFutureLogic<DeepLinkSendStatusEntity, String> {
+class SendDeepLink implements AbstractFutureLogic<bool, String> {
   final DeepLinksContract contract;
 
   SendDeepLink({required this.contract});
