fadf94c1975fd3a9e1768253f426734b64850f3e
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-19 13:16:43 -0600

    2️⃣🔥 who_has_the_question_entity + model

--- packages/client/lib/app/modules/purpose_session/data/models/who_has_the_question_model.dart
+++
@@ -1,5 +0,0 @@
-import 'package:nokhte/app/modules/purpose_session/domain/domain.dart';
-
-class WhoHasTheQuestionModel extends WhoHasTheQuestionEntity {
-  const WhoHasTheQuestionModel({required super.hasTheQuestion});
-}
--- packages/client/lib/app/modules/purpose_session/domain/entities/who_has_the_question_entity.dart
+++
@@ -1,9 +0,0 @@
-import 'package:nokhte/app/core/entities/base_single_boolean_entity.dart';
-
-class WhoHasTheQuestionEntity extends BaseSingleBooleanEntity {
-  const WhoHasTheQuestionEntity({required bool hasTheQuestion})
-      : super(isTrue: hasTheQuestion);
-
-  factory WhoHasTheQuestionEntity.initial() =>
-      const WhoHasTheQuestionEntity(hasTheQuestion: false);
-}
