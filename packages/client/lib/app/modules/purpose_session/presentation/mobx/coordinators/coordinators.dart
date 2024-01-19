374cb64a887cb78db3303f1439fcf553b0ce6e6b
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-19 07:57:45 -0600

    2️⃣🛢️ update purpose session exports

--- packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/coordinators.dart
+++ packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/coordinators.dart
@@ -1,2 +1,4 @@
-export "./phase0/phase0.dart";
-export "./phase1/phase1.dart";
+export "phase0/phase0.dart";
+export "phase1/phase1.dart";
+export "phase2/phase2.dart";
+export "phase3/phase3.dart";
--- packages/client/lib/app/modules/purpose_session/presentation/views/views.dart
+++ packages/client/lib/app/modules/purpose_session/presentation/views/views.dart
@@ -1,2 +1,4 @@
 export 'purpose_session_phase0_greeter.dart';
 export 'purpose_session_phase1_consultation.dart';
+export "purpose_session_phase2_reflection.dart";
+export 'purpose_session_phase3_expand_their_ideas.dart';
