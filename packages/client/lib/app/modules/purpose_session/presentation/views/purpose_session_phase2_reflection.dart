28213330b082302d308a2c4972eb8c5242c9c5d5
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-19 07:57:12 -0600

    2️⃣🧱 add phase 2 ui infra

---
+++ packages/client/lib/app/modules/purpose_session/presentation/views/purpose_session_phase2_reflection.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nokhte/app/modules/purpose_session/presentation/mobx/coordinators/phase2/phase2.dart';
+
+class PurposeSessionPhase2Reflection extends HookWidget {
+  final PurposeSessionPhase2Coordinator coordinator;
+  const PurposeSessionPhase2Reflection({
+    super.key,
+    required this.coordinator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
