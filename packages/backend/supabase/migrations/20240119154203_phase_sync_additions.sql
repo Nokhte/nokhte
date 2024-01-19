6998235837f90a0e26258fd44e0be8f3ee202554
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-19 11:18:19 -0600

    1️⃣♻️ change default phases to 1

--- packages/backend/supabase/migrations/20240119154203_phase_sync_additions.sql
+++ packages/backend/supabase/migrations/20240119154203_phase_sync_additions.sql
@@ -1,4 +1,4 @@
-alter table "public"."existing_collaborations" add column "current_phases" smallint[] not null default '{0,0}'::smallint[];
+alter table "public"."existing_collaborations" add column "current_phases" smallint[] not null default '{1,1}'::smallint[];
 
 alter table "public"."existing_collaborations" add constraint "existing_collaborations_current_phases_check" CHECK ((array_length(current_phases, 1) = 2)) not valid;
 
