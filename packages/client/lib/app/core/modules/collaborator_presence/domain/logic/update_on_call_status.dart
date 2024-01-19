74bada15aa7ce0f9f1a7af62aae1a182e0085470
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-19 09:25:14 -0600

    2️⃣♻️ update logic methods to use update presence property params

--- packages/client/lib/app/core/modules/collaborator_presence/domain/logic/update_on_call_status.dart
+++ packages/client/lib/app/core/modules/collaborator_presence/domain/logic/update_on_call_status.dart
@@ -1,27 +1,14 @@
-import 'package:equatable/equatable.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/modules/collaborator_presence/domain/domain.dart';
 
 class UpdateOnCallStatus
     implements
-        AbstractFutureLogic<CallUpdateStatusEntity, UpdateOnCallStatusParams> {
+        AbstractFutureLogic<CallUpdateStatusEntity,
+            UpdatePresencePropertyParams> {
   final CollaboratorPresenceContract contract;
 
   UpdateOnCallStatus({required this.contract});
 
   @override
   call(params) async => await contract.updateOnCallStatus(params);
-}
-
-class UpdateOnCallStatusParams extends Equatable {
-  final bool shouldUpdateCollaboratorsIndex;
-  final bool newStatus;
-
-  const UpdateOnCallStatusParams({
-    required this.newStatus,
-    this.shouldUpdateCollaboratorsIndex = false,
-  });
-
-  @override
-  List<Object> get props => [];
 }
