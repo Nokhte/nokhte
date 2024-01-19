cbc613e35fb6c5d78c9cebea1c8670bcd2a0fce1
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-19 13:15:06 -0600

    2️⃣♻️ add_name_to_database (entity => bool)

--- packages/client/lib/app/modules/home/domain/logic/add_name_to_database.dart
+++ packages/client/lib/app/modules/home/domain/logic/add_name_to_database.dart
@@ -1,17 +1,11 @@
-import 'package:dartz/dartz.dart';
-import 'package:nokhte/app/core/error/failure.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/modules/home/domain/contracts/home_contract.dart';
-import 'package:nokhte/app/modules/home/domain/entities/name_creation_status_entity.dart';
 
-class AddNameToDatabase
-    extends AbstractFutureLogic<NameCreationStatusEntity, NoParams> {
+class AddNameToDatabase extends AbstractFutureLogic<bool, NoParams> {
   final HomeContract contract;
 
   AddNameToDatabase({required this.contract});
 
   @override
-  Future<Either<Failure, NameCreationStatusEntity>> call(
-          NoParams params) async =>
-      await contract.addNameToDatabase(params);
+  call(NoParams params) async => await contract.addNameToDatabase(params);
 }
