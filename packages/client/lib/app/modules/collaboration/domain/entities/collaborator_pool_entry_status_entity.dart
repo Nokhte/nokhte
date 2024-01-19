f0ea208451d39e85b3d6b258704620676090c788
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-19 13:08:28 -0600

    2️⃣🔥 collaboration module entities & models

--- packages/client/lib/app/modules/collaboration/domain/entities/collaborator_pool_entry_status_entity.dart
+++
@@ -1,9 +0,0 @@
-import 'package:nokhte/app/core/entities/base_single_boolean_entity.dart';
-
-class CollaboratorPoolEntryStatusEntity extends BaseSingleBooleanEntity {
-  const CollaboratorPoolEntryStatusEntity({required hasEntered})
-      : super(isTrue: hasEntered);
-
-  factory CollaboratorPoolEntryStatusEntity.initial() =>
-      const CollaboratorPoolEntryStatusEntity(hasEntered: false);
-}
--- packages/client/lib/app/modules/collaboration/domain/entities/collaborator_pool_exit_status_entity.dart
+++
@@ -1,9 +0,0 @@
-import 'package:nokhte/app/core/entities/base_single_boolean_entity.dart';
-
-class CollaboratorPoolExitStatusEntity extends BaseSingleBooleanEntity {
-  const CollaboratorPoolExitStatusEntity({required hasLeft})
-      : super(isTrue: hasLeft);
-
-  factory CollaboratorPoolExitStatusEntity.initial() =>
-      const CollaboratorPoolExitStatusEntity(hasLeft: false);
-}
