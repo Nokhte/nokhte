727d7f78a7b59b92f63e34d00cf08b3472b81447
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-19 07:57:28 -0600

    2️⃣🧱 add phase 3 ui infra

---
+++ packages/client/lib/app/modules/purpose_session/presentation/views/purpose_session_phase3_expand_their_ideas.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nokhte/app/modules/purpose_session/presentation/presentation.dart';
+
+class PurposeSessionPhase3ExpandTheirIdeas extends HookWidget {
+  final PurposeSessionPhase3Coordinator coordinator;
+  const PurposeSessionPhase3ExpandTheirIdeas({
+    super.key,
+    required this.coordinator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
