07f4b66e31af74638961f5d4003ba74b5df0d292
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-19 08:00:03 -0600

    2️⃣🧮 add widget stores to main stores as dependency (phase 2 & 3)

--- packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase2/purpose_session_phase2_coordinator.dart
+++ packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase2/purpose_session_phase2_coordinator.dart
@@ -1,10 +1,17 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'purpose_session_phase2_widgets_coordinator.dart';
 part 'purpose_session_phase2_coordinator.g.dart';
 
 class PurposeSessionPhase2Coordinator = _PurposeSessionPhase2CoordinatorBase
     with _$PurposeSessionPhase2Coordinator;
 
 abstract class _PurposeSessionPhase2CoordinatorBase
-    extends BaseWidgetsCoordinator with Store {}
+    extends BaseWidgetsCoordinator with Store {
+  final PurposeSessionPhase2WidgetsCoordinator widgets;
+
+  _PurposeSessionPhase2CoordinatorBase({
+    required this.widgets,
+  });
+}
