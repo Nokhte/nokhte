6710f0e71f87f7deafaa3a11baf7733a227b3bdc
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-19 13:13:48 -0600

    2️⃣♻️ home_contract + impl (entity => bool)

--- packages/client/lib/app/modules/home/data/contracts/home_contract_impl.dart
+++ packages/client/lib/app/modules/home/data/contracts/home_contract_impl.dart
@@ -1,12 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:nokhte/app/core/constants/failure_constants.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/mixins/response_to_status.dart';
 import 'package:nokhte/app/modules/home/data/models/models.dart';
 import 'package:nokhte/app/modules/home/data/sources/home_remote_source.dart';
 import 'package:nokhte/app/core/network/network_info.dart';
 import 'package:nokhte/app/modules/home/domain/domain.dart';
 
-class HomeContractImpl implements HomeContract {
+class HomeContractImpl with ResponseToStatus implements HomeContract {
   final HomeRemoteSource remoteSource;
   final NetworkInfo networkInfo;
 
@@ -19,7 +20,7 @@ class HomeContractImpl implements HomeContract {
   addNameToDatabase(NoParams params) async {
     if (await networkInfo.isConnected) {
       final res = await remoteSource.addNamesToDatabase();
-      return Right(NameCreationStatusModel.fromSupabase(res));
+      return Right(fromSupabase(res));
     } else {
       return Left(FailureConstants.internetConnectionFailure);
     }
@@ -32,8 +33,8 @@ class HomeContractImpl implements HomeContract {
           await remoteSource.checkIfTheyHaveACollaboration();
       final initRes = ExistingCollaborationsInfoModel.fromSupabase(
         collaborationRes: collaborationRes,
-        perspectivesSessionRes: [],
-        committedPurposeRes: [],
+        perspectivesSessionRes: const [],
+        committedPurposeRes: const [],
       );
       if (initRes.hasACollaboration) {
         final perspectivesRes =
--- packages/client/lib/app/modules/home/domain/contracts/home_contract.dart
+++ packages/client/lib/app/modules/home/domain/contracts/home_contract.dart
@@ -4,8 +4,7 @@ import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/modules/home/domain/entities/entities.dart';
 
 abstract class HomeContract {
-  Future<Either<Failure, NameCreationStatusEntity>> addNameToDatabase(
-      NoParams params);
+  Future<Either<Failure, bool>> addNameToDatabase(NoParams params);
   Future<Either<Failure, ExistingCollaborationsInfoEntity>>
       getExistingCollaborationInfo(NoParams params);
 }
