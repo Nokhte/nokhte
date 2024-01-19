ac6d4f00659eb07d7cd7d41ae40d7f92afdecdc8
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-19 13:08:52 -0600

    2️⃣♻️ collaboration_contract & impl (entities => bool)

--- packages/client/lib/app/modules/collaboration/data/contract/collaboration_contract_impl.dart
+++ packages/client/lib/app/modules/collaboration/data/contract/collaboration_contract_impl.dart
@@ -1,12 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:nokhte/app/core/constants/constants.dart';
-import 'package:nokhte/app/core/error/failure.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/mixins/response_to_status.dart';
 import 'package:nokhte/app/modules/collaboration/domain/domain.dart';
 import 'package:nokhte/app/modules/collaboration/data/data.dart';
 import 'package:nokhte/app/core/network/network_info.dart';
 
-class CollaborationContractImpl implements CollaborationContract {
+class CollaborationContractImpl
+    with ResponseToStatus
+    implements CollaborationContract {
   final CollaborationRemoteSource remoteSource;
   final NetworkInfo networkInfo;
 
@@ -14,41 +16,37 @@ class CollaborationContractImpl implements CollaborationContract {
       {required this.remoteSource, required this.networkInfo});
 
   @override
-  Future<Either<Failure, CollaboratorStreamCancellationStatusEntity>>
-      cancelCollaboratorSearchStream(NoParams params) async {
+  cancelCollaboratorSearchStream(NoParams params) async {
     if (await networkInfo.isConnected) {
       final res = remoteSource.cancelStream();
-      return Right(CollaboratorStreamCancellationStatusModel.fromSupabase(res));
+      return Right(res);
     } else {
       return Left(FailureConstants.internetConnectionFailure);
     }
   }
 
   @override
-  Future<Either<Failure, CollaboratorPoolEntryStatusEntity>>
-      enterTheCollaboratorPool(String collaboratorUID) async {
+  enterTheCollaboratorPool(String collaboratorUID) async {
     if (await networkInfo.isConnected) {
       final res = await remoteSource.enterThePool(collaboratorUID);
-      return Right(CollaboratorPoolEntryStatusModel.fromSupabase(res));
+      return Right(fromFunctionResponse(res));
     } else {
       return Left(FailureConstants.internetConnectionFailure);
     }
   }
 
   @override
-  Future<Either<Failure, CollaboratorPoolExitStatusEntity>>
-      exitCollaboratorPool(NoParams params) async {
+  exitCollaboratorPool(NoParams params) async {
     if (await networkInfo.isConnected) {
       final res = await remoteSource.exitThePool();
-      return Right(CollaboratorPoolExitStatusModel.fromSupabase(res));
+      return Right(fromFunctionResponse(res));
     } else {
       return Left(FailureConstants.internetConnectionFailure);
     }
   }
 
   @override
-  Future<Either<Failure, Stream<bool>>> getCollaboratorSearchStatus(
-      NoParams params) async {
+  getCollaboratorSearchStatus(NoParams params) async {
     if (await networkInfo.isConnected) {
       return Right(remoteSource.getCollaboratorSearchStatus());
     } else {
--- packages/client/lib/app/modules/collaboration/domain/contract/collaboration_contract.dart
+++ packages/client/lib/app/modules/collaboration/domain/contract/collaboration_contract.dart
@@ -1,16 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:nokhte/app/core/error/failure.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
-import 'package:nokhte/app/modules/collaboration/domain/domain.dart';
-import 'package:nokhte/app/modules/collaboration/domain/entities/entities.dart';
 
 abstract class CollaborationContract {
-  Future<Either<Failure, CollaboratorPoolEntryStatusEntity>>
-      enterTheCollaboratorPool(String collaboratorUID);
-  Future<Either<Failure, CollaboratorPoolExitStatusEntity>>
-      exitCollaboratorPool(NoParams params);
+  Future<Either<Failure, bool>> enterTheCollaboratorPool(
+      String collaboratorUID);
+  Future<Either<Failure, bool>> exitCollaboratorPool(NoParams params);
   Future<Either<Failure, Stream<bool>>> getCollaboratorSearchStatus(
       NoParams params);
-  Future<Either<Failure, CollaboratorStreamCancellationStatusEntity>>
-      cancelCollaboratorSearchStream(NoParams params);
+  Future<Either<Failure, bool>> cancelCollaboratorSearchStream(NoParams params);
 }
