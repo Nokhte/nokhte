7a41eb06829c0ebfda4f8c450c90ee5bb65c7bea
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-19 12:59:50 -0600

    2️⃣🔥 has_sent_an_invitation_update_status_entity & model

--- packages/client/lib/app/core/modules/user_information/domain/entities/has_sent_an_invitation_update_status_entity.dart
+++
@@ -1,9 +0,0 @@
-import 'package:nokhte/app/core/entities/base_single_boolean_entity.dart';
-
-class HasSentAnInvitationUpdateStatusEntity extends BaseSingleBooleanEntity {
-  const HasSentAnInvitationUpdateStatusEntity({required bool isUpdated})
-      : super(isTrue: isUpdated);
-
-  static HasSentAnInvitationUpdateStatusEntity get initial =>
-      const HasSentAnInvitationUpdateStatusEntity(isUpdated: false);
-}
