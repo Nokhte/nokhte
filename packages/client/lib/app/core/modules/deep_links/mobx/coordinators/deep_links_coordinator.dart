bb903ae5e3c20120292fd42e728abda9458eb7bd
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-20 17:52:46 -0600

    5️⃣☯️send_deep_link_store && get_deep_link_url_store => coordinator

--- packages/client/lib/app/core/modules/deep_links/mobx/coordinators/deep_links_coordinator.dart
+++ packages/client/lib/app/core/modules/deep_links/mobx/coordinators/deep_links_coordinator.dart
@@ -1,23 +1,45 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
-import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/modules/deep_links/constants/types/deep_link_types.dart';
+import 'package:nokhte/app/core/modules/deep_links/domain/domain.dart';
 import 'package:nokhte/app/core/modules/deep_links/mobx/mobx.dart';
 part 'deep_links_coordinator.g.dart';
 
 class DeepLinksCoordinator = _DeepLinksCoordinatorBase
     with _$DeepLinksCoordinator;
 
-abstract class _DeepLinksCoordinatorBase extends Equatable with Store {
-  final GetDeepLinkURLStore getDeepLinkURL;
-  final ListenForOpenedDeepLinkStore listenForOpenedDeepLink;
-  final SendDeepLinkStore sendDeepLink;
+abstract class _DeepLinksCoordinatorBase extends BaseMobxDBStore with Store {
+  final GetDeepLinkURL getDeepLinkURL;
+  final SendDeepLink sendDeepLink;
+  final ListenForOpenedDeepLinkStore listenForOpenedDeepLinkStore;
 
   _DeepLinksCoordinatorBase({
     required this.getDeepLinkURL,
-    required this.listenForOpenedDeepLink,
     required this.sendDeepLink,
+    required this.listenForOpenedDeepLinkStore,
   });
 
-  @override
-  List<Object> get props => [];
+  @observable
+  String link = "";
+
+  @observable
+  bool isShared = false;
+
+  @action
+  getDeepLink(DeepLinkTypes params) async {
+    state = StoreState.loading;
+    final res = await getDeepLinkURL(params);
+    res.fold((failure) => errorUpdater(failure), (url) => link = url);
+    state = StoreState.loaded;
+  }
+
+  @action
+  share(String link) async {
+    state = StoreState.loading;
+    final res = await sendDeepLink(link);
+    res.fold((failure) => errorUpdater(failure),
+        (isShared) => this.isShared = isShared);
+    state = StoreState.loaded;
+  }
 }
