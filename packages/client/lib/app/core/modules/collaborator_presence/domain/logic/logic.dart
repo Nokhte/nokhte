d20a3ebf0374eb0a318b6d5d6dc38cdaf37f8569
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-20 12:25:15 -0600

    2️⃣🛢️ collaborator_presence exports

--- packages/client/lib/app/core/modules/collaborator_presence/domain/logic/logic.dart
+++ packages/client/lib/app/core/modules/collaborator_presence/domain/logic/logic.dart
@@ -1,4 +1,5 @@
 export "params/params.dart";
+export "cancel_session_metadata_stream.dart";
 export 'get_session_metadata.dart';
 export "update_current_phase.dart";
 export 'update_on_call_status.dart';
--- packages/client/lib/app/core/modules/collaborator_presence/mobx/main/main.dart
+++ packages/client/lib/app/core/modules/collaborator_presence/mobx/main/main.dart
@@ -1,3 +1,4 @@
+export "cancel_session_metadata_stream_store.dart";
 export 'get_session_metadata_store.dart';
 export 'update_on_call_status_store.dart';
 export 'update_online_status_store.dart';
