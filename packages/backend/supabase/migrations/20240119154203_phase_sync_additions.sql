4c2303c1f377204a7a27ff1c1169586d1231b4d6
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-20 08:58:30 -0600

    2️⃣🗃️ make current_phase float4

--- packages/backend/supabase/migrations/20240119154203_phase_sync_additions.sql
+++ packages/backend/supabase/migrations/20240119154203_phase_sync_additions.sql
@@ -3,3 +3,7 @@ alter table "public"."existing_collaborations" add column "current_phases" small
 alter table "public"."existing_collaborations" add constraint "existing_collaborations_current_phases_check" CHECK ((array_length(current_phases, 1) = 2)) not valid;
 
 alter table "public"."existing_collaborations" validate constraint "existing_collaborations_current_phases_check";
+
+alter table "public"."existing_collaborations" alter column "current_phases" set default '{1.0,1.0}'::real[];
+
+alter table "public"."existing_collaborations" alter column "current_phases" set data type real[] using "current_phases"::real[];
