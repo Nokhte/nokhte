8adc2dd563beb9ed92ddeff659ab4b49f53a3704
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-20 17:59:44 -0600

    2️⃣🛢️ user_information/mobx/main exports

--- packages/client/lib/app/core/modules/user_information/mobx/main/main.dart
+++ packages/client/lib/app/core/modules/user_information/mobx/main/main.dart
@@ -1,4 +1,1 @@
 export './get_user_info_store.dart';
-export './update_has_gone_through_invitation_flow_store.dart';
-export './update_has_sent_an_invitation_store.dart';
-export 'update_wants_to_repeat_invitation_flow_store.dart';
