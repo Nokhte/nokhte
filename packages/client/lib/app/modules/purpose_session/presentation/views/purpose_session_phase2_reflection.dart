f43a56401ffa287d57ca1184994f4959e799804a
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-20 15:32:29 -0600

    2️⃣🧮 add widgets to phase 2

--- packages/client/lib/app/modules/purpose_session/presentation/views/purpose_session_phase2_reflection.dart
+++ packages/client/lib/app/modules/purpose_session/presentation/views/purpose_session_phase2_reflection.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/purpose_session/presentation/mobx/coordinators/phase2/phase2.dart';
 
 class PurposeSessionPhase2Reflection extends HookWidget {
@@ -11,6 +13,43 @@ class PurposeSessionPhase2Reflection extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    useEffect(() {
+      coordinator.constructor();
+      return null;
+    }, []);
+    useOnAppLifecycleStateChange(
+        (previous, current) => coordinator.onAppLifeCycleStateChange(
+              current,
+              onResumed: () => null,
+              onInactive: () => null,
+              onDetached: () => null,
+            ));
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Swipe(
+        store: coordinator.swipe,
+        child: MultiHitStack(
+          children: [
+            FullScreen(
+              child: BeachWaves(
+                store: coordinator.widgets.beachWaves,
+              ),
+            ),
+            Center(
+                child: SmartText(
+              store: coordinator.widgets.primarySmartText,
+              bottomPadding: 180,
+              opacityDuration: Seconds.get(1),
+            )),
+            // add solo doc
+            WifiDisconnectOverlay(
+              store: coordinator.widgets.wifiDisconnectOverlay,
+            ),
+
+            // FullScreen(child: BeachWaves(store: coo,))
+          ],
+        ),
+      ),
+    );
   }
 }
