d833cb4473caaa27533470c7af7c75730b6e9b5e
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-20 17:57:05 -0600

    5️⃣☯️ condense update stores into user_information_coordinator

--- packages/client/lib/app/core/modules/user_information/mobx/coordinators/user_information_coordinator.dart
+++ packages/client/lib/app/core/modules/user_information/mobx/coordinators/user_information_coordinator.dart
@@ -1,26 +1,64 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
-import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/modules/user_information/domain/domain.dart';
 import 'package:nokhte/app/core/modules/user_information/mobx/mobx.dart';
 part 'user_information_coordinator.g.dart';
 
 class UserInformationCoordinator = _UserInformationCoordinatorBase
     with _$UserInformationCoordinator;
 
-abstract class _UserInformationCoordinatorBase extends Equatable with Store {
+abstract class _UserInformationCoordinatorBase extends BaseMobxDBStore
+    with Store {
   final GetUserInfoStore getUserInfo;
-  final UpdateHasGoneThroughInvitationFlowStore
-      updateHasGoneThroughInvitationFlow;
-  final UpdateHasSentAnInvitationStore updateHasSentAnInvitation;
-  final UpdateWantsToRepeatInvitationFlowStore
-      updateWantsToRepeatInvitationFlow;
+  final UpdateHasGoneThroughInvitationFlow
+      updateHasGoneThroughInvitationFlowLogic;
+  final UpdateHasSentAnInvitation updateHasSentAnInvitationLogic;
+  final UpdateWantsToRepeatInvitationFlow
+      updateWantsToRepeatInvitationFlowLogic;
 
   _UserInformationCoordinatorBase({
     required this.getUserInfo,
-    required this.updateHasGoneThroughInvitationFlow,
-    required this.updateHasSentAnInvitation,
-    required this.updateWantsToRepeatInvitationFlow,
+    required this.updateHasGoneThroughInvitationFlowLogic,
+    required this.updateHasSentAnInvitationLogic,
+    required this.updateWantsToRepeatInvitationFlowLogic,
   });
+
+  @observable
+  bool invitationFlowCompletionStatusIsUpdated = false;
+
+  @observable
+  bool invitationSendStatusIsUpdated = false;
+
+  @observable
+  bool invitationRepeatStatusIsUpdated = false;
+
+  @action
+  updateHasGoneThroughInvitationFlow(bool newStatus) async {
+    state = StoreState.loading;
+    final res = await updateHasGoneThroughInvitationFlowLogic(newStatus);
+    res.fold((failure) => errorUpdater(failure),
+        (status) => invitationFlowCompletionStatusIsUpdated = status);
+    state = StoreState.loaded;
+  }
+
+  @action
+  updateHasSentAnInvitation(bool newStatus) async {
+    state = StoreState.loading;
+    final res = await updateHasSentAnInvitationLogic(newStatus);
+    res.fold((failure) => errorUpdater(failure),
+        (status) => invitationSendStatusIsUpdated = status);
+    state = StoreState.loaded;
+  }
+
+  @action
+  updateWantsToRepeatInvitationFlow(bool newStatus) async {
+    state = StoreState.loading;
+    final res = await updateWantsToRepeatInvitationFlowLogic(newStatus);
+    res.fold((failure) => errorUpdater(failure),
+        (status) => invitationRepeatStatusIsUpdated = status);
+    state = StoreState.loaded;
+  }
 
   @override
   List<Object> get props => [];
--- packages/client/lib/app/core/modules/user_information/mobx/coordinators/user_information_coordinator.g.dart
+++ packages/client/lib/app/core/modules/user_information/mobx/coordinators/user_information_coordinator.g.dart
@@ -9,10 +9,97 @@ part of 'user_information_coordinator.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$UserInformationCoordinator on _UserInformationCoordinatorBase, Store {
+  late final _$invitationFlowCompletionStatusIsUpdatedAtom = Atom(
+      name:
+          '_UserInformationCoordinatorBase.invitationFlowCompletionStatusIsUpdated',
+      context: context);
+
+  @override
+  bool get invitationFlowCompletionStatusIsUpdated {
+    _$invitationFlowCompletionStatusIsUpdatedAtom.reportRead();
+    return super.invitationFlowCompletionStatusIsUpdated;
+  }
+
+  @override
+  set invitationFlowCompletionStatusIsUpdated(bool value) {
+    _$invitationFlowCompletionStatusIsUpdatedAtom
+        .reportWrite(value, super.invitationFlowCompletionStatusIsUpdated, () {
+      super.invitationFlowCompletionStatusIsUpdated = value;
+    });
+  }
+
+  late final _$invitationSendStatusIsUpdatedAtom = Atom(
+      name: '_UserInformationCoordinatorBase.invitationSendStatusIsUpdated',
+      context: context);
+
+  @override
+  bool get invitationSendStatusIsUpdated {
+    _$invitationSendStatusIsUpdatedAtom.reportRead();
+    return super.invitationSendStatusIsUpdated;
+  }
+
+  @override
+  set invitationSendStatusIsUpdated(bool value) {
+    _$invitationSendStatusIsUpdatedAtom
+        .reportWrite(value, super.invitationSendStatusIsUpdated, () {
+      super.invitationSendStatusIsUpdated = value;
+    });
+  }
+
+  late final _$invitationRepeatStatusIsUpdatedAtom = Atom(
+      name: '_UserInformationCoordinatorBase.invitationRepeatStatusIsUpdated',
+      context: context);
+
+  @override
+  bool get invitationRepeatStatusIsUpdated {
+    _$invitationRepeatStatusIsUpdatedAtom.reportRead();
+    return super.invitationRepeatStatusIsUpdated;
+  }
+
+  @override
+  set invitationRepeatStatusIsUpdated(bool value) {
+    _$invitationRepeatStatusIsUpdatedAtom
+        .reportWrite(value, super.invitationRepeatStatusIsUpdated, () {
+      super.invitationRepeatStatusIsUpdated = value;
+    });
+  }
+
+  late final _$updateHasGoneThroughInvitationFlowAsyncAction = AsyncAction(
+      '_UserInformationCoordinatorBase.updateHasGoneThroughInvitationFlow',
+      context: context);
+
+  @override
+  Future updateHasGoneThroughInvitationFlow(bool newStatus) {
+    return _$updateHasGoneThroughInvitationFlowAsyncAction
+        .run(() => super.updateHasGoneThroughInvitationFlow(newStatus));
+  }
+
+  late final _$updateHasSentAnInvitationAsyncAction = AsyncAction(
+      '_UserInformationCoordinatorBase.updateHasSentAnInvitation',
+      context: context);
+
+  @override
+  Future updateHasSentAnInvitation(bool newStatus) {
+    return _$updateHasSentAnInvitationAsyncAction
+        .run(() => super.updateHasSentAnInvitation(newStatus));
+  }
+
+  late final _$updateWantsToRepeatInvitationFlowAsyncAction = AsyncAction(
+      '_UserInformationCoordinatorBase.updateWantsToRepeatInvitationFlow',
+      context: context);
+
+  @override
+  Future updateWantsToRepeatInvitationFlow(bool newStatus) {
+    return _$updateWantsToRepeatInvitationFlowAsyncAction
+        .run(() => super.updateWantsToRepeatInvitationFlow(newStatus));
+  }
+
   @override
   String toString() {
     return '''
-
+invitationFlowCompletionStatusIsUpdated: ${invitationFlowCompletionStatusIsUpdated},
+invitationSendStatusIsUpdated: ${invitationSendStatusIsUpdated},
+invitationRepeatStatusIsUpdated: ${invitationRepeatStatusIsUpdated}
     ''';
   }
 }
