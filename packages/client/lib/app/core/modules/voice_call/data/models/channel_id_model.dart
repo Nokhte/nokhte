4d1f3b4e23232c11b4efe43a2ab80003d0e26021
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-20 18:02:50 -0600

    2️⃣♻️ channel_id_model to return string

--- packages/client/lib/app/core/modules/voice_call/data/models/channel_id_model.dart
+++ packages/client/lib/app/core/modules/voice_call/data/models/channel_id_model.dart
@@ -3,15 +3,15 @@ import 'package:nokhte/app/core/modules/voice_call/domain/domain.dart';
 class ChannelIdModel extends ChannelIdEntity {
   const ChannelIdModel({required super.channelId});
 
-  factory ChannelIdModel.fromSupabase(List sbQueryRes) {
+  static String fromSupabase(List sbQueryRes) {
     if (sbQueryRes.isEmpty) {
-      return const ChannelIdModel(channelId: '');
+      return '';
     } else {
       final String collaboratorOne = sbQueryRes.first.first["collaborator_one"];
       final String collaboratorTwo = sbQueryRes.first.first["collaborator_two"];
       final channelId =
           "${collaboratorOne.substring(0, 19)}${collaboratorTwo.substring(0, 19)}";
-      return ChannelIdModel(channelId: channelId);
+      return channelId;
     }
   }
 }
