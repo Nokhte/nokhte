f68f0ef06b7caa53e4cfe311d24b542eed23b510
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-21 16:43:19 -0600

    flesh out phase 2 screen w/ necessssary widgets

--- packages/client/lib/app/modules/purpose_session/presentation/views/purpose_session_phase2_reflection.dart
+++ packages/client/lib/app/modules/purpose_session/presentation/views/purpose_session_phase2_reflection.dart
@@ -1,5 +1,7 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nokhte/app/core/hooks/hooks.dart';
 import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/purpose_session/presentation/mobx/coordinators/phase2/phase2.dart';
@@ -13,6 +15,7 @@ class PurposeSessionPhase2Reflection extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = useSquareSize(relativeLength: .20);
     useEffect(() {
       coordinator.constructor();
       return null;
@@ -38,9 +41,40 @@ class PurposeSessionPhase2Reflection extends HookWidget {
             Center(
                 child: SmartText(
               store: coordinator.widgets.primarySmartText,
-              bottomPadding: 180,
+              bottomPadding: 0,
               opacityDuration: Seconds.get(1),
             )),
+            Center(
+              child: TextEditor(
+                store: coordinator.widgets.textEditor,
+                bottomPadding: 220,
+                fadeInDuration: Seconds.get(1),
+              ),
+            ),
+            GestureCross(
+              showGlowAndOutline: true,
+              config: GestureCrossConfiguration(
+                top: Right(
+                  NokhteGradientConfig(
+                    gradientType: NokhteGradientTypes.vibrantBlue,
+                  ),
+                ),
+              ),
+              size: size,
+              store: coordinator.widgets.unsubmittedGestureCross,
+            ),
+            GestureCross(
+              showGlowAndOutline: true,
+              config: GestureCrossConfiguration(
+                bottom: Right(
+                  NokhteGradientConfig(
+                    gradientType: NokhteGradientTypes.vibrantBlue,
+                  ),
+                ),
+              ),
+              size: size,
+              store: coordinator.widgets.submittedGestureCross,
+            ),
             // add solo doc
             WifiDisconnectOverlay(
               store: coordinator.widgets.wifiDisconnectOverlay,
