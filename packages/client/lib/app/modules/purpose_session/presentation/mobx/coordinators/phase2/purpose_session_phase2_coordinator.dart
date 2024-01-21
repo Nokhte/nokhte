ecb00fbd6fdc34a678ad4f29644252ce333a17fc
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-21 16:41:12 -0600

    add phase 2 logic minus times up to main coordinator

--- packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase2/purpose_session_phase2_coordinator.dart
+++ packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase2/purpose_session_phase2_coordinator.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/modules/solo_docs/domain/domain.dart';
+import 'package:nokhte/app/core/modules/solo_docs/mobx/mobx.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/purpose_session/presentation/mobx/mobx.dart';
 part 'purpose_session_phase2_coordinator.g.dart';
@@ -12,14 +15,61 @@ abstract class _PurposeSessionPhase2CoordinatorBase extends BaseCoordinator
     with Store {
   final SwipeDetector swipe;
   final PurposeSessionPhase2WidgetsCoordinator widgets;
+  final SoloDocsCoordinator soloDoc;
 
   _PurposeSessionPhase2CoordinatorBase({
     required this.widgets,
     required this.swipe,
+    required this.soloDoc,
   });
 
+  @observable
+  bool canSwipeUp = false;
+
+  @observable
+  bool hasSwipedUp = false;
+
   @action
-  constructor() {
-    widgets.constructor();
+  constructor() async {
+    widgets.constructor(
+      onKeyboardDown: onKeyboardDown,
+      onKeyboardUp: onKeyboardUp,
+    );
+    await soloDoc.create(SoloDocTypes.purpose);
+    swipeReactor();
   }
+
+  @action
+  onKeyboardUp() {
+    canSwipeUp = false;
+  }
+
+  @action
+  onKeyboardDown() {
+    if (widgets.isOverThreeWords) {
+      canSwipeUp = true;
+    }
+  }
+
+  swipeReactor() => reaction((p0) => swipe.directionsType, (p0) async {
+        switch (p0) {
+          case GestureDirections.up:
+            if (canSwipeUp &&
+                !hasSwipedUp &&
+                widgets.hasCompletedIntroduction) {
+              hasSwipedUp = true;
+              canSwipeUp = false;
+              await soloDoc.submit(widgets.textEditor.controller.text);
+              widgets.onSwipeUp();
+            }
+          case GestureDirections.down:
+            if (hasSwipedUp) {
+              widgets.onSwipeDown();
+              hasSwipedUp = false;
+              canSwipeUp = true;
+            }
+          default:
+            break;
+        }
+      });
 }
--- packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase2/purpose_session_phase2_coordinator.g.dart
+++ packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase2/purpose_session_phase2_coordinator.g.dart
@@ -10,16 +10,72 @@ part of 'purpose_session_phase2_coordinator.dart';
 
 mixin _$PurposeSessionPhase2Coordinator
     on _PurposeSessionPhase2CoordinatorBase, Store {
+  late final _$canSwipeUpAtom = Atom(
+      name: '_PurposeSessionPhase2CoordinatorBase.canSwipeUp',
+      context: context);
+
+  @override
+  bool get canSwipeUp {
+    _$canSwipeUpAtom.reportRead();
+    return super.canSwipeUp;
+  }
+
+  @override
+  set canSwipeUp(bool value) {
+    _$canSwipeUpAtom.reportWrite(value, super.canSwipeUp, () {
+      super.canSwipeUp = value;
+    });
+  }
+
+  late final _$hasSwipedUpAtom = Atom(
+      name: '_PurposeSessionPhase2CoordinatorBase.hasSwipedUp',
+      context: context);
+
+  @override
+  bool get hasSwipedUp {
+    _$hasSwipedUpAtom.reportRead();
+    return super.hasSwipedUp;
+  }
+
+  @override
+  set hasSwipedUp(bool value) {
+    _$hasSwipedUpAtom.reportWrite(value, super.hasSwipedUp, () {
+      super.hasSwipedUp = value;
+    });
+  }
+
+  late final _$constructorAsyncAction = AsyncAction(
+      '_PurposeSessionPhase2CoordinatorBase.constructor',
+      context: context);
+
+  @override
+  Future constructor() {
+    return _$constructorAsyncAction.run(() => super.constructor());
+  }
+
   late final _$_PurposeSessionPhase2CoordinatorBaseActionController =
       ActionController(
           name: '_PurposeSessionPhase2CoordinatorBase', context: context);
 
   @override
-  dynamic constructor() {
+  dynamic onKeyboardUp() {
     final _$actionInfo = _$_PurposeSessionPhase2CoordinatorBaseActionController
-        .startAction(name: '_PurposeSessionPhase2CoordinatorBase.constructor');
+        .startAction(name: '_PurposeSessionPhase2CoordinatorBase.onKeyboardUp');
     try {
-      return super.constructor();
+      return super.onKeyboardUp();
+    } finally {
+      _$_PurposeSessionPhase2CoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic onKeyboardDown() {
+    final _$actionInfo =
+        _$_PurposeSessionPhase2CoordinatorBaseActionController.startAction(
+            name: '_PurposeSessionPhase2CoordinatorBase.onKeyboardDown');
+    try {
+      return super.onKeyboardDown();
     } finally {
       _$_PurposeSessionPhase2CoordinatorBaseActionController
           .endAction(_$actionInfo);
@@ -29,7 +85,8 @@ mixin _$PurposeSessionPhase2Coordinator
   @override
   String toString() {
     return '''
-
+canSwipeUp: ${canSwipeUp},
+hasSwipedUp: ${hasSwipedUp}
     ''';
   }
 }
