95b89641bf594c8781ea79ecb8e9a02932156f7d
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-21 16:39:57 -0600

    2️⃣🧮 remove flutterPlatformWidgets

--- packages/client/lib/app/app_widget.dart
+++ packages/client/lib/app/app_widget.dart
@@ -1,29 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return PlatformProvider(
-      builder: (context) => PlatformTheme(
-        cupertinoLightTheme: const CupertinoThemeData(
-          brightness: Brightness.light,
-          primaryColor: Color.fromRGBO(53, 53, 53, 1.0),
-        ),
-        builder: (context) => PlatformApp.router(
-          debugShowCheckedModeBanner: false,
-          localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
-            DefaultMaterialLocalizations.delegate,
-            DefaultWidgetsLocalizations.delegate,
-            DefaultCupertinoLocalizations.delegate,
-          ],
-          routerConfig: Modular.routerConfig,
-        ),
-      ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+        DefaultMaterialLocalizations.delegate,
+        DefaultWidgetsLocalizations.delegate,
+        DefaultCupertinoLocalizations.delegate,
+      ],
+      routerConfig: Modular.routerConfig,
     );
   }
 }
