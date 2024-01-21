d8a67971426d787d0c84ee26f8632b580666e49e
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-21 16:40:52 -0600

    2️⃣🧮 add logic for phase 2 (minus timer stuff)

--- packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase2/purpose_session_phase2_widgets_coordinator.dart
+++ packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase2/purpose_session_phase2_widgets_coordinator.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/extensions/extensions.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/beach_widgets/shared/types/types.dart';
 import 'package:nokhte/app/core/widgets/smart_text/stack/constants/constants.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
@@ -15,38 +17,138 @@ abstract class _PurposeSessionPhase2WidgetsCoordinatorBase
   final BeachWavesStore beachWaves;
   final SmartTextStore primarySmartText;
   final WifiDisconnectOverlayStore wifiDisconnectOverlay;
-  final GestureCrossStore gestureCross;
+  final GestureCrossStore unsubmittedGestureCross;
+  final GestureCrossStore submittedGestureCross;
+  final TextEditorStore textEditor;
 
   _PurposeSessionPhase2WidgetsCoordinatorBase({
     required this.beachWaves,
     required this.primarySmartText,
     required this.wifiDisconnectOverlay,
-    required this.gestureCross,
+    required this.submittedGestureCross,
+    required this.unsubmittedGestureCross,
+    required this.textEditor,
   });
 
   @action
-  constructor() {
+  constructor({
+    required Function onKeyboardUp,
+    required Function onKeyboardDown,
+  }) {
     beachWaves
         .setMovieMode(BeachWaveMovieModes.suspendedAtTheDepthsToTimesUpStart);
+    unsubmittedGestureCross.setPhase2PurposeSession();
+    submittedGestureCross.setPhase2PurposeSession();
     beachWaves.currentStore.initMovie(NoParams());
     primarySmartText.setMessagesData(MessagesData.purposeSessionPhase2List);
     primarySmartText.startRotatingText();
-    initReactors();
+    textEditor.toggleWidgetVisibility();
+    initReactors(
+      onKeyboardDown: onKeyboardDown,
+      onKeyboardUp: onKeyboardUp,
+    );
   }
 
+  @observable
+  bool isOverThreeWords = false;
+
+  @observable
+  bool isFirstTimeOverThreeWords = true;
+
+  @observable
+  bool hasCompletedIntroduction = false;
+
   @action
-  initReactors() {
+  initReactors({
+    required Function onKeyboardUp,
+    required Function onKeyboardDown,
+  }) {
     smartTextIndexReactor();
     wifiDisconnectOverlay.connectionReactor(
       onConnected: () {},
       onDisconnected: () {},
     );
+    textEditor.focusNode.addListener(() {
+      if (!textEditor.focusNode.hasFocus) {
+        if (textEditor.controller.text
+            .split(' ')
+            .length
+            .isGreaterThanOrEqualTo(3)) {
+          isOverThreeWords = true;
+        } else {
+          isOverThreeWords = false;
+        }
+        onKeyboardDown();
+      } else {
+        onKeyboardUp();
+      }
+    });
+    threeWordsReactor();
+    submittedGestureCrossReactor();
+    unsubmittedGestureCrossReactor();
   }
+
+  @action
+  onSwipeUp() {
+    textEditor.toggleWidgetVisibility();
+    unsubmittedGestureCross.initMoveAndRegenerate(CircleOffsets.top);
+    if (primarySmartText.currentIndex == 3) {
+      primarySmartText.startRotatingText(isResuming: true);
+    }
+  }
+
+  @action
+  onSwipeDown() {
+    submittedGestureCross.initMoveAndRegenerate(CircleOffsets.bottom);
+    if (primarySmartText.currentIndex == 4) {
+      primarySmartText.startRotatingText(isResuming: true);
+    }
+  }
+
+  submittedGestureCrossReactor() =>
+      reaction((p0) => submittedGestureCross.centerCrossNokhte.movieStatus,
+          (p0) {
+        if (p0 == MovieStatus.finished) {
+          unsubmittedGestureCross.toggleAll();
+          textEditor.toggleWidgetVisibility();
+          submittedGestureCross.toggleAll();
+        }
+      });
+
+  unsubmittedGestureCrossReactor() =>
+      reaction((p0) => unsubmittedGestureCross.centerCrossNokhte.movieStatus,
+          (p0) {
+        if (p0 == MovieStatus.finished) {
+          unsubmittedGestureCross.toggleAll();
+          submittedGestureCross.toggleAll();
+        }
+      });
 
   smartTextIndexReactor() =>
       reaction((p0) => primarySmartText.currentIndex, (p0) {
         if (p0 == 1) {
-          // show document
+          textEditor.toggleWidgetVisibility();
+        } else if (p0 == 3 && !hasCompletedIntroduction) {
+          hasCompletedIntroduction = true;
+          textEditor.toggleWidgetVisibility();
+        } else if (p0 == 5) {
+          primarySmartText.setCurrentIndex(3);
+        }
+      });
+
+  threeWordsReactor() => reaction((p0) => isOverThreeWords, (p0) {
+        if (p0) {
+          if (isFirstTimeOverThreeWords) {
+            textEditor.toggleWidgetVisibility();
+            unsubmittedGestureCross.toggleAll();
+
+            primarySmartText.startRotatingText(isResuming: true);
+            isFirstTimeOverThreeWords = false;
+          } else {
+            unsubmittedGestureCross.toggleAll();
+          }
+        } else {
+          unsubmittedGestureCross.toggleAll();
         }
       });
 }
--- packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase2/purpose_session_phase2_widgets_coordinator.g.dart
+++ packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase2/purpose_session_phase2_widgets_coordinator.g.dart
@@ -10,20 +10,77 @@ part of 'purpose_session_phase2_widgets_coordinator.dart';
 
 mixin _$PurposeSessionPhase2WidgetsCoordinator
     on _PurposeSessionPhase2WidgetsCoordinatorBase, Store {
+  late final _$isOverThreeWordsAtom = Atom(
+      name: '_PurposeSessionPhase2WidgetsCoordinatorBase.isOverThreeWords',
+      context: context);
+
+  @override
+  bool get isOverThreeWords {
+    _$isOverThreeWordsAtom.reportRead();
+    return super.isOverThreeWords;
+  }
+
+  @override
+  set isOverThreeWords(bool value) {
+    _$isOverThreeWordsAtom.reportWrite(value, super.isOverThreeWords, () {
+      super.isOverThreeWords = value;
+    });
+  }
+
+  late final _$isFirstTimeOverThreeWordsAtom = Atom(
+      name:
+          '_PurposeSessionPhase2WidgetsCoordinatorBase.isFirstTimeOverThreeWords',
+      context: context);
+
+  @override
+  bool get isFirstTimeOverThreeWords {
+    _$isFirstTimeOverThreeWordsAtom.reportRead();
+    return super.isFirstTimeOverThreeWords;
+  }
+
+  @override
+  set isFirstTimeOverThreeWords(bool value) {
+    _$isFirstTimeOverThreeWordsAtom
+        .reportWrite(value, super.isFirstTimeOverThreeWords, () {
+      super.isFirstTimeOverThreeWords = value;
+    });
+  }
+
+  late final _$hasCompletedIntroductionAtom = Atom(
+      name:
+          '_PurposeSessionPhase2WidgetsCoordinatorBase.hasCompletedIntroduction',
+      context: context);
+
+  @override
+  bool get hasCompletedIntroduction {
+    _$hasCompletedIntroductionAtom.reportRead();
+    return super.hasCompletedIntroduction;
+  }
+
+  @override
+  set hasCompletedIntroduction(bool value) {
+    _$hasCompletedIntroductionAtom
+        .reportWrite(value, super.hasCompletedIntroduction, () {
+      super.hasCompletedIntroduction = value;
+    });
+  }
+
   late final _$_PurposeSessionPhase2WidgetsCoordinatorBaseActionController =
       ActionController(
           name: '_PurposeSessionPhase2WidgetsCoordinatorBase',
           context: context);
 
   @override
-  dynamic constructor() {
+  dynamic constructor(
+      {required Function onKeyboardUp, required Function onKeyboardDown}) {
     final _$actionInfo =
         _$_PurposeSessionPhase2WidgetsCoordinatorBaseActionController
             .startAction(
                 name:
                     '_PurposeSessionPhase2WidgetsCoordinatorBase.constructor');
     try {
-      return super.constructor();
+      return super.constructor(
+          onKeyboardUp: onKeyboardUp, onKeyboardDown: onKeyboardDown);
     } finally {
       _$_PurposeSessionPhase2WidgetsCoordinatorBaseActionController
           .endAction(_$actionInfo);
@@ -31,14 +88,45 @@ mixin _$PurposeSessionPhase2WidgetsCoordinator
   }
 
   @override
-  dynamic initReactors() {
+  dynamic initReactors(
+      {required Function onKeyboardUp, required Function onKeyboardDown}) {
     final _$actionInfo =
         _$_PurposeSessionPhase2WidgetsCoordinatorBaseActionController
             .startAction(
                 name:
                     '_PurposeSessionPhase2WidgetsCoordinatorBase.initReactors');
     try {
-      return super.initReactors();
+      return super.initReactors(
+          onKeyboardUp: onKeyboardUp, onKeyboardDown: onKeyboardDown);
+    } finally {
+      _$_PurposeSessionPhase2WidgetsCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic onSwipeUp() {
+    final _$actionInfo =
+        _$_PurposeSessionPhase2WidgetsCoordinatorBaseActionController
+            .startAction(
+                name: '_PurposeSessionPhase2WidgetsCoordinatorBase.onSwipeUp');
+    try {
+      return super.onSwipeUp();
+    } finally {
+      _$_PurposeSessionPhase2WidgetsCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic onSwipeDown() {
+    final _$actionInfo =
+        _$_PurposeSessionPhase2WidgetsCoordinatorBaseActionController
+            .startAction(
+                name:
+                    '_PurposeSessionPhase2WidgetsCoordinatorBase.onSwipeDown');
+    try {
+      return super.onSwipeDown();
     } finally {
       _$_PurposeSessionPhase2WidgetsCoordinatorBaseActionController
           .endAction(_$actionInfo);
@@ -48,7 +136,9 @@ mixin _$PurposeSessionPhase2WidgetsCoordinator
   @override
   String toString() {
     return '''
-
+isOverThreeWords: ${isOverThreeWords},
+isFirstTimeOverThreeWords: ${isFirstTimeOverThreeWords},
+hasCompletedIntroduction: ${hasCompletedIntroduction}
     ''';
   }
 }
