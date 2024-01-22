cc0aa5449eb39c653a22f9fd73accfbcbd3c5282
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-22 07:56:52 -0600

    transition to times up in phase 0

--- packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase0/purpose_session_phase0_widgets_coordinator.dart
+++ packages/client/lib/app/modules/purpose_session/presentation/mobx/coordinators/phase0/purpose_session_phase0_widgets_coordinator.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
 import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widget_constants.dart';
@@ -44,7 +45,9 @@ abstract class _PurposeSessionPhase0WidgetsCoordinatorBase
 
   @action
   constructor() {
-    beachWaves.setMovieMode(BeachWaveMovieModes.suspendedAtTheDepths);
+    beachWaves
+        .setMovieMode(BeachWaveMovieModes.suspendedAtTheDepthsToTimesUpStart);
+    beachWaves.currentStore.initMovie(NoParams());
     primarySmartText
         .setMessagesData(MessagesData.primaryPurposeSessionPhase0List);
     primarySmartText.startRotatingText();
