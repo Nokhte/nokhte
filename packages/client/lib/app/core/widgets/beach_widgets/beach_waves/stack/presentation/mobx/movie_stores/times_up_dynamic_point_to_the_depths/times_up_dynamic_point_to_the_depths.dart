aac0bbf15957c8480bb28df9c4f7d58c49da7b96
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-22 07:50:36 -0600

    refactor times_up_dynamic_point_to_the_depths to use param'zd movie

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/times_up_dynamic_point_to_the_depths/times_up_dynamic_point_to_the_depths.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/times_up_dynamic_point_to_the_depths/times_up_dynamic_point_to_the_depths.dart
@@ -1,2 +1,1 @@
-export './times_up_dynamic_point_to_the_depths_movie.dart';
-export './times_up_dynamic_point_to_the_depths_movie_store.dart';
+export 'times_up_dynamic_point_to_the_depths_movie_store.dart';
--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/times_up_dynamic_point_to_the_depths/times_up_dynamic_point_to_the_depths_movie_store.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/times_up_dynamic_point_to_the_depths/times_up_dynamic_point_to_the_depths_movie_store.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/widgets/beach_widgets/beach_waves/stack/movies/times_up_dynamic_point_to_dynamic_end.dart';
 import 'package:nokhte/app/core/widgets/beach_widgets/shared/shared.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
@@ -13,16 +14,18 @@ abstract class _TimesUpDynamicPointToTheDepthsMovieStoreBase
   _TimesUpDynamicPointToTheDepthsMovieStoreBase()
       : super(
           callsOnCompleteTwice: true,
-          shouldPaintSand: TimesUpDynamicPointToTheDepthsMovie.shouldPaintSand,
+          shouldPaintSand: TimesUpDynamicPointToDynamicEndMovie.shouldPaintSand,
         ) {
-    movie = TimesUpDynamicPointToTheDepthsMovie.getMovie(
+    movie = TimesUpDynamicPointToDynamicEndMovie.getMovie(
+      WaterColorsAndStops.toTheDepthsWater,
       WaterColorsAndStops.toTheDepthsWater,
     );
   }
 
   @override
   initMovie(List<ColorAndStop> param) {
-    movie = TimesUpDynamicPointToTheDepthsMovie.getMovie(param);
+    final toTheDepths = WaterColorsAndStops.toTheDepthsWater;
+    movie = TimesUpDynamicPointToDynamicEndMovie.getMovie(param, toTheDepths);
     control = Control.playFromStart;
   }
 }
