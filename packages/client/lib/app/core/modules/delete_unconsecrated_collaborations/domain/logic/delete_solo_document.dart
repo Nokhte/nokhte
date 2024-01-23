dd7de64d627275cdaae0c72c284f453153b7ecd8
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-23 10:38:48 -0600

    update delete_solo_document to take new params

--- packages/client/lib/app/core/modules/delete_unconsecrated_collaborations/domain/logic/delete_solo_document.dart
+++ packages/client/lib/app/core/modules/delete_unconsecrated_collaborations/domain/logic/delete_solo_document.dart
@@ -1,11 +1,26 @@
+import 'package:equatable/equatable.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/modules/delete_unconsecrated_collaborations/domain/domain.dart';
 
-class DeleteSoloDocument implements AbstractFutureLogic<bool, NoParams> {
+class DeleteSoloDocument
+    implements AbstractFutureLogic<bool, DeleteSoloDocumentParams> {
   final DeleteUnconsecratedCollaborationsContract contract;
 
   DeleteSoloDocument({required this.contract});
 
   @override
   call(params) async => await contract.deleteSoloDocument(params);
+}
+
+class DeleteSoloDocumentParams extends Equatable {
+  final String ownerUID;
+  final String collaboratorUID;
+
+  const DeleteSoloDocumentParams({
+    required this.ownerUID,
+    required this.collaboratorUID,
+  });
+
+  @override
+  List<Object> get props => [ownerUID, collaboratorUID];
 }
