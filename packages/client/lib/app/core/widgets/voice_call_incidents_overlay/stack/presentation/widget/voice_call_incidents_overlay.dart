12fdc0b6ca71e9c6f5cf74c765cf563626415db6
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-24 07:15:04 -0600

    add voice_call_incidents_overlay widget

---
+++ packages/client/lib/app/core/widgets/voice_call_incidents_overlay/stack/presentation/widget/voice_call_incidents_overlay.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:nokhte/app/core/hooks/hooks.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/voice_call_incidents_overlay/stack/presentation/mobx/voice_call_incidents_overlay_store.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+
+class VoiceCallIncidentsOverlay extends StatelessWidget {
+  final VoiceCallIncidentsOverlayStore store;
+  const VoiceCallIncidentsOverlay({
+    super.key,
+    required this.store,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(
+      builder: (context) {
+        return Stack(
+          children: [
+            FullScreen(
+              child: NokhteBlur(
+                store: store.blur,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 150.0),
+              child: Center(
+                child: AnimatedOpacity(
+                  opacity: useWidgetOpacity(store.showJoiningCall),
+                  duration: Seconds.get(1),
+                  child: const Jost(
+                    "Joining Call",
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 150.0),
+              child: Center(
+                child: AnimatedOpacity(
+                  opacity: useWidgetOpacity(store.showWaitingOnCollaborator),
+                  duration: Seconds.get(1),
+                  child: const Jost(
+                    "Waiting For Collaborator",
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
