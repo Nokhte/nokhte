6b6d599fbacaf3e9c10c1ef56f4b45f6dd0aa11e
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-26 14:43:13 -0600

    test: home_screen_coordinator_test

--- packages/client/test/app/modules/home/presentation/mobx/coordinator/home_screen_coordinator_test.dart
+++ packages/client/test/app/modules/home/presentation/mobx/coordinator/home_screen_coordinator_test.dart
@@ -3,27 +3,60 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/modules/deep_links/mobx/coordinators/coordinators.dart';
 import 'package:nokhte/app/core/modules/user_information/mobx/mobx.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/home/presentation/presentation.dart';
 
+import '../../../../shared/shared_mocks.mocks.dart';
 import '../../../fixtures/home_stack_mock_gen.mocks.dart';
 
 void main() {
   late MockAddNameToDatabaseStore mockAddNameToDatabase;
   late MockGetExistingCollaborationsInfoStore mockGetExistingCollaborationInfo;
-  late MockHomeScreenWidgetsCoordinator mockWidgets;
+  late HomeScreenWidgetsCoordinator mockWidgets;
   late HomeScreenCoordinator testStore;
   late UserInformationCoordinator mockUserInformation;
   late MockDeepLinksCoordinator mockDeepLinks;
   late MockDeleteUnconsecratedCollaborationsCoordinator mockDelete;
+  late BeachWavesStore beachWaves;
+  late MockWifiDisconnectOverlayStore wifiDisconnectOverlay;
+  late MockGestureCrossStore gestureCross;
+  late SmartTextStore primarySmartText;
+  late SmartTextStore secondarySmartText;
+  late HomeScreenWidgetsCoordinator widgetsStoreStore;
+  late NokhteBlurStore nokhteBlurStore;
+  late TimeAlignmentModelCoordinator timeModel;
 
   setUp(() {
     mockAddNameToDatabase = MockAddNameToDatabaseStore();
     mockDelete = MockDeleteUnconsecratedCollaborationsCoordinator();
     mockDeepLinks = MockDeepLinksCoordinator();
     mockGetExistingCollaborationInfo = MockGetExistingCollaborationsInfoStore();
-    mockWidgets = MockHomeScreenWidgetsCoordinator();
+    timeModel = TimeAlignmentModelCoordinator(
+      clockFace: ClockFaceStore(),
+      availabilitySectors: AvailabilitySectorsStore(),
+    );
+    secondarySmartText = SmartTextStore();
+    beachWaves = BeachWavesStore();
+    wifiDisconnectOverlay = MockWifiDisconnectOverlayStore();
+    gestureCross = MockGestureCrossStore();
+    nokhteBlurStore = NokhteBlurStore();
+    primarySmartText = SmartTextStore();
+    mockWidgets = HomeScreenWidgetsCoordinator(
+      timeModel: timeModel,
+      nokhteBlur: nokhteBlurStore,
+      deepLinks: DeepLinksCoordinator(
+        getDeepLinkUrlLogic: MockGetDeepLinkURL(),
+        listenForOpenedDeepLinkStore: MockListenForOpenedDeepLinkStore(),
+        sendDeepLinkLogic: MockSendDeepLink(),
+      ),
+      beachWaves: beachWaves,
+      wifiDisconnectOverlay: wifiDisconnectOverlay,
+      gestureCross: gestureCross,
+      primarySmartText: primarySmartText,
+      secondarySmartText: secondarySmartText,
+    );
     mockUserInformation = UserInformationCoordinator(
       getUserInfoStore: MockGetUserInfoStore(),
       updateHasGoneThroughInvitationFlowLogic:
@@ -48,11 +81,8 @@ void main() {
       when(mockUserInformation.getUserInfoStore.hasGoneThroughInvitationFlow)
           .thenAnswer((realInvocation) => true);
       await testStore.constructor();
-      verify(mockWidgets.initReactors(testStore.repeatTheFlow));
-      verify(mockWidgets.constructor());
       verify(testStore.deleteUnconsecratedCollaborations(NoParams()));
       verify(mockUserInformation.getUserInfoStore(NoParams()));
-      verify(mockWidgets.postInvitationFlowConstructor());
       verify(mockGetExistingCollaborationInfo(NoParams()));
       verify(mockAddNameToDatabase(NoParams()));
     });
@@ -61,11 +91,8 @@ void main() {
       when(mockUserInformation.getUserInfoStore.hasGoneThroughInvitationFlow)
           .thenAnswer((realInvocation) => false);
       await testStore.constructor();
-      verify(mockWidgets.initReactors(testStore.repeatTheFlow));
-      verify(mockWidgets.constructor());
       verify(testStore.deleteUnconsecratedCollaborations(NoParams()));
       verify(mockUserInformation.getUserInfoStore(NoParams()));
-      verify(mockWidgets.invitationFlowConstructor());
       verify(mockGetExistingCollaborationInfo(NoParams()));
       verify(mockAddNameToDatabase(NoParams()));
     });
