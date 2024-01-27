42450af428822aa0dcb91d29d91ef341fd01c4cd
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-27 09:22:24 -0600

    move extension creation to seed.sql

--- packages/backend/supabase/migrations/20230613184615_moved_public_db_functions_to_internal_functions_schema.sql
+++ packages/backend/supabase/migrations/20230613184615_moved_public_db_functions_to_internal_functions_schema.sql
@@ -1,4 +1,4 @@
 create schema if not exists "internal_functions" AUTHORIZATION postgres;
 
 -- Enable the "pg_jsonschema" extension
--- create extension IF NOT EXISTS pg_jsonschema with schema extensions;
+
