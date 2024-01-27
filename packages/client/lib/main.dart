4ff466fa393726d2b04ea2724a325fbb8fe169c7
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-27 17:45:52 -0600

    test gitlab sync

--- packages/client/lib/main.dart
+++ packages/client/lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/foundation.dart';
 
 void main() async {
+  // test gitlab sync
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterBranchSdk.init(
     disableTracking: true,
