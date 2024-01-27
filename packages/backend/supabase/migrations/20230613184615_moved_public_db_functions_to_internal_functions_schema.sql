85c6016050a4a5109713684f2c3a6402339bc2ad
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-27 10:47:21 -0600

    try alternate extensions config for ci/cd

--- packages/backend/supabase/migrations/20230613184615_moved_public_db_functions_to_internal_functions_schema.sql
+++ packages/backend/supabase/migrations/20230613184615_moved_public_db_functions_to_internal_functions_schema.sql
@@ -1,4 +1,5 @@
 create schema if not exists "internal_functions" AUTHORIZATION postgres;
 
 -- Enable the "pg_jsonschema" extension
+create schema if not exists extensions;
 create extension pg_jsonschema with schema extensions;
