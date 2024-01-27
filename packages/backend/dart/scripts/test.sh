6816686786d8cf8e8736eabb38e4562cedb801bd
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-27 12:20:06 -0600

    add dart context for test.sh

--- packages/backend/dart/scripts/test.sh
+++ packages/backend/dart/scripts/test.sh
@@ -1,12 +1,12 @@
-flutter test test/01_user_names_table_test.dart
-flutter test test/02_synchronous_search_test.dart
-flutter test test/03_get_agora_token_test.dart
-flutter test test/04_existing_collaborations_table_test.dart
-flutter test test/05_solo_sharable_documents_table_test.dart
-flutter test test/06_finished_collaborative_documents_test.dart
-flutter test test/07_working_collaborative_documents_test.dart
-flutter test test/08_collaborative_scheduling_table_test.dart
-flutter test test/09_p2p_perspectives_tracking_table_test.dart
-# flutter test test/10_working_perspectives_positioning_table_test.dart
-flutter test test/11_individual_sessions_and_perspectives_audio_storage_test.dart
-# flutter test test/12_timer_information_table_test.dart
+flutter test dart/test/01_user_names_table_test.dart
+flutter test dart/test/02_synchronous_search_test.dart
+flutter test dart/test/03_get_agora_token_test.dart
+flutter test dart/test/04_existing_collaborations_table_test.dart
+flutter test dart/test/05_solo_sharable_documents_table_test.dart
+flutter test dart/test/06_finished_collaborative_documents_test.dart
+flutter test dart/test/07_working_collaborative_documents_test.dart
+flutter test dart/test/08_collaborative_scheduling_table_test.dart
+flutter test dart/test/09_p2p_perspectives_tracking_table_test.dart
+# flutter test dart/test/10_working_perspectives_positioning_table_test.dart
+flutter test dart/test/11_individual_sessions_and_perspectives_audio_storage_test.dart
+# flutter test dart/test/12_timer_information_table_test.dart
