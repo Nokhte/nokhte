f8dcfa15c31518e461c302705da8123873589ee6
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-27 22:23:38 -0600

    add nokhte_session_phase0_greeter

---
+++ packages/client/lib/app/modules/nokhte_session/presentation/views/nokhte_session_phase0_greeter.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:nokhte/app/modules/nokhte_session/presentation/presentation.dart';
+
+class NokhteSessionPhase0Greeter extends HookWidget {
+  final NokhteSessionPhase0WidgetsCoordinator coordinator;
+  const NokhteSessionPhase0Greeter({
+    super.key,
+    required this.coordinator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    useEffect(() {
+      coordinator.constructor();
+      return null;
+    }, []);
+    useOnAppLifecycleStateChange(
+        (previous, current) => coordinator.onAppLifeCycleStateChange(
+              current,
+              onResumed: () => coordinator.onResumed(),
+              onInactive: () => coordinator.onInactive(),
+            ));
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: MultiHitStack(
+        children: [
+          FullScreen(
+            child: BeachWaves(
+              store: coordinator.beachWaves,
+            ),
+          ),
+          Center(
+              child: SmartText(
+            store: coordinator.primarySmartText,
+            bottomPadding: 180,
+            opacityDuration: Seconds.get(1),
+          )),
+          WifiDisconnectOverlay(
+            store: coordinator.wifiDisconnectOverlay,
+          ),
+
+          // FullScreen(child: BeachWaves(store: coo,))
+        ],
+      ),
+    );
+  }
+}
