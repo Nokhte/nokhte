79fc735b40602507d3572fd6bf034a78289d5a60
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-29 17:51:08 -0600

    add: skeleton nokhte phase1 coordinators

---
+++ packages/client/lib/app/modules/nokhte_session/presentation/mobx/coordinators/phase1/nokhte_session_phase1_coordinator.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'nokhte_session_phase1_widgets_coordinator.dart';
+part 'nokhte_session_phase1_coordinator.g.dart';
+
+class NokhteSessionPhase1Coordinator = _NokhteSessionPhase1CoordinatorBase
+    with _$NokhteSessionPhase1Coordinator;
+
+abstract class _NokhteSessionPhase1CoordinatorBase extends BaseCoordinator
+    with Store {
+  final NokhteSessionPhase1WidgetsCoordinator widgets;
+
+  _NokhteSessionPhase1CoordinatorBase({
+    required this.widgets,
+  });
+}
