f4f5c9bb799dacc69c98192fae1b8cadcb8c7866
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-28 19:58:17 -0600

    add onShoreToTimesUp to beach_waves_store

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/beach_waves_store.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/beach_waves_store.dart
@@ -21,6 +21,8 @@ abstract class _BeachWavesStoreBase extends Equatable with Store {
   final OnShoreToOceanDiveMovieStore onShoreToOceanDiveMovieStore =
       OnShoreToOceanDiveMovieStore();
   final OnShoreMovieStore onShoreMovieStore = OnShoreMovieStore();
+  final OnShoreToTimesUpMovieStore onShoreToTimesUpStartMovieStore =
+      OnShoreToTimesUpMovieStore();
   final SuspendedAtOceanDiveStore suspendedAtOceanDiveStore =
       SuspendedAtOceanDiveStore();
   final SuspendedAtTheDepthsMovieStore suspendedAtTheDepthsMovieStore =
@@ -58,6 +60,7 @@ abstract class _BeachWavesStoreBase extends Equatable with Store {
       BeachWaveMovieModes.oceanDiveToOnShore: oceanDiveToOnShoreMovieStore,
       BeachWaveMovieModes.onShore: onShoreMovieStore,
       BeachWaveMovieModes.onShoreToOceanDive: onShoreToOceanDiveMovieStore,
+      BeachWaveMovieModes.onShoreToTimesUp: onShoreToTimesUpStartMovieStore,
       BeachWaveMovieModes.none: BaseBeachWaveMovieStore(),
       BeachWaveMovieModes.timesUp: timesUpMovieStore,
       BeachWaveMovieModes.timesUpDynamicPointToTheDepths:
