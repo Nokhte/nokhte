941e22d7f2fd99fa4634e5e0dc18fb868ad22ff0
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-29 10:53:25 -0600

    add active_nokhte_sessions streams & queries

---
+++ packages/backend/dart/lib/tables/_real_time_enabled/active_nokhte_sessions/streams.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class ActiveNokhteSessionsStream {
+  final String userUID;
+  final SupabaseClient supabase;
+
+  static const TABLE_NAME = 'active_nokhte_sessions';
+
+  bool getActiveNokhteSessionCreationListingingStatus = false;
+
+  ActiveNokhteSessionsStream({required this.supabase})
+      : userUID = supabase.auth.currentUser?.id ?? '';
+
+  Stream<bool> getActiveNokhteSessionCreationStatus() async* {
+    getActiveNokhteSessionCreationListingingStatus = true;
+    await for (var event
+        in supabase.from(TABLE_NAME).stream(primaryKey: ['id'])) {
+      if (!getActiveNokhteSessionCreationListingingStatus) {
+        break;
+      }
+      if (event.isNotEmpty) {
+        yield true;
+      } else {
+        yield false;
+      }
+    }
+  }
+}
