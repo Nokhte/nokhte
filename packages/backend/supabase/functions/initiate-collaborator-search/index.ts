4251d912c6fc1f52e9dc761356423c4656482371
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-29 10:51:11 -0600

    feat: make initiate_collaborator_search aware of invitation type

--- packages/backend/supabase/functions/initiate-collaborator-search/index.ts
+++ packages/backend/supabase/functions/initiate-collaborator-search/index.ts
@@ -1,10 +1,10 @@
 import { serve } from "std/server";
-import { isNotEmptyOrNull, isEmptyOrNull } from "../utils/array_utils.ts";
 import { supabaseAdmin } from "../constants/supabase.ts";
-import { randomNumber } from "random_number";
+import { isANokhteInvitation } from "./is_a_nokhte_invitation.ts";
+import { isAColaborationInvitation } from "./is_a_collaboration_invitation.ts";
 
 serve(async (req) => {
-  const { wayfarerUID, queryUID } = await req.json();
+  const { wayfarerUID, queryUID, invitationType } = await req.json();
   await supabaseAdmin
     .from("p2p_collaborator_pool")
     .delete()
@@ -15,51 +15,15 @@ serve(async (req) => {
     .insert({
       wayfarer_uid: wayfarerUID,
       query_uid: queryUID,
+      invitation_type: invitationType,
     })
     .select();
-  const wayfarerQueryRes = await supabaseAdmin
-    .from("p2p_collaborator_pool")
-    .select()
-    .eq("wayfarer_uid", queryUID);
-
-  let returnRes = wayfarerQueryRes.data;
-
-  if (isNotEmptyOrNull(wayfarerQueryRes?.data)) {
-    const whoGetsTheQuestion = randomNumber();
-    const matchedUID = wayfarerQueryRes.data?.[0]?.["wayfarer_uid"];
-    const matchedAdjectiveId =
-      wayfarerQueryRes.data?.[0]?.["wayfarer_adjective_id"];
-    const matchedNounId = wayfarerQueryRes.data?.[0]?.["wayfarer_noun_id"];
-    await supabaseAdmin
-      .from("p2p_collaborator_pool")
-      .update({
-        matched_uid: matchedUID,
-      })
-      .eq("wayfarer_uid", mostRecentEntrant.data?.[0]["wayfarer_uid"]);
-    const checkRespose = await supabaseAdmin
-      .from("existing_collaborations")
-      .select()
-      .or(
-        `collaboration_id.eq.${wayfarerUID}_${matchedUID},collaboration_id.eq.${matchedUID}_${wayfarerUID}`
-      );
-    if (isEmptyOrNull(checkRespose?.data)) {
-      await supabaseAdmin.from("existing_collaborations").insert({
-        collaborator_one: wayfarerUID,
-        collaborator_two: matchedUID,
-        collaboration_id: `${wayfarerUID}_${matchedUID}`,
-        who_gets_the_question: whoGetsTheQuestion % 2 === 0 ? 1 : 2,
-      });
-      await supabaseAdmin
-        .from("p2p_collaborator_pool")
-        .delete()
-        .eq("wayfarer_uid", wayfarerUID);
-      await supabaseAdmin
-        .from("p2p_collaborator_pool")
-        .delete()
-        .eq("wayfarer_uid", matchedUID);
-    }
+  if (invitationType === "NOKHTE_SESSION") {
+    isANokhteInvitation(queryUID, mostRecentEntrant);
+  } else {
+    isAColaborationInvitation(queryUID, wayfarerUID, mostRecentEntrant);
   }
-  returnRes = [
+  const returnRes = [
     {
       status: 200,
       message: "collaboration successfully forged",
