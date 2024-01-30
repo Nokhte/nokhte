16dc67c7e6fe83e83fe6c247ec31bde8c27f0ce0
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-29 19:33:47 -0600

    style: soumi test commit

--- packages/client/lib/main.dart
+++ packages/client/lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/foundation.dart';
 
 void main() async {
+  //test commit
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterBranchSdk.init(
     disableTracking: true,
