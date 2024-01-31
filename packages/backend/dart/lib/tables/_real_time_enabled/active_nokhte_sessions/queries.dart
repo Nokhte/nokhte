3d8b4c40d2e3c2477a85e9d6e2f466929161b20a
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-31 08:43:29 -0600

    add: active_nokhte_sessions_queries for the new columns: 1. current_phases 2. speaker_spotlight 3. is_online

--- packages/backend/dart/lib/tables/_real_time_enabled/active_nokhte_sessions/queries.dart
+++ packages/backend/dart/lib/tables/_real_time_enabled/active_nokhte_sessions/queries.dart
@@ -1,26 +1,127 @@
 // ignore_for_file: constant_identifier_names
 
+import 'constants/constants.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class ActiveNokhteSessionQueries {
-  static const TABLE_NAME = 'active_nokhte_sessions';
-  static const COLLABORATOR_ONE_UID = 'collaborator_one_uid';
-  static const COLLABORATOR_TWO_UID = 'collaborator_two_uid';
-  static const MEETING_UID = 'meeting_uid';
-
+class ActiveNokhteSessionQueries with ActiveNokhteSessionsConstants {
   final SupabaseClient supabase;
-  final String currentUserUID;
+  final String userUID;
+  int userIndex = -1;
+  int collaboratorIndex = -1;
+  String collaboratorUID = '';
+  String userColumn = '';
+  String collaboratorColumn = '';
+
+  computeCollaboratorInformation() async {
+    if (userIndex == -1) {
+      final row = (await select()).first;
+      if (row[COLLABORATOR_ONE_UID] == userUID) {
+        userIndex = 0;
+        collaboratorIndex = 1;
+        collaboratorUID = row[COLLABORATOR_TWO_UID];
+        userColumn = COLLABORATOR_ONE_UID;
+        collaboratorColumn = COLLABORATOR_TWO_UID;
+      } else {
+        userIndex = 1;
+        collaboratorIndex = 0;
+        collaboratorUID = row[COLLABORATOR_ONE_UID];
+        userColumn = COLLABORATOR_TWO_UID;
+        collaboratorColumn = COLLABORATOR_ONE_UID;
+      }
+    }
+  }
 
   ActiveNokhteSessionQueries({
     required this.supabase,
-  }) : currentUserUID = supabase.auth.currentUser?.id ?? '';
+  }) : userUID = supabase.auth.currentUser?.id ?? '';
 
-  select(String meetingUID) async =>
-      await supabase.from(TABLE_NAME).select().eq(MEETING_UID, meetingUID);
+  select() async => await supabase.from(TABLE_NAME).select();
 
   delete() async => await supabase
       .from(TABLE_NAME)
       .delete()
-      .or('collaborator_one_uid.eq.$currentUserUID, collaborator_two_uid.eq.$currentUserUID')
+      .or('collaborator_one_uid.eq.$userUID, collaborator_two_uid.eq.$userUID')
       .select();
+
+  Future _getProperty(String property) async =>
+      (await select()).first[property];
+
+  Future<String> getMeetingUID() async => await _getProperty(MEETING_UID);
+  Future<String> getCollaboratorOne() async =>
+      await _getProperty(COLLABORATOR_ONE_UID);
+  Future<String> getCollaboratorTwo() async =>
+      await _getProperty(COLLABORATOR_TWO_UID);
+  Future<List> getWhoIsOnline() async => await _getProperty(IS_ONLINE);
+  Future<String?> getSpeakerSpotlight() async =>
+      await _getProperty(SPEAKER_SPOTLIGHT);
+  Future<List> getCurrentPhases() async => await _getProperty(CURRENT_PHASES);
+
+  Future<List> updateOnlineStatus(
+    bool isOnlineParam, {
+    bool shouldEditCollaboratorsInfo = false,
+  }) async {
+    await computeCollaboratorInformation();
+    final currentOnlineStatus = await getWhoIsOnline();
+    final indexToEdit =
+        shouldEditCollaboratorsInfo ? collaboratorIndex : userIndex;
+    currentOnlineStatus[indexToEdit] = isOnlineParam;
+    return await _onCurrentActiveNokhteSession(
+        supabase.from(TABLE_NAME).update({
+      IS_ONLINE: currentOnlineStatus,
+    }));
+  }
+
+  Future<List> updateCurrentPhases(
+    int newPhase, {
+    bool shouldEditCollaboratorsInfo = false,
+  }) async {
+    await computeCollaboratorInformation();
+    final currentPhases = await getCurrentPhases();
+    final indexToEdit =
+        shouldEditCollaboratorsInfo ? collaboratorIndex : userIndex;
+    currentPhases[indexToEdit] = newPhase;
+    return await _onCurrentActiveNokhteSession(
+        supabase.from(TABLE_NAME).update({
+      CURRENT_PHASES: currentPhases,
+    }));
+  }
+
+  Future<List> setUserAsTheCurrentTalker() async {
+    await computeCollaboratorInformation();
+    final currentSpeaker = await getSpeakerSpotlight();
+    if (currentSpeaker == null) {
+      return await _onCurrentActiveNokhteSession(
+        supabase.from(TABLE_NAME).update({
+          SPEAKER_SPOTLIGHT: userUID,
+        }),
+      );
+    } else {
+      return [];
+    }
+  }
+
+  Future<List> clearTheCurrentTalker() async {
+    await computeCollaboratorInformation();
+    final currentSpeaker = await getSpeakerSpotlight();
+    if (currentSpeaker == userUID) {
+      return await _onCurrentActiveNokhteSession(
+          supabase.from(TABLE_NAME).update({SPEAKER_SPOTLIGHT: null}));
+    } else {
+      return [];
+    }
+  }
+
+  Future<bool> checkIfUserHasTheQuestion() async {
+    await computeCollaboratorInformation();
+    final meetingUID = await getMeetingUID();
+    return meetingUID == userUID;
+  }
+
+  _onCurrentActiveNokhteSession(PostgrestFilterBuilder query) async {
+    await computeCollaboratorInformation();
+    return await query
+        .eq(userColumn, userUID)
+        .eq(collaboratorColumn, collaboratorUID)
+        .select();
+  }
 }
