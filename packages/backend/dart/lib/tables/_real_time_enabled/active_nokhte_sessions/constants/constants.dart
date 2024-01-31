5581f9123f548862e78f952d66e39c3006cf6ba5
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-31 08:43:15 -0600

    add: active_nokhte_sessions_constants

---
+++ packages/backend/dart/lib/tables/_real_time_enabled/active_nokhte_sessions/constants/constants.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
+
+mixin ActiveNokhteSessionsConstants {
+  String TABLE_NAME = "active_nokhte_sessions";
+  String MEETING_UID = 'meeting_uid';
+  String COLLABORATOR_ONE_UID = 'collaborator_one_uid';
+  String COLLABORATOR_TWO_UID = 'collaborator_two_uid';
+  String SPEAKER_SPOTLIGHT = 'speaker_spotlight';
+  String CURRENT_PHASES = 'current_phases';
+  String IS_ONLINE = 'is_online';
+  String CREATED_AT = 'created_at';
+}
