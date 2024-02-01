1bdaba4b0db0bfeb1f308e425720625c9ee8c9ae
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-31 19:51:23 -0600

    move: update_who_is_talking to shared

--- packages/client/lib/app/core/modules/presence_modules/modules/shared/domain/logic/update_who_is_talking.dart
+++ packages/client/lib/app/core/modules/presence_modules/modules/shared/domain/logic/update_who_is_talking.dart
@@ -1,9 +1,9 @@
 import 'package:nokhte/app/core/interfaces/logic.dart';
-import 'package:nokhte/app/core/modules/collaborator_presence/domain/domain.dart';
+import 'package:nokhte/app/core/modules/presence_modules/modules/shared/shared.dart';
 
 class UpdateWhoIsTalking
     implements AbstractFutureLogic<bool, UpdateWhoIsTalkingParams> {
-  final CollaboratorPresenceContract contract;
+  final BasePresenceContract contract;
 
   UpdateWhoIsTalking({required this.contract});
 
