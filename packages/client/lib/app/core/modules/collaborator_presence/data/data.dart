e5b0ce5d5b000968e45e23c965769999ceb9466d
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-31 20:06:45 -0600

    move+update: collaborator_presence exports

--- packages/client/lib/app/core/modules/collaborator_presence/data/data.dart
+++
@@ -1,2 +0,0 @@
-export 'contracts/collaborator_presence_contract_impl.dart';
-export 'sources/collaborator_presence_remote_source.dart';
---
+++ packages/client/lib/app/core/modules/presence_modules/modules/purpose_session_presence/data/data.dart
@@ -0,0 +1,2 @@
+export 'contracts/purpose_presence_contract_impl.dart';
+export 'sources/purpose_session_presence_remote_source.dart';
