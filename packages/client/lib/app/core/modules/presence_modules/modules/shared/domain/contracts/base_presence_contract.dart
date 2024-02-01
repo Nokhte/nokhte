4728516a070cda7adc36b77d87a48b5dfd7e7789
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-31 20:10:18 -0600

    add base_presence_contract & impl

---
+++ packages/client/lib/app/core/modules/presence_modules/modules/shared/domain/contracts/base_presence_contract.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:nokhte/app/core/error/failure.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/modules/presence_modules/modules/shared/shared.dart';
+
+abstract class BasePresenceContract<T> {
+  Future<Either<Failure, bool>> updateOnlineStatus(
+      UpdatePresencePropertyParams params);
+  Future<Either<Failure, bool>> updateWhoIsTalking(
+      UpdateWhoIsTalkingParams params);
+  Future<Either<Failure, bool>> updateCurrentPhase(double params);
+  Future<Either<Failure, Stream<T>>> getSessionMetadata(NoParams params);
+  bool cancelSessionMetadataStream(NoParams params);
+}
