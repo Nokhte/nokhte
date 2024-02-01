761659ff275a21c3029a49df48136dd8a85a7c34
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-01 14:07:04 -0600

    add: nokhte_session_remote_source implementation

--- packages/client/lib/app/modules/nokhte_session/data/source/nokhte_session_remote_source.dart
+++ packages/client/lib/app/modules/nokhte_session/data/source/nokhte_session_remote_source.dart
@@ -4,3 +4,17 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 abstract class NokhteSessionRemoteSource {
   Future<bool> checkifUserHasTheQuestion();
 }
+
+class NokhteSessionRemoteSourceImpl implements NokhteSessionRemoteSource {
+  final SupabaseClient supabase;
+
+  final ActiveNokhteSessionQueries queries;
+
+  NokhteSessionRemoteSourceImpl({
+    required this.supabase,
+  }) : queries = ActiveNokhteSessionQueries(supabase: supabase);
+
+  @override
+  Future<bool> checkifUserHasTheQuestion() async =>
+      await queries.checkIfUserHasTheQuestion();
+}
