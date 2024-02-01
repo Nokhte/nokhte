01805997f9a39cdf411b2f2f6a9c99d38be7be07
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-01 14:10:39 -0600

    add nokhte_session_phase1_coordinator impl (purpose_session parity)

--- packages/client/lib/app/modules/nokhte_session/presentation/mobx/coordinators/phase1/nokhte_session_phase1_coordinator.dart
+++ packages/client/lib/app/modules/nokhte_session/presentation/mobx/coordinators/phase1/nokhte_session_phase1_coordinator.dart
@@ -1,7 +1,14 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
 import 'package:nokhte/app/core/modules/presence_modules/presence_modules.dart';
+import 'package:nokhte/app/core/modules/voice_call/domain/logic/logic.dart';
+import 'package:nokhte/app/core/modules/voice_call/mobx/mobx.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:nokhte/app/modules/nokhte_session/presentation/mobx/coordinators/logic/nokhte_session_logic_coordinator.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'nokhte_session_phase1_widgets_coordinator.dart';
 part 'nokhte_session_phase1_coordinator.g.dart';
 
@@ -12,9 +19,138 @@ abstract class _NokhteSessionPhase1CoordinatorBase extends BaseCoordinator
     with Store {
   final NokhteSessionPhase1WidgetsCoordinator widgets;
   final NokhteSessionPresenceCoordinator presence;
+  final VoiceCallCoordinator voiceCall;
+  final HoldDetector hold;
+  final SwipeDetector swipe;
+  final NokhteSessionLogicCoordinator logic;
 
   _NokhteSessionPhase1CoordinatorBase({
     required this.widgets,
     required this.presence,
+    required this.voiceCall,
+    required this.hold,
+    required this.swipe,
+    required this.logic,
   });
+
+  @observable
+  bool canSpeak = false;
+
+  @observable
+  int speakerCount = 0;
+
+  @action
+  constructor() async {
+    setDisableAllTouchFeedback(true);
+    widgets.constructor();
+    await Permission.microphone.request();
+    await voiceCall.joinCall(
+      shouldEnterTheCallMuted: true,
+      callType: GetChannelIdParams.forNokhteSession,
+    );
+    initReactors();
+    await presence.listen();
+    await presence
+        .updateOnlineStatus(UpdatePresencePropertyParams.userAffirmative());
+    await logic.checkIfUserHasTheQuestion();
+    widgets.setHasTheQuesion(logic.hasTheQuestion);
+    if (logic.hasTheQuestion) {
+      widgets.hasTheQuestionConstructor();
+      canSpeak = true;
+    } else {
+      widgets.doesNotHaveTheQuestionConstructor();
+    }
+  }
+
+  @action
+  onInactive() async {
+    await presence
+        .updateOnlineStatus(UpdatePresencePropertyParams.userNegative());
+    await presence.updateWhoIsTalking(UpdateWhoIsTalkingParams.clearOut);
+  }
+
+  @action
+  onResumed() async {
+    await presence
+        .updateOnlineStatus(UpdatePresencePropertyParams.userAffirmative());
+    if (presence.getSessionMetadataStore.collaboratorIsOnline) {
+      presence.incidentsOverlayStore.onCollaboratorJoined();
+    }
+  }
+
+  initReactors() {
+    voiceCall.initReactors(onBothJoinedCall: () {
+      setDisableAllTouchFeedback(false);
+    });
+    presence.initReactors(
+      onCollaboratorJoined: () {
+        setDisableAllTouchFeedback(false);
+      },
+      onCollaboratorLeft: () {
+        setDisableAllTouchFeedback(true);
+      },
+    );
+    collaboratorTalkingStatusReactor();
+    userTalkingStatusReactor();
+    holdReactor();
+    letGoReactor();
+    widgets.wifiDisconnectOverlayReactor(
+      onConnectionFinished: () async {
+        await presence
+            .updateOnlineStatus(UpdatePresencePropertyParams.userAffirmative());
+      },
+    );
+  }
+
+  holdReactor() => reaction((p0) => hold.holdCount, (p0) async {
+        ifTouchIsNotDisabled(() async {
+          if (canSpeak &&
+              voiceCall.voiceCallStatusStore.inCall == CallStatus.joined &&
+              !widgets.isDisconnected &&
+              presence.getSessionMetadataStore.collaboratorIsOnline) {
+            await presence
+                .updateWhoIsTalking(UpdateWhoIsTalkingParams.setUserAsTalker);
+          }
+        });
+      });
+
+  letGoReactor() => reaction((p0) => hold.letGoCount, (p0) async {
+        if (canSpeak &&
+            voiceCall.voiceCallStatusStore.inCall == CallStatus.joined &&
+            !widgets.isDisconnected &&
+            presence.getSessionMetadataStore.collaboratorIsOnline) {
+          await presence.updateWhoIsTalking(UpdateWhoIsTalkingParams.clearOut);
+        }
+      });
+
+  collaboratorTalkingStatusReactor() =>
+      reaction((p0) => presence.getSessionMetadataStore.collaboratorIsTalking,
+          (p0) {
+        if (p0) {
+          speakerCount++;
+          canSpeak = false;
+        } else {
+          if (!logic.hasTheQuestion && speakerCount == 1) {
+            // widgets.onFirstCollaboratorFinishSpeaking();
+          }
+          canSpeak = true;
+        }
+      });
+  userTalkingStatusReactor() =>
+      reaction((p0) => presence.getSessionMetadataStore.userIsTalking,
+          (p0) async {
+        if (p0) {
+          await voiceCall.voiceCallActionsStore.unmuteAudio(NoParams());
+          widgets.onHold();
+          // if (!hasInitializedTimer &&
+          //     checkIfUserHasTheQuestion.hasTheQuestion) {
+          //   await presence.updateTimerStatus(true);
+          //   hasInitializedTimer = true;
+          //   widgets.initTimer();
+          // }
+        } else {
+          widgets.onLetGo();
+          await voiceCall.voiceCallActionsStore.muteAudio(NoParams());
+        }
+      });
 }
--- packages/client/lib/app/modules/nokhte_session/presentation/mobx/coordinators/phase1/nokhte_session_phase1_coordinator.g.dart
+++ packages/client/lib/app/modules/nokhte_session/presentation/mobx/coordinators/phase1/nokhte_session_phase1_coordinator.g.dart
@@ -10,10 +10,71 @@ part of 'nokhte_session_phase1_coordinator.dart';
 
 mixin _$NokhteSessionPhase1Coordinator
     on _NokhteSessionPhase1CoordinatorBase, Store {
+  late final _$canSpeakAtom = Atom(
+      name: '_NokhteSessionPhase1CoordinatorBase.canSpeak', context: context);
+
+  @override
+  bool get canSpeak {
+    _$canSpeakAtom.reportRead();
+    return super.canSpeak;
+  }
+
+  @override
+  set canSpeak(bool value) {
+    _$canSpeakAtom.reportWrite(value, super.canSpeak, () {
+      super.canSpeak = value;
+    });
+  }
+
+  late final _$speakerCountAtom = Atom(
+      name: '_NokhteSessionPhase1CoordinatorBase.speakerCount',
+      context: context);
+
+  @override
+  int get speakerCount {
+    _$speakerCountAtom.reportRead();
+    return super.speakerCount;
+  }
+
+  @override
+  set speakerCount(int value) {
+    _$speakerCountAtom.reportWrite(value, super.speakerCount, () {
+      super.speakerCount = value;
+    });
+  }
+
+  late final _$constructorAsyncAction = AsyncAction(
+      '_NokhteSessionPhase1CoordinatorBase.constructor',
+      context: context);
+
+  @override
+  Future constructor() {
+    return _$constructorAsyncAction.run(() => super.constructor());
+  }
+
+  late final _$onInactiveAsyncAction = AsyncAction(
+      '_NokhteSessionPhase1CoordinatorBase.onInactive',
+      context: context);
+
+  @override
+  Future onInactive() {
+    return _$onInactiveAsyncAction.run(() => super.onInactive());
+  }
+
+  late final _$onResumedAsyncAction = AsyncAction(
+      '_NokhteSessionPhase1CoordinatorBase.onResumed',
+      context: context);
+
+  @override
+  Future onResumed() {
+    return _$onResumedAsyncAction.run(() => super.onResumed());
+  }
+
   @override
   String toString() {
     return '''
-
+canSpeak: ${canSpeak},
+speakerCount: ${speakerCount}
     ''';
   }
 }
