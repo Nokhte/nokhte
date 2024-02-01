b94dbf0cea89b00d7f3d764af567ee9e6612c8d0
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-01 14:09:40 -0600

    add:nokhte_phase1_widgets_coordinator logic (purpose_session parity)

--- packages/client/lib/app/modules/nokhte_session/presentation/mobx/coordinators/phase1/nokhte_session_phase1_widgets_coordinator.dart
+++ packages/client/lib/app/modules/nokhte_session/presentation/mobx/coordinators/phase1/nokhte_session_phase1_widgets_coordinator.dart
@@ -1,10 +1,111 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/widgets/beach_widgets/shared/types/types.dart';
+import 'package:nokhte/app/core/widgets/smart_text/stack/constants/constants.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 part 'nokhte_session_phase1_widgets_coordinator.g.dart';
 
 class NokhteSessionPhase1WidgetsCoordinator = _NokhteSessionPhase1WidgetsCoordinatorBase
     with _$NokhteSessionPhase1WidgetsCoordinator;
 
 abstract class _NokhteSessionPhase1WidgetsCoordinatorBase
-    extends BaseWidgetsCoordinator with Store {}
+    extends BaseWidgetsCoordinator with Store {
+  final BeachWavesStore beachWaves;
+  final BorderGlowStore borderGlow;
+  final SmartTextStore primarySmartText;
+  final SmartTextStore secondarySmartText;
+  final WifiDisconnectOverlayStore wifiDisconnectOverlay;
+  final TextEditorStore textEditor;
+
+  _NokhteSessionPhase1WidgetsCoordinatorBase({
+    required this.beachWaves,
+    required this.borderGlow,
+    required this.primarySmartText,
+    required this.secondarySmartText,
+    required this.wifiDisconnectOverlay,
+    required this.textEditor,
+  });
+
+  @observable
+  bool hasTheQuestion = false;
+
+  @action
+  setHasTheQuesion(bool newVal) => hasTheQuestion = newVal;
+
+  @observable
+  bool isInTheCall = false;
+
+  @action
+  constructor() {
+    beachWaves.setMovieMode(BeachWaveMovieModes.timesUp);
+    primarySmartText.setMessagesData(MessagesData.empty);
+    secondarySmartText.setMessagesData(MessagesData.empty);
+    initReactors();
+  }
+
+  @action
+  initReactors() {
+    wifiDisconnectOverlay.initReactors(
+      onQuickConnected: () {
+        setIsDisconnected(false);
+      },
+      onLongReConnected: () {
+        setIsDisconnected(false);
+      },
+      onDisconnected: () {
+        setIsDisconnected(true);
+      },
+    );
+  }
+
+  @action
+  onConnected() {
+    setIsDisconnected(false);
+  }
+
+  @action
+  onDisconnected() {
+    setIsDisconnected(true);
+  }
+
+  @action
+  onHold() {
+    borderGlow.initMovie(NoParams());
+  }
+
+  @action
+  onLetGo() {
+    borderGlow.initGlowDown();
+    // hideSecondaryText();
+  }
+
+  @action
+  onInactive() {
+    borderGlow.initGlowDown();
+  }
+
+  @action
+  hasTheQuestionConstructor() {
+    primarySmartText
+        .setMessagesData(MessagesData.primaryPurposeSessionHasTheQuestion);
+    // showSecondaryText();
+    primarySmartText.startRotatingText();
+  }
+
+  @action
+  doesNotHaveTheQuestionConstructor() {
+    primarySmartText.setMessagesData(
+        MessagesData.primaryPurposeSessionDoesNotHaveTheQuestion);
+    primarySmartText.startRotatingText();
+  }
+
+  wifiDisconnectOverlayReactor({required Function onConnectionFinished}) =>
+      reaction((p0) => wifiDisconnectOverlay.movieStatus, (p0) {
+        if (wifiDisconnectOverlay.movieMode ==
+            WifiDisconnectMovieModes.removeTheCircle) {
+          setIsDisconnected(false);
+        }
+      });
+}
--- packages/client/lib/app/modules/nokhte_session/presentation/mobx/coordinators/phase1/nokhte_session_phase1_widgets_coordinator.g.dart
+++ packages/client/lib/app/modules/nokhte_session/presentation/mobx/coordinators/phase1/nokhte_session_phase1_widgets_coordinator.g.dart
@@ -10,10 +10,189 @@ part of 'nokhte_session_phase1_widgets_coordinator.dart';
 
 mixin _$NokhteSessionPhase1WidgetsCoordinator
     on _NokhteSessionPhase1WidgetsCoordinatorBase, Store {
+  late final _$hasTheQuestionAtom = Atom(
+      name: '_NokhteSessionPhase1WidgetsCoordinatorBase.hasTheQuestion',
+      context: context);
+
+  @override
+  bool get hasTheQuestion {
+    _$hasTheQuestionAtom.reportRead();
+    return super.hasTheQuestion;
+  }
+
+  @override
+  set hasTheQuestion(bool value) {
+    _$hasTheQuestionAtom.reportWrite(value, super.hasTheQuestion, () {
+      super.hasTheQuestion = value;
+    });
+  }
+
+  late final _$isInTheCallAtom = Atom(
+      name: '_NokhteSessionPhase1WidgetsCoordinatorBase.isInTheCall',
+      context: context);
+
+  @override
+  bool get isInTheCall {
+    _$isInTheCallAtom.reportRead();
+    return super.isInTheCall;
+  }
+
+  @override
+  set isInTheCall(bool value) {
+    _$isInTheCallAtom.reportWrite(value, super.isInTheCall, () {
+      super.isInTheCall = value;
+    });
+  }
+
+  late final _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController =
+      ActionController(
+          name: '_NokhteSessionPhase1WidgetsCoordinatorBase', context: context);
+
+  @override
+  dynamic setHasTheQuesion(bool newVal) {
+    final _$actionInfo =
+        _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController.startAction(
+            name:
+                '_NokhteSessionPhase1WidgetsCoordinatorBase.setHasTheQuesion');
+    try {
+      return super.setHasTheQuesion(newVal);
+    } finally {
+      _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic constructor() {
+    final _$actionInfo =
+        _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+            .startAction(
+                name: '_NokhteSessionPhase1WidgetsCoordinatorBase.constructor');
+    try {
+      return super.constructor();
+    } finally {
+      _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic initReactors() {
+    final _$actionInfo =
+        _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+            .startAction(
+                name:
+                    '_NokhteSessionPhase1WidgetsCoordinatorBase.initReactors');
+    try {
+      return super.initReactors();
+    } finally {
+      _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic onConnected() {
+    final _$actionInfo =
+        _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+            .startAction(
+                name: '_NokhteSessionPhase1WidgetsCoordinatorBase.onConnected');
+    try {
+      return super.onConnected();
+    } finally {
+      _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic onDisconnected() {
+    final _$actionInfo =
+        _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController.startAction(
+            name: '_NokhteSessionPhase1WidgetsCoordinatorBase.onDisconnected');
+    try {
+      return super.onDisconnected();
+    } finally {
+      _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic onHold() {
+    final _$actionInfo =
+        _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+            .startAction(
+                name: '_NokhteSessionPhase1WidgetsCoordinatorBase.onHold');
+    try {
+      return super.onHold();
+    } finally {
+      _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic onLetGo() {
+    final _$actionInfo =
+        _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+            .startAction(
+                name: '_NokhteSessionPhase1WidgetsCoordinatorBase.onLetGo');
+    try {
+      return super.onLetGo();
+    } finally {
+      _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic onInactive() {
+    final _$actionInfo =
+        _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+            .startAction(
+                name: '_NokhteSessionPhase1WidgetsCoordinatorBase.onInactive');
+    try {
+      return super.onInactive();
+    } finally {
+      _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic hasTheQuestionConstructor() {
+    final _$actionInfo =
+        _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController.startAction(
+            name:
+                '_NokhteSessionPhase1WidgetsCoordinatorBase.hasTheQuestionConstructor');
+    try {
+      return super.hasTheQuestionConstructor();
+    } finally {
+      _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic doesNotHaveTheQuestionConstructor() {
+    final _$actionInfo =
+        _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController.startAction(
+            name:
+                '_NokhteSessionPhase1WidgetsCoordinatorBase.doesNotHaveTheQuestionConstructor');
+    try {
+      return super.doesNotHaveTheQuestionConstructor();
+    } finally {
+      _$_NokhteSessionPhase1WidgetsCoordinatorBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+hasTheQuestion: ${hasTheQuestion},
+isInTheCall: ${isInTheCall}
     ''';
   }
 }
