f66b7037f086f860a819ea8f67e21e52750e32fe
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-03 15:47:10 -0600

    refactor: recording_contract for simplicity (no failure cases needed)

--- packages/client/lib/app/core/modules/recording/domain/contracts/recording_contract.dart
+++ packages/client/lib/app/core/modules/recording/domain/contracts/recording_contract.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
-import 'package:dartz/dartz.dart';
-import 'package:nokhte/app/core/error/failure.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 
 abstract class RecordingContract {
-  Future<Either<Failure, bool>> startRecording(String path);
-  Future<Either<Failure, File>> stopRecording(NoParams params);
+  Future<bool> startRecording(String path);
+  Future<File> stopRecording(NoParams params);
 }
