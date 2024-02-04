458fd869cfdf16f07b4ef480a097836ba70c599f
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-04 09:39:22 -0600

    refactor: voice_call_actions_store: concise naming + add recording logic

--- packages/client/lib/app/core/modules/voice_call/mobx/main/voice_call_actions_store.g.dart
+++ packages/client/lib/app/core/modules/voice_call/mobx/main/voice_call_actions_store.g.dart
@@ -41,31 +41,78 @@ mixin _$VoiceCallActionsStore on _VoiceCallActionsStoreBase, Store {
     });
   }
 
-  late final _$muteOrUnmuteAudioAsyncAction = AsyncAction(
-      '_VoiceCallActionsStoreBase.muteOrUnmuteAudio',
-      context: context);
+  late final _$isRecordingAtom =
+      Atom(name: '_VoiceCallActionsStoreBase.isRecording', context: context);
 
   @override
-  Future<void> muteOrUnmuteAudio({required bool wantToMute}) {
-    return _$muteOrUnmuteAudioAsyncAction
-        .run(() => super.muteOrUnmuteAudio(wantToMute: wantToMute));
+  bool get isRecording {
+    _$isRecordingAtom.reportRead();
+    return super.isRecording;
   }
 
-  late final _$enterOrLeaveCallAsyncAction = AsyncAction(
-      '_VoiceCallActionsStoreBase.enterOrLeaveCall',
+  @override
+  set isRecording(bool value) {
+    _$isRecordingAtom.reportWrite(value, super.isRecording, () {
+      super.isRecording = value;
+    });
+  }
+
+  late final _$startRecordingAsyncAction = AsyncAction(
+      '_VoiceCallActionsStoreBase.startRecording',
       context: context);
 
   @override
-  Future<void> enterOrLeaveCall(Either<NoParams, JoinCallParams> params) {
-    return _$enterOrLeaveCallAsyncAction
-        .run(() => super.enterOrLeaveCall(params));
+  Future startRecording(dynamic fileName) {
+    return _$startRecordingAsyncAction
+        .run(() => super.startRecording(fileName));
+  }
+
+  late final _$stopRecordingAsyncAction =
+      AsyncAction('_VoiceCallActionsStoreBase.stopRecording', context: context);
+
+  @override
+  Future stopRecording() {
+    return _$stopRecordingAsyncAction.run(() => super.stopRecording());
+  }
+
+  late final _$unmuteAsyncAction =
+      AsyncAction('_VoiceCallActionsStoreBase.unmute', context: context);
+
+  @override
+  Future unmute() {
+    return _$unmuteAsyncAction.run(() => super.unmute());
+  }
+
+  late final _$muteAsyncAction =
+      AsyncAction('_VoiceCallActionsStoreBase.mute', context: context);
+
+  @override
+  Future mute() {
+    return _$muteAsyncAction.run(() => super.mute());
+  }
+
+  late final _$joinCallAsyncAction =
+      AsyncAction('_VoiceCallActionsStoreBase.joinCall', context: context);
+
+  @override
+  Future joinCall(JoinCallParams params) {
+    return _$joinCallAsyncAction.run(() => super.joinCall(params));
+  }
+
+  late final _$leaveCallAsyncAction =
+      AsyncAction('_VoiceCallActionsStoreBase.leaveCall', context: context);
+
+  @override
+  Future leaveCall(NoParams params) {
+    return _$leaveCallAsyncAction.run(() => super.leaveCall(params));
   }
 
   @override
   String toString() {
     return '''
 callStatus: ${callStatus},
-isMuted: ${isMuted}
+isMuted: ${isMuted},
+isRecording: ${isRecording}
     ''';
   }
 }
