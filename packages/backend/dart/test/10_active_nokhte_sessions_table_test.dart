1dbd3d5a1aeb4edf413ab83148129739adb43864
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-05 17:06:13 -0600

    test: active_nokhte_sessions to accompany refactors & new queries

--- packages/backend/dart/test/10_active_nokhte_sessions_table_test.dart
+++ packages/backend/dart/test/10_active_nokhte_sessions_table_test.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:nokhte_backend/tables/_real_time_disabled/finished_nokhte_sessions/queries.dart';
 import 'package:nokhte_backend/tables/_real_time_enabled/active_nokhte_sessions/active_nohte_sessions.dart';
 import 'package:nokhte_backend/tables/_real_time_enabled/active_nokhte_sessions/types/nokhte_session_metadata.dart';
 
@@ -10,21 +11,30 @@ void main() {
   late ActiveNokhteSessionQueries user1Queries;
   late ActiveNokhteSessionQueries user2Queries;
   late ActiveNokhteSessionsStream user1Stream;
+  late FinishedNokhteSessionQueries user1FinishedQueries;
   final tSetup = CommonCollaborativeTestFunctions();
+  List sortedArr = [];
 
   setUpAll(() async {
     await tSetup.setUp(shouldMakeCollaboration: false);
+    sortedArr = [tSetup.firstUserUID, tSetup.secondUserUID]..sort();
     user1Queries = ActiveNokhteSessionQueries(supabase: tSetup.user1Supabase);
+    user1FinishedQueries =
+        FinishedNokhteSessionQueries(supabase: tSetup.user1Supabase);
     user1Stream = ActiveNokhteSessionsStream(supabase: tSetup.user1Supabase);
     user2Queries = ActiveNokhteSessionQueries(supabase: tSetup.user2Supabase);
   });
 
+  tearDownAll(() async {
+    await tSetup.supabaseAdmin
+        .from("finished_nokhte_sessions")
+        .delete()
+        .eq("collaborator_uids", sortedArr);
+  });
+
   test("select", () async {
-    await tSetup.supabaseAdmin.from("active_nokhte_sessions").insert({
-      "meeting_uid": tSetup.firstUserUID,
-      "collaborator_one_uid": tSetup.firstUserUID,
-      "collaborator_two_uid": tSetup.secondUserUID,
-    });
+    await tSetup.supabaseAdmin.from("active_nokhte_sessions").insert(
+        {"meeting_uid": tSetup.firstUserUID, "collaborator_uids": sortedArr});
     final res = await user1Queries.select();
     expect(res, isNotEmpty);
   });
@@ -57,6 +67,13 @@ void main() {
   test("getCurrentPhases", () async {
     final res = await user1Queries.getCurrentPhases();
     expect(res, [1, 1]);
+  });
+
+  test("getCreatedAt", () async {
+    final res = await user1Queries.getCreatedAt();
+    final parsed = DateTime.parse(res);
+    expect(res, isA<String>());
+    expect(parsed, isA<DateTime>());
   });
 
   test("updateOnlineStatus", () async {
@@ -102,8 +119,8 @@ void main() {
     expect(res, true);
   });
 
-  test("getSessionMetadata", () async {
-    final stream = user1Stream.getSessionMetadata();
+  test("getPresenceMetadata", () async {
+    final stream = user1Stream.getPresenceMetadata();
     expect(
       stream,
       emits(
@@ -119,9 +136,58 @@ void main() {
     );
   });
 
-  test("delete", () async {
-    await user1Queries.delete();
-    final res = await user1Queries.select();
-    expect(res, isEmpty);
+  test("composePath", () async {
+    final res = await user1Queries.composePath("test");
+    final timeStamp = await user1Queries.getCreatedAt();
+    expect(res.split('/')[0], "${tSetup.firstUserUID}_${tSetup.secondUserUID}");
+    expect(res.split('/')[1], timeStamp);
+    expect(res.split('/')[2], "test.wav");
+  });
+
+  test("addNewAudioClipToSeessionMetadata", () async {
+    await user1Queries.addNewAudioClipToSessionMetadata("test");
+    final res = await user1Queries.getSessionMetadata();
+    expect(res.first['audioID'], "test");
+    final indexRes = await user1Queries.getMetadataIndex();
+    expect(indexRes, 0);
+  });
+  test("updateCurentMetadataIndexContent", () async {
+    await user1Queries.updateCurrentMetadataIndexContent("test");
+    final res = await user1Queries.getSessionMetadata();
+    expect(res.first['content'], "test");
+  });
+
+  test("getAudioIds", () async {
+    final res = await user1Queries.getAudioIdPaths();
+    final firstPath = res.first;
+    final timeStamp = await user1Queries.getCreatedAt();
+    expect(firstPath.split('/')[0],
+        "${tSetup.firstUserUID}_${tSetup.secondUserUID}");
+    expect(firstPath.split('/')[1], timeStamp);
+    expect(firstPath.split('/')[2], "test.wav");
+  });
+
+  test("getAudioIdPaths", () async {
+    final res = await user1Queries.getAudioIdPaths();
+    final firstPath = res.first;
+    final timeStamp = await user1Queries.getCreatedAt();
+    expect(firstPath.split('/')[0],
+        "${tSetup.firstUserUID}_${tSetup.secondUserUID}");
+    expect(firstPath.split('/')[1], timeStamp);
+    expect(firstPath.split('/')[2], "test.wav");
+  });
+
+  test("completeSession", () async {
+    final sessionTimestamp = await user1Queries.getCreatedAt();
+    await user1Queries.completeSession();
+    final res = await user1FinishedQueries.select();
+    expect(res.first["session_metadata"], [
+      {
+        "audioID": "test",
+        "content": "test",
+      }
+    ]);
+    expect(res.first["collaborator_uids"], sortedArr);
+    expect(res.first["session_timestamp"], sessionTimestamp);
   });
 }
