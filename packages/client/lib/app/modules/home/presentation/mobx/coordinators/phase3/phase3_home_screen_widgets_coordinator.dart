01c46fee7c36f2ac77634549b98563cd6f12cd85
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-13 16:42:35 -0600

    add: make phase3 like phase2 in terms of smartText & gestureCross logic

--- packages/client/lib/app/modules/home/presentation/mobx/coordinators/phase3/phase3_home_screen_widgets_coordinator.dart
+++ packages/client/lib/app/modules/home/presentation/mobx/coordinators/phase3/phase3_home_screen_widgets_coordinator.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widget_constants.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/home/presentation/mobx/coordinators/shared/base_home_screen_widgets_coordinator.dart';
@@ -24,6 +25,10 @@ abstract class _Phase3HomeScreenWidgetsCoordinatorBase
   bool gracePeriodHasExpired = false;
 
   @action
+  toggleGracePeriodHasExpired() =>
+      gracePeriodHasExpired = !gracePeriodHasExpired;
+
+  @action
   constructor() {
     if (Modular.args.data["resumeOnShoreParams"] != null) {
       params = Modular.args.data["resumeOnShoreParams"];
@@ -32,6 +37,13 @@ abstract class _Phase3HomeScreenWidgetsCoordinatorBase
     beachWaves.currentStore.initMovie(Modular.args.data["resumeOnShoreParams"]);
     gestureCross.setHomeScreen();
     primarySmartText.setMessagesData(MessagesData.firstTimeHomeList);
+    Future.delayed(Seconds.get(3), () {
+      if (!hasSwipedUp) {
+        gestureCross.startBlinking();
+        primarySmartText.startRotatingText();
+        toggleGracePeriodHasExpired();
+      }
+    });
     initReactors();
   }
 
@@ -57,9 +69,12 @@ abstract class _Phase3HomeScreenWidgetsCoordinatorBase
       beachWaves.currentStore.setControl(Control.stop);
       toggleHasInitiatedBlur();
       gestureCross.stopBlinking();
-
-      primarySmartText.setCurrentIndex(1);
-      primarySmartText.startRotatingText();
+      if (gracePeriodHasExpired) {
+        primarySmartText.startRotatingText(isResuming: true);
+      } else {
+        primarySmartText.setCurrentIndex(1);
+        primarySmartText.startRotatingText();
+      }
     }
   }
 }
