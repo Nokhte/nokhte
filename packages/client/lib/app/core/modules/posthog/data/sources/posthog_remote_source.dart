c1477b3a5bd558ea791bad17c8e8fc12430aee65
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-15 10:54:29 -0600

    add: posthog_remote_source_impl

--- packages/client/lib/app/core/modules/posthog/data/sources/posthog_remote_source.dart
+++ packages/client/lib/app/core/modules/posthog/data/sources/posthog_remote_source.dart
@@ -9,3 +9,41 @@ abstract class PosthogRemoteSource {
   Future<void> captureShareNokhteSessionInvitation();
   Future<void> captureScreen(Screens screen);
 }
+
+class PosthogRemoteSourceImpl
+    with PosthogEventConstants, PosthogPageNameConstants
+    implements PosthogRemoteSource {
+  final SupabaseClient supabase;
+  final Posthog posthog;
+
+  PosthogRemoteSourceImpl({required this.supabase}) : posthog = Posthog();
+
+  @override
+  Future<void> captureNokhteSessionEnd() async {
+    await Posthog().capture(eventName: END_NOKHTE_SESSION, properties: {
+      "time": DateTime.now().toIso8601String(),
+    });
+  }
+
+  @override
+  Future<void> captureNokhteSessionStart() async {
+    await Posthog().capture(eventName: START_NOKHTE_SESSION, properties: {
+      "time": DateTime.now().toIso8601String(),
+    });
+  }
+
+  @override
+  Future<void> captureShareNokhteSessionInvitation() async {
+    await posthog.capture(eventName: SHARE_NOKHTE_SESSION_INVITATION);
+  }
+
+  @override
+  Future<void> identifyUser() async {
+    await posthog.identify(userId: supabase.auth.currentUser!.id);
+  }
+
+  @override
+  Future<void> captureScreen(Screens screen) async {
+    await posthog.capture(eventName: getScreenName(screen));
+  }
+}
