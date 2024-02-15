cb37eb59f763b3eab3899f0db2a5d1c1494d72c3
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-15 06:39:23 -0600

    refactor: update_authorized_viewers for clarity + analytics

--- packages/backend/supabase/functions/initiate-collaborator-search/update_authorized_viewers.ts
+++ packages/backend/supabase/functions/initiate-collaborator-search/update_authorized_viewers.ts
@@ -1,28 +1,39 @@
 import { supabaseAdmin } from "../constants/supabase.ts";
+import { captureNewNokhteSessionCollaborator } from "../posthog/capture/new_nokhte_session_collaborator.ts";
 
 async function updateAuthorizedViewers(matchedUID: string, userUID: string) {
   const userAuthorizedViewersRes: Array<string> = (
     await supabaseAdmin.from("user_names").select().eq("uid", userUID)
   ).data?.[0]["authorized_viewers"];
-  if (userAuthorizedViewersRes.includes(matchedUID)) return;
-  userAuthorizedViewersRes.push(matchedUID);
+
+  if (userAuthorizedViewersRes.includes(matchedUID)) {
+    return;
+  } else {
+    userAuthorizedViewersRes.push(matchedUID);
+    await supabaseAdmin
+      .from("user_names")
+      .update({
+        authorized_viewers: userAuthorizedViewersRes,
+      })
+      .eq("uid", userUID);
+    captureNewNokhteSessionCollaborator(userUID);
+  }
   const collaboratorRowRes: Array<string> = (
     await supabaseAdmin.from("user_names").select().eq("uid", matchedUID)
   ).data?.[0]["authorized_viewers"];
-  if (collaboratorRowRes.includes(userUID)) return;
-  collaboratorRowRes.push(userUID);
-  await supabaseAdmin
-    .from("user_names")
-    .update({
-      authorized_viewers: userAuthorizedViewersRes,
-    })
-    .eq("uid", userUID);
-  await supabaseAdmin
-    .from("user_names")
-    .update({
-      authorized_viewers: collaboratorRowRes,
-    })
-    .eq("uid", matchedUID);
+
+  if (collaboratorRowRes.includes(userUID)) {
+    return;
+  } else {
+    collaboratorRowRes.push(userUID);
+    await supabaseAdmin
+      .from("user_names")
+      .update({
+        authorized_viewers: collaboratorRowRes,
+      })
+      .eq("uid", matchedUID);
+    captureNewNokhteSessionCollaborator(matchedUID);
+  }
 }
 
 export { updateAuthorizedViewers };
