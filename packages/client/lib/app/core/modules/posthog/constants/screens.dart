969bc209ad1749cf37154601f17992c8cf44b911
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-15 10:35:23 -0600

    add: screens unum for use in screen capturing

---
+++ packages/client/lib/app/core/modules/posthog/constants/screens.dart
@@ -0,0 +1,12 @@
+enum Screens {
+  login,
+  homePhase0,
+  homePhase1,
+  homePhase2,
+  homePhase3,
+  collaborationHome,
+  collaboratorPool,
+  nokhteSessionPhase0,
+  nokhteSessionPhase1,
+  nokhteSessionPhase2
+}
