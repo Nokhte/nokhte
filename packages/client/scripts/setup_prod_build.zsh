ac533245dc5aa4911efd94d4878713ce3fa647cf
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-02 05:46:51 -0600

    add: working staging supabase & branch impl

--- packages/client/scripts/setup_prod_build.zsh
+++ packages/client/scripts/setup_prod_build.zsh
@@ -2,14 +2,9 @@
 cd ios/Runner
 # CFBundleURLName & Scheme
 sed -i '' -e 's/com.staging.nokhte/com.nokhte.nokhte/' info.plist  
-# branch keys
-sed -i '' -e 's/key_live_hqdu5OWr8Tsdo7keyaCk1egjDykdkeRi/key_live_oFdaQmf0vYYD43VtksjHYlfmyzjlnKnL/' info.plist  
-sed -i '' -e 's/key_test_kDgs4IXrYGvog6bgvesgWlooABmmjkLq/key_test_ayadRkn9EZ9xY7IFlqmQHnipzxiihPjD/' info.plist  
-# branch domains
-sed -i '' -e 's/59q6i.app.link/app.nokhte.com/' info.plist
-sed -i '' -e 's/59q6i-alternate.app.link/2cf02-alternate.app.link/' info.plist
-sed -i '' -e 's/59q6i.test-app.link/2cf02.test-app.link/' info.plist
-sed -i '' -e 's/59q6i-alternate.test-app.link/2cf02-alternate.test-app.link/' info.plist
+
+sed -i '' -e 's/staging_match_certificates/match_certificates/' Matchfile 
+
 # bundle id
 cd ../Runner.xcodeproj
 sed -i '' -e 's/PRODUCT_BUNDLE_IDENTIFIER = com.staging.nokhte/PRODUCT_BUNDLE_IDENTIFIER = com.nokhte.nokhte/' project.pbxproj  
