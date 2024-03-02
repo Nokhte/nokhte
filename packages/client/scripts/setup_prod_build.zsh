c8264b0d0ffaef4361adee62fff306090697b841
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-02 06:11:16 -0600

    fix: setup_prod_build shell script

--- packages/client/scripts/setup_prod_build.zsh
+++ packages/client/scripts/setup_prod_build.zsh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 cd ios/Runner
 # CFBundleURLName & Scheme
-sed -i '' -e 's/com.staging.nokhte/com.nokhte.nokhte/' info.plist  
-
+sed -i '' -e 's/com.nokhte.staging/com.nokhte.nokhte/' info.plist  
+# fastlane configs
+cd ../fastlane
 sed -i '' -e 's/staging_match_certificates/match_certificates/' Matchfile 
-
+sed -i '' -e 's/com.staging.nokhte/com.nokhte.nokhte/' Appfile
 # bundle id
 cd ../Runner.xcodeproj
 sed -i '' -e 's/PRODUCT_BUNDLE_IDENTIFIER = com.staging.nokhte/PRODUCT_BUNDLE_IDENTIFIER = com.nokhte.nokhte/' project.pbxproj  
