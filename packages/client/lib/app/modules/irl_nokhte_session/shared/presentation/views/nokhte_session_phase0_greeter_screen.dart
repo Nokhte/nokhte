e273f85d8693935d1e5b8d89bdb640a17c1d332e
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-04 17:16:37 -0600

    add: nokhte_session_phase0_greeter_screen

---
+++ packages/client/lib/app/modules/irl_nokhte_session/shared/presentation/views/nokhte_session_phase0_greeter_screen.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: must_be_immutable
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nokhte/app/core/hooks/hooks.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:nokhte/app/modules/irl_nokhte_session/shared/presentation/presentation.dart';
+
+class IrlNokhteSessionPhase0Screen extends HookWidget {
+  final IrlNokhteSessionPhase0Coordinator coordinator;
+  const IrlNokhteSessionPhase0Screen({
+    super.key,
+    required this.coordinator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final height = useFullScreenSize().height;
+    useEffect(() {
+      coordinator.constructor();
+      return null;
+    }, []);
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Tap(
+        store: coordinator.tap,
+        child: MultiHitStack(
+          children: [
+            FullScreen(
+              child: BeachWaves(
+                store: coordinator.widgets.beachWaves,
+              ),
+            ),
+            Center(
+              child: SmartText(
+                store: coordinator.widgets.primarySmartText,
+                bottomPadding: height * .05,
+                opacityDuration: Seconds.get(1),
+              ),
+            ),
+            Center(
+              child: SmartText(
+                store: coordinator.widgets.secondarySmartText,
+                topPadding: height * .8,
+                bottomPadding: 0,
+                opacityDuration: Seconds.get(1),
+              ),
+            ),
+            WifiDisconnectOverlay(
+              store: coordinator.widgets.wifiDisconnectOverlay,
+            ),
+          ],
+        ),
+      ),
+      // ),
+    );
+  }
+}
---
+++ packages/client/lib/app/modules/irl_nokhte_session/shared/presentation/views/screens.dart
@@ -0,0 +1,1 @@
+export 'nokhte_session_phase0_greeter_screen.dart';
