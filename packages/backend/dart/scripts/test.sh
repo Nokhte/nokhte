212215a822b9bcd32f5814a390f04459bd488042
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-04 08:24:52 -0600

    fix: test.sh naming for test

--- packages/backend/dart/scripts/test.sh
+++ packages/backend/dart/scripts/test.sh
@@ -3,4 +3,4 @@ flutter test test/01.5_user_names_table_test.dart
 flutter test test/02_get_agora_token_test.dart
 flutter test test/03_synchronous_search_test.dart
 flutter test test/04_active_nokhte_sessions_table_test.dart
-flutter test test/05_nokhte_sessions_audio_bucket_test.dart
+flutter test test/05_finished_nokhte_sessions_table_test.dart
