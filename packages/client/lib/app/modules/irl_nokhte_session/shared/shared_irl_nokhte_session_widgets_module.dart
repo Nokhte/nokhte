61e193c626bce9b43a2ce6090deb1b3620784c8a
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-04 17:10:22 -0600

    add: shared_irl_nokhte_session_widgets_module

---
+++ packages/client/lib/app/modules/irl_nokhte_session/shared/shared_irl_nokhte_session_widgets_module.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:nokhte/app/core/widgets/modules.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:nokhte/app/modules/irl_nokhte_session/shared/presentation/mobx/coordinators/phase0/irl_nokhte_session_phase0_widgets_coordinator.dart';
+
+class SharedIrlNokhteSessionWidgetsModule extends Module {
+  @override
+  List<Module> get imports => [
+        SmartTextModule(),
+        WifiDisconnectOverlayModule(),
+        BeachWavesModule(),
+      ];
+  @override
+  void exportedBinds(Injector i) {
+    i.add<IrlNokhteSessionPhase0WidgetsCoordinator>(
+      () => IrlNokhteSessionPhase0WidgetsCoordinator(
+          primarySmartText: Modular.get<SmartTextStore>(),
+          secondarySmartText: Modular.get<SmartTextStore>(),
+          wifiDisconnectOverlay: Modular.get<WifiDisconnectOverlayStore>(),
+          beachWaves: Modular.get<BeachWavesStore>()),
+    );
+  }
+}
