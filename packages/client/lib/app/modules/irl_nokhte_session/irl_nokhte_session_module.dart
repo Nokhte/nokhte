97040676ddae4bd9753a231a6ce83d697274c107
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-04 17:04:39 -0600

    add: nokhte session routing binds

--- packages/client/lib/app/modules/irl_nokhte_session/irl_nokhte_session_module.dart
+++ packages/client/lib/app/modules/irl_nokhte_session/irl_nokhte_session_module.dart
@@ -1,3 +1,22 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:nokhte/app/modules/irl_nokhte_session/shared/presentation/presentation.dart';
+import 'package:nokhte/app/modules/irl_nokhte_session/shared/presentation/views/nokhte_session_phase0_greeter_screen.dart';
 
-class IrlNokhteSessionModule extends Module {}
+import 'shared/shared_irl_nokhte_session_module.dart';
+
+class IrlNokhteSessionModule extends Module {
+  @override
+  List<Module> get imports => [
+        SharedIrlNokhteSessionModule(),
+      ];
+
+  @override
+  void routes(r) {
+    r.child(
+      '/',
+      child: (context) => IrlNokhteSessionPhase0Screen(
+        coordinator: Modular.get<IrlNokhteSessionPhase0Coordinator>(),
+      ),
+    );
+  }
+}
