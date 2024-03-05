53734689de5b1d816c5e1221c3ad16b624bcf56f
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-05 16:38:39 -0600

    add: nokhte session phase 1 placeholder

---
+++ packages/client/lib/app/modules/irl_nokhte_session/shared/presentation/views/irl_nokhte_session_phase1_placeholder.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class IrlNokhteSessionPhase1PlaceHolderScreen extends StatelessWidget {
+  const IrlNokhteSessionPhase1PlaceHolderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Phase 1"),
+      ),
+    );
+  }
+}
