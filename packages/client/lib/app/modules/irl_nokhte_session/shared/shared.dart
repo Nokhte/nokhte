4e675c96bce74cc065032ad11c6c9c29d6b771d4
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-05 10:09:39 -0600

    add: export for shared sub-module of irl_nokhte_session

---
+++ packages/client/lib/app/modules/irl_nokhte_session/shared/shared.dart
@@ -0,0 +1,5 @@
+export './data/data.dart';
+export './domain/domain.dart';
+export './presentation/presentation.dart';
+export './shared_irl_nokhte_session_widgets_module.dart';
+export './shared_irl_nokhte_session_module.dart';
