8417576c0e23fedc549ff4da534e2b82845a60bd
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-06 09:10:10 -0600

    add: new irl_nokhte_session folders to shared export

--- packages/client/lib/app/modules/irl_nokhte_session/shared/shared.dart
+++ packages/client/lib/app/modules/irl_nokhte_session/shared/shared.dart
@@ -1,3 +1,6 @@
 export './presentation/presentation.dart';
+export './data/data.dart';
+export './domain/domain.dart';
 export './shared_irl_nokhte_session_widgets_module.dart';
 export './shared_irl_nokhte_session_module.dart';
+export './types/types.dart';
