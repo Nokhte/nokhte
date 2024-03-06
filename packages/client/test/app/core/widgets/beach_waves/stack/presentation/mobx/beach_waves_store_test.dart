715ada87c9ef6b6e9a4ca51230ca62fbae90d2f9
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-06 17:32:58 -0600

    test: update tests for new beach wave type conventions

--- packages/client/test/app/core/widgets/beach_waves/stack/presentation/mobx/beach_waves_store_test.dart
+++ packages/client/test/app/core/widgets/beach_waves/stack/presentation/mobx/beach_waves_store_test.dart
@@ -103,12 +103,12 @@ void main() {
       });
       test("suspendedAtOceanDive key", () {
         final res = testStore
-            .movieModeToStoreLookup[BeachWaveMovieModes.suspendedAtOceanDive];
+            .movieModeToStoreLookup[BeachWaveMovieModes.staticOceanDive];
         expect(res?.movie.duration, Seconds.get(1));
       });
       test("suspendedAtTheDepths key", () {
-        final res = testStore
-            .movieModeToStoreLookup[BeachWaveMovieModes.suspendedAtTheDepths];
+        final res =
+            testStore.movieModeToStoreLookup[BeachWaveMovieModes.staticDepths];
         expect(res?.movie.duration, Seconds.get(1));
       });
 
