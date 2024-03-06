b8f6ddb25215f803aa0c7bfe61e76d770d5d05ab
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-06 09:25:59 -0600

    add: notes & talking placeholder screens for nokhte_session

---
+++ packages/client/lib/app/modules/irl_nokhte_session/shared/presentation/views/irl_nokhte_session_talking_placeholder.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class IrlNokhteSessionTalkingPlaceHolderScreen extends StatelessWidget {
+  const IrlNokhteSessionTalkingPlaceHolderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Talking"),
+      ),
+    );
+  }
+}
