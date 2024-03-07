bded8c1e3eba1a062e9b179e10ff36011f6c9924
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-07 08:41:06 -0600

    add: speaking_phone exports

---
+++ packages/client/lib/app/modules/irl_nokhte_session/speaking_phone/speaking_phone.dart
@@ -0,0 +1,5 @@
+export 'data/data.dart';
+export 'domain/domain.dart';
+export 'presentation/presentation.dart';
+export 'speaking_phone_nokhte_session_module.dart';
+export 'speaking_phone_nokhte_session_widgets_module.dart';
