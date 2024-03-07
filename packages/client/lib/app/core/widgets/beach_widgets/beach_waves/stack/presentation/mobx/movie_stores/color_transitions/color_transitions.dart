3c09331f3632a0b8d1cfaaa9c134be937d8682c2
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-06 19:39:53 -0600

    add: vibrant_blue_grad_to_half_and_half_movie_store && export

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/color_transitions/color_transitions.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/color_transitions/color_transitions.dart
@@ -7,4 +7,5 @@ export 'times_up_dynamic_point_to_times_up_start_movie_store.dart';
 export 'times_up_end_to_ocean_dive_movie_store.dart';
 export 'times_up_end_to_the_depths_movie_store.dart';
 export 'times_up_end_to_times_up_start_movie_store.dart';
+export 'vibrant_blue_grad_to_half_and_half_movie_store.dart';
 export 'vibrant_blue_gradient_to_times_up_movie_store.dart';
