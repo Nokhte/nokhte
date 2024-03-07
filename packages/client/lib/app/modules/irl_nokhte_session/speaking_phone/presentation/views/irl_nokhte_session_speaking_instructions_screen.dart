c5c9f1fa65a9d5586dc056d8de70b24f1bc8a9d3
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-07 14:08:50 -0600

    add: Tap, TouchRipple & MirroredText to irl_nokhte_session_speaking_instructions_screen

--- packages/client/lib/app/modules/irl_nokhte_session/speaking_phone/presentation/views/irl_nokhte_session_speaking_instructions_screen.dart
+++ packages/client/lib/app/modules/irl_nokhte_session/speaking_phone/presentation/views/irl_nokhte_session_speaking_instructions_screen.dart
@@ -4,7 +4,7 @@ import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/irl_nokhte_session/irl_nokhte_session.dart';
 
 class IrlNokhteSessionSpeakingInstructionsScreen extends HookWidget {
-  final IrlNokhteSessionSpeakingScreenCoordinator coordinator;
+  final IrlNokhteSessionSpeakingInstructionsScreenCoordinator coordinator;
   const IrlNokhteSessionSpeakingInstructionsScreen({
     super.key,
     required this.coordinator,
@@ -14,52 +14,35 @@ class IrlNokhteSessionSpeakingInstructionsScreen extends HookWidget {
   Widget build(BuildContext context) {
     useEffect(() {
       coordinator.constructor();
+
       return null;
     }, []);
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Hold(
-        store: coordinator.hold,
-        child: Swipe(
-          store: coordinator.swipe,
-          child: MultiHitStack(
-            children: [
-              FullScreen(
-                child: BeachWaves(
-                  store: coordinator.widgets.beachWaves,
-                ),
+      body: Tap(
+        store: coordinator.tap,
+        child: MultiHitStack(
+          children: [
+            FullScreen(
+              child: BeachWaves(
+                store: coordinator.widgets.beachWaves,
               ),
-              MirroredText(
-                store: coordinator.widgets.mirroredText,
+            ),
+            MirroredText(
+              store: coordinator.widgets.mirroredText,
+            ),
+            FullScreen(
+              child: TouchRipple(
+                store: coordinator.widgets.touchRipple,
               ),
-              // Center(
-              //   child: SmartText(
-              //     store: coordinator.widgets.primarySmartText,
-              //     bottomPadding: height * .05,
-              //     opacityDuration: Seconds.get(1),
-              //   ),
-              // ),
-              // Center(
-              //   child: SmartText(
-              //     store: coordinator.widgets.secondarySmartText,
-              //     topPadding: height * .8,
-              //     bottomPadding: 0,
-              //     opacityDuration: Seconds.get(1),
-              //   ),
-              // ),
-              FullScreen(
-                child: TouchRipple(
-                  store: coordinator.widgets.touchRipple,
-                ),
-              ),
-              // CollaboratorPresenceIncidentsOverlay(
-              //   store: coordinator.presence.incidentsOverlayStore,
-              // ),
-              // WifiDisconnectOverlay(
-              //   store: coordinator.widgets.wifiDisconnectOverlay,
-              // ),
-            ],
-          ),
+            ),
+            // CollaboratorPresenceIncidentsOverlay(
+            //   store: coordinator.presence.incidentsOverlayStore,
+            // ),
+            // WifiDisconnectOverlay(
+            //   store: coordinator.widgets.wifiDisconnectOverlay,
+            // ),
+          ],
         ),
       ),
       // ),
