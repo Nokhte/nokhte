4dd352b3b6b66f7baf83a9e31fc14e73202de679
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-07 14:07:51 -0600

    add: tap functionality & proofing to speaking_instructions_widgets_coordinator

--- packages/client/lib/app/modules/irl_nokhte_session/speaking_phone/presentation/mobx/coordinators/irl_nokhte_session_speaking_instructions_screen_widgets_coordinator.dart
+++ packages/client/lib/app/modules/irl_nokhte_session/speaking_phone/presentation/mobx/coordinators/irl_nokhte_session_speaking_instructions_screen_widgets_coordinator.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'dart:ui';
+
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/extensions/extensions.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
 import 'package:nokhte/app/core/types/types.dart';
@@ -25,6 +28,16 @@ abstract class _IrlNokhteSessionSpeakingInstructionsScreenWidgetsCoordinatorBase
     required this.touchRipple,
   });
 
+  @observable
+  bool disableTouchInput = true;
+
+  @observable
+  MirroredTextOrientations currentActiveOrientation =
+      MirroredTextOrientations.rightSideUp;
+
+  @observable
+  int tapCount = 0;
+
   @action
   constructor() {
     beachWaves.setMovieMode(BeachWaveMovieModes.vibrantBlueGradToHalfAndHalf);
@@ -40,14 +53,73 @@ abstract class _IrlNokhteSessionSpeakingInstructionsScreenWidgetsCoordinatorBase
     beachWavesMovieStatusReactor();
   }
 
+  @action
+  onTap(Offset tapPosition) {
+    if (!disableTouchInput) {
+      touchRipple.onTap(tapPosition, adjustColorBasedOnPosition: true);
+      if (hasTappedOnTheRightSide && textIsDoneFadingInOrOut) {
+        toggleCurrentActiveOrientation();
+        if (isFirstTap) {
+          mirroredText.startRotatingUpsideDown();
+          mirroredText.startRotatingRightSideUp(isResuming: true);
+        } else if (isStillInMutualInstructionMode) {
+          mirroredText.startBothRotatingText(isResuming: true);
+        }
+        tapCount++;
+      }
+    }
+  }
+
+  @action
+  toggleCurrentActiveOrientation() {
+    switch (currentActiveOrientation) {
+      case MirroredTextOrientations.rightSideUp:
+        currentActiveOrientation = MirroredTextOrientations.upsideDown;
+      case MirroredTextOrientations.upsideDown:
+        currentActiveOrientation = MirroredTextOrientations.rightSideUp;
+      default:
+        break;
+    }
+  }
+
   beachWavesMovieStatusReactor() =>
       reaction((p0) => beachWaves.movieStatus, (p0) {
         if (p0 == MovieStatus.finished &&
             beachWaves.movieMode ==
                 BeachWaveMovieModes.vibrantBlueGradToHalfAndHalf) {
-          mirroredText.startRotatingText(
-            orientation: MirroredTextOrientations.rightSideUp,
-          );
+          mirroredText.startRotatingRightSideUp();
+          disableTouchInput = false;
         }
       });
+
+  @computed
+  bool get hasTappedOnTheRightSide =>
+      rightSideUpTextIsVisible && hasTappedOnTheBottomHalf ||
+      upsideDownTextIsVisible && hasTappedOnTheTopHalf;
+
+  @computed
+  bool get rightSideUpTextIsVisible =>
+      currentActiveOrientation == MirroredTextOrientations.rightSideUp;
+
+  @computed
+  bool get hasTappedOnTheBottomHalf =>
+      touchRipple.tapPlacement == TapPlacement.bottomHalf;
+
+  @computed
+  bool get upsideDownTextIsVisible =>
+      currentActiveOrientation == MirroredTextOrientations.upsideDown;
+
+  @computed
+  bool get hasTappedOnTheTopHalf =>
+      touchRipple.tapPlacement == TapPlacement.topHalf;
+
+  @computed
+  bool get isStillInMutualInstructionMode => tapCount.isLessThan(4);
+
+  @computed
+  bool get isFirstTap => tapCount == 0;
+
+  @computed
+  bool get textIsDoneFadingInOrOut =>
+      mirroredText.textIsDoneAnimating || isFirstTap;
 }
