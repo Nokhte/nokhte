c3abe1a2699371284a0553358c076d5c0d16fe04
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-08 10:16:06 -0600

    remove: times_up export from movie_stores_barrel

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/movie_stores.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/movie_stores.dart
@@ -3,5 +3,4 @@ export './any_to_on_shore/any_to_on_shore.dart';
 export 'static/static.dart';
 export 'on_shore/on_shore.dart';
 export 'on_shore_to_any/on_shore_to_any.dart';
-export 'times_up/times_up.dart';
 export 'water_from_top_to_on_shore/water_from_top_to_on_shore.dart';
