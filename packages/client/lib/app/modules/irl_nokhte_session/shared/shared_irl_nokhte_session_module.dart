3eaf9e9cc88482ffc0dcbdce98e21b2ab79eed4a
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-08 05:51:13 -0600

    remove: phase0 coordinator from shared_irl_nokhte_session_module

--- packages/client/lib/app/modules/irl_nokhte_session/shared/shared_irl_nokhte_session_module.dart
+++ packages/client/lib/app/modules/irl_nokhte_session/shared/shared_irl_nokhte_session_module.dart
@@ -1,12 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:nokhte/app/core/modules/legacy_connectivity/legacy_connectivity_module.dart';
-import 'package:nokhte/app/core/modules/posthog/domain/domain.dart';
-import 'package:nokhte/app/core/modules/posthog/posthog_module.dart';
-import 'package:nokhte/app/core/modules/presence_modules/presence_modules.dart';
 import 'package:nokhte/app/core/modules/supabase/supabase_module.dart';
 import 'package:nokhte/app/core/network/network_info.dart';
-import 'package:nokhte/app/core/widgets/modules.dart';
-import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/irl_nokhte_session/shared/shared.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -14,10 +9,6 @@ class SharedIrlNokhteSessionModule extends Module {
   @override
   List<Module> get imports => [
         LegacyConnectivityModule(),
-        PosthogModule(),
-        SharedIrlNokhteSessionWidgetsModule(),
-        IrlNokhteSessionPresenceModule(),
-        GesturesModule(),
         SupabaseModule(),
       ];
   @override
@@ -35,15 +26,6 @@ class SharedIrlNokhteSessionModule extends Module {
     i.add<DecidePhoneRole>(
       () => DecidePhoneRole(
         contract: i<SharedIrlNokhteSessionContractImpl>(),
-      ),
-    );
-    i.add<IrlNokhteSessionPhase0Coordinator>(
-      () => IrlNokhteSessionPhase0Coordinator(
-        decidePhoneRoleLogic: Modular.get<DecidePhoneRole>(),
-        presence: Modular.get<IrlNokhteSessionPresenceCoordinator>(),
-        captureScreen: Modular.get<CaptureScreen>(),
-        widgets: Modular.get<IrlNokhteSessionPhase0WidgetsCoordinator>(),
-        tap: Modular.get<TapDetector>(),
       ),
     );
   }
