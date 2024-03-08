f39b070a2d4487fd114beab9be61b0b8e057efd2
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-08 05:50:16 -0600

    revmoe: speaking_phone nokhte module this was mainly due to the presence module not resolving as a singleton

--- packages/client/lib/app/modules/irl_nokhte_session/speaking_phone/speaking_phone.dart
+++ packages/client/lib/app/modules/irl_nokhte_session/speaking_phone/speaking_phone.dart
@@ -1,5 +1,4 @@
 export 'data/data.dart';
 export 'domain/domain.dart';
 export 'presentation/presentation.dart';
-export 'speaking_phone_nokhte_session_module.dart';
 export 'speaking_phone_nokhte_session_widgets_module.dart';
