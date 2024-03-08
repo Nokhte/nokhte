16e3db582a8d060971ac4192407eeccd8256b194
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-08 10:18:36 -0600

    add:half_and_half to dry sand movie

---
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/movie_stores/color_transitions/half_and_half_to_dry_sand_movie_store.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/widgets/beach_widgets/shared/shared.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
+part 'half_and_half_to_dry_sand_movie_store.g.dart';
+
+class HalfAndHalfDrySandMovieStore = _HalfAndHalfToDrySandMovieStoreBase
+    with _$HalfAndHalfDrySandMovieStore;
+
+abstract class _HalfAndHalfToDrySandMovieStoreBase
+    extends BaseBeachWaveMovieStore<NoParams> with Store {
+  _HalfAndHalfToDrySandMovieStoreBase()
+      : super(
+          shouldPaintSand: TwoSecondBeachTransitionMovie.shouldPaintSand,
+        ) {
+    movie = TwoSecondBeachTransitionMovie.getMovie(
+      WaterColorsAndStops.halfWaterAndSand,
+      WaterColorsAndStops.drySand,
+    );
+  }
+
+  @override
+  @action
+  initMovie(NoParams params) {
+    control = Control.playFromStart;
+  }
+}
