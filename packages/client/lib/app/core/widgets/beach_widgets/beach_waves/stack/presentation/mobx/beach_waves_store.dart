f5ba77bc66e26c3f990f748fafbb8ff43000fe9e
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-08 16:59:18 -0600

    add: currentColorsAndStops as computed in beach_waves_store

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/beach_waves_store.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/presentation/mobx/beach_waves_store.dart
@@ -59,6 +59,18 @@ abstract class _BeachWavesStoreBase extends Equatable with Store {
     ]);
   }
 
+  @computed
+  List<ColorAndStop> get currentColorsAndStops => [
+        ColorAndStop(currentAnimationValues[1], currentAnimationValues[9]),
+        ColorAndStop(currentAnimationValues[2], currentAnimationValues[10]),
+        ColorAndStop(currentAnimationValues[3], currentAnimationValues[11]),
+        ColorAndStop(currentAnimationValues[4], currentAnimationValues[12]),
+        ColorAndStop(currentAnimationValues[5], currentAnimationValues[13]),
+        ColorAndStop(currentAnimationValues[6], currentAnimationValues[14]),
+        ColorAndStop(currentAnimationValues[7], currentAnimationValues[15]),
+        ColorAndStop(currentAnimationValues[8], currentAnimationValues[16]),
+      ];
+
   @observable
   MovieStatus movieStatus = MovieStatus.idle;
 
