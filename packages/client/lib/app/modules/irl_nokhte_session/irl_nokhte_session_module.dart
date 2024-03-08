a5253c348f392b11c0c9fb0d99a11029c54bdcc3
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-08 05:55:28 -0600

    update: binds in irl_nokhte_session_module

--- packages/client/lib/app/modules/irl_nokhte_session/irl_nokhte_session_module.dart
+++ packages/client/lib/app/modules/irl_nokhte_session/irl_nokhte_session_module.dart
@@ -1,14 +1,44 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:nokhte/app/modules/irl_nokhte_session/shared/presentation/presentation.dart';
-import 'shared/shared_irl_nokhte_session_module.dart';
+import 'package:nokhte/app/core/modules/posthog/domain/domain.dart';
+import 'package:nokhte/app/core/modules/posthog/posthog_module.dart';
+import 'package:nokhte/app/core/modules/presence_modules/presence_modules.dart';
+import 'package:nokhte/app/core/widgets/modules.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'speaking_phone/speaking_phone.dart';
+import 'shared/shared.dart';
 
 class IrlNokhteSessionModule extends Module {
   @override
   List<Module> get imports => [
         SharedIrlNokhteSessionModule(),
-        SpeakingPhoneNokhteSessionModule(),
+        SharedIrlNokhteSessionWidgetsModule(),
+        SpeakingPhoneNokhteSessionWidgetsModule(),
+        PosthogModule(),
+        IrlNokhteSessionPresenceModule(),
+        GesturesModule(),
       ];
+
+  @override
+  void binds(i) {
+    i.add<IrlNokhteSessionPhase0Coordinator>(
+      () => IrlNokhteSessionPhase0Coordinator(
+        decidePhoneRoleLogic: Modular.get<DecidePhoneRole>(),
+        presence: Modular.get<IrlNokhteSessionPresenceCoordinator>(),
+        captureScreen: Modular.get<CaptureScreen>(),
+        widgets: Modular.get<IrlNokhteSessionPhase0WidgetsCoordinator>(),
+        tap: Modular.get<TapDetector>(),
+      ),
+    );
+    i.add<IrlNokhteSessionSpeakingInstructionsScreenCoordinator>(
+      () => IrlNokhteSessionSpeakingInstructionsScreenCoordinator(
+        presence: Modular.get<IrlNokhteSessionPresenceCoordinator>(),
+        captureScreen: Modular.get<CaptureScreen>(),
+        widgets: Modular.get<
+            IrlNokhteSessionSpeakingInstructionsScreenWidgetsCoordinator>(),
+        tap: Modular.get<TapDetector>(),
+      ),
+    );
+  }
 
   @override
   void routes(r) {
