6d06e8bd9ed5c86c28d8cd4c8024695e9e401d5a
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-08 05:55:06 -0600

    add: the logic to the speaking_instructions_screen_coordinator

--- packages/client/lib/app/modules/irl_nokhte_session/speaking_phone/presentation/mobx/coordinators/irl_nokhte_session_speaking_instructions_screen_coordinator.dart
+++ packages/client/lib/app/modules/irl_nokhte_session/speaking_phone/presentation/mobx/coordinators/irl_nokhte_session_speaking_instructions_screen_coordinator.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'dart:async';
+
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/base_coordinator.dart';
 import 'package:nokhte/app/core/modules/presence_modules/presence_modules.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'irl_nokhte_session_speaking_instructions_screen_widgets_coordinator.dart';
 part 'irl_nokhte_session_speaking_instructions_screen_coordinator.g.dart';
@@ -14,17 +17,57 @@ abstract class _IrlNokhteSessionSpeakingInstructionsScreenCoordinatorBase
   final TapDetector tap;
   final IrlNokhteSessionSpeakingInstructionsScreenWidgetsCoordinator widgets;
   final IrlNokhteSessionPresenceCoordinator presence;
+  final GetIrlNokhteSessionMetadataStore sessionMetadata;
+
   _IrlNokhteSessionSpeakingInstructionsScreenCoordinatorBase({
     required super.captureScreen,
     required this.widgets,
     required this.tap,
     required this.presence,
-  });
+  }) : sessionMetadata = presence.getSessionMetadataStore;
 
   @action
   constructor() {
     widgets.constructor();
+    initReactors();
+  }
+
+  initReactors() {
     tapReactor();
+    presence.initReactors(
+      onCollaboratorJoined: () {
+        widgets.setDisableTouchInput(false);
+      },
+      onCollaboratorLeft: () {
+        widgets.setDisableTouchInput(true);
+      },
+    );
+    collaboratorPhaseReactor();
+    widgets.wifiDisconnectOverlay.initReactors(
+      onQuickConnected: () => setDisableAllTouchFeedback(false),
+      onLongReConnected: () {
+        widgets.setDisableTouchInput(false);
+      },
+      onDisconnected: () {
+        widgets.setDisableTouchInput(true);
+      },
+    );
+  }
+
+  @action
+  onInactive() async {
+    await presence
+        .updateOnlineStatus(UpdatePresencePropertyParams.userNegative());
+    // await presence.updateWhoIsTalking(UpdateWhoIsTalkingParams.clearOut);
+  }
+
+  @action
+  onResumed() async {
+    await presence
+        .updateOnlineStatus(UpdatePresencePropertyParams.userAffirmative());
+    if (sessionMetadata.collaboratorIsOnline) {
+      presence.incidentsOverlayStore.onCollaboratorJoined();
+    }
   }
 
   tapReactor() => reaction(
@@ -33,10 +76,28 @@ abstract class _IrlNokhteSessionSpeakingInstructionsScreenCoordinatorBase
           () async {
             widgets.onTap(
               tap.currentTapPosition,
-              onFlowFinished: () async =>
-                  await presence.updateCurrentPhase(2.0),
+              onFlowFinished: () async => await updateCurrentPhase(),
             );
           },
         ),
       );
+
+  collaboratorPhaseReactor() => reaction(
+        (p0) => sessionMetadata.collaboratorPhase,
+        (p0) {
+          if (sessionMetadata.canMoveIntoSession) {
+            // Modular.to.navigate("/irl_nokhte_session/speaking_screen")
+          }
+        },
+      );
+
+  updateCurrentPhase() async {
+    Timer.periodic(Seconds.get(0, milli: 500), (timer) async {
+      if (presence.getSessionMetadataStore.userPhase != 2.0) {
+        await presence.updateCurrentPhase(2.0);
+      } else {
+        timer.cancel();
+      }
+    });
+  }
 }
