5f629bb2cfe08134a60299219b7d69fc6a39179b
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-08 10:22:27 -0600

    add: new grad colorways & remove deprecated colorways

--- packages/client/lib/app/core/widgets/beach_widgets/shared/data/water_colors_and_stops.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/shared/data/water_colors_and_stops.dart
@@ -154,97 +154,17 @@ class WaterColorsAndStops {
         ColorAndStop(Color(0xFFF6E2A5), 0.9952),
         ColorAndStop(Color(0xFFF5DC91), 1.00),
       ];
-  static List<ColorAndStop> get schedulingDuskWaterFullScreen => const [
-        ColorAndStop(Color(0xFF1D3741), 0),
-        ColorAndStop(Color(0xFF1D3741), 0),
-        ColorAndStop(Color(0xFF1D3741), 0),
-        ColorAndStop(Color(0xFF1D3741), .02480),
-        ColorAndStop(Color(0xFF1D3741), .25),
-        ColorAndStop(Color(0xFF122430), .501),
-        ColorAndStop(Color(0xFF23364A), .7248),
-        ColorAndStop(Color(0xFF274D4F), 1.0),
-      ];
-  static List<ColorAndStop> get schedulingDuskWaterHalfScreen => const [
-        ColorAndStop(Color(0xFF1D3741), 0),
-        ColorAndStop(Color(0xFF1D3741), .1302),
-        ColorAndStop(Color(0xFF1D3741), .2548),
-        ColorAndStop(Color(0xFF1D3741), .4480),
-        ColorAndStop(Color(0xFF1D3741), .55),
-        ColorAndStop(Color(0xFF122430), .601),
-        ColorAndStop(Color(0xFF23364A), .7248),
-        ColorAndStop(Color(0xFF274D4F), .8620),
-      ];
-  static List<ColorAndStop> get schedulingMorningWaterFullScreen => const [
-        ColorAndStop(Color(0xFF1D3741), 0),
-        ColorAndStop(Color(0xFF274D4F), .202),
-        ColorAndStop(Color(0xFF274D4F), .3548),
-        ColorAndStop(Color(0xFF3E8077), .4480),
-        ColorAndStop(Color(0xFF36C0A8), .5501),
-        ColorAndStop(Color(0xFF229982), .7248),
-        ColorAndStop(Color(0xFF569F88), .8620),
-        ColorAndStop(Color(0xFFC0DAB4), 1.00),
-      ];
-  static List<ColorAndStop> get schedulingMorningWaterHalfScreen => const [
-        ColorAndStop(Color(0xFF1D3741), 0),
-        ColorAndStop(Color(0xFF274D4F), .2),
-        ColorAndStop(Color(0xFF274D4F), .56),
-        ColorAndStop(Color(0xFF3E8077), .59),
-        ColorAndStop(Color(0xFF36C0A8), .701),
-        ColorAndStop(Color(0xFF229982), .8048),
-        ColorAndStop(Color(0xFF569F88), .9020),
-        ColorAndStop(Color(0xFFC0DAB4), 1.00),
-      ];
 
-  static List<ColorAndStop> get schedulingDayWaterFullScreen => const [
-        ColorAndStop(Color(0xFF1A6395), 0),
-        ColorAndStop(Color(0xFF1A76A6), .1302),
-        ColorAndStop(Color(0xFF1C7490), .2548),
-        ColorAndStop(Color(0xFF2599B3), .3480),
-        ColorAndStop(Color(0xFF2997AB), .5501),
-        ColorAndStop(Color(0xFF48A1A9), .7248),
-        ColorAndStop(Color(0xFF73999D), .8620),
-        ColorAndStop(Color(0xFFAAB5B4), 1.00),
-      ];
-  static List<ColorAndStop> get schedulingDayWaterHalfScreen => const [
-        ColorAndStop(Color(0xFF1A6395), 0),
-        ColorAndStop(Color(0xFF1A76A6), .2),
-        ColorAndStop(Color(0xFF1C7490), .56),
-        ColorAndStop(Color(0xFF2599B3), .59),
-        ColorAndStop(Color(0xFF2997AB), .701),
-        ColorAndStop(Color(0xFF48A1A9), .7548),
-        ColorAndStop(Color(0xFF73999D), .9020),
-        ColorAndStop(Color(0xFFAAB5B4), 1.00),
-      ];
-
-  static List<ColorAndStop> get schedulingEveningWaterFullScreen => const [
-        ColorAndStop(Color(0xFF1D3741), 0.05),
-        ColorAndStop(Color(0xFF1D3741), .1302),
-        ColorAndStop(Color(0xFF274D4F), .2548),
-        ColorAndStop(Color(0xFF274D4F), .3480),
-        ColorAndStop(Color(0xFF2E5C5B), .5501),
-        ColorAndStop(Color(0xFF236663), .7248),
-        ColorAndStop(Color(0xFF3A7770), .8620),
-        ColorAndStop(Color(0xFF3C7B73), .97),
-      ];
-  static List<ColorAndStop> get schedulingEveningWaterHalfScreen => const [
-        ColorAndStop(Color(0xFF1D3741), 0),
-        ColorAndStop(Color(0xFF1D3741), .58),
-        ColorAndStop(Color(0xFF274D4F), .63),
-        ColorAndStop(Color(0xFF274D4F), .68),
-        ColorAndStop(Color(0xFF2E5C5B), .81),
-        ColorAndStop(Color(0xFF236663), .88),
-        ColorAndStop(Color(0xFF3A7770), .95210),
-        ColorAndStop(Color(0xFF3C7B73), 1.00),
-      ];
-  static List<ColorAndStop> get audioImmersionWater => const [
-        ColorAndStop(Color(0xFF51DCF2), 0.00),
-        ColorAndStop(Color(0xFF51C6E7), 0.084),
-        ColorAndStop(Color(0xFF03ABD2), 0.2075),
-        ColorAndStop(Color(0xFF0083AA), 0.3611),
-        ColorAndStop(Color(0xFF1287B2), 0.5092),
-        ColorAndStop(Color(0xFF238BB9), 0.6483),
-        ColorAndStop(Color(0xFF358FC1), 0.7995),
-        ColorAndStop(Color(0xFF012E45), 0.9863),
+  static List<List<ColorAndStop>> get timesUpSequence => [
+        timesUpPhase1,
+        timesUpPhase2,
+        timesUpPhase3,
+        timesUpPhase4,
+        timesUpPhase5,
+        timesUpPhase6,
+        timesUpPhase7,
+        timesUpPhase8,
+        timesUpPhase9,
       ];
 
   static List<ColorAndStop> get drySand => const [
@@ -267,5 +187,189 @@ class WaterColorsAndStops {
         ColorAndStop(Color(0xFFFAF2C3), .79),
         ColorAndStop(Color(0xFFFFFFDC), .9),
         ColorAndStop(Color(0xFFF5DC91), 1.00)
+      ];
+
+  static List<List<ColorAndStop>> get drySandToVibrantBlueFromTopSequence => [
+        drySand,
+        drySandToVibrantBlueGradFromTopPhase2,
+        drySandToVibrantBlueGradFromTopPhase3,
+        drySandToVibrantBlueGradFromTopPhase4,
+        drySandToVibrantBlueGradFromTopPhase5,
+        drySandToVibrantBlueGradFromTopPhase6,
+        drySandToVibrantBlueGradFromTopPhase7,
+        drySandToVibrantBlueGradFromTopPhase8,
+        drySandToVibrantBlueGradForBothPhase9,
+      ];
+
+  static List<ColorAndStop> get drySandToVibrantBlueGradFromTopPhase2 => const [
+        ColorAndStop(Color(0xFF6aEAB9), 0.0),
+        ColorAndStop(Color(0xFFF4DD98), .19),
+        ColorAndStop(Color(0xFFF6E2A5), .38),
+        ColorAndStop(Color(0xFFF6E4AD), .58),
+        ColorAndStop(Color(0xFFF9EFB7), .68),
+        ColorAndStop(Color(0xFFFAF2C3), .79),
+        ColorAndStop(Color(0xFFFFFFDC), .9),
+        ColorAndStop(Color(0xFFFFFFE3), 1.00)
+      ];
+  static List<ColorAndStop> get drySandToVibrantBlueGradFromTopPhase3 => const [
+        ColorAndStop(Color(0xFF6aEAB9), 0.0),
+        ColorAndStop(Color(0xFF66E7C2), .19),
+        ColorAndStop(Color(0xFFF6E2A5), .38),
+        ColorAndStop(Color(0xFFF6E4AD), .58),
+        ColorAndStop(Color(0xFFF9EFB7), .68),
+        ColorAndStop(Color(0xFFFAF2C3), .79),
+        ColorAndStop(Color(0xFFFFFFDC), .9),
+        ColorAndStop(Color(0xFFFFFFE3), 1.00)
+      ];
+  static List<ColorAndStop> get drySandToVibrantBlueGradFromTopPhase4 => const [
+        ColorAndStop(Color(0xFF6aEAB9), 0.0),
+        ColorAndStop(Color(0xFF66E7C2), .19),
+        ColorAndStop(Color(0xFF64E6C8), .38),
+        ColorAndStop(Color(0xFFF6E4AD), .58),
+        ColorAndStop(Color(0xFFF9EFB7), .68),
+        ColorAndStop(Color(0xFFFAF2C3), .79),
+        ColorAndStop(Color(0xFFFFFFDC), .9),
+        ColorAndStop(Color(0xFFFFFFE3), 1.00)
+      ];
+  static List<ColorAndStop> get drySandToVibrantBlueGradFromTopPhase5 => const [
+        ColorAndStop(Color(0xFF6aEAB9), 0.0),
+        ColorAndStop(Color(0xFF66E7C2), .19),
+        ColorAndStop(Color(0xFF64E6C8), .38),
+        ColorAndStop(Color(0xFF57DEDE), .58),
+        ColorAndStop(Color(0xFFF9EFB7), .68),
+        ColorAndStop(Color(0xFFFAF2C3), .79),
+        ColorAndStop(Color(0xFFFFFFDC), .9),
+        ColorAndStop(Color(0xFFFFFFE3), 1.00)
+      ];
+  static List<ColorAndStop> get drySandToVibrantBlueGradFromTopPhase6 => const [
+        ColorAndStop(Color(0xFF6aEAB9), 0.0),
+        ColorAndStop(Color(0xFF66E7C2), .19),
+        ColorAndStop(Color(0xFF64E6C8), .38),
+        ColorAndStop(Color(0xFF57DEDE), .58),
+        ColorAndStop(Color(0xFF53DCE3), .68),
+        ColorAndStop(Color(0xFFFAF2C3), .79),
+        ColorAndStop(Color(0xFFFFFFDC), .9),
+        ColorAndStop(Color(0xFFFFFFE3), 1.00)
+      ];
+  static List<ColorAndStop> get drySandToVibrantBlueGradFromTopPhase7 => const [
+        ColorAndStop(Color(0xFF6aEAB9), 0.0),
+        ColorAndStop(Color(0xFF66E7C2), .19),
+        ColorAndStop(Color(0xFF64E6C8), .38),
+        ColorAndStop(Color(0xFF57DEDE), .58),
+        ColorAndStop(Color(0xFF53DCE3), .68),
+        ColorAndStop(Color(0xFF4FD9E8), .79),
+        ColorAndStop(Color(0xFFFFFFDC), .9),
+        ColorAndStop(Color(0xFFFFFFE3), 1.00)
+      ];
+  static List<ColorAndStop> get drySandToVibrantBlueGradFromTopPhase8 => const [
+        ColorAndStop(Color(0xFF6aEAB9), 0.0),
+        ColorAndStop(Color(0xFF66E7C2), .19),
+        ColorAndStop(Color(0xFF64E6C8), .38),
+        ColorAndStop(Color(0xFF57DEDE), .58),
+        ColorAndStop(Color(0xFF53DCE3), .68),
+        ColorAndStop(Color(0xFF4FD9E8), .79),
+        ColorAndStop(Color(0xFF6AEAB9), .9),
+        ColorAndStop(Color(0xFFFFFFE3), 1.00)
+      ];
+  static List<ColorAndStop> get drySandToVibrantBlueGradForBothPhase9 => const [
+        ColorAndStop(Color(0xFF6aEAB9), 0.0),
+        ColorAndStop(Color(0xFF66E7C2), .19),
+        ColorAndStop(Color(0xFF64E6C8), .38),
+        ColorAndStop(Color(0xFF57DEDE), .58),
+        ColorAndStop(Color(0xFF53DCE3), .68),
+        ColorAndStop(Color(0xFF4FD9E8), .79),
+        ColorAndStop(Color(0xFF6AEAB9), .9),
+        ColorAndStop(Color(0xFF6AEAB9), 1.00)
+      ];
+
+  static List<ColorAndStop> get drySandToVibrantBlueGradFromBottomPhase2 =>
+      const [
+        ColorAndStop(Color(0xFFFFFFE3), 0.0),
+        ColorAndStop(Color(0xFFFFFFDC), .19),
+        ColorAndStop(Color(0xFFFAF2C3), .38),
+        ColorAndStop(Color(0xFFF9EFB7), .58),
+        ColorAndStop(Color(0xFFF6E4AD), .68),
+        ColorAndStop(Color(0xFFF6E2A5), .79),
+        ColorAndStop(Color(0xFFF4DD98), .90),
+        ColorAndStop(Color(0xFF6aEAB9), 1.0),
+      ];
+  static List<ColorAndStop> get drySandToVibrantBlueGradFromBottomPhase3 =>
+      const [
+        ColorAndStop(Color(0xFFFFFFE3), 0.0),
+        ColorAndStop(Color(0xFFFFFFDC), .19),
+        ColorAndStop(Color(0xFFFAF2C3), .38),
+        ColorAndStop(Color(0xFFF9EFB7), .58),
+        ColorAndStop(Color(0xFFF6E4AD), .68),
+        ColorAndStop(Color(0xFFF6E2A5), .79),
+        ColorAndStop(Color(0xFF66E7C2), .9),
+        ColorAndStop(Color(0xFF6aEAB9), 1.0),
+      ];
+  static List<ColorAndStop> get drySandToVibrantBlueGradFromBottomPhase4 =>
+      const [
+        ColorAndStop(Color(0xFFFFFFE3), 0.0),
+        ColorAndStop(Color(0xFFFFFFDC), .19),
+        ColorAndStop(Color(0xFFFAF2C3), .38),
+        ColorAndStop(Color(0xFFF9EFB7), .58),
+        ColorAndStop(Color(0xFFF6E4AD), .68),
+        ColorAndStop(Color(0xFF64E6C8), .79),
+        ColorAndStop(Color(0xFF66E7C2), .9),
+        ColorAndStop(Color(0xFF6aEAB9), 1.0),
+      ];
+  static List<ColorAndStop> get drySandToVibrantBlueGradFromBottomPhase5 =>
+      const [
+        ColorAndStop(Color(0xFF6aEAB9), 0.0),
+        ColorAndStop(Color(0xFF66E7C2), .19),
+        ColorAndStop(Color(0xFF64E6C8), .38),
+        ColorAndStop(Color(0xFF57DEDE), .58),
+        ColorAndStop(Color(0xFFF9EFB7), .68),
+        ColorAndStop(Color(0xFFFAF2C3), .79),
+        ColorAndStop(Color(0xFFFFFFDC), .9),
+        ColorAndStop(Color(0xFFFFFFE3), 1.00)
+      ];
+  static List<ColorAndStop> get drySandToVibrantBlueGradFromBottomPhase6 =>
+      const [
+        ColorAndStop(Color(0xFFFFFFE3), 0.0),
+        ColorAndStop(Color(0xFFFFFFDC), .19),
+        ColorAndStop(Color(0xFFFAF2C3), .39),
+        ColorAndStop(Color(0xFF53DCE3), .58),
+        ColorAndStop(Color(0xFF57DEDE), .68),
+        ColorAndStop(Color(0xFF64E6C8), .79),
+        ColorAndStop(Color(0xFF66E7C2), .9),
+        ColorAndStop(Color(0xFF6aEAB9), 1.0),
+      ];
+  static List<ColorAndStop> get drySandToVibrantBlueGradFromBottomPhase7 =>
+      const [
+        ColorAndStop(Color(0xFFFFFFE3), 0.0),
+        ColorAndStop(Color(0xFFFFFFDC), .19),
+        ColorAndStop(Color(0xFF4FD9E8), .38),
+        ColorAndStop(Color(0xFF53DCE3), .58),
+        ColorAndStop(Color(0xFF57DEDE), .68),
+        ColorAndStop(Color(0xFF64E6C8), .79),
+        ColorAndStop(Color(0xFF66E7C2), .9),
+        ColorAndStop(Color(0xFF6aEAB9), 1.0),
+      ];
+  static List<ColorAndStop> get drySandToVibrantBlueGradFromBottomPhase8 =>
+      const [
+        ColorAndStop(Color(0xFFFFFFE3), 0.0),
+        ColorAndStop(Color(0xFF6AEAB9), .19),
+        ColorAndStop(Color(0xFF4FD9E8), .38),
+        ColorAndStop(Color(0xFF53DCE3), .58),
+        ColorAndStop(Color(0xFF57DEDE), .68),
+        ColorAndStop(Color(0xFF64E6C8), .79),
+        ColorAndStop(Color(0xFF66E7C2), .9),
+        ColorAndStop(Color(0xFF6aEAB9), 1.0),
+      ];
+
+  static List<List<ColorAndStop>> get drySandToVibrantBlueFromBottomSequence =>
+      [
+        drySand,
+        drySandToVibrantBlueGradFromBottomPhase2,
+        drySandToVibrantBlueGradFromBottomPhase3,
+        drySandToVibrantBlueGradFromBottomPhase4,
+        drySandToVibrantBlueGradFromBottomPhase5,
+        drySandToVibrantBlueGradFromBottomPhase6,
+        drySandToVibrantBlueGradFromBottomPhase7,
+        drySandToVibrantBlueGradFromBottomPhase8,
+        drySandToVibrantBlueGradForBothPhase9,
       ];
 }
