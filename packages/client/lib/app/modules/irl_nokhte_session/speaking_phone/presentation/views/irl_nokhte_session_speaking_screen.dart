f89f4b9ae56baecbc6fb94ffe1fe428ce4579215
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-08 08:37:47 -0600

    add: speaking_phone screen

---
+++ packages/client/lib/app/modules/irl_nokhte_session/speaking_phone/presentation/views/irl_nokhte_session_speaking_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:nokhte/app/modules/irl_nokhte_session/irl_nokhte_session.dart';
+
+class IrlNokhteSessionSpeakingScreen extends HookWidget {
+  final IrlNokhteSessionSpeakingScreenCoordinator coordinator;
+  const IrlNokhteSessionSpeakingScreen({
+    super.key,
+    required this.coordinator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    useEffect(() {
+      coordinator.constructor();
+
+      return null;
+    }, []);
+    // useOnAppLifecycleStateChange(
+    // (previous, current) => coordinator.onAppLifeCycleStateChange(
+    //       current,
+    //       onResumed: () => coordinator.onResumed(),
+    //       onInactive: () => coordinator.onInactive(),
+    //     ));
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Swipe(
+        store: coordinator.swipe,
+        child: Hold(
+          store: coordinator.hold,
+          child: MultiHitStack(
+            children: [
+              FullScreen(
+                child: BeachWaves(
+                  store: coordinator.widgets.beachWaves,
+                ),
+              ),
+              MirroredText(
+                store: coordinator.widgets.mirroredText,
+              ),
+              FullScreen(
+                child: TouchRipple(
+                  store: coordinator.widgets.touchRipple,
+                ),
+              ),
+              CollaboratorPresenceIncidentsOverlay(
+                store: coordinator.presence.incidentsOverlayStore,
+              ),
+              WifiDisconnectOverlay(
+                store: coordinator.widgets.wifiDisconnectOverlay,
+              ),
+            ],
+          ),
+        ),
+      ),
+      // ),
+    );
+  }
+}
