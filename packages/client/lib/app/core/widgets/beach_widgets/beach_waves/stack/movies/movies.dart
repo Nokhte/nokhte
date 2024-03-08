d907fa90fd3d9039d6371b55e93bf7e78af5dba1
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-08 10:19:36 -0600

    add: export for sequence_movie

--- packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/movies/movies.dart
+++ packages/client/lib/app/core/widgets/beach_widgets/beach_waves/stack/movies/movies.dart
@@ -1,6 +1,5 @@
 export './static_beach_waves_movie.dart';
-export './dynamic_to_times_up_start.dart';
-export './times_up_end_to_dynamic.dart';
 export './two_second_beach_transition_movie.dart';
+export 'sequence_transition_movie.dart';
 export 'any_to_on_shore_movie.dart';
 export './on_shore_to_any_movie.dart';
