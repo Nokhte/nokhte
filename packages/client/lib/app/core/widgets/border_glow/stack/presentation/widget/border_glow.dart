21571dcd92abbde1a6d430573757e333c00399ef
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-09 11:05:12 -0600

    refactor: border_glow to be simpler

--- packages/client/lib/app/core/widgets/border_glow/stack/presentation/widget/border_glow.dart
+++ packages/client/lib/app/core/widgets/border_glow/stack/presentation/widget/border_glow.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:nokhte/app/core/hooks/hooks.dart';
-import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
 import 'dart:ui';
@@ -17,39 +15,34 @@ class BorderGlow extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return Observer(
-      builder: (context) => AnimatedOpacity(
-        onEnd: () => store.toggleHasFadedIn(),
-        opacity: useWidgetOpacity(store.showWidget),
-        duration: Seconds.get(1),
-        child: CustomAnimationBuilder(
-          tween: store.movie,
-          control: store.control,
-          duration: store.movie.duration,
-          onCompleted: () => store.onCompleted(),
-          builder: (context, value, child) {
-            store.setAnimationValues(
-              color: value.get('color'),
-              width: value.get('width'),
-            );
-            return ClipPath(
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: value.get('color'),
-                    width: value.get('width'),
-                  ),
-                ),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(
-                    sigmaX: 40,
-                    sigmaY: 40,
-                  ),
-                  child: Container(),
+      builder: (context) => CustomAnimationBuilder(
+        tween: store.movie,
+        control: store.control,
+        duration: store.movie.duration,
+        onCompleted: () => store.onCompleted(),
+        builder: (context, value, child) {
+          store.setAnimationValues(
+            color: value.get('color'),
+            width: value.get('width'),
+          );
+          return ClipPath(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: value.get('color'),
+                  width: value.get('width'),
                 ),
               ),
-            );
-          },
-        ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 40,
+                  sigmaY: 40,
+                ),
+                child: Container(),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
