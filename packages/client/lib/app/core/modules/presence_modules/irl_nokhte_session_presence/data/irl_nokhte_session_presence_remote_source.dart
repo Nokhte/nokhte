0e3defb51e0ceff035789a9d3869dc8d5e3ca105
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-09 17:17:48 -0600

    add: midified updateCurrentPhase for ease of testing

--- packages/client/lib/app/core/modules/presence_modules/irl_nokhte_session_presence/data/irl_nokhte_session_presence_remote_source.dart
+++ packages/client/lib/app/core/modules/presence_modules/irl_nokhte_session_presence/data/irl_nokhte_session_presence_remote_source.dart
@@ -1,4 +1,5 @@
 import 'package:nokhte/app/core/modules/presence_modules/presence_modules.dart';
+import 'package:nokhte/app/core/types/seconds.dart';
 import 'package:nokhte_backend/tables/_real_time_enabled/active_irl_nokhte_sessions/types/types.dart';
 import 'package:nokhte_backend/tables/irl_active_nokhte_sessions.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -25,7 +26,16 @@ class IrlNokhteSessionPresenceRemoteSourceImpl
   setUserAsCurrentTalker() async => await queries.setUserAsTheCurrentTalker();
 
   @override
-  updateCurrentPhase(params) async => await queries.updateCurrentPhases(params);
+  updateCurrentPhase(params) async {
+    // TODO take this out if the build is stable
+    if (params == 2.0) {
+      Future.delayed(Seconds.get(10), () async {
+        await queries.updateCurrentPhases(params,
+            shouldEditCollaboratorsInfo: true);
+      });
+    }
+    return await queries.updateCurrentPhases(params);
+  }
 
   @override
   updateOnlineStatus(params) async => await queries.updateOnlineStatus(
