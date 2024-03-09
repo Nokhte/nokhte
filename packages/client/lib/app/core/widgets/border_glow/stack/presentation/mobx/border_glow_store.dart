b79f86c912f22284ec67b837d5ee8c7a970eef7d
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-09 16:59:12 -0600

    fix: glowDown problem by bifurcating into 2 separate controls & movies

--- packages/client/lib/app/core/widgets/border_glow/stack/presentation/mobx/border_glow_store.dart
+++ packages/client/lib/app/core/widgets/border_glow/stack/presentation/mobx/border_glow_store.dart
@@ -19,6 +19,12 @@ abstract class _BorderGlowStoreBase
   @observable
   bool isGlowingUp = false;
 
+  @computed
+  MovieTween get activeMovie => isGlowingUp ? movie : altMovie;
+
+  @computed
+  Control get activeControl => isGlowingUp ? control : altControl;
+
   @action
   @override
   initMovie(param) {
@@ -30,15 +36,8 @@ abstract class _BorderGlowStoreBase
 
   @action
   initGlowDown() {
-    setMovieStatus(MovieStatus.inProgress);
     isGlowingUp = false;
-    setMovie(BorderGlowDownMovie.getMovie(
-      lastColor: currentColor,
-      lastWidth: currentWidth,
-    ));
-    setControl(Control.playFromStart);
-    setControl(Control.stop);
-    setControl(Control.play);
+    altControl = Control.playFromStart;
   }
 
   @observable
@@ -47,12 +46,25 @@ abstract class _BorderGlowStoreBase
   @observable
   double currentWidth = 0.0;
 
+  @observable
+  Control altControl = Control.stop;
+
+  @observable
+  MovieTween altMovie = BorderGlowDownMovie.getMovie();
+
   @action
   setAnimationValues({
     required Color color,
     required double width,
   }) {
-    currentColor = color;
-    currentWidth = width;
+    if (isGlowingUp) {
+      currentColor = color;
+      currentWidth = width;
+
+      altMovie = BorderGlowDownMovie.getMovie(
+        lastColor: currentColor,
+        lastWidth: currentWidth,
+      );
+    }
   }
 }
