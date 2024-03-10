c8e79e01c8c05528fe91646d29130a7c7d7c846b
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-10 12:10:38 -0500

    format: clear commented out import out of home_screen_phase0_coordinator

--- packages/client/lib/app/modules/home/presentation/mobx/coordinators/phase0/home_screen_phase0_coordinator.dart
+++ packages/client/lib/app/modules/home/presentation/mobx/coordinators/phase0/home_screen_phase0_coordinator.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
-// import 'dart:async';
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/modules/delete_unconsecrated_collaborations/mobx/mobx.dart';
