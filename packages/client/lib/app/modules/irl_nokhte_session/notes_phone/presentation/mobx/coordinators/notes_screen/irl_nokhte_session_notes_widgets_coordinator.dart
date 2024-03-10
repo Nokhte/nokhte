ae16fac8e9762d9095ac10dd248734a6b66a7836
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-10 12:12:41 -0500

    add: skeletal irl_nokhte_session_notes_widgets_coordinator impl

--- packages/client/lib/app/modules/irl_nokhte_session/notes_phone/presentation/mobx/coordinators/notes_screen/irl_nokhte_session_notes_widgets_coordinator.dart
+++ packages/client/lib/app/modules/irl_nokhte_session/notes_phone/presentation/mobx/coordinators/notes_screen/irl_nokhte_session_notes_widgets_coordinator.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/widgets/beach_widgets/shared/shared.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 part 'irl_nokhte_session_notes_widgets_coordinator.g.dart';
 
@@ -10,18 +11,21 @@ class IrlNokhteSessionNotesWidgetsCoordinator = _IrlNokhteSessionNotesWidgetsCoo
 abstract class _IrlNokhteSessionNotesWidgetsCoordinatorBase
     extends BaseWidgetsCoordinator with Store {
   final BeachWavesStore beachWaves;
-  final MirroredTextStore mirroredText;
-  // final TheStonesThrow stonesThrow;
+  final TouchRippleStore touchRipple;
+  final ThrowingStoneStore throwingStone;
+  final TextEditorStore textEditor;
   final WifiDisconnectOverlayStore wifiDisconnectOverlay;
   _IrlNokhteSessionNotesWidgetsCoordinatorBase({
     required this.beachWaves,
-    required this.mirroredText,
     required this.wifiDisconnectOverlay,
-    // required this.stonesThrow,
+    required this.touchRipple,
+    required this.throwingStone,
+    required this.textEditor,
   });
 
   @action
   constructor() {
-    //
+    beachWaves.setMovieMode(BeachWaveMovieModes.vibrantBlueGradToHalfAndHalf);
+    textEditor.initFadeIn();
   }
 }
