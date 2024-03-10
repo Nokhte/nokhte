1bd490da8432855d84c2db1a5aa99131985c1769
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-10 12:11:38 -0500

    add: irl_nokhte_session_notes_instructions_coordinator impl

--- packages/client/lib/app/modules/irl_nokhte_session/notes_phone/presentation/mobx/coordinators/notes_instructions/irl_nokhte_session_notes_instructions_coordinator.dart
+++ packages/client/lib/app/modules/irl_nokhte_session/notes_phone/presentation/mobx/coordinators/notes_instructions/irl_nokhte_session_notes_instructions_coordinator.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'dart:async';
+
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/base_coordinator.dart';
 import 'package:nokhte/app/core/modules/presence_modules/presence_modules.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'irl_nokhte_session_notes_instructions_widgets_coordinator.dart';
 part 'irl_nokhte_session_notes_instructions_coordinator.g.dart';
@@ -23,10 +26,43 @@ abstract class _IrlNokhteSessionNotesInstructionsCoordinatorBase
     required this.presence,
   }) : sessionMetadata = presence.getSessionMetadataStore;
 
+  @observable
+  bool instructionModeIsUnlocked = false;
+
   @action
-  constructor() {
+  constructor() async {
     widgets.constructor();
+    initReactors();
   }
+
+  @action
+  initReactors() {
+    tapReactor();
+    presence.initReactors(
+      onCollaboratorJoined: () {
+        widgets.setDisableTouchInput(false);
+      },
+      onCollaboratorLeft: () {
+        if (instructionModeIsUnlocked) {
+          widgets.setDisableTouchInput(true);
+        }
+      },
+    );
+    collaboratorPhaseReactor();
+    widgets.wifiDisconnectOverlay.initReactors(
+      onQuickConnected: () => widgets.setDisableTouchInput(false),
+      onLongReConnected: () {
+        widgets.setDisableTouchInput(false);
+      },
+      onDisconnected: () {
+        if (instructionModeIsUnlocked) {
+          widgets.setDisableTouchInput(true);
+        }
+      },
+    );
+  }
+
+  // ok so the tap protocol is basically
 
   @action
   onInactive() async {
@@ -41,5 +77,36 @@ abstract class _IrlNokhteSessionNotesInstructionsCoordinatorBase
     if (sessionMetadata.collaboratorIsOnline) {
       presence.incidentsOverlayStore.onCollaboratorJoined();
     }
+  }
+
+  collaboratorPhaseReactor() => reaction(
+        (p0) => sessionMetadata.collaboratorPhase,
+        (p0) {
+          if (sessionMetadata.canMoveIntoSecondInstructionsSet) {
+            widgets.onInstructionModeUnlocked();
+            // widgets.mirroredText.startBothRotatingText(isResuming: true);
+          }
+        },
+      );
+
+  tapReactor() => reaction(
+        (p0) => tap.tapCount,
+        (p0) => ifTouchIsNotDisabled(
+          () async {
+            widgets.onTap(
+              tap.currentTapPosition,
+              onFlowFinished: () async => await updateCurrentPhase(),
+            );
+          },
+        ),
+      );
+  updateCurrentPhase() async {
+    Timer.periodic(Seconds.get(0, milli: 500), (timer) async {
+      if (presence.getSessionMetadataStore.userPhase != 2.0) {
+        await presence.updateCurrentPhase(2.0);
+      } else {
+        timer.cancel();
+      }
+    });
   }
 }
