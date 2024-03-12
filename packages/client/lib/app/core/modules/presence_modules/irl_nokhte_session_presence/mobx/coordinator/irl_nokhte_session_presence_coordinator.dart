d44136a1a105b1bd1fad7f5c171c99d38edac23b
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-12 09:19:48 -0500

    add: content update & finish methods to irl_nokhte_session_presence_coordinator

--- packages/client/lib/app/core/modules/presence_modules/irl_nokhte_session_presence/mobx/coordinator/irl_nokhte_session_presence_coordinator.dart
+++ packages/client/lib/app/core/modules/presence_modules/irl_nokhte_session_presence/mobx/coordinator/irl_nokhte_session_presence_coordinator.dart
@@ -2,6 +2,7 @@
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/modules/presence_modules/irl_nokhte_session_presence/domain/domain.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/core/modules/presence_modules/presence_modules.dart';
 part 'irl_nokhte_session_presence_coordinator.g.dart';
@@ -14,6 +15,8 @@ abstract class _IrlNokhteSessionPresenceCoordinatorBase
   final GetIrlNokhteSessionMetadataStore getSessionMetadataStore;
   final NokhteBlurStore blur;
   final CollaboratorPresenceIncidentsOverlayStore incidentsOverlayStore;
+  final AddContent addContentLogic;
+  final CompleteTheSession completeTheSessionLogic;
 
   _IrlNokhteSessionPresenceCoordinatorBase({
     required super.updateWhoIsTalkingLogic,
@@ -21,11 +24,19 @@ abstract class _IrlNokhteSessionPresenceCoordinatorBase
     required super.updateCurrentPhaseLogic,
     required super.updateOnlineStatusLogic,
     required this.getSessionMetadataStore,
+    required this.addContentLogic,
+    required this.completeTheSessionLogic,
     required this.blur,
   }) : incidentsOverlayStore = CollaboratorPresenceIncidentsOverlayStore(
           sessionMetadataStore: getSessionMetadataStore,
           blur: blur,
         );
+
+  @observable
+  bool contentIsUpdated = false;
+
+  @observable
+  bool sessionIsFinished = false;
 
   @override
   @action
@@ -49,6 +60,24 @@ abstract class _IrlNokhteSessionPresenceCoordinatorBase
   listen() {
     state = StoreState.loading;
     getSessionMetadataStore.get(NoParams());
+  }
+
+  @action
+  addContent(String params) async {
+    final res = await addContentLogic(params);
+    res.fold(
+      (failure) => errorUpdater(failure),
+      (contentUpdateStatus) => contentIsUpdated = contentUpdateStatus,
+    );
+  }
+
+  @action
+  completeTheSession() async {
+    final res = await completeTheSessionLogic(NoParams());
+    res.fold(
+      (failure) => errorUpdater(failure),
+      (sessionUpdateStatus) => sessionIsFinished = sessionUpdateStatus,
+    );
   }
 
   @action
