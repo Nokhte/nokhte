546a7f8a86d7f9f0041cafd3862ebc0cdfb89a47
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-16 11:32:31 -0500

    add: storage_contract_impl

---
+++ packages/client/lib/app/modules/storage/data/contracts/storage_contract_impl.dart
@@ -0,0 +1,45 @@
+import 'package:dartz/dartz.dart';
+import 'package:nokhte/app/core/constants/constants.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/mixins/mixin.dart';
+import 'package:nokhte/app/modules/storage/domain/domain.dart';
+import 'package:nokhte/app/modules/storage/data/data.dart';
+import 'package:nokhte/app/core/network/network_info.dart';
+
+class StorageContractImpl with ResponseToStatus implements StorageContract {
+  final StorageRemoteSource remoteSource;
+  final NetworkInfo networkInfo;
+
+  StorageContractImpl({
+    required this.remoteSource,
+    required this.networkInfo,
+  });
+
+  @override
+  getNokhteSessionArtifacts(NoParams params) async {
+    if (await networkInfo.isConnected) {
+      final nokhteSessionRes = await remoteSource.getNokhteSessionArtifacts();
+      final collaboratorRowsRes = await remoteSource.getCollaboratorRows();
+      final userUID = remoteSource.getUserUID();
+      return Right(
+        NokhteSessionArtifactModel.fromSupabase(
+          nokhteSessionRes: nokhteSessionRes,
+          collaboratorRowsRes: collaboratorRowsRes,
+          userUID: userUID,
+        ),
+      );
+    } else {
+      return Left(FailureConstants.internetConnectionFailure);
+    }
+  }
+
+  @override
+  updateSessionAlias(UpdateSessionAliasParams params) async {
+    if (await networkInfo.isConnected) {
+      final res = await remoteSource.updateSessionAlias(params);
+      return Right(fromSupabase(res));
+    } else {
+      return Left(FailureConstants.internetConnectionFailure);
+    }
+  }
+}
