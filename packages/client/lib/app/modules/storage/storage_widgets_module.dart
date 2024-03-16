0d8e415def30271109ab1c49b5762a5105bf49ba
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-16 11:34:55 -0500

    add: storage_widgets_module

---
+++ packages/client/lib/app/modules/storage/storage_widgets_module.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:nokhte/app/core/widgets/modules.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'presentation/presentation.dart';
+
+class StorageWidgetsModule extends Module {
+  @override
+  List<Module> get imports => [
+        GestureCrossModule(),
+        WifiDisconnectOverlayModule(),
+      ];
+  @override
+  exportedBinds(i) {
+    i.add<StorageHomeWidgetsCoordinator>(
+      () => StorageHomeWidgetsCoordinator(
+        beachWaves: BeachWavesStore(),
+        gestureCross: Modular.get<GestureCrossStore>(),
+        wifiDisconnectOverlay: Modular.get<WifiDisconnectOverlayStore>(),
+      ),
+    );
+  }
+}
