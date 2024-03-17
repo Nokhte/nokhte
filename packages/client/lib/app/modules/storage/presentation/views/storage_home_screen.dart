a0935c34c3257ebba79db660a9724f7ee542fd8e
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-17 15:58:12 -0500

    add: storage_home_screen functionality

--- packages/client/lib/app/modules/storage/presentation/views/storage_home_screen.dart
+++ packages/client/lib/app/modules/storage/presentation/views/storage_home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:nokhte/app/core/hooks/hooks.dart';
+import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
-import 'package:nokhte/app/modules/storage/presentation/mobx/storage_home_coordinator.dart';
+import 'package:nokhte/app/modules/storage/presentation/mobx/mobx.dart';
 
 class StorageHomeScreen extends HookWidget {
   final StorageHomeCoordinator coordinator;
@@ -15,39 +17,57 @@ class StorageHomeScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final size = useSquareSize(relativeLength: .20);
+    final height = useFullScreenSize().height;
     useEffect(() {
       coordinator.constructor();
       return null;
     }, []);
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Swipe(
-        store: coordinator.swipe,
-        child: MultiHitStack(
-          children: [
-            FullScreen(
+    return Observer(builder: (context) {
+      return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Swipe(
+          store: coordinator.swipe,
+          child: MultiHitStack(
+            children: [
+              FullScreen(
                 child: BeachWaves(
-              store: coordinator.widgets.beachWaves,
-            )),
-            GestureCross(
-              showGlowAndOutline: true,
-              config: GestureCrossConfiguration(
-                left: Right(
-                  NokhteGradientConfig(
-                    gradientType: NokhteGradientTypes.onShore,
-                  ),
+                  store: coordinator.widgets.beachWaves,
                 ),
               ),
-              size: size,
-              store: coordinator.widgets.gestureCross,
-            ),
-            WifiDisconnectOverlay(
-              store: coordinator.widgets.wifiDisconnectOverlay,
-            ),
-          ],
+              Center(
+                  child: SmartText(
+                store: coordinator.widgets.smartText,
+                bottomPadding: height * .75,
+                opacityDuration: Seconds.get(1),
+              )),
+              Padding(
+                padding: EdgeInsets.only(top: height * .11),
+                child: SessionCard(
+                  store: coordinator.widgets.sessionCard,
+                  sessions: coordinator.nokhteSessionArtifacts,
+                ),
+              ),
+              GestureCross(
+                crossShouldUseObserver: coordinator.crossShouldUseObserver,
+                showGlowAndOutline: true,
+                config: GestureCrossConfiguration(
+                  left: Right(
+                    NokhteGradientConfig(
+                      gradientType: NokhteGradientTypes.onShore,
+                    ),
+                  ),
+                ),
+                size: size,
+                store: coordinator.widgets.gestureCross,
+              ),
+              WifiDisconnectOverlay(
+                store: coordinator.widgets.wifiDisconnectOverlay,
+              ),
+            ],
+          ),
         ),
-      ),
-      // ),
-    );
+        // ),
+      );
+    });
   }
 }
