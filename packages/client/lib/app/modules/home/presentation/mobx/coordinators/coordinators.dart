2fd7bea8d0cda07f3293e6c2503efb552b66329e
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-17 15:51:55 -0500

    add: phase 4 exports to home coordinators

--- packages/client/lib/app/modules/home/presentation/mobx/coordinators/coordinators.dart
+++ packages/client/lib/app/modules/home/presentation/mobx/coordinators/coordinators.dart
@@ -2,3 +2,4 @@ export 'phase0/phase0.dart';
 export "phase1/phase1.dart";
 export "phase2/phase2.dart";
 export "phase3/phase3.dart";
+export 'phase4/phase4.dart';
--- packages/client/lib/app/modules/home/presentation/screens/screens.dart
+++ packages/client/lib/app/modules/home/presentation/screens/screens.dart
@@ -2,3 +2,4 @@ export 'home_screen_phase0_root_decider.dart';
 export 'home_screen_phase1_no_invitation_flow_no_session.dart';
 export 'home_screen_phase2_done_invitation_flow_no_session.dart';
 export 'home_screen_phase3_has_done_session.dart';
+export 'home_screen_phase4_has_gone_into_storage.dart';
