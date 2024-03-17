c0f0d3dbf7ce3f342ad073f50138b6b3d805a54f
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-17 15:55:40 -0500

    add: cards export in widgets

--- packages/client/lib/app/core/widgets/widgets.dart
+++ packages/client/lib/app/core/widgets/widgets.dart
@@ -1,5 +1,6 @@
 export "border_glow/border_glow.dart";
 export 'beach_widgets/beach_widgets.dart';
+export 'cards/cards.dart';
 export "collaborator_presence_incidents_overlay/collaborator_presence_incidents_overlay.dart";
 export 'explanatory_model_widgets/explanatory_model_widgets.dart';
 export 'full_screen/full_screen.dart';
