4af78d7e0101a858f60f4da74d81d7ad007be67b
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-17 15:56:14 -0500

    refactor: nokhte_session_artifact_entity for less strict typing + factory

--- packages/client/lib/app/modules/storage/domain/entities/nokhte_session_artifact_entity.dart
+++ packages/client/lib/app/modules/storage/domain/entities/nokhte_session_artifact_entity.dart
@@ -4,13 +4,22 @@ class NokhteSessionArtifactEntity extends Equatable {
   final String title;
   final String date;
   final int id;
-  final List<String> content;
+  final List content;
   const NokhteSessionArtifactEntity({
     required this.title,
     required this.content,
     required this.id,
     required this.date,
   });
+
+  factory NokhteSessionArtifactEntity.initial() =>
+      const NokhteSessionArtifactEntity(
+        title: '',
+        content: [],
+        id: 0,
+        date: '',
+      );
+
   @override
   List<Object?> get props => [title, content, id];
 }
