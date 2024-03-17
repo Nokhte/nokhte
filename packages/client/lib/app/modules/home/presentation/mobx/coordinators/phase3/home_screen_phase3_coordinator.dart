434e67377c4c8728062dc4b257914229dfad0b11
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-17 15:45:13 -0500

    add: complete home phase 3 impl

--- packages/client/lib/app/modules/home/presentation/mobx/coordinators/phase3/home_screen_phase3_coordinator.dart
+++ packages/client/lib/app/modules/home/presentation/mobx/coordinators/phase3/home_screen_phase3_coordinator.dart
@@ -1,7 +1,11 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api, annotate_overrides, overridden_fields
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/modules/posthog/constants/constants.dart';
+import 'package:nokhte/app/core/modules/user_information/mobx/coordinators/user_information_coordinator.dart';
 import 'package:nokhte/app/modules/home/presentation/mobx/coordinators/shared/base_home_screen_coordinator.dart';
+import 'package:nokhte/app/modules/storage/domain/domain.dart';
 import 'home_screen_phase3_widgets_coordinator.dart';
 part 'home_screen_phase3_coordinator.g.dart';
 
@@ -11,20 +15,29 @@ class HomeScreenPhase3Coordinator = _HomeScreenPhase3CoordinatorBase
 abstract class _HomeScreenPhase3CoordinatorBase
     extends BaseHomeScreenCoordinator with Store {
   final HomeScreenPhase3WidgetsCoordinator widgets;
+  final GetNokhteSessionArtifacts getNokhteSessionArtifactsLogic;
+  final UserInformationCoordinator userInformation;
 
   _HomeScreenPhase3CoordinatorBase({
     required super.collaborationLogic,
     required super.swipe,
     required super.deepLinks,
     required this.widgets,
+    required this.getNokhteSessionArtifactsLogic,
+    required this.userInformation,
     required super.captureScreen,
   }) : super(widgets: widgets);
+
+  @observable
+  ObservableList<NokhteSessionArtifactEntity> nokhteSessionArtifacts =
+      ObservableList();
 
   @action
   constructor() async {
     widgets.constructor();
     initReactors();
     await captureScreen(Screens.homePhase3);
+    await getNokhteSessionArtifacts();
   }
 
   @override
@@ -32,9 +45,36 @@ abstract class _HomeScreenPhase3CoordinatorBase
     super.initReactors();
     swipeReactor(
       onSwipeUp: widgets.onSwipeUp,
-      onSwipeRight: () {
+      onSwipeRight: () async {
         widgets.onSwipeRight();
         setDisableAllTouchFeedback(true);
+        await userInformation.updateHasEnteredStorageLogic(true);
+      },
+    );
+    widgets.beachWavesMovieStatusReactor(
+      onShoreToOceanDiveComplete: onShoreToOceanDiveComplete,
+      onShoreToVibrantBlueComplete: onShoreToVibrantBlueComplete,
+      onVirginStorageEntry: onVirginStorageEntry,
+      onSubsequentStorageEntry: () {},
+    );
+  }
+
+  @action
+  getNokhteSessionArtifacts() async {
+    final res = await getNokhteSessionArtifactsLogic(NoParams());
+    res.fold(
+      (failure) => errorUpdater(failure),
+      (artifacts) => nokhteSessionArtifacts = ObservableList.of(artifacts),
+    );
+  }
+
+  @action
+  onVirginStorageEntry() {
+    Modular.to.navigate(
+      '/storage/content',
+      arguments: {
+        "content": nokhteSessionArtifacts.first,
+        "isFirstTime": true,
       },
     );
   }
