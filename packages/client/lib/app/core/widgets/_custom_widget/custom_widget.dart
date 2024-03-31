2175b20ad39e5e2d05907ad24854c43862778e50
Sonny Vesali <sonny.vesali@protonmail.com>
2024-03-31 09:01:36 -0500

    re-order: _custom_widget file hierarchy

---
+++ packages/client/lib/app/core/widgets/_custom_widget/custom_widget.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: unused_import
+
+import 'canvas/custom_widget_painter.dart';
+export 'constants/constants.dart';
+export 'mobx/custom_mobx_store.dart';
+export 'types/types.dart';
