f93eace811a8dd22ec9e8b59674163b301b14673
Sonny Vesali <sonny.vesali@protonmail.com>
2024-04-02 10:51:12 -0500

    refactor: phase2 to fit new home screen instruction requirements

--- packages/client/lib/app/modules/home/presentation/mobx/phase2/home_screen_phase2_widgets_coordinator.dart
+++ packages/client/lib/app/modules/home/presentation/mobx/phase2/home_screen_phase2_widgets_coordinator.dart
@@ -1,7 +1,11 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'dart:async';
+
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/extensions/extensions.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/home/presentation/mobx/mobx.dart';
@@ -22,6 +26,8 @@ abstract class _HomeScreenPhase2WidgetsCoordinatorBase
     required super.errorSmartText,
     required super.secondaryErrorSmartText,
     required super.touchRipple,
+    required super.centerInstructionalNokhte,
+    required super.instructionalGradientNokhte,
   });
 
   @observable
@@ -38,14 +44,14 @@ abstract class _HomeScreenPhase2WidgetsCoordinatorBase
   @action
   constructor(Offset offset) {
     super.constructor(offset);
-    primarySmartText.setMessagesData(MessagesData.firstTimeHomeList);
-    Timer(Seconds.get(3), () {
-      if (!hasSwipedUp) {
-        gestureCross.startBlinking();
-        primarySmartText.startRotatingText();
-        toggleGracePeriodHasExpired();
-      }
-    });
+    primarySmartText
+        .setMessagesData(MessagesData.hasDoneSessionInformationFlow);
+    primarySmartText.startRotatingText();
+    gestureCross.fadeIn(onFadeIn: Left(() {
+      instructionalGradientNokhte.prepareInvertedBeachWave(offset);
+      centerInstructionalNokhte.setWidgetVisibility(true);
+      instructionalGradientNokhte.setWidgetVisibility(true);
+    }));
     initReactors();
   }
 
@@ -53,11 +59,65 @@ abstract class _HomeScreenPhase2WidgetsCoordinatorBase
   @override
   initReactors() {
     gestureCrossTapReactor();
+    centerInstructionalNokhteReactor();
     super.initReactors();
   }
 
   @action
-  onSwipeUp() => prepForNavigation(excludeUnBlur: !hasInitiatedBlur);
+  onSwipeUp() {
+    if (centerInstructionalNokhte.movieStatus != MovieStatus.inProgress) {
+      if (hasInitiatedBlur) {
+        centerInstructionalNokhte.moveUp();
+        hasSwipedUp = true;
+        primarySmartText.startRotatingText(isResuming: true);
+        setSmartTextPadding(topPadding: 0);
+      } else if (primarySmartText.currentIndex.isLessThan(1)) {
+        print("hi??");
+        hasSwipedUp = false;
+        gestureCross.centerCrossNokhte.setWidgetVisibility(true);
+        gestureCross.gradientNokhte.setWidgetVisibility(true);
+        centerInstructionalNokhte.setWidgetVisibility(false);
+        instructionalGradientNokhte.setWidgetVisibility(false);
+        prepForNavigation(excludeUnBlur: true);
+      }
+    }
+  }
+  //add reactor for re-setting smart text
+
+  @action
+  onTap(Offset offset) {
+    if (!touchIsDisabled) {
+      if (primarySmartText.currentIndex == 2) {
+        primarySmartText.startRotatingText(isResuming: true);
+        touchRipple.onTap(offset);
+        nokhteBlur.reverse();
+        beachWaves.currentStore.setControl(Control.mirror);
+        toggleHasInitiatedBlur();
+        Timer(Seconds.get(1, milli: 500), () {
+          centerInstructionalNokhte.moveBackFromTop();
+          instructionalGradientNokhte.moveBack();
+          primarySmartText.reset();
+          primarySmartText.startRotatingText();
+        });
+        hasInitiatedBlur = false;
+      }
+    }
+  }
+
+  centerInstructionalNokhteReactor() =>
+      reaction((p0) => centerInstructionalNokhte.movieStatus, (p0) {
+        if (p0 == MovieStatus.finished &&
+            centerInstructionalNokhte.movieMode ==
+                CenterInstructionalNokhteMovieModes.moveBack) {
+          gestureCross.centerCrossNokhte.setWidgetVisibility(true);
+          gestureCross.gradientNokhte.setWidgetVisibility(true);
+          Timer(Seconds.get(1), () {
+            centerInstructionalNokhte.setWidgetVisibility(false);
+            instructionalGradientNokhte.setWidgetVisibility(false);
+            setTouchIsDisabled(false);
+          });
+        }
+      });
 
   gestureCrossTapReactor() => reaction(
         (p0) => gestureCross.tapCount,
@@ -66,16 +126,33 @@ abstract class _HomeScreenPhase2WidgetsCoordinatorBase
 
   @action
   onGestureCrossTap() {
-    if (!hasInitiatedBlur && !isEnteringNokhteSession && !hasSwipedUp) {
-      nokhteBlur.init();
-      beachWaves.currentStore.setControl(Control.stop);
-      toggleHasInitiatedBlur();
-      gestureCross.stopBlinking();
-      if (gracePeriodHasExpired) {
+    if (!isEnteringNokhteSession &&
+        !hasSwipedUp &&
+        !isInErrorMode &&
+        centerInstructionalNokhte.movieStatus != MovieStatus.inProgress) {
+      if (!hasInitiatedBlur) {
+        hasSwipedUp = false;
+        nokhteBlur.init();
+        beachWaves.currentStore.setControl(Control.stop);
+        hasInitiatedBlur = true;
+        instructionalGradientNokhte.prepareInvertedBeachWave(center);
+        instructionalGradientNokhte.initMovie(NoParams());
+        gestureCross.centerCrossNokhte.setWidgetVisibility(false);
+        gestureCross.gradientNokhte.setWidgetVisibility(false);
         primarySmartText.startRotatingText(isResuming: true);
-      } else {
-        primarySmartText.setCurrentIndex(1);
+        centerInstructionalNokhte.moveToCenter(center);
+        setSmartTextPadding(bottomPadding: .14);
+      } else if (hasInitiatedBlur) {
+        hasSwipedUp = false;
+        centerInstructionalNokhte.moveBackFromCenter();
+        instructionalGradientNokhte.moveBack();
+        nokhteBlur.reverse();
+        beachWaves.currentStore.setControl(Control.mirror);
+        hasInitiatedBlur = false;
+        primarySmartText.reset();
         primarySmartText.startRotatingText();
+        setSmartTextPadding();
+        //
       }
     }
   }
