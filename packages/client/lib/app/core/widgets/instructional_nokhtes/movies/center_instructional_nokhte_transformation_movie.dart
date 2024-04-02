e2ea1e1e17cabe95c6619625746c4a6c13851855
Sonny Vesali <sonny.vesali@protonmail.com>
2024-04-02 10:48:29 -0500

    add: reverse option for center_instructional_nokhte_transformation_movie

--- packages/client/lib/app/core/widgets/instructional_nokhtes/movies/center_instructional_nokhte_transformation_movie.dart
+++ packages/client/lib/app/core/widgets/instructional_nokhtes/movies/center_instructional_nokhte_transformation_movie.dart
@@ -4,31 +4,34 @@ import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
 
 class CenterInstructionalNokhteTransformationMovie {
-  static MovieTween getMovie(Offset screenCenter) => MovieTween()
-    ..scene(
-      begin: Seconds.get(0),
-      end: Seconds.get(2),
-    )
-        .tween(
-            'dx',
-            Tween<double>(
-              begin: CircleOffsets.center.dx,
-              end: CircleOffsets.center.dx,
-            ))
-        .tween(
-          'dy',
-          Tween<double>(
-            begin: CircleOffsets.center.dy,
-            end: (-screenCenter.dy) * .8,
-          ),
-          curve: Curves.easeInOutCubicEmphasized,
+  static MovieTween getMovie(Offset screenCenter, {bool reverse = false}) =>
+      MovieTween()
+        ..scene(
+          begin: Seconds.get(0),
+          end: Seconds.get(2),
         )
-        .tween(
-          'radii',
-          Tween<double>(
-            begin: 4.5,
-            end: 25,
-          ),
-          curve: Curves.easeInOutCubicEmphasized,
-        );
+            .tween(
+                'dx',
+                Tween<double>(
+                  begin: CircleOffsets.center.dx,
+                  end: CircleOffsets.center.dx,
+                ))
+            .tween(
+              'dy',
+              Tween<double>(
+                begin:
+                    reverse ? (-screenCenter.dy) * .8 : CircleOffsets.center.dy,
+                end:
+                    reverse ? CircleOffsets.center.dy : (-screenCenter.dy) * .8,
+              ),
+              curve: Curves.easeInOutCubicEmphasized,
+            )
+            .tween(
+              'radii',
+              Tween<double>(
+                begin: reverse ? 25 : 4.5,
+                end: reverse ? 4.5 : 25,
+              ),
+              curve: Curves.easeInOutCubicEmphasized,
+            );
 }
