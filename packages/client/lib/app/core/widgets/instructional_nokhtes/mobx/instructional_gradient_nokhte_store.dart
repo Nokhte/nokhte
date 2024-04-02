86136b1d6653360f44c622d178c9d37e5692638c
Sonny Vesali <sonny.vesali@protonmail.com>
2024-04-02 10:48:05 -0500

    refactor: instructional_gradient_nokhte_store for phase 2 functionality

--- packages/client/lib/app/core/widgets/instructional_nokhtes/mobx/instructional_gradient_nokhte_store.dart
+++ packages/client/lib/app/core/widgets/instructional_nokhtes/mobx/instructional_gradient_nokhte_store.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
 import 'dart:ui';
 import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/instructional_nokhtes/instructional_nokhtes.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
 part 'instructional_gradient_nokhte_store.g.dart';
@@ -10,11 +13,11 @@ class InstructionalGradientNokhteStore = _InstructionalGradientNokhteStoreBase
     with _$InstructionalGradientNokhteStore;
 
 abstract class _InstructionalGradientNokhteStoreBase
-    extends BaseCustomAnimatedWidgetStore with Store {
+    extends BaseCustomAnimatedWidgetStore<NoParams> with Store {
   _InstructionalGradientNokhteStoreBase() {
     setWidgetVisibility(false);
     setMovie(
-      VirginInstructionalToInvertedBeachWavesMovie.getMovie(Offset.zero),
+      YellowDiamondToInvertedBeachWavesMovie.getMovie(Offset.zero),
     );
   }
 
@@ -22,20 +25,38 @@ abstract class _InstructionalGradientNokhteStoreBase
   Offset center = Offset.zero;
 
   @action
-  setPosition(Offset position) {
+  prepareYellowDiamond(Offset position) {
+    setMovieStatus(MovieStatus.inProgress);
     center = position;
     setMovie(
-      VirginInstructionalToInvertedBeachWavesMovie.getMovie(position),
+      YellowDiamondToInvertedBeachWavesMovie.getMovie(position),
     );
   }
 
   @action
-  initTransformation() {
+  prepareInvertedBeachWave(Offset position) {
+    setMovieStatus(MovieStatus.inProgress);
+    center = position;
+    setMovie(InvertedBeachWavesToTopMovie.getMovie(position));
+  }
+
+  @override
+  @action
+  initMovie(params) {
+    setMovieStatus(MovieStatus.inProgress);
     setControl(Control.playFromStart);
+  }
+
+  @override
+  @action
+  reverseMovie(params) {
+    setMovieStatus(MovieStatus.inProgress);
+    setControl(Control.playReverseFromEnd);
   }
 
   @action
   moveBack() {
+    setMovieStatus(MovieStatus.inProgress);
     setMovie(MoveInvertedBeachWavesToCross.getMovie(center));
     setControl(Control.playFromStart);
   }
