0d8d414546bf2feaf903c01c48f5a95b41446d64
Sonny Vesali <sonny.vesali@protonmail.com>
2024-04-03 08:40:17 -0500

    add: starting & ending color params  in on_shore movies

--- packages/client/lib/app/core/widgets/beach_waves/movies/on_shore_to_any_movie.dart
+++ packages/client/lib/app/core/widgets/beach_waves/movies/on_shore_to_any_movie.dart
@@ -8,6 +8,7 @@ class OnShoreToAny {
   static MovieTween getMovie({
     required double startingWaterMovement,
     required List<ColorAndStop> endingColors,
+    List<ColorAndStop> startingColors = WaterColorsAndStops.onShoreWater,
   }) =>
       MovieTween()
         ..scene(
@@ -24,112 +25,112 @@ class OnShoreToAny {
             .tween(
               BeachWaveAnimationKeys.color1,
               ColorTween(
-                begin: WaterColorsAndStops.onShoreWater.first.color,
+                begin: startingColors.first.color,
                 end: endingColors.first.color,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.color2,
               ColorTween(
-                begin: WaterColorsAndStops.onShoreWater[1].color,
+                begin: startingColors[1].color,
                 end: endingColors[1].color,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.color3,
               ColorTween(
-                begin: WaterColorsAndStops.onShoreWater[2].color,
+                begin: startingColors[2].color,
                 end: endingColors[2].color,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.color4,
               ColorTween(
-                begin: WaterColorsAndStops.onShoreWater[3].color,
+                begin: startingColors[3].color,
                 end: endingColors[3].color,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.color5,
               ColorTween(
-                begin: WaterColorsAndStops.onShoreWater[4].color,
+                begin: startingColors[4].color,
                 end: endingColors[4].color,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.color6,
               ColorTween(
-                begin: WaterColorsAndStops.onShoreWater[5].color,
+                begin: startingColors[5].color,
                 end: endingColors[5].color,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.color7,
               ColorTween(
-                begin: WaterColorsAndStops.onShoreWater[6].color,
+                begin: startingColors[6].color,
                 end: endingColors[6].color,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.color8,
               ColorTween(
-                begin: WaterColorsAndStops.onShoreWater[7].color,
+                begin: startingColors[7].color,
                 end: endingColors[7].color,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.stop1,
               Tween<double>(
-                begin: WaterColorsAndStops.onShoreWater.first.stop,
+                begin: startingColors.first.stop,
                 end: endingColors.first.stop,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.stop2,
               Tween<double>(
-                begin: WaterColorsAndStops.onShoreWater[1].stop,
+                begin: startingColors[1].stop,
                 end: endingColors[1].stop,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.stop3,
               Tween<double>(
-                begin: WaterColorsAndStops.onShoreWater[2].stop,
+                begin: startingColors[2].stop,
                 end: endingColors[2].stop,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.stop4,
               Tween<double>(
-                begin: WaterColorsAndStops.onShoreWater[3].stop,
+                begin: startingColors[3].stop,
                 end: endingColors[3].stop,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.stop5,
               Tween<double>(
-                begin: WaterColorsAndStops.onShoreWater[4].stop,
+                begin: startingColors[4].stop,
                 end: endingColors[4].stop,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.stop6,
               Tween<double>(
-                begin: WaterColorsAndStops.onShoreWater[5].stop,
+                begin: startingColors[5].stop,
                 end: endingColors[5].stop,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.stop7,
               Tween<double>(
-                begin: WaterColorsAndStops.onShoreWater[6].stop,
+                begin: startingColors[6].stop,
                 end: endingColors[6].stop,
               ),
             )
             .tween(
               BeachWaveAnimationKeys.stop8,
               Tween<double>(
-                begin: WaterColorsAndStops.onShoreWater[7].stop,
+                begin: startingColors[7].stop,
                 end: endingColors[7].stop,
               ),
             );
