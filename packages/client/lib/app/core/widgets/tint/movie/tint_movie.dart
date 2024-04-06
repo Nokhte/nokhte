8c0fe3a729128454e5e91cf49a9c40a228ab4f67
Sonny Vesali <sonny.vesali@protonmail.com>
2024-04-06 18:17:45 -0500

    add: tint widget

---
+++ packages/client/lib/app/core/widgets/tint/movie/tint_movie.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+class TintMovie {
+  static MovieTween get movie => MovieTween()
+    ..scene(
+      begin: Seconds.get(0),
+      end: Seconds.get(1),
+    ).tween<double>(
+      'opacity',
+      Tween<double>(
+        begin: 0,
+        end: .5,
+      ),
+    );
+}
