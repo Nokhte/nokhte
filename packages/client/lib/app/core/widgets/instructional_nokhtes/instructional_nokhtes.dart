f71fa392cbf58e2a4cb3e30c7e25af3d794b2df5
Sonny Vesali <sonny.vesali@protonmail.com>
2024-04-11 13:06:24 -0500

    add: defaultParams to resume_on_shore_params

--- packages/client/lib/app/core/widgets/instructional_nokhtes/instructional_nokhtes.dart
+++ packages/client/lib/app/core/widgets/instructional_nokhtes/instructional_nokhtes.dart
@@ -3,6 +3,7 @@
 export 'canvas/canvas.dart';
 export 'mobx/mobx.dart';
 export 'movies/movies.dart';
+export 'utils/instructional_nokhte_utils.dart';
 export 'types/types.dart';
 export 'widgets/widgets.dart';
 
