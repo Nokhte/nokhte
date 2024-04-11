f3e0c87dc711376b3ef2d823683c08886eeaf58f
Sonny Vesali <sonny.vesali@protonmail.com>
2024-04-11 13:31:50 -0500

    move: onSubsequentStorageEntry to base_home_screen_coordinator

--- packages/client/lib/app/modules/home/presentation/mobx/has_session/storage_accessed/qr_code_accessed/qr_and_storage_adept_coordinator.dart
+++ packages/client/lib/app/modules/home/presentation/mobx/has_session/storage_accessed/qr_code_accessed/qr_and_storage_adept_coordinator.dart
@@ -1,11 +1,9 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api, annotate_overrides, overridden_fields
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/modules/posthog/constants/constants.dart';
-import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/modules/home/presentation/mobx/shared/base_home_screen_coordinator.dart';
 import 'package:nokhte/app/modules/storage/domain/domain.dart';
 import 'qr_and_storage_adept_widgets_coordinator.dart';
@@ -68,15 +66,5 @@ abstract class _QrAndStorageAdeptCoordinatorBase
       (failure) => errorUpdater(failure),
       (artifacts) => nokhteSessionArtifacts = ObservableList.of(artifacts),
     );
-  }
-
-  @action
-  onSubsequentStorageEntry() {
-    Timer.periodic(Seconds.get(0, milli: 100), (timer) {
-      if (widgets.touchRipple.movieStatus == MovieStatus.finished) {
-        Modular.to.navigate('/storage/');
-        timer.cancel();
-      }
-    });
   }
 }
