79f70e13a61a7e07bf961bbde667c76bfcad335e
Sonny Vesali <sonny.vesali@protonmail.com>
2024-04-11 13:31:14 -0500

    add: new requirements for storage_guide

--- packages/client/lib/app/modules/home/presentation/screens/storage_guide_screen.dart
+++ packages/client/lib/app/modules/home/presentation/screens/storage_guide_screen.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: must_be_immutable, annotate_overrides, overridden_fields
-import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/home/presentation/presentation.dart';
@@ -10,18 +9,19 @@ class StorageGuideScreen extends BaseHomeScreen {
     required StorageGuideCoordinator coordinator,
   }) : super(
           coordinator: coordinator,
-          gestureCrossConfig: GestureCrossConfiguration(
-            top: Right(
-              NokhteGradientConfig(
-                gradientType: NokhteGradientTypes.invertedShore,
+          gestureCrossConfig: coordinator.widgets.gestureCrossConfig,
+          instructionalNokhtes: Stack(
+            children: [
+              CenterInstructionalNokhte(
+                store: coordinator.widgets.centerInstructionalNokhte,
               ),
-            ),
-            right: Right(
-              NokhteGradientConfig(
-                gradientType: NokhteGradientTypes.vibrantBlue,
+              InstructionalGradientNokhte(
+                store: coordinator.widgets.primaryInstructionalGradientNokhte,
               ),
-            ),
+              InstructionalGradientNokhte(
+                store: coordinator.widgets.secondaryInstructionalGradientNokhte,
+              ),
+            ],
           ),
-          instructionalNokhtes: Container(),
         );
 }
