fe7515822cc04314864a1b109fa93ddf8d45d2bc
Sonny Vesali <sonny.vesali@protonmail.com>
2024-04-21 08:30:49 -0500

    fix: duplicate import in irl_nokhte_session_module

--- packages/client/lib/app/modules/irl_nokhte_session/irl_nokhte_session_module.dart
+++ packages/client/lib/app/modules/irl_nokhte_session/irl_nokhte_session_module.dart
@@ -2,7 +2,6 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:nokhte/app/core/modules/clean_up_collaboration_artifacts/clean_up_collaboration_artifacts_module.dart';
 import 'package:nokhte/app/core/modules/clean_up_collaboration_artifacts/mobx/mobx.dart';
 import 'package:nokhte/app/core/modules/gyroscopic/gyroscopic.dart';
-import 'package:nokhte/app/core/modules/gyroscopic/mobx/mobx.dart';
 import 'package:nokhte/app/core/modules/posthog/domain/domain.dart';
 import 'package:nokhte/app/core/modules/posthog/posthog_module.dart';
 import 'package:nokhte/app/core/modules/presence_modules/presence_modules.dart';
