bd703a6ee41de1bd8e3c11a70e865b09b367a330
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-04-26 16:17:36 -0500

    remove: un-neededi mport in session_seating_guide

--- packages/client/lib/app/core/widgets/session_seating_guide/session_seating_guide.dart
+++ packages/client/lib/app/core/widgets/session_seating_guide/session_seating_guide.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:nokhte/app/core/hooks/hooks.dart';
