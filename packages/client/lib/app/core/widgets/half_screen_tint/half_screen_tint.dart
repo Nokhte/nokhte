95688b4b6793db7828626fd3f0ad0420401b8c4b
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-04-27 16:46:21 -0500

    add: half_screen_tint & exports

---
+++ packages/client/lib/app/core/widgets/half_screen_tint/half_screen_tint.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:nokhte/app/core/extensions/extensions.dart';
+import 'package:nokhte/app/core/hooks/hooks.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
+export 'half_screen_tint_store.dart';
+
+class HalfScreenTint extends HookWidget {
+  final HalfScreenTintStore store;
+  const HalfScreenTint({
+    super.key,
+    required this.store,
+  });
+
+  @override
+  Widget build(context) {
+    final size = useFullScreenSize();
+    final halfHeight = size.height.half();
+    final halfWidth = size.width;
+    return Observer(
+      builder: (context) {
+        return CustomAnimationBuilder(
+          tween: store.movie,
+          duration: store.movie.duration,
+          control: store.control,
+          builder: (context, value, child) {
+            return Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.black.withOpacity(
+                      value.get('opacity1'),
+                    ),
+                    Colors.black.withOpacity(
+                      value.get('opacity2'),
+                    ),
+                  ],
+                ),
+              ),
+              height: halfHeight,
+              width: halfWidth,
+            );
+          },
+        );
+      },
+    );
+  }
+}
---
+++ packages/client/lib/app/core/widgets/half_screen_tint/half_screen_tint_store.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/mobx/base_custom_animated_widget_store.dart';
+import 'half_screen_tint_movie.dart';
+part 'half_screen_tint_store.g.dart';
+
+class HalfScreenTintStore = _HalfScreenTintStoreBase with _$HalfScreenTintStore;
+
+abstract class _HalfScreenTintStoreBase extends BaseCustomAnimatedWidgetStore
+    with Store {
+  _HalfScreenTintStoreBase() {
+    setMovie(HalfScreenTintMovie.movie);
+  }
+}
