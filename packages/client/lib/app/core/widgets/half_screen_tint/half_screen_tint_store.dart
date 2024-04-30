50c483d4d420bde8d720ba78bb9e1e139d8d467c
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-04-30 17:35:21 -0500

    add: tint color setting in half_screen_tint

--- packages/client/lib/app/core/widgets/half_screen_tint/half_screen_tint_store.dart
+++ packages/client/lib/app/core/widgets/half_screen_tint/half_screen_tint_store.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/base_custom_animated_widget_store.dart';
 import 'half_screen_tint_movie.dart';
@@ -11,6 +12,12 @@ abstract class _HalfScreenTintStoreBase extends BaseCustomAnimatedWidgetStore
   _HalfScreenTintStoreBase() {
     setMovie(HalfScreenTintMovie.movie);
   }
+
+  @observable
+  Color tintcolor = Colors.black;
+
+  @action
+  setTintColor(Color newVal) => tintcolor = newVal;
 
   @observable
   bool shouldCoverBottom = true;
