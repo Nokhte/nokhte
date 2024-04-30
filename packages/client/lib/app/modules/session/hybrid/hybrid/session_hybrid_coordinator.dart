f8f2f0e67129b93fc55e06686871f3541533d0e1
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-04-30 17:35:05 -0500

    add: session_hybrid_coordinator

---
+++ packages/client/lib/app/modules/session/hybrid/hybrid/session_hybrid_coordinator.dart
@@ -0,0 +1,184 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'dart:async';
+
+import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/extensions/extensions.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/modules/gyroscopic/gyroscopic.dart';
+import 'package:nokhte/app/core/modules/posthog/posthog.dart';
+import 'package:nokhte/app/core/modules/session_presence/session_presence.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'session_hybrid_widgets_coordinator.dart';
+part 'session_hybrid_coordinator.g.dart';
+
+class SessionHybridCoordinator = _SessionHybridCoordinatorBase
+    with _$SessionHybridCoordinator;
+
+abstract class _SessionHybridCoordinatorBase extends BaseCoordinator
+    with Store {
+  final SessionHybridWidgetsCoordinator widgets;
+  final TapDetector tap;
+  final SwipeDetector swipe;
+  final HoldDetector hold;
+  final SessionPresenceCoordinator presence;
+  final GetSessionMetadataStore sessionMetadata;
+  final GyroscopicCoordinator gyroscopic;
+  _SessionHybridCoordinatorBase({
+    required super.captureScreen,
+    required this.widgets,
+    required this.swipe,
+    required this.hold,
+    required this.tap,
+    required this.gyroscopic,
+    required this.presence,
+  }) : sessionMetadata = presence.getSessionMetadataStore;
+
+  @action
+  constructor() async {
+    widgets.constructor();
+    initReactors(sessionMetadata.shouldAdjustToFallbackExitProtocol);
+    gyroscopic.listen(NoParams());
+    setBlockPhoneTiltReactor(false);
+    await captureScreen(Screens.nokhteSessionSpeaking);
+  }
+
+  @observable
+  bool blockPhoneTiltReactor = false;
+
+  @action
+  setBlockPhoneTiltReactor(bool newValue) => blockPhoneTiltReactor = newValue;
+
+  initReactors(bool shouldAdjustToFallbackExitProtocol) {
+    holdReactor();
+    letGoReactor();
+    widgets.wifiDisconnectOverlay.initReactors(
+      onQuickConnected: () => setDisableAllTouchFeedback(false),
+      onLongReConnected: () {
+        setDisableAllTouchFeedback(false);
+      },
+      onDisconnected: () {
+        setDisableAllTouchFeedback(true);
+        if (hold.holdCount.isGreaterThan(hold.letGoCount)) {
+          widgets.onLetGo();
+        }
+      },
+    );
+    presence.initReactors(
+      onCollaboratorJoined: () {
+        setDisableAllTouchFeedback(false);
+        widgets.onCollaboratorJoined();
+      },
+      onCollaboratorLeft: () {
+        setDisableAllTouchFeedback(true);
+        if (hold.holdCount.isGreaterThan(hold.letGoCount)) {
+          widgets.onLetGo();
+        }
+        widgets.onCollaboratorLeft();
+      },
+    );
+    if (shouldAdjustToFallbackExitProtocol) {
+      swipeReactor();
+    } else {
+      phoneTiltStateReactor();
+    }
+    userPhaseReactor();
+    collaboratorPhaseReactor();
+    tapReactor();
+  }
+
+  swipeReactor() => reaction((p0) => swipe.directionsType, (p0) {
+        switch (p0) {
+          case GestureDirections.down:
+            ifTouchIsNotDisabled(() async {
+              await presence.updateCurrentPhase(3);
+            });
+          default:
+            break;
+        }
+      });
+
+  tapReactor() => reaction(
+        (p0) => tap.tapCount,
+        (p0) => ifTouchIsNotDisabled(
+          () async {
+            widgets.onTap(tap.currentTapPosition, onTap: () async {
+              await presence.updateCurrentPhase(2);
+            });
+          },
+        ),
+      );
+
+  holdReactor() => reaction((p0) => hold.holdCount, (p0) {
+        ifTouchIsNotDisabled(() async {
+          if (presence.getSessionMetadataStore.everyoneIsOnline) {
+            setBlockPhoneTiltReactor(true);
+            widgets.adjustSpeakLessSmileMoreRotation(hold.placement);
+            widgets.onHold(hold.placement);
+            setDisableAllTouchFeedback(true);
+            await presence.updateCurrentPhase(2);
+          }
+        });
+      });
+
+  letGoReactor() => reaction((p0) => hold.letGoCount, (p0) {
+        if (presence.getSessionMetadataStore.everyoneIsOnline) {
+          widgets.onLetGo();
+          setBlockPhoneTiltReactor(false);
+          Timer(Seconds.get(2), () {
+            setDisableAllTouchFeedback(false);
+          });
+        }
+      });
+
+  phoneTiltStateReactor() =>
+      reaction((p0) => gyroscopic.holdingState, (p0) async {
+        if (!blockPhoneTiltReactor &&
+            sessionMetadata.currentPhases
+                .every((e) => e.isGreaterThanOrEqualTo(2))) {
+          if (p0 == PhoneHoldingState.isPickedUp) {
+            await presence.updateCurrentPhase(3);
+          } else if (p0 == PhoneHoldingState.isDown) {
+            await presence.updateCurrentPhase(2);
+          }
+        }
+      });
+
+  collaboratorPhaseReactor() => reaction(
+        (p0) => sessionMetadata.currentPhases,
+        (p0) async {
+          if (sessionMetadata.canExitTheSession) {
+            await gyroscopic.dispose();
+            widgets.onExit();
+            setBlockPhoneTiltReactor(true);
+          }
+        },
+      );
+
+  userPhaseReactor() => reaction(
+        (p0) => sessionMetadata.userPhase,
+        (p0) async {
+          if (sessionMetadata.canExitTheSession) {
+            await gyroscopic.dispose();
+            widgets.onExit();
+            setBlockPhoneTiltReactor(true);
+          }
+        },
+      );
+
+  @action
+  onInactive() async {
+    await presence
+        .updateOnlineStatus(UpdatePresencePropertyParams.userNegative());
+  }
+
+  @action
+  onResumed() async {
+    await presence
+        .updateOnlineStatus(UpdatePresencePropertyParams.userAffirmative());
+    if (sessionMetadata.everyoneIsOnline) {
+      presence.incidentsOverlayStore.onCollaboratorJoined();
+    }
+  }
+}
---
+++ packages/client/lib/app/modules/session/hybrid/hybrid/session_hybrid_widgets_coordinator.dart
@@ -0,0 +1,301 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/interfaces/logic.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:nokhte/app/modules/session/constants/constants.dart';
+import 'package:simple_animations/simple_animations.dart';
+part 'session_hybrid_widgets_coordinator.g.dart';
+
+class SessionHybridWidgetsCoordinator = _SessionHybridWidgetsCoordinatorBase
+    with _$SessionHybridWidgetsCoordinator;
+
+abstract class _SessionHybridWidgetsCoordinatorBase
+    extends BaseWidgetsCoordinator with Store {
+  final MirroredTextStore mirroredText;
+  final BeachWavesStore beachWaves;
+  final BorderGlowStore borderGlow;
+  final TouchRippleStore touchRipple;
+  final SpeakLessSmileMoreStore speakLessSmileMore;
+  final HalfScreenTintStore halfScreenTint;
+  final SmartTextStore smartText;
+
+  _SessionHybridWidgetsCoordinatorBase({
+    required this.mirroredText,
+    required this.halfScreenTint,
+    required this.smartText,
+    required this.beachWaves,
+    required this.borderGlow,
+    required super.wifiDisconnectOverlay,
+    required this.touchRipple,
+    required this.speakLessSmileMore,
+  });
+
+  @action
+  constructor() {
+    beachWaves.setMovieMode(BeachWaveMovieModes.invertedHalfAndHalfToDrySand);
+    mirroredText.setMessagesData(MirroredTextContent.hybrid);
+    smartText.setMessagesData(SessionLists.speakLessWriteMore);
+    smartText.setStaticAltMovie(NokhteSessionConstants.blue);
+    smartText.startRotatingText();
+    smartText.setWidgetVisibility(false);
+    mirroredText.startBothRotatingText();
+    setIsPickingUp(false);
+    isGoingToNotes = false;
+    initReactors();
+    halfScreenTint.setTintColor(Colors.white);
+    Timer(const Duration(minutes: 9), () {
+      if (tapCount == 0) {
+        initSpeakLessWriteMore();
+      }
+    });
+  }
+
+  @observable
+  bool isLettingGo = false;
+
+  @observable
+  bool isHolding = false;
+
+  @observable
+  bool canTap = false;
+
+  @observable
+  bool canHold = true;
+
+  @observable
+  bool isPickingUp = false;
+
+  @observable
+  bool isGoingToNotes = false;
+
+  @computed
+  bool get speakLessWriteMoreIsVisible => speakLessWriteMoreVisiblities.last;
+
+  @computed
+  bool get pastSpeakLessWriteMoreVisiblity =>
+      speakLessWriteMoreVisiblities[speakLessWriteMoreVisiblities.length - 2];
+
+  @observable
+  ObservableList speakLessWriteMoreVisiblities =
+      ObservableList.of(List.filled(2, false));
+
+  @action
+  setSpeakLessWriteMoreVisiblity(bool newVal) =>
+      speakLessWriteMoreVisiblities.add(newVal);
+
+  @action
+  setIsHolding(bool newBool) => isHolding = newBool;
+
+  @action
+  setIsPickingUp(bool newBool) => isPickingUp = newBool;
+
+  @observable
+  bool collaboratorHasLeft = false;
+
+  @observable
+  int holdCount = 0;
+
+  @observable
+  int tapCount = 0;
+
+  @action
+  initSpeakLessWriteMore() {
+    smartText.setWidgetVisibility(true);
+    halfScreenTint.setControl(Control.playFromStart);
+    mirroredText.setRightsideUpVisibility(false);
+    setSpeakLessWriteMoreVisiblity(true);
+  }
+
+  @action
+  reverseSpeakLessWriteMore() {
+    smartText.setWidgetVisibility(false);
+    halfScreenTint.setControl(Control.playReverse);
+    if (!isHolding) {
+      mirroredText.setRightsideUpVisibility(true);
+    }
+    if (collaboratorHasLeft) {
+      mirroredText.setRightsideUpVisibility(false);
+    }
+    setSpeakLessWriteMoreVisiblity(false);
+  }
+
+  @action
+  onCollaboratorLeft() {
+    mirroredText.setWidgetVisibility(false);
+    collaboratorHasLeft = true;
+    if (speakLessWriteMoreIsVisible) {
+      reverseSpeakLessWriteMore();
+    }
+  }
+
+  @action
+  onCollaboratorJoined() {
+    mirroredText.setWidgetVisibility(true);
+    collaboratorHasLeft = false;
+    if (pastSpeakLessWriteMoreVisiblity) {
+      initSpeakLessWriteMore();
+    }
+  }
+
+  @action
+  onHold(GesturePlacement holdPosition) {
+    if (holdPosition == GesturePlacement.topHalf && canHold) {
+      isHolding = true;
+      canHold = false;
+      holdCount++;
+      beachWaves.setMovieMode(
+        BeachWaveMovieModes.invertedHalfAndHalfToDrySand,
+      );
+      beachWaves.currentStore.initMovie(NoParams());
+      mirroredText.setWidgetVisibility(false);
+      if (speakLessWriteMoreIsVisible) {
+        reverseSpeakLessWriteMore();
+      }
+    }
+  }
+
+  @action
+  onTap(
+    Offset tapPosition, {
+    required Function onTap,
+  }) async {
+    touchRipple.onTap(tapPosition, overridedColor: Colors.white);
+    if (!speakLessWriteMoreIsVisible) {
+      if (!isHolding && canHold) {
+        await onTap();
+        tapCount++;
+        if (hasTappedOnTheBottomHalf) {
+          initFullScreenNotes();
+        }
+      }
+    } else {
+      if (hasTappedOnTheBottomHalf) {
+        reverseSpeakLessWriteMore();
+      }
+    }
+  }
+
+  @action
+  onLetGo() {
+    initGlowDown();
+    beachWaves
+        .setMovieMode(BeachWaveMovieModes.dynamicPointToInvertedHalfAndHalf);
+    beachWaves.currentStore.initMovie(beachWaves.currentColorsAndStops);
+    speakLessSmileMore.hideBoth();
+  }
+
+  @action
+  onLetGoCompleted() {
+    canHold = true;
+    isHolding = false;
+    isLettingGo = false;
+    if (pastSpeakLessWriteMoreVisiblity) {
+      initSpeakLessWriteMore();
+    }
+    if (!collaboratorHasLeft) {
+      mirroredText.setUpsideDownVisibility(true);
+    }
+  }
+
+  @action
+  initFullScreenNotes() {
+    isGoingToNotes = true;
+    mirroredText.setWidgetVisibility(false);
+    beachWaves.setMovieMode(
+      BeachWaveMovieModes.vibrantBlueGradToInvertedHalfAndHalf,
+    );
+    beachWaves.currentStore.reverseMovie(NoParams());
+    //
+  }
+
+  @action
+  initBorderGlow() {
+    borderGlow.initMovie(NoParams());
+  }
+
+  @action
+  initGlowDown() {
+    borderGlow.initGlowDown();
+  }
+
+  @action
+  adjustSpeakLessSmileMoreRotation(GesturePlacement holdPlacement) {
+    if (holdPlacement == GesturePlacement.topHalf) {
+      speakLessSmileMore.setShouldBeUpsideDown(true);
+    } else {
+      speakLessSmileMore.setShouldBeUpsideDown(false);
+    }
+  }
+
+  @action
+  onExit() {
+    setIsPickingUp(true);
+    mirroredText.setWidgetVisibility(false);
+    beachWaves
+        .setMovieMode(BeachWaveMovieModes.vibrantBlueGradToInvertedHalfAndHalf);
+    beachWaves.currentStore.reverseMovie(NoParams());
+    if (speakLessWriteMoreIsVisible) {
+      reverseSpeakLessWriteMore();
+    }
+  }
+
+  @action
+  initReactors() {
+    firstBorderGlowReactor();
+    beachWavesMovieStatusReactor();
+  }
+
+  onBorderGlowComplete(MovieStatus p0, BorderGlowStore store) {
+    if (p0 == MovieStatus.finished &&
+        store.isGlowingUp &&
+        isHolding &&
+        beachWaves.movieMode == BeachWaveMovieModes.anyToVibrantBlueGrad) {
+      speakLessSmileMore.setSpeakLess(true);
+      Timer(Seconds.get(2), () {
+        if (isHolding) {
+          speakLessSmileMore.setSmileMore(true);
+        }
+      });
+    }
+  }
+
+  beachWavesMovieStatusReactor() =>
+      reaction((p0) => beachWaves.movieStatus, (p0) {
+        if (p0 == MovieStatus.finished) {
+          if (beachWaves.movieMode ==
+              BeachWaveMovieModes.vibrantBlueGradToInvertedHalfAndHalf) {
+            if (isPickingUp) {
+              Modular.to.navigate("/session/shared/exit");
+            } else if (isGoingToNotes) {
+              Modular.to.navigate("/session/hybrid/notes");
+            }
+          } else if (beachWaves.movieMode ==
+              BeachWaveMovieModes.dynamicPointToInvertedHalfAndHalf) {
+            onLetGoCompleted();
+          } else if (beachWaves.movieMode ==
+              BeachWaveMovieModes.invertedHalfAndHalfToDrySand) {
+            initBorderGlow();
+          }
+        }
+      });
+
+  firstBorderGlowReactor() => reaction((p0) => borderGlow.movieStatus, (p0) {
+        if (p0 == MovieStatus.finished && borderGlow.isGlowingUp) {
+          speakLessSmileMore.setSpeakLess(true);
+          Timer(Seconds.get(2), () {
+            if (borderGlow.isGlowingUp) {
+              speakLessSmileMore.setSmileMore(true);
+            }
+          });
+        }
+      });
+
+  @computed
+  bool get hasTappedOnTheBottomHalf =>
+      touchRipple.tapPlacement == GesturePlacement.bottomHalf;
+}
