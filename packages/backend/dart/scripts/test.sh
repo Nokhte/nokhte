fcc9cbe68d9b1b978b94474782c67555ac8adb8c
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-05-07 16:10:36 -0500

    refactor: test.sh script to reflect new test file names

--- packages/backend/dart/scripts/test.sh
+++ packages/backend/dart/scripts/test.sh
@@ -1,6 +1,6 @@
-flutter test test/01_user_names_table_test.dart
-flutter test test/01.5_user_names_table_test.dart
-flutter test test/02_get_agora_token_test.dart
-flutter test test/03_synchronous_search_test.dart
-flutter test test/04_active_nokhte_sessions_table_test.dart
-flutter test test/05_finished_nokhte_sessions_table_test.dart
+flutter test test/01_user_information_table_test.dart
+flutter test test/01.5_user_information_table_test.dart
+flutter test test/02_synchronous_search_test.dart
+flutter test test/03_active_nokhte_sessions_table_test.dart
+flutter test test/04_finished_nokhte_sessions_table_test.dart
+flutter test test/05_user_metadata_table_test.dart
