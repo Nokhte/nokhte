1d8932376cf712db22f7938a4241e8852d392420
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-05-09 09:15:03 -0500

    add: unique-ness to subscriber_ids as well as updated update_nokhte_session_phase

--- packages/backend/supabase/migrations/20240507181652_monetization_changes.sql
+++ packages/backend/supabase/migrations/20240507181652_monetization_changes.sql
@@ -470,3 +470,17 @@ with check ((((array_length(collaborator_uids, 1) <= 3) AND (auth.uid() = ANY (c
    FROM user_metadata user_metadata_1
   WHERE ((auth.uid() = user_metadata_1.uid) AND ((user_metadata_1.is_subscribed = true) OR (user_metadata_1.has_used_trial = false))))))));
 
+
+CREATE UNIQUE INDEX user_metadata_subscriber_id_key ON public.user_metadata USING btree (subscriber_id);
+
+alter table "public"."user_metadata" add constraint "user_metadata_subscriber_id_key" UNIQUE using index "user_metadata_subscriber_id_key";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.update_nokhte_session_phase(incoming_uids uuid[], index_to_edit integer, new_value real)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$BEGIN
+    update public.active_nokhte_sessions SET current_phases[index_to_edit+1] = new_value WHERE collaborator_uids = incoming_uids;
+END;$function$
+;
