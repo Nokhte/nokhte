8d717d3904429cdad3f31dd3f8ae8f111337eb2e
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-05-09 08:58:32 -0500

    add: back glassfy initializaiton in client main.dart

--- packages/client/lib/main.dart
+++ packages/client/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_branch_sdk/flutter_branch_sdk.dart';
+import 'package:glassfy_flutter/glassfy_flutter.dart';
 import 'package:nokhte/app/app_module.dart';
 import 'package:nokhte/app/app_widget.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -10,6 +11,10 @@ import 'package:flutter/foundation.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
+  final glassfyAPIKey = dotenv.env['GLASSFY_API_KEY'] ?? '';
+  if (glassfyAPIKey.isNotEmpty) {
+    await Glassfy.initialize(glassfyAPIKey);
+  }
 
   bool shouldUseTestKey = false;
 
