e28ddffdc783e9ef446f4dc05717bd97a9e8982a
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-05-11 15:01:11 -0500

    delete: deprecated tests

--- packages/client/test/app/core/widgets/explanatory_model_widgets/explanatory_model_widgets_mock_gen.dart
+++
@@ -1,9 +0,0 @@
-import 'package:mockito/annotations.dart';
-import 'package:nokhte/app/core/widgets/widgets.dart';
-
-@GenerateNiceMocks([
-  MockSpec<DumbGradientCircleStore>(),
-  MockSpec<SmartGradientCircleStore>(),
-  MockSpec<AccompanyingTextStore>(),
-])
-void main() {}
