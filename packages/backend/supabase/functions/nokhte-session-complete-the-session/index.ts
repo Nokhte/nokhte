75e499902f32059dd9dbf1e1c4b8f8d3774c42ea
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-05-12 16:06:51 -0500

    fix: authorized viewer behavior in complete-the-session

--- packages/backend/supabase/functions/nokhte-session-complete-the-session/index.ts
+++ packages/backend/supabase/functions/nokhte-session-complete-the-session/index.ts
@@ -1,65 +1,75 @@
 import { supabaseAdmin } from "../constants/supabase.ts";
-import { isEmptyOrNull, onlyUnique } from "../utils/array_utils.ts";
+import {
+  isEmptyOrNull,
+  isNotEmptyOrNull,
+  onlyUnique,
+} from "../utils/array_utils.ts";
 import { serve } from "std/server";
 
 serve(async (req) => {
   const { collaboratorUIDs } = await req.json();
 
-  const sessionRes = (
-    await supabaseAdmin
-      .from("active_nokhte_sessions")
-      .delete()
-      .eq("collaborator_uids", collaboratorUIDs)
-      .select()
-  )?.data?.[0];
-
-  const content = sessionRes["content"];
-  const sessionTimestamp = sessionRes["created_at"];
-  const sessionUID = sessionRes["session_uid"];
-  const collaboratorUIDsArr = sessionRes["collaborator_uids"];
-
-  const duplicateCheckRes = (
-    await supabaseAdmin
-      .from("finished_nokhte_session")
-      .select()
-      .eq("collaborator_uids", collaboratorUIDs)
-      .eq("content", content)
-  )?.data;
-
+  const sessionRes = await supabaseAdmin
+    .from("active_nokhte_sessions")
+    .delete()
+    .eq("collaborator_uids", collaboratorUIDs)
+    .select();
   let returnRes = {
     status: 200,
     message: "success",
   };
 
-  if (isEmptyOrNull(duplicateCheckRes)) {
-    const { error } = await supabaseAdmin
-      .from("finished_nokhte_sessions")
-      .insert({
-        collaborator_uids: collaboratorUIDs,
-        content: content,
-        session_timestamp: sessionTimestamp,
-        aliases: Array(collaboratorUIDsArr.length).fill(""),
-        session_uid: sessionUID,
-      })
-      .select();
+  if (isNotEmptyOrNull(sessionRes?.data)) {
+    const content = sessionRes?.data?.[0]["content"];
+    const sessionTimestamp = sessionRes?.data?.[0]["created_at"];
+    const sessionUID = sessionRes?.data?.[0]["session_uid"];
+    const collaboratorUIDsArr = sessionRes?.data?.[0]["collaborator_uids"];
 
-    if (collaboratorUIDsArr.length > 3) {
+    const duplicateCheckRes = (
+      await supabaseAdmin
+        .from("finished_nokhte_session")
+        .select()
+        .eq("collaborator_uids", collaboratorUIDs)
+        .eq("content", content)
+    )?.data;
+    if (isEmptyOrNull(duplicateCheckRes)) {
+      const { error } = await supabaseAdmin
+        .from("finished_nokhte_sessions")
+        .insert({
+          collaborator_uids: collaboratorUIDs,
+          content: content,
+          session_timestamp: sessionTimestamp,
+          aliases: Array(collaboratorUIDsArr.length).fill(""),
+          session_uid: sessionUID,
+        })
+        .select();
+
       for (let i = 0; i < collaboratorUIDsArr.length; i++) {
-        await supabaseAdmin
-          .from("user_metadata")
-          .update({
-            has_used_trial: true,
-          })
-          .eq("uid", collaboratorUIDsArr[i]);
+        if (collaboratorUIDsArr.length > 3) {
+          await supabaseAdmin
+            .from("user_metadata")
+            .update({
+              has_used_trial: true,
+            })
+            .eq("uid", collaboratorUIDsArr[i]);
+        }
 
-        const userAuthorizedViewers = (
+        const userAuthorizedViewersRes = (
           await supabaseAdmin
             .from("user_information")
             .select()
             .eq("uid", collaboratorUIDsArr[i])
         )?.data?.[0]?.["authorized_viewers"];
-        userAuthorizedViewers.concat(collaboratorUIDsArr);
-        const uniqueViewers = userAuthorizedViewers.filter(onlyUnique);
+        console.log(
+          "What about the session collaborators",
+          collaboratorUIDsArr
+        );
+        const userAuthorizedViewers =
+          userAuthorizedViewersRes.concat(collaboratorUIDsArr);
+        const viewersMinusUserUID = userAuthorizedViewers.filter(
+          (x: string) => x !== collaboratorUIDsArr[i]
+        );
+        const uniqueViewers = viewersMinusUserUID.filter(onlyUnique);
         uniqueViewers.sort();
         await supabaseAdmin
           .from("user_information")
@@ -68,15 +78,16 @@ serve(async (req) => {
           })
           .eq("uid", collaboratorUIDsArr[i]);
       }
-    }
 
-    if (error != null) {
-      returnRes = {
-        status: 400,
-        message: "failed query",
-      };
+      if (error != null) {
+        returnRes = {
+          status: 400,
+          message: "failed query",
+        };
+      }
     }
   }
+
   return new Response(JSON.stringify(returnRes), {
     headers: { "Content-Type": "application/json" },
   });
