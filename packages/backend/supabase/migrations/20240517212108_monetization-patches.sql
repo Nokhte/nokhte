bcec02ead380cddba4a48a0845e6ea1fe05bc4b1
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-05-20 09:32:03 -0500

    add: column translation to monetization_patches

--- packages/backend/supabase/migrations/20240517212108_monetization-patches.sql
+++ packages/backend/supabase/migrations/20240517212108_monetization-patches.sql
@@ -1,5 +1,9 @@
-alter table "public"."user_information" drop column "has_gone_through_invitation_flow";
 
 alter table "public"."user_information" drop column "has_sent_an_invitation";
 
 alter table "public"."user_information" add column "has_accessed_qr_code" boolean not null default false;
+
+UPDATE "public"."user_information"
+SET "has_accessed_qr_code" = "has_gone_through_invitation_flow";
+
+alter table "public"."user_information" drop column "has_gone_through_invitation_flow";
