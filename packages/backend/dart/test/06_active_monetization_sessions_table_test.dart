9954a1b7516961abdc7c67003ac3faa92dce1b0d
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-05-24 22:05:11 -0500

    fix: monetization_table_test

--- packages/backend/dart/test/06_active_monetization_sessions_table_test.dart
+++ packages/backend/dart/test/06_active_monetization_sessions_table_test.dart
@@ -18,13 +18,13 @@ void main() {
 
   tearDownAll(() async {
     await tSetup.supabaseAdmin
-        .from('active_nokhte_sessions')
+        .from('st_active_nokhte_sessions')
         .delete()
         .contains("collaborator_uids", "{${tSetup.firstUserUID}}");
   });
 
   test("startSession", () async {
-    await tSetup.supabaseAdmin.from("active_nokhte_sessions").insert({
+    await tSetup.supabaseAdmin.from("st_active_nokhte_sessions").insert({
       "collaborator_uids": sortedUIDs,
       "leader_uid": tSetup.firstUserUID,
       "has_premium_access": [false, false],
