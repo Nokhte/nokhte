ac7334e9f60c95d04446a3588339c53f34f02b55
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-05-26 17:31:36 -0500

    add: half_notes_instructions

---
+++ packages/client/lib/app/modules/session/notes/half_notes_instructions/half_notes_instructions.dart
@@ -0,0 +1,65 @@
+export 'half_session_notes_instructions_coordinator.dart';
+export 'half_session_notes_instructions_widgets_coordinator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'half_session_notes_instructions_coordinator.dart';
+
+class HalfSessionNotesInstructionsScreen extends HookWidget {
+  final HalfSessionNotesInstructionsCoordinator coordinator;
+  const HalfSessionNotesInstructionsScreen({
+    super.key,
+    required this.coordinator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    useEffect(() {
+      coordinator.constructor();
+      return null;
+    }, []);
+    useOnAppLifecycleStateChange(
+        (previous, current) => coordinator.onAppLifeCycleStateChange(
+              current,
+              onResumed: () => coordinator.onResumed(),
+              onInactive: () => coordinator.onInactive(),
+            ));
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Tap(
+        store: coordinator.tap,
+        child: MultiHitStack(
+          children: [
+            FullScreen(
+              child: BeachWaves(
+                store: coordinator.widgets.beachWaves,
+              ),
+            ),
+            HalfScreenTint(
+              store: coordinator.widgets.halfScreenTint,
+            ),
+            BorderGlow(store: BorderGlowStore()),
+            Tint(
+              store: coordinator.widgets.tint,
+            ),
+            MirroredText(
+              store: coordinator.widgets.mirroredText,
+            ),
+            FullScreen(
+              child: TouchRipple(
+                store: coordinator.widgets.touchRipple,
+              ),
+            ),
+            CollaboratorPresenceIncidentsOverlay(
+              store: coordinator.presence.incidentsOverlayStore,
+            ),
+            WifiDisconnectOverlay(
+              store: coordinator.widgets.wifiDisconnectOverlay,
+            ),
+          ],
+        ),
+      ),
+      // ),
+    );
+  }
+}
