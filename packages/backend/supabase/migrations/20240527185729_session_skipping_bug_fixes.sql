4d9310865c80304ce151945e621b2e97ce8548dc
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-05-27 19:53:41 -0500

    fix: duplicatesessions + leader is white listed in session info

--- packages/backend/supabase/migrations/20240527185729_session_skipping_bug_fixes.sql
+++ packages/backend/supabase/migrations/20240527185729_session_skipping_bug_fixes.sql
@@ -1,1 +1,17 @@
 alter table "public"."st_active_nokhte_sessions" add column "leader_is_whitelisted" boolean not null;
+
+alter table "public"."st_active_nokhte_sessions" drop column "leader_is_whitelisted";
+
+alter table "public"."st_active_nokhte_sessions" add column "is_whitelisted" boolean not null;
+
+
+alter table "public"."finished_nokhte_sessions" drop constraint "finished_nokhte_sessions_pkey";
+
+drop index if exists "public"."finished_nokhte_sessions_pkey";
+
+alter table "public"."finished_nokhte_sessions" drop column "id";
+
+CREATE UNIQUE INDEX finished_nokhte_sessions_pkey ON public.finished_nokhte_sessions USING btree (session_uid);
+
+alter table "public"."finished_nokhte_sessions" add constraint "finished_nokhte_sessions_pkey" PRIMARY KEY using index "finished_nokhte_sessions_pkey";
+
