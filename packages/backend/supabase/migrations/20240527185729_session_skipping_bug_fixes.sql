bb04e3e199569e2fc041e4e1419acbf42d2c9527
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-05-27 20:01:00 -0500

    chore: bump ios & android version numbers

--- packages/backend/supabase/migrations/20240527185729_session_skipping_bug_fixes.sql
+++ packages/backend/supabase/migrations/20240527185729_session_skipping_bug_fixes.sql
@@ -15,3 +15,12 @@ CREATE UNIQUE INDEX finished_nokhte_sessions_pkey ON public.finished_nokhte_sess
 
 alter table "public"."finished_nokhte_sessions" add constraint "finished_nokhte_sessions_pkey" PRIMARY KEY using index "finished_nokhte_sessions_pkey";
 
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_valid_app_versions()
+ RETURNS text[]
+ LANGUAGE plpgsql
+AS $function$BEGIN
+    RETURN ARRAY['1.60.0', '1.61.0', '1.62.0', '1.63.0'];
+END;$function$
+;
