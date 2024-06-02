32bb5990b5b1c88664c24f8b7db2f4fb60c73118
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-06-02 17:49:21 -0500

    chore: bump backend valid app versions

---
+++ packages/backend/supabase/migrations/20240602224905_update_get_valid_app_versions.sql
@@ -0,0 +1,9 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_valid_app_versions()
+ RETURNS text[]
+ LANGUAGE plpgsql
+AS $function$BEGIN
+    RETURN ARRAY['1.66.0'];
+END;$function$
+;
