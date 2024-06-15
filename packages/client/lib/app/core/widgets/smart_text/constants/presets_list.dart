c8ee4f29c4852e1e6faf9a562112a7a6a514d298
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-06-14 20:31:35 -0500

    add: presets list

---
+++ packages/client/lib/app/core/widgets/smart_text/constants/presets_list.dart
@@ -0,0 +1,18 @@
+import 'package:nokhte/app/core/widgets/widgets.dart';
+
+class PresetsLists {
+  static List<RotatingTextData> presetsInstructions = [
+    RotatingTextData(
+      mainMessage: "Tap to explore",
+      pauseHere: true,
+    ),
+    RotatingTextData(
+      mainMessage: "",
+      pauseHere: true,
+    ),
+    RotatingTextData(
+      mainMessage: "Hold",
+      pauseHere: true,
+    ),
+  ];
+}
