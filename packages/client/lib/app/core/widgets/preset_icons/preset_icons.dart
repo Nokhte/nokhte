7a8b4a9a050ea00ddda3bf1a7d2e8e4cceda27ce
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-06-17 20:43:36 -0500

    add: presets icons widget

---
+++ packages/client/lib/app/core/widgets/preset_icons/preset_icons.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: unused_import
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:nokhte/app/core/hooks/hooks.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+export 'preset_icons_store.dart';
+
+class PresetIcons extends HookWidget with PresetTags {
+  final PresetIconsStore store;
+  const PresetIcons({
+    super.key,
+    required this.store,
+  });
+
+  Widget buildHorizontalIcons(
+    int i, {
+    required double containerSize,
+  }) =>
+      Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: fromTagsToWidgets(
+          store.tags,
+          containerSize: containerSize,
+        ),
+      );
+
+  Widget buildVerticalIcons(
+    double containerSize,
+  ) =>
+      Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: fromTagsToWidgets(
+            store.tags,
+            containerSize: containerSize,
+            shouldAnimate: true,
+            isHorizontal: false,
+            bottomPadding: containerSize * .1,
+            addText: true,
+          ));
+
+  @override
+  Widget build(BuildContext context) {
+    print("are you building ${store.tags}");
+    final height = useFullScreenSize().height;
+    final containerSize = height * store.containerSize;
+    return Observer(
+      builder: (context) {
+        return AnimatedOpacity(
+            opacity: useWidgetOpacity(store.showWidget),
+            duration: Seconds.get(1),
+            child: store.isHorizontal
+                ? Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: fromTagsToWidgets(
+                      store.tags,
+                      containerSize: containerSize,
+                    ),
+                  )
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: fromTagsToWidgets(
+                      store.tags,
+                      containerSize: containerSize,
+                      shouldAnimate: true,
+                      isHorizontal: false,
+                      bottomPadding: containerSize * .1,
+                      addText: true,
+                    ),
+                  ));
+      },
+    );
+    // return store.isHorizontal ? Container() : Container();
+  }
+}
--- packages/client/lib/app/core/widgets/widgets.dart
+++ packages/client/lib/app/core/widgets/widgets.dart
@@ -19,6 +19,7 @@ export 'nokhte_qr_code/nokhte_qr_code.dart';
 export 'session_phone_placement_guide/session_phone_placement_guide.dart';
 export "nokhte_custom_animation_builder/nokhte_custom_animation_builder.dart";
 export 'preset_cards/preset_cards.dart';
+export 'preset_icons/preset_icons.dart';
 export 'session_seating_guide/session_seating_guide.dart';
 export 'session_exit_status_indicator/session_exit_status_indicator.dart';
 export 'speak_less_smile_more/speak_less_smile_more.dart';
