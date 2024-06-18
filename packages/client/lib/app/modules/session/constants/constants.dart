8b1829732ddd07cd73fb3300bfa198890d4773ef
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-06-18 17:42:57 -0500

    fix: seession paths problem

--- packages/client/lib/app/modules/session/constants/constants.dart
+++ packages/client/lib/app/modules/session/constants/constants.dart
@@ -29,35 +29,37 @@ mixin SessionConstants {
   static const relativePaywall = '/paywall';
   static const relativeWaitingPatron = '/waiting_patron';
 
-  static const lobby = '$module/core$relativeLobby';
-  static const duoGreeter = '$module/core$relativeDuoGreeter';
-  static const groupGreeter = '$module/core$relativeGroupGreeter';
-  static const trialGreeter = '$module/core$relativeTrialGreeter';
-  static const exit = '$module/core$relativeExit';
+  static const lobby = '${module}core$relativeLobby';
+  static const duoGreeter = '${module}core$relativeDuoGreeter';
+  static const groupGreeter = '${module}core$relativeGroupGreeter';
+  static const trialGreeter = '${module}core$relativeTrialGreeter';
+  static const exit = '${module}core$relativeExit';
 
-  static const hybridRouter = '$module/hybrid$relativeRoot';
+  static const hybridRouter = '${module}hybrid$relativeRoot';
   static const relativeHybrid = '/hybrid';
-  static const hybrid = '$module/hybrid$relativeHybrid';
+  static const hybrid = '${module}hybrid$relativeHybrid';
   static const hybridSpeakingInstructions =
-      '$module/hybrid$relativeSpeakingInstructions';
-  static const hybridWaiting = '$module/hybrid$relativeWaiting';
+      '${module}hybrid$relativeSpeakingInstructions';
+  static const hybridWaiting = '${module}hybrid$relativeWaiting';
   static const hybridNotesInstructions =
-      '$module/hybrid$relativeNotesInstructions';
-  static const hybridNotes = '$module/hybrid$relativeNotes';
+      '${module}hybrid$relativeNotesInstructions';
+  static const hybridNotes = '${module}hybrid$relativeNotes';
 
-  static const paywall = '$module/monetize$relativePaywall';
-  static const waitingPatron = '$module/monetize$relativeWaitingPatron';
+  static const paywall = '${module}monetize$relativePaywall';
+  static const waitingPatron = '${module}monetize$relativeWaitingPatron';
 
-  static const notesWaiting = '$module/notes$relativeWaiting';
-  static const notesFullInstructions = '$module/notes$relativeFullInstructions';
-  static const notesHalfInstructions = '$module/notes$relativeHalfInstructions';
-  static const notes = '$module/notes$relativeRoot';
+  static const notesWaiting = '${module}notes$relativeWaiting';
+  static const notesFullInstructions =
+      '${module}notes$relativeFullInstructions';
+  static const notesHalfInstructions =
+      '${module}notes$relativeHalfInstructions';
+  static const notes = '${module}notes$relativeRoot';
 
-  static const speakingRouter = '$module/speaking$relativeRoot';
-  static const speaking = '$module/speaking$relativeSpeaking';
+  static const speakingRouter = '${module}speaking$relativeRoot';
+  static const speaking = '${module}speaking$relativeSpeaking';
   static const speakingFullInstructions =
-      '$module/speaking$relativeFullInstructions';
+      '${module}speaking$relativeFullInstructions';
   static const speakingHalfInstructions =
-      '$module/speaking$relativeHalfInstructions';
-  static const speakingWaiting = '$module/speaking$relativeWaiting';
+      '${module}speaking$relativeHalfInstructions';
+  static const speakingWaiting = '${module}speaking$relativeWaiting';
 }
