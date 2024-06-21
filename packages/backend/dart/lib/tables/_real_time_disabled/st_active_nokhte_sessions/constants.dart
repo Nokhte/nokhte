52122eb0214b125d23228e4a6ee51d40a0c44430
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-06-20 21:28:14 -0500

    add: static constants to static session constants

--- packages/backend/dart/lib/tables/_real_time_disabled/st_active_nokhte_sessions/constants.dart
+++ packages/backend/dart/lib/tables/_real_time_disabled/st_active_nokhte_sessions/constants.dart
@@ -11,4 +11,15 @@ mixin STActiveNokhteSessionsConstants {
   String PRESET_UID = 'preset_uid';
   String HAS_PREMIUM_ACCESS = 'has_premium_access';
   String SHOULD_SKIP_INSTRUCTIONS = 'should_skip_instructions';
+
+  static String S_TABLE = "st_active_nokhte_sessions";
+  static String S_COLLABORATOR_UIDS = 'collaborator_uids';
+  static String S_CREATED_AT = 'created_at';
+  static String S_IS_WHITELISTED = 'is_whitelisted';
+  static String S_CONTENT = 'content';
+  static String S_SESSION_UID = 'session_uid';
+  static String S_LEADER_UID = 'leader_uid';
+  static String S_PRESET_UID = 'preset_uid';
+  static String S_HAS_PREMIUM_ACCESS = 'has_premium_access';
+  static String S_SHOULD_SKIP_INSTRUCTIONS = 'should_skip_instructions';
 }
