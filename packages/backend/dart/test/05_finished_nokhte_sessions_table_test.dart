3014bca321f79a5309890f9dab825a1486dbada5
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-06-21 10:35:09 -0500

    test: fix 05_finished_nokhte_sessions_table_test

--- packages/backend/dart/test/05_finished_nokhte_sessions_table_test.dart
+++ packages/backend/dart/test/05_finished_nokhte_sessions_table_test.dart
@@ -1,12 +1,14 @@
 // ignore_for_file: file_names
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:nokhte_backend/tables/_real_time_disabled/company_presets/queries.dart';
 import 'package:nokhte_backend/tables/_real_time_disabled/finished_nokhte_sessions/queries.dart';
 
 import 'shared/shared.dart';
 
 void main() {
   late FinishedNokhteSessionQueries user1Queries;
+  late CompanyPresetsQueries companyPresetsQueries;
   final tSetup = CommonCollaborativeTestFunctions();
   final tSessionContent = ["test1", 'test2', 'test3'];
   final now = DateTime.now().toIso8601String();
@@ -17,6 +19,8 @@ void main() {
     sortedUIDs = [tSetup.firstUserUID, tSetup.secondUserUID];
     sortedUIDs.sort();
     user1Queries = FinishedNokhteSessionQueries(supabase: tSetup.user1Supabase);
+    companyPresetsQueries =
+        CompanyPresetsQueries(supabase: tSetup.user1Supabase);
   });
 
   tearDownAll(() async {
@@ -31,12 +35,15 @@ void main() {
   });
 
   test("select", () async {
+    final presetUID =
+        await companyPresetsQueries.getUnifiedUID(PresetTypes.collaborative);
     await tSetup.supabaseAdmin.from("finished_nokhte_sessions").insert({
       "collaborator_uids": sortedUIDs,
       "content": tSessionContent,
       "session_timestamp": now,
       "session_uid": tSetup.firstUserUID,
-      "aliases": ['', '']
+      "aliases": ['', ''],
+      "preset_uid": presetUID,
     });
     final res = await user1Queries.select();
     expect(res, isNotEmpty);
