aca21ebb20e53e94530eead67339d88701b6302b
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-06-24 19:48:38 -0500

    add: collaboration full instructions impl

---
+++ packages/client/lib/app/modules/session/instructions/collaboration_instructions/collaboration_instructions.dart
@@ -0,0 +1,1 @@
+export './full_instructions/collaboration_full_instructions.dart';
---
+++ packages/client/lib/app/modules/session/instructions/collaboration_instructions/full_instructions/collaboration_full_instructions_coordinator.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api, overridden_fields, annotate_overrides
+import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:nokhte/app/core/modules/session_presence/session_presence.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:nokhte/app/modules/session/session.dart';
+part 'collaboration_full_instructions_coordinator.g.dart';
+
+class CollaborationFullInstructionsCoordinator = _CollaborationFullInstructionsCoordinatorBase
+    with _$CollaborationFullInstructionsCoordinator;
+
+abstract class _CollaborationFullInstructionsCoordinatorBase
+    extends BaseCoordinator with Store {
+  final CollaborationFullInstructionsWidgetsCoordinator widgets;
+  final SessionPresenceCoordinator presence;
+  final SessionMetadataStore sessionMetadata;
+  final TapDetector tap;
+  _CollaborationFullInstructionsCoordinatorBase({
+    required super.captureScreen,
+    required this.widgets,
+    required this.presence,
+    required this.tap,
+  }) : sessionMetadata = presence.sessionMetadataStore;
+
+  @action
+  constructor() {
+    widgets.constructor();
+    initReactors();
+  }
+
+  @action
+  initReactors() {
+    disposers.addAll(widgets.wifiDisconnectOverlay.initReactors(
+      onQuickConnected: () => setDisableAllTouchFeedback(false),
+      onLongReConnected: () {
+        setDisableAllTouchFeedback(false);
+      },
+      onDisconnected: () {
+        setDisableAllTouchFeedback(true);
+      },
+    ));
+    disposers.add(tapReactor());
+  }
+
+  tapReactor() => reaction((p0) => tap.currentTapPosition, (p0) {
+        ifTouchIsNotDisabled(() {
+          widgets.onTap(p0);
+        });
+      });
+}
