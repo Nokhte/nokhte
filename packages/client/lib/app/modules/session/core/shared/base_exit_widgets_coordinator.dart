9cf5681f5c0af8218538caa71042102ad8ac00bd
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-07-01 17:14:33 -0500

    add: shared base exit main and widgets coordinator

---
+++ packages/client/lib/app/modules/session/core/shared/base_exit_widgets_coordinator.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/types/types.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+
+mixin BaseExitWidgetsCoordinator {
+  ExitStatusIndicatorStore get exitStatusIndicator;
+  SmartTextStore get primarySmartText;
+  SmartTextStore get secondarySmartText;
+
+  initStartingMovie({
+    required int totalNumberOfCollaborators,
+    required int totalAffirmative,
+  }) {
+    exitStatusIndicator.initStartingMovie(
+      numberOfAffirmative: totalAffirmative,
+      total: totalNumberOfCollaborators,
+    );
+  }
+
+  onNumOfAffirmativeChanged({
+    required int totalNumberOfCollaborators,
+    required int totalAffirmative,
+  }) {
+    setTotalNumberOfCollaborators(totalNumberOfCollaborators);
+    Timer.periodic(Seconds.get(0, milli: 500), (timer) async {
+      if (exitStatusIndicator.movieStatus != MovieStatus.inProgress) {
+        if (totalAffirmative == totalNumberOfCollaborators) {
+          exitStatusIndicator.initComplete();
+          timer.cancel();
+        } else {
+          exitStatusIndicator.initAdjust(totalAffirmative);
+          timer.cancel();
+        }
+      }
+    });
+  }
+
+  onCollaboratorLeft() {
+    exitStatusIndicator.setWidgetVisibility(false);
+    primarySmartText.setWidgetVisibility(false);
+    secondarySmartText.setWidgetVisibility(false);
+  }
+
+  onCollaboratorJoined() {
+    exitStatusIndicator.setWidgetVisibility(
+      exitStatusIndicator.pastShowWidget,
+    );
+    primarySmartText.setWidgetVisibility(
+      primarySmartText.pastShowWidget,
+    );
+    secondarySmartText.setWidgetVisibility(
+      secondarySmartText.pastShowWidget,
+    );
+  }
+
+  final _totalNumberOfCollaborators = Observable(0);
+  int get totalNumberOfCollaborators => _totalNumberOfCollaborators.value;
+  _setTotalNumberOfCollaborators(int value) =>
+      _totalNumberOfCollaborators.value = value;
+  Action actionSetTotalNumberOfCollaborators = Action(() {});
+
+  setTotalNumberOfCollaborators(int value) {
+    actionSetTotalNumberOfCollaborators([value]);
+  }
+
+  initBaseExitWidgetsCoordinatorActions() {
+    actionSetTotalNumberOfCollaborators =
+        Action(_setTotalNumberOfCollaborators);
+  }
+}
--- packages/client/lib/app/modules/session/core/shared/shared.dart
+++ packages/client/lib/app/modules/session/core/shared/shared.dart
@@ -1,3 +1,5 @@
 export 'choose_greeter_type.dart';
 export 'session_router.dart';
 export 'session_presence.dart';
+export './base_exit_coordinator.dart';
+export './base_exit_widgets_coordinator.dart';
