73cb9152b8cb437bf3450d1eaceda5ba7ba6d88f
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-07-09 08:40:27 -0500

    add: routing mixins

---
+++ packages/client/lib/app/core/mixins/routing/en_route.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:nokhte/app/modules/home/home.dart';
+
+mixin EnRouteRouter on EnRoute {
+  getModularArgs(ResumeOnShoreParams params) => {
+        HomeConstants.RESUME_ON_SHORE_PARAMS: params,
+      };
+}
+
+mixin EnRoute {
+  final _params = Observable(const ResumeOnShoreParams());
+
+  ResumeOnShoreParams get params => _params.value;
+
+  _setParams(ResumeOnShoreParams params) => _params.value = params;
+
+  Action actionSetParams = Action(() {});
+
+  initEnRouteActions() {
+    actionSetParams = Action(_setParams);
+  }
+
+  setParams(ResumeOnShoreParams params) => actionSetParams([params]);
+
+  bool get hasReceivedRoutingArgs => Modular.args.data.toString() != 'null';
+}
+
+mixin EnRouteConsumer on EnRoute {
+  consumeRoutingArgs() {
+    if (hasReceivedRoutingArgs) {
+      setParams(Modular.args.data[HomeConstants.RESUME_ON_SHORE_PARAMS]);
+    }
+  }
+}
