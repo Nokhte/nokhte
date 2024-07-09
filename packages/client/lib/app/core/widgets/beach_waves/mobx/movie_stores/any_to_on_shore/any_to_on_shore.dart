c610fba53a55b1adfb2b42114236f6682493dcf1
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-07-09 16:30:18 -0500

    move: simplify any_to_on_shore file hierarchy

--- packages/client/lib/app/core/widgets/beach_waves/mobx/movie_stores/any_to_on_shore/any_to_on_shore.dart
+++ packages/client/lib/app/core/widgets/beach_waves/mobx/movie_stores/any_to_on_shore/any_to_on_shore.dart
@@ -1,1 +1,24 @@
-export 'any_to_on_shore_movie_store.dart';
+// ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'package:mobx/mobx.dart';
+import 'package:nokhte/app/core/widgets/widgets.dart';
+import 'package:simple_animations/simple_animations.dart';
+part 'any_to_on_shore.g.dart';
+
+class AnyToOnShore = _AnyToOnShoreBase with _$AnyToOnShore;
+
+abstract class _AnyToOnShoreBase
+    extends BaseBeachWaveMovieStore<AnyToOnShoreParams> with Store {
+  _AnyToOnShoreBase()
+      : super(
+          shouldPaintSand: AnyToShoreMovie.shouldPaintSand,
+        ) {
+    movie = AnyToShoreMovie.getMovie(WaterColorsAndStops.oceanDiveWater);
+  }
+  @override
+  @action
+  initMovie(AnyToOnShoreParams params) {
+    movie = AnyToShoreMovie.getMovie(params.startingColors,
+        endingColors: params.endingColors);
+    control = Control.playFromStart;
+  }
+}
