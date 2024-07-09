0cec39879ed7ea3de856d37f871d4fd3ea784812
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-07-09 16:40:03 -0500

    add: init static end api into base beach waves movie store

--- packages/client/lib/app/core/widgets/beach_waves/types/base_beach_wave_movie_store.dart
+++ packages/client/lib/app/core/widgets/beach_waves/types/base_beach_wave_movie_store.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
+import 'dart:async';
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
+import 'package:simple_animations/simple_animations.dart';
 part 'base_beach_wave_movie_store.g.dart';
 
 class BaseBeachWaveMovieStore<T> = _BaseBeachWaveMovieStore<T>
@@ -13,4 +15,12 @@ abstract class _BaseBeachWaveMovieStore<T> extends BaseWidgetStore<T>
     this.shouldPaintSand = true,
     super.callsOnCompleteTwice = false,
   });
+
+  @action
+  initStaticEnd() {
+    setControl(Control.playReverseFromEnd);
+    Timer(Duration.zero, () {
+      setControl(Control.stop);
+    });
+  }
 }
