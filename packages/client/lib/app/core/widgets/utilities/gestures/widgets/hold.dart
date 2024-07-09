9b799ee11dc364bec6348d76425d2a278ff1629a
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-07-08 19:11:19 -0500

    refactor: utility widgets into utilities

--- packages/client/lib/app/core/widgets/utilities/gestures/widgets/hold.dart
+++ packages/client/lib/app/core/widgets/utilities/gestures/widgets/hold.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:nokhte/app/core/hooks/hooks.dart';
-import 'package:nokhte/app/core/widgets/gestures/mobx/hold_detector.dart';
+import 'package:nokhte/app/core/widgets/utilities/gestures/mobx/hold_detector.dart';
 
 class Hold extends HookWidget {
   final HoldDetector store;
