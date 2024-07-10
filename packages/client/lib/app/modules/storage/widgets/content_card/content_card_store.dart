14cebd5bd4310759cecc9c12e6f30a5d18dc5de9
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-07-10 10:04:19 -0500

    refactor: move init fade in the base widget store

--- packages/client/lib/app/modules/storage/widgets/content_card/content_card_store.dart
+++ packages/client/lib/app/modules/storage/widgets/content_card/content_card_store.dart
@@ -1,22 +1,12 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
-import 'dart:async';
-
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
-import 'package:nokhte/app/core/types/types.dart';
 part 'content_card_store.g.dart';
 
 class ContentCardStore = _ContentCardStoreBase with _$ContentCardStore;
 
 abstract class _ContentCardStoreBase extends BaseWidgetStore
     with Store {
-  @action
-  initFadeIn() {
-    setWidgetVisibility(false);
-    Timer(Seconds.get(0, milli: 1), () {
-      setWidgetVisibility(true);
-    });
-  }
 
   @observable
   bool disableTouchInput = false;
--- packages/client/lib/app/modules/storage/widgets/session_card/session_card_store.dart
+++ packages/client/lib/app/modules/storage/widgets/session_card/session_card_store.dart
@@ -1,23 +1,12 @@
 // ignore_for_file: must_be_immutable, library_private_types_in_public_api
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:nokhte/app/core/mobx/mobx.dart';
-import 'package:nokhte/app/core/types/types.dart';
 part 'session_card_store.g.dart';
 
 class SessionCardStore = _SessionCardStoreBase with _$SessionCardStore;
 
 abstract class _SessionCardStoreBase extends BaseWidgetStore with Store {
-  @action
-  initFadeIn() {
-    setWidgetVisibility(false);
-    Timer(Seconds.get(0, milli: 1), () {
-      setWidgetVisibility(true);
-    });
-  }
-
   @observable
   ObservableList<FocusNode> focusNodes = ObservableList();
 
