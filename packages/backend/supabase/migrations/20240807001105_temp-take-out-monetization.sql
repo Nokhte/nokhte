e423b6d05659adc4189e3f6983ac49db34d0850c
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-08-07 19:41:21 -0500

    chore: bump ios and android app versions

--- packages/backend/supabase/migrations/20240807001105_temp-take-out-monetization.sql
+++ packages/backend/supabase/migrations/20240807001105_temp-take-out-monetization.sql
@@ -28,3 +28,14 @@ drop index if exists "public"."user_metadata_subscriber_id_key";
 alter table "public"."user_metadata" drop column "subscriber_id";
 
 alter table "public"."st_active_nokhte_sessions" drop column "is_whitelisted";
+
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_valid_app_versions()
+ RETURNS text[]
+ LANGUAGE plpgsql
+AS $function$BEGIN
+    RETURN ARRAY['1.81.0'];
+END;$function$
+;
