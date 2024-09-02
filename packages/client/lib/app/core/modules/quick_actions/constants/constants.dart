52a919e83eed8ab7fa9e4ba9a6f4002fc81f50c9
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-09-02 10:05:22 -0500

    add: prod complete quick actions impl

---
+++ packages/client/lib/app/core/modules/quick_actions/constants/constants.dart
@@ -0,0 +1,70 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:nokhte/app/core/modules/hive/hive.dart';
+import 'package:nokhte/app/core/modules/quick_actions/quick_actions.dart';
+import 'package:nokhte/app/core/modules/user_information/user_information.dart';
+import 'package:nokhte/app/modules/session_starters/session_starters_widgets.dart';
+import 'package:nokhte/app/modules/storage/storage.dart';
+import 'package:quick_actions/quick_actions.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class QuickActionsConstants {
+  static List<ShortcutItem> shortcutItems = [
+    ShortcutItem(
+      type: QuickActionsEnum.startSession.toString(),
+      localizedTitle: 'Start Session',
+      icon: 'qr_code',
+    ),
+    ShortcutItem(
+      type: QuickActionsEnum.joinSession.toString(),
+      localizedTitle: 'Join Session',
+      icon: 'camera',
+    ),
+    ShortcutItem(
+      type: QuickActionsEnum.viewStorage.toString(),
+      localizedTitle: 'View Storage',
+      icon: 'doc',
+    ),
+  ];
+
+  static initQuickActions() async {
+    QuickActions quickActions = const QuickActions();
+    await quickActions.setShortcutItems(QuickActionsConstants.shortcutItems);
+    await Hive.openBox(HiveBoxes.userInformation.toString());
+  }
+
+  static route(Function onRoute) {
+    final userInfoBox = Hive.box(HiveBoxes.userInformation.toString());
+    if (userInfoBox.isNotEmpty &&
+        Supabase.instance.client.auth.currentSession != null) {
+      final userInfo = UserJourneyInfoModel.fromSupabase(
+        userNamesRes: [userInfoBox.toMap()],
+        finishedNokhteSessionsRes: List.empty(),
+        isUpToDate: true,
+      );
+
+      const QuickActions quickActions = QuickActions();
+      quickActions.initialize((String shortcutType) {
+        final startSession = QuickActionsEnum.startSession.toString();
+        final joinSession = QuickActionsEnum.joinSession.toString();
+        final viewStorage = QuickActionsEnum.viewStorage.toString();
+        if (shortcutType == startSession) {
+          if (userInfo.hasAccessedQrCode) {
+            onRoute();
+            Modular.to.navigate(SessionStarterConstants.sessionStarter);
+          }
+        } else if (shortcutType == joinSession) {
+          if (userInfo.hasAccessedQrCodeScanner) {
+            onRoute();
+            Modular.to.navigate(SessionStarterConstants.sessionJoiner);
+          }
+        } else if (shortcutType == viewStorage) {
+          if (userInfo.hasEnteredStorage) {
+            onRoute();
+            Modular.to.navigate(StorageConstants.root);
+          }
+        }
+      });
+    }
+  }
+}
--- packages/client/lib/main.dart
+++ packages/client/lib/main.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:nokhte/app/app_module.dart';
 import 'package:nokhte/app/app_widget.dart';
-import 'package:nokhte/app/modules/session_starters/session_starters_widgets.dart';
-import 'package:nokhte/app/modules/storage/storage.dart';
+import 'package:nokhte/app/core/modules/quick_actions/constants/constants.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/foundation.dart';
-import 'package:quick_actions/quick_actions.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,39 +15,6 @@ void main() async {
   Modular.setInitialRoute('/login/');
 
   await Hive.initFlutter();
-
-  QuickActions quickActions = const QuickActions();
-
-  quickActions.setShortcutItems(<ShortcutItem>[
-    const ShortcutItem(
-      type: 'Start Session',
-      localizedTitle: 'Start Session',
-      icon: 'qr_code',
-    ),
-    const ShortcutItem(
-      type: 'Join Session',
-      localizedTitle: 'Join Session',
-      icon: 'camera',
-    ),
-    const ShortcutItem(
-      type: 'View Storage',
-      localizedTitle: 'View Storage',
-      icon: 'doc',
-    ),
-  ]);
-
-  quickActions.initialize((String shortcutType) {
-    switch (shortcutType) {
-      case 'Start Session':
-        Modular.to.navigate(SessionStarterConstants.sessionStarter);
-      case 'Join Session':
-        Modular.to.navigate(SessionStarterConstants.sessionJoiner);
-      case 'View Storage':
-        Modular.to.navigate(StorageConstants.root);
-      default:
-        break;
-    }
-  });
 
   late String supabaseUrl;
   late String supabaseAnonKey;
@@ -70,6 +35,8 @@ void main() async {
     url: supabaseUrl,
     anonKey: supabaseAnonKey,
   );
+
+  await QuickActionsConstants.initQuickActions();
 
   runApp(
     ModularApp(
