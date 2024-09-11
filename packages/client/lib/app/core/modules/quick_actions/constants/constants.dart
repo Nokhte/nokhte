cd1629d1e322b6bd715d6f2ef8908ec855f7fe9a
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-09-11 16:39:43 -0500

    refactor: quick actions to not use user information

--- packages/client/lib/app/core/modules/quick_actions/constants/constants.dart
+++ packages/client/lib/app/core/modules/quick_actions/constants/constants.dart
@@ -2,12 +2,10 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:nokhte/app/core/modules/hive/hive.dart';
 import 'package:nokhte/app/core/modules/quick_actions/quick_actions.dart';
-import 'package:nokhte/app/core/modules/user_information/user_information.dart';
 import 'package:nokhte/app/modules/session_joiner/session_joiner.dart';
 import 'package:nokhte/app/modules/session_starters/session_starters_widgets.dart';
 import 'package:nokhte/app/modules/storage/storage.dart';
 import 'package:quick_actions/quick_actions.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 class QuickActionsConstants {
   static List<ShortcutItem> shortcutItems = [
@@ -35,37 +33,20 @@ class QuickActionsConstants {
   }
 
   static route(Function onRoute) {
-    final userInfoBox = Hive.box(HiveBoxes.userInformation.toString());
-    if (userInfoBox.isNotEmpty &&
-        Supabase.instance.client.auth.currentSession != null) {
-      final userInfo = UserJourneyInfoModel.fromSupabase(
-        userNamesRes: [userInfoBox.toMap()],
-        finishedNokhteSessionsRes: List.empty(),
-        isUpToDate: true,
-      );
-
-      const QuickActions quickActions = QuickActions();
-      quickActions.initialize((String shortcutType) {
-        final startSession = QuickActionsEnum.startSession.toString();
-        final joinSession = QuickActionsEnum.joinSession.toString();
-        final viewStorage = QuickActionsEnum.viewStorage.toString();
-        if (shortcutType == startSession) {
-          if (userInfo.hasAccessedQrCode) {
-            onRoute();
-            Modular.to.navigate(SessionStarterConstants.sessionStarter);
-          }
-        } else if (shortcutType == joinSession) {
-          if (userInfo.hasAccessedQrCodeScanner) {
-            onRoute();
-            Modular.to.navigate(SessionJoinerConstants.sessionJoiner);
-          }
-        } else if (shortcutType == viewStorage) {
-          if (userInfo.hasEnteredStorage) {
-            onRoute();
-            Modular.to.navigate(StorageConstants.home);
-          }
-        }
-      });
-    }
+    const QuickActions quickActions = QuickActions();
+    quickActions.initialize((String shortcutType) {
+      final startSession = QuickActionsEnum.startSession.toString();
+      final joinSession = QuickActionsEnum.joinSession.toString();
+      final viewStorage = QuickActionsEnum.viewStorage.toString();
+      onRoute();
+      if (shortcutType == startSession) {
+        Modular.to.navigate(SessionStarterConstants.sessionStarter);
+      } else if (shortcutType == joinSession) {
+        Modular.to.navigate(SessionJoinerConstants.sessionJoiner);
+      } else if (shortcutType == viewStorage) {
+        Modular.to.navigate(StorageConstants.home);
+      }
+    });
   }
 }
+// }
