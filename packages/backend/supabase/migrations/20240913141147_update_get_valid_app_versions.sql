1e3fb584aa21591e184e47382f33b4e0c4ac35d1
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-09-13 09:13:47 -0500

    chore: bump iOS & android version numbers

---
+++ packages/backend/supabase/migrations/20240913141147_update_get_valid_app_versions.sql
@@ -0,0 +1,11 @@
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_valid_app_versions()
+ RETURNS text[]
+ LANGUAGE plpgsql
+AS $function$BEGIN 
+    RETURN ARRAY['2.4.0'];
+END;$function$
+;
+
