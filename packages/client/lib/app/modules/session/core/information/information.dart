941c74f84a85c30d3453fbd9915ade43fb9f02ba
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-09-27 07:30:08 -0500

    fix: session information

--- packages/client/lib/app/modules/session/core/information/information.dart
+++ packages/client/lib/app/modules/session/core/information/information.dart
@@ -3,7 +3,6 @@ export 'session_information_widgets_coordinator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:nokhte/app/core/modules/connectivity/connectivity.dart';
-import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/session/session.dart';
 
@@ -41,28 +40,8 @@ class SessionInformationScreen extends HookWidget {
             Tint(
               store: coordinator.widgets.tint,
             ),
-            Center(
-              child: SmartText(
-                store: coordinator.widgets.primarySmartText,
-                bottomPadding: .75,
-                opacityDuration: Seconds.get(1),
-              ),
-            ),
-            // Padding(
-            //   padding: EdgeInsets.only(
-            //     top: useScaledSize(
-            //       baseValue: .03,
-            //       screenSize: screenSize,
-            //     ),
-            //   ),
-            //   child: ExpandedPresetsCards(
-            //     store: coordinator.widgets.presetCard,
-            //   ),
-            // ),
-            FullScreen(
-              child: TouchRipple(
-                store: coordinator.widgets.touchRipple,
-              ),
+            PresetArticle(
+              store: coordinator.widgets.presetArticle,
             ),
             WifiDisconnectOverlay(
               store: coordinator.widgets.wifiDisconnectOverlay,
