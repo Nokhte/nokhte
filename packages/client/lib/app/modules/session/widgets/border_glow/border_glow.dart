5d58349c52184a6be3112e7e2cd022e815553b05
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-10-19 14:00:00 -0500

    refactor: border glow to use color constants instead of hardcoded values

--- packages/client/lib/app/modules/session/widgets/border_glow/border_glow.dart
+++ packages/client/lib/app/modules/session/widgets/border_glow/border_glow.dart
@@ -8,7 +8,8 @@ import 'dart:ui';
 import 'border_glow.dart';
 export 'movies/movies.dart';
 export 'border_glow.dart';
-export 'border_glow_store.dart';
+export 'movies/border_glow_store.dart';
+export 'constants/constants.dart';
 
 class BorderGlow extends HookWidget {
   final BorderGlowStore store;
