282cb17f0722997903ebb0f175938701526c193c
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-10-21 11:33:57 -0500

    refactor: article body info to work with more than two session types

--- packages/client/lib/app/core/widgets/preset_article/widgets/body/types/article_body_info.dart
+++ packages/client/lib/app/core/widgets/preset_article/widgets/body/types/article_body_info.dart
@@ -4,6 +4,7 @@ import 'package:nokhte_backend/tables/company_presets.dart';
 
 class ArticleBodyInfo extends Equatable {
   final PresetTypes presetType;
+  final List<SessionTags> presetTags;
   final List<String> speakingInstructions;
   final List<String> sittingInstructions;
   final List<String> notesInstructions;
@@ -15,93 +16,134 @@ class ArticleBodyInfo extends Equatable {
   final String tagline;
   final String uniqueFeature;
   final PowerupInfo powerup;
-  // next add the article impl i recommend a separate widget
 
-  ArticleBodyInfo({required this.presetType})
-      : speakingInstructions = _getInstructions(presetType, 'speaking'),
-        sittingInstructions = _getInstructions(presetType, 'sitting'),
-        notesInstructions = _getInstructions(presetType, 'notes'),
-        speakingJustifications = _getJustifications(presetType, 'speaking'),
-        sittingJustifications = _getJustifications(presetType, 'sitting'),
+  ArticleBodyInfo({
+    required this.presetTags,
+    required this.presetType,
+  })  : speakingInstructions = _getSpeakingInstructions(presetTags),
+        sittingInstructions = _getSittingInstructions(presetTags),
+        notesInstructions = _getNotesInstructions(presetTags),
+        speakingJustifications = _getSpeakingJustifications(presetTags),
+        sittingJustifications = _getSittingJustifications(presetTags),
+        notesJustifications = _getNotesJustifications(presetTags),
         title = _getTitle(presetType),
         tagline = _getTagline(presetType),
         uniqueFeature = _getUniqueFeature(presetType),
-        notesJustifications = [
-          'Write with brevity **(104 characters)**',
-          '**Keep** track of ideas'
-        ],
         powerup = PowerupInfo(
           presetType == PresetTypes.collaborative
               ? Powerups.letEmCook
               : Powerups.rally,
         );
 
-  static List<String> _getInstructions(PresetTypes type, String context) {
-    final collaborative = {
-      'speaking': [
+  static List<String> _getSpeakingInstructions(List<SessionTags> tags) {
+    if (tags.contains(SessionTags.tapToSpeak)) {
+      return [
         '**Tap** on the yellow side to speak',
         '**Tap again** when you are finished'
-      ],
-      'sitting': ['**Sit or stand** anywhere', '**Keep** your phone with you'],
-      'notes': [
-        '**Tap** on the blue to write your note',
-        '**Swipe up** to submit'
-      ]
-    };
-
-    final consultative = {
-      'speaking': [
+      ];
+    } else if (tags.contains(SessionTags.holdToSpeak)) {
+      return [
         'Locate the phone **to your right**',
         '**Hold** on the yellow side to speak',
         '**Let go** when you are finished'
-      ],
-      'sitting': [
-        'Sit in a **circle**',
-        'Place your phone **halfway** between you and the person to your right'
-      ],
-      'notes': [
-        'Locate the **phone to your left**',
-        '**Tap** on the blue to write your note',
-        '**Swipe up** to submit'
-      ]
-    };
-
-    return type == PresetTypes.collaborative
-        ? collaborative[context]!
-        : consultative[context]!;
+      ];
+    }
+    return [];
   }
 
-  static String _getTitle(PresetTypes type) =>
-      type == PresetTypes.collaborative ? 'Collaboration' : 'Consultation';
-  static String _getTagline(PresetTypes type) =>
-      type == PresetTypes.collaborative
-          ? 'Great with smaller groups, and works best in creative endeavors'
-          : 'Great for beginners, and groups of any size \n';
-  static String _getUniqueFeature(PresetTypes type) => type ==
-          PresetTypes.collaborative
-      ? 'Flexibility: sit anywhere, notes anytime'
-      : 'One at a time: The group is either listening or writing notes, not both.';
+  static List<String> _getSittingInstructions(List<SessionTags> tags) {
+    if (tags.contains(SessionTags.flexibleSeating)) {
+      return ['**Sit or stand** anywhere', '**Keep** your phone with you'];
+    } else if (tags.contains(SessionTags.strictSeating)) {
+      return [
+        'Sit in a **circle**',
+        'Place your phone **halfway** between you and the person to your right'
+      ];
+    } else if (tags.contains(SessionTags.noSeating)) {
+      return ['No specific seating arrangement required'];
+    }
+    return [];
+  }
 
-  static List<String> _getJustifications(PresetTypes type, String context) {
-    final collaborative = {
-      'speaking': ['Allows you to do other things **while talking**'],
-      'sitting': [
-        'Can **move anytime** in the meeting',
-        'Can be used in **more spaces**'
-      ]
-    };
+  static List<String> _getNotesInstructions(List<SessionTags> tags) {
+    List<String> instructions = [
+      '**Tap** on the blue to write your note',
+      '**Swipe up** to submit'
+    ];
+    if (tags.contains(SessionTags.monoFocalNotes)) {
+      instructions.insert(0, 'Locate the **phone to your left**');
+    }
+    return instructions;
+  }
 
-    final consultative = {
-      'speaking': [
+  static List<String> _getSpeakingJustifications(List<SessionTags> tags) {
+    if (tags.contains(SessionTags.tapToSpeak)) {
+      return ['Allows you to do other things **while talking**'];
+    } else if (tags.contains(SessionTags.holdToSpeak)) {
+      return [
         'Encourages speaking with **brevity**',
         'Gives you less time to **repeat yourself**'
-      ],
-      'sitting': ['**See** everyone’s face', '**Hear** people clearly']
-    };
+      ];
+    }
+    return [];
+  }
 
-    return type == PresetTypes.collaborative
-        ? collaborative[context]!
-        : consultative[context]!;
+  static List<String> _getSittingJustifications(List<SessionTags> tags) {
+    if (tags.contains(SessionTags.flexibleSeating)) {
+      return [
+        'Can **move anytime** in the meeting',
+        'Can be used in **more spaces**'
+      ];
+    } else if (tags.contains(SessionTags.strictSeating)) {
+      return ["**See** everyone's face', '**Hear** people clearly"];
+    }
+    return [];
+  }
+
+  static List<String> _getNotesJustifications(List<SessionTags> tags) {
+    return [
+      'Write with brevity **(104 characters)**',
+      '**Keep** track of ideas'
+    ];
+  }
+
+  static String _getTitle(PresetTypes type) {
+    switch (type) {
+      case PresetTypes.collaborative:
+        return 'Collaboration';
+      case PresetTypes.consultative:
+        return 'Consultation';
+      case PresetTypes.solo:
+        return 'Solo';
+      default:
+        throw ArgumentError('Unsupported preset type: $type');
+    }
+  }
+
+  static String _getTagline(PresetTypes type) {
+    switch (type) {
+      case PresetTypes.collaborative:
+        return 'Great with smaller groups, and works best in creative endeavors';
+      case PresetTypes.consultative:
+        return 'Great for beginners, and groups of any size \n';
+      case PresetTypes.solo:
+        return 'Use the app in group settings without group approval \n'; // Empty for now
+      default:
+        throw ArgumentError('Unsupported preset type: $type');
+    }
+  }
+
+  static String _getUniqueFeature(PresetTypes type) {
+    switch (type) {
+      case PresetTypes.collaborative:
+        return 'Flexibility: sit anywhere, notes anytime';
+      case PresetTypes.consultative:
+        return 'One at a time: The group is either listening or writing notes, not both.';
+      case PresetTypes.solo:
+        return 'Adaptive: Change the session to fit your needs '; // Empty for now
+      default:
+        throw ArgumentError('Unsupported preset type: $type');
+    }
   }
 
   @override
