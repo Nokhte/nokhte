3b4ef936298d672e3affb6d580e9427e69d14471
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-10-22 17:06:03 -0500

    `refactor: expand company presets model to include all preset data`

--- packages/client/lib/app/modules/presets/data/models/company_presets_model.dart
+++ packages/client/lib/app/modules/presets/data/models/company_presets_model.dart
@@ -1,11 +1,14 @@
-import 'package:nokhte/app/modules/presets/domain/domain.dart';
-import 'package:nokhte_backend/tables/_real_time_disabled/company_presets/queries.dart';
+import 'package:nokhte/app/modules/presets/presets.dart';
+import 'package:nokhte_backend/tables/company_presets.dart';
+import 'package:nokhte_backend/tables/company_presets_preferences.dart';
 
 class CompanyPresetsModel extends CompanyPresetsEntity {
   const CompanyPresetsModel({
     required super.uids,
     required super.tags,
-    required super.names,
+    required super.presets,
+    required super.articles,
+    required super.screenTypes,
   });
 
   static CompanyPresetsModel fromSupabase(List presetsRes) {
@@ -13,24 +16,113 @@ class CompanyPresetsModel extends CompanyPresetsEntity {
       return const CompanyPresetsModel(
         uids: [],
         tags: [],
-        names: [],
+        presets: [],
+        articles: [],
+        screenTypes: [],
       );
     } else {
       final List<String> uids = [];
       final List<List<SessionTags>> tags = [];
-      final List<String> names = [];
+      final List<PresetTypes> types = [];
+      final List<PresetArticleEntity> articles = [];
+      final List<SessionScreenTypes> screenTypes = [];
+
       for (var preset in presetsRes) {
+        final formattedTags = CompanyPresetsUtils.mapTagsToEnum(
+            preset[CompanyPresetsQueries.TAGS]);
+        final presetType = CompanyPresetsUtils.mapStringToPresetType(
+            preset[CompanyPresetsQueries.NAME]);
+        final prefsRes = preset[CompanyPresetsPreferencesQueries.TABLE];
+        final formattedScreenType = CompanyPresetsUtils.fromRawScreenType(
+          preset[CompanyPresetsQueries.PHONE_TYPE],
+        );
+
+        screenTypes.add(formattedScreenType);
         uids.add(preset[CompanyPresetsQueries.UID]);
-        tags.add(CompanyPresetsQueries.mapTagsToEnum(
-            preset[CompanyPresetsQueries.TAGS]));
-        names.add(preset[CompanyPresetsQueries.NAME]);
+        tags.add(formattedTags);
+        types.add(presetType);
+        articles.add(
+          PresetArticleEntity(
+            presetTags: formattedTags,
+            presetType: presetType,
+            articleSections: getArticleSections(
+              presetType,
+              formattedTags,
+              prefsRes,
+            ),
+          ),
+        );
       }
 
       return CompanyPresetsModel(
+        screenTypes: screenTypes,
         uids: uids,
         tags: tags,
-        names: names,
+        presets: types,
+        articles: articles,
       );
     }
+  }
+
+  static TagCategory categorizeTag(SessionTags tag) {
+    switch (tag) {
+      case SessionTags.strictSeating:
+      case SessionTags.flexibleSeating:
+        return TagCategory.sitting;
+      case SessionTags.noSeating:
+        return TagCategory.ghost;
+      case SessionTags.holdToSpeak:
+      case SessionTags.tapToSpeak:
+        return TagCategory.speaking;
+      case SessionTags.monoFocalNotes:
+      case SessionTags.multiFocalNotes:
+      case SessionTags.deactivatedNotes:
+        return TagCategory.notes;
+      default:
+        return TagCategory.ghost;
+    }
+  }
+
+  static List<ArticleSection> getArticleSections(
+    PresetTypes presetType,
+    List<SessionTags> possibleTags,
+    List userPrefsRes,
+  ) {
+    List<ArticleSection> sections;
+
+    if (userPrefsRes.isEmpty && possibleTags.length == 3) {
+      sections = possibleTags
+          .map((tag) => ArticleSection(
+                presetType: presetType,
+                tag: tag,
+                category: categorizeTag(tag),
+              ))
+          .toList();
+    } else if (userPrefsRes.isEmpty && possibleTags.length != 3) {
+      final defaultTags =
+          CompanyPresetsPreferencesQueries.mapTypeToChosenDefaultTags(
+              presetType);
+      sections = defaultTags
+          .map((tag) => ArticleSection(
+                presetType: presetType,
+                tag: tag,
+                category: categorizeTag(tag),
+              ))
+          .toList();
+    } else {
+      final userTags = CompanyPresetsUtils.mapTagsToEnum(
+          userPrefsRes.first[CompanyPresetsQueries.TAGS]);
+      sections = userTags
+          .map((tag) => ArticleSection(
+                presetType: presetType,
+                tag: tag,
+                category: categorizeTag(tag),
+              ))
+          .toList();
+    }
+
+    return sections
+        .where((section) => section.category != TagCategory.ghost)
+        .toList();
   }
 }
--- packages/client/lib/app/modules/presets/domain/entities/company_presets_entity.dart
+++ packages/client/lib/app/modules/presets/domain/entities/company_presets_entity.dart
@@ -1,17 +1,31 @@
 import 'package:equatable/equatable.dart';
+import 'package:nokhte/app/modules/presets/presets.dart';
 import 'package:nokhte_backend/tables/company_presets.dart';
 
+// we need to refactor this to be an individual preset now so that it can be used for single fetch queries as well as group queries
 class CompanyPresetsEntity extends Equatable {
   final List<String> uids;
   final List<List<SessionTags>> tags;
-  final List<String> names;
+  final List<PresetTypes> presets;
+  final List<PresetArticleEntity> articles;
+  final List<SessionScreenTypes> screenTypes;
 
   const CompanyPresetsEntity({
     required this.uids,
     required this.tags,
-    required this.names,
+    required this.presets,
+    required this.articles,
+    required this.screenTypes,
   });
 
+  factory CompanyPresetsEntity.initial() => const CompanyPresetsEntity(
+        uids: [],
+        tags: [],
+        presets: [],
+        articles: [],
+        screenTypes: [],
+      );
+
   @override
-  List<Object> get props => [uids, tags, names];
+  List<Object> get props => [uids, tags, presets];
 }
