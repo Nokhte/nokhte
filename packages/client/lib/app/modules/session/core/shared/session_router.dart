f5ff61e15454fe9a520e2bd09aa12cb04371d041
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-10-22 17:28:10 -0500

    refactor: session screen type dependencies

--- packages/client/lib/app/modules/session/core/shared/session_router.dart
+++ packages/client/lib/app/modules/session/core/shared/session_router.dart
@@ -4,16 +4,17 @@ import 'package:nokhte/app/core/interfaces/logic.dart';
 import 'package:nokhte/app/core/types/types.dart';
 import 'package:nokhte/app/core/widgets/widgets.dart';
 import 'package:nokhte/app/modules/session/session.dart';
+import 'package:nokhte_backend/tables/company_presets.dart';
 export './choose_greeter_type.dart';
 export './session_router.dart';
 
 mixin SessionRouter {
   BeachWavesStore get beachWaves;
   initTransition(SessionScreenTypes phoneType) {
-    if (phoneType != SessionScreenTypes.notes) {
+    
       beachWaves.setMovieMode(BeachWaveMovieModes.skyToHalfAndHalf);
       beachWaves.currentStore.initMovie(NoParams());
-    }
+    
   }
 
   route({
